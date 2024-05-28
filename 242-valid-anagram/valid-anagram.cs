799ea2c2c8e3cd9766f67d1551bf83f8af1d06f8
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-05-27 22:57:54 -0400

    Time: 56 ms (79.80%) | Memory: 42.1 MB (62.37%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        // Console.WriteLine(WordToDict(s).ToString());
+        if (s.Length != t.Length)
+            return false;
+        
+        Dictionary<char, int> ds = WordToDict(s);
+        Dictionary<char, int> dt = WordToDict(t);
+        foreach(char key in ds.Keys)
+        {
+            if (dt.ContainsKey(key) == false
+                || dt[key] != ds[key])
+                return false;
+        }
+
+        return true;
+    }
+
+    private Dictionary<char, int> WordToDict(string word) {
+        Dictionary<char, int> result = new Dictionary<char, int>();
+
+        foreach(char c in word)
+            result[c] = result.GetValueOrDefault(c, 0) + 1;
+
+        return result;
+    }
+}
