d2cd76eaba4d81061b1c39c5bc6f65fd0c2f49b1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-05-28 12:04:10 -0400

    Time: 63 ms (56.76%) | Memory: 46.4 MB (24.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cs
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -8,24 +8,16 @@ public class Solution {
 
     private bool IsPalindromeRec(string s, int left, int right)
     {
-        if (left >= right)
-            return true;
-
-        // Console.WriteLine($"s: \"{s}\", left: {left}, right: {right}");
-
         while (left < right && !char.IsLetterOrDigit(s[left]))
             left++;
         while (left < right && !char.IsLetterOrDigit(s[right]))
             right--;
         
-        // if (left >= right)
-        //     return true;
-
+        if (left >= right)
+            return true;
+        
         if (char.ToLower(s[left]) != char.ToLower(s[right]))
             return false;
-        
-        // if (left + 1 >= right - 1)
-        //     return true;
         
         return IsPalindromeRec(s, left + 1, right - 1);
     }
