c2ca7da3bf291464a93b3bc23234be2cb076beec
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-05-28 12:20:48 -0400

    Time: 103 ms (67.77%) | Memory: 17.2 MB (98.29%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -2,9 +2,6 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
 
-        # while numbers[right] > target:
-        #     right -= 1
-
         while numbers[left] + numbers[right] != target:
             if numbers[left] + numbers[right] > target:
                 # need to make sum smaller, so 
@@ -15,4 +12,4 @@ class Solution:
                 # increment left pointer by 1
                 left += 1
         
-        return sorted([left + 1, right + 1])
+        return (min(left, right) + 1, max(left, right) + 1)
