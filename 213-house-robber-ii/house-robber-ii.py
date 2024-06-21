d81e2c0dc7432b3f94bc92c616e7c05cb41f175c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-21 00:52:32 -0700

    Time: 41 ms (22.11%) | Memory: 16.4 MB (63.68%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def classic_rob(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return nums[0] if len(nums) > 0 else 0
+        
+        n = len(nums)
+        # Case 1: rob last home
+        if (n - 2) not in self.memo:
+            self.memo[n - 2] = self.classic_rob(nums[:n - 2])
+        case1 = nums[-1] + self.memo[n - 2]
+
+        # Case 2: don't rob last home
+        if (n - 1) not in self.memo:
+            self.memo[n - 1] = self.classic_rob(nums[:n - 1])
+        case2 = self.memo[n - 1]
+
+        return max(case1, case2)
+    def rob(self, nums: List[int]) -> int:
+        res = max(nums[0], self.classic_rob(nums[1:]))
+        self.memo = {}
+        return max(res, self.classic_rob(nums[:-1]))
