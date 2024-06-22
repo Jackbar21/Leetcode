cd40509d1bb7f95215b2e78da49c1150626e87b2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-22 00:23:09 -0700

    Time: 2942 ms (5.13%) | Memory: 192.3 MB (5.11%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,33 @@
+class Solution:
+    def __init__(self):
+        self.cuts = []
+        self.memo = {}
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        self.cuts = cuts
+        for i in range(n):
+            self.memo[(i,i)] = 0
+        return self.minCostDp(0, n)
+
+    def minCostDp(self, l, r):
+        if (l,r) in self.memo:
+            return self.memo[(l,r)]
+
+        self.memo[(l, r)] = float("inf")
+        for cut in self.cuts:
+            if l < cut < r:
+                cur_stick_cost = r - l
+                left_stick_cost = self.minCostDp(l, cut)
+                right_stick_cost = self.minCostDp(cut, r)
+                self.memo[(l, r)] = min(self.memo[(l, r)],
+                    cur_stick_cost
+                    + left_stick_cost
+                    + right_stick_cost
+                )
+        
+        # if no valid cut left, then we are done!
+        if self.memo[(l, r)] == float("inf"):
+            self.memo[(l, r)] = 0
+        
+        return self.memo[(l, r)]
+
+        
