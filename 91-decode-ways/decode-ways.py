91e69ec5e9212cdfd5fbcbdf7c857a9630a8f08f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-21 20:02:39 -0700

    Time: 35 ms (66.12%) | Memory: 16.4 MB (82.04%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,29 @@
+class Solution:
+    def __init__(self):
+        self.length = 0
+        self.memo = {}
+    def numDecodings(self, s: str) -> int:
+        self.length = len(s)
+        self.memo[len(s)] = 1
+        return self.numDecPos(s, 0)
+        
+    def numDecPos(self, s, pos):
+        if pos in self.memo:
+            return self.memo[pos]
+    
+        # if self.length - pos <= 0:
+        #     return 0
+        
+        # if s[pos] == "0":
+        #     return 0
+        
+        # if pos+1 < self.length and s[pos+1] == "0":
+        #     return self.numDecPos(s, pos+2)
+        
+        self.memo[pos] = 0 if s[pos] == "0" else self.numDecPos(s, pos+1)
+
+        if pos+1 < self.length and (s[pos] == "1" or (s[pos] == "2" and 0 <= int(s[pos+1]) <= 6)):
+            self.memo[pos] += self.numDecPos(s, pos+2)
+        
+        return self.memo[pos]
+        
