4698c29de1600016ca162a0e4a4c52abfdeb1cf0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-22 00:34:44 -0700

    Time: 1519 ms (5.55%) | Memory: 17.1 MB (56.96%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -1,36 +1,32 @@
 class Solution:
     def __init__(self):
-        self.cuts = []
+        # This leetcode problem showed up on my
+        # advanced algorithms final exam during college :)
+        self.cuts = None
         self.memo = {}
+
     def minCost(self, n: int, cuts: List[int]) -> int:
         self.cuts = cuts
-        # for i in range(n):
-        #     self.memo[(i,i)] = 0
         return self.minCostDp(0, n)
+    
+    def minCostDp(self, i, j):
+        if (i,j) in self.memo:
+            return self.memo[(i,j)]
 
-    def minCostDp(self, l, r):
-        # if r - l == 1:
-        #     return 0
-
-        if (l,r) in self.memo:
-            return self.memo[(l,r)]
-
-        self.memo[(l, r)] = float("inf")
+        self.memo[(i,j)] = float("inf")
         for cut in self.cuts:
-            if l < cut < r:
-                cur_stick_cost = r - l
-                left_stick_cost = self.minCostDp(l, cut)
-                right_stick_cost = self.minCostDp(cut, r)
-                self.memo[(l, r)] = min(self.memo[(l, r)],
-                    cur_stick_cost
-                    + left_stick_cost
-                    + right_stick_cost
+            if i < cut < j:
+                self.memo[(i,j)] = min(self.memo[(i,j)], 
+                    j - i
+                    + self.minCostDp(i, cut)
+                    + self.minCostDp(cut, j)
                 )
         
-        # if no valid cut left, then we are done!
-        if self.memo[(l, r)] == float("inf"):
-            self.memo[(l, r)] = 0
+        # In case no cut ever fit in (i,j) interval,
+        # in which case we're already done!
+        if self.memo[(i,j)] == float("inf"):
+            self.memo[(i,j)] = 0
         
-        return self.memo[(l, r)]
+        return self.memo[(i,j)]
 
         
