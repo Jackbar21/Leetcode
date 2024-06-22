156e1f23d999f5dc5e357b1c7d2b636efb988f1b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-21 19:10:57 -0700

    Time: 94 ms (5.07%) | Memory: 20.7 MB (6.08%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        return self.minCostAcc(cost, 0)
+    def minCostAcc(self, cost, pos):
+        if len(cost) <= 1:
+            return 0
+        
+        # Case 1: start at index 0
+        if (pos+1) not in self.memo:
+            self.memo[pos+1] = self.minCostAcc(cost[1:], pos+1)
+        case1 = cost[0] + self.memo[pos+1]
+
+        # Case 2: start at index 1
+        if (pos+2) not in self.memo:
+            self.memo[pos+2] = self.minCostAcc(cost[2:], pos+2)
+        case2 = cost[1] + self.memo[pos+2]
+
+        return min(case1, case2)
+
+
+        
