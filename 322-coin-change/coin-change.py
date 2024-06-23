0ef651fc5c768fa2098c927346b53edfec646ad2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-22 22:39:14 -0700

    Time: 1238 ms (12.17%) | Memory: 19.3 MB (27.28%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount in self.memo:
+            return self.memo[amount]
+        
+        if amount <= 0:
+            return 0
+        
+        if amount in coins:
+            return 1
+        
+        res = float("inf")
+        for coin in coins:
+            if amount - coin >= 0:
+                val = self.coinChange(coins, amount - coin)
+                if val != -1:
+                    res = min(res, val+1)
+        
+        if res == float("inf"):
+            res = -1
+        
+        self.memo[amount] = res
+        return res
