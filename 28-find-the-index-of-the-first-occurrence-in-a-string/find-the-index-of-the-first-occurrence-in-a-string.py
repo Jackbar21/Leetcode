8410a4bc7aec85e531b6e8d70755ddc0bdc432ed
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-06-30 15:26:43 -0700

    Time: 41 ms (16.57%) | Memory: 16.5 MB (69.35%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def __init__(self):
+        self.haystack = None
+        self.needle = None
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
+        self.haystack = haystack
+        self.needle = needle
+        return self.helper(0,0)
+    def helper(self, i, j):
+        if j >= len(self.needle):
+            return i - j
+
+        if i >= len(self.haystack):
+            return -1        
+        
+        if self.haystack[i] == self.needle[j]:
+            k = 0
+            while (
+                j+k < len(self.needle) 
+                and i+k < len(self.haystack)
+                and self.haystack[i+k] == self.needle[j+k]
+            ):
+                k += 1
+            if k >= len(self.needle):
+                return i
+        
+        return self.helper(i+1, 0)
+            
