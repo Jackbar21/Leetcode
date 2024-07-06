fe5f5fb957bd866a3ff39fc18cde62a45985c582
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-06 09:45:50 -0700

    Time: 33 ms (66.33%) | Memory: 16.5 MB (30.38%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -1,8 +1,4 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         pos = time % (2*(n-1))
-        if pos < n:
-            return pos + 1
-        
-        pos -= n
-        return n - pos - 1
+        return pos + 1 if pos < n else 2*n - pos - 1
