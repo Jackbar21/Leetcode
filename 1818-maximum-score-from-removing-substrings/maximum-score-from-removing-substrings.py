22eb1a7dad712e49c31e5152824a5520c54dca6b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-11 19:18:56 -0700

    Time: 626 ms (14.93%) | Memory: 18.3 MB (38.96%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -13,38 +13,18 @@ class Solution:
         return res
 
     def removeAllSubstrings(self, s, sub):
-        new_s = []
-        count = 0
-        sub_len = len(sub)
-
-        for char in s:
-            new_s.append(char)
-            if len(new_s) >= sub_len and ''.join(new_s[-sub_len:]) == sub:
-                count += 1
-                del new_s[-sub_len:]
-        
-        return count, ''.join(new_s)
         # removes all occurrances of 'sub' in 's'
         # returns two-item tuple with:
         #    (1) count of all removed occurrances
         #    (2) updated string s itself
-        # Prerequisite: 'sub' must be of length 2
-
-        new_s = [s[0]]
+        new_s = []
         count = 0
 
-        prev = s[0]
-        for i in range(1,len(s)):
-            cur = s[i]
-
-            if prev + cur == sub:
+        for char in s:
+            new_s.append(char)
+            if len(new_s) >= len(sub) and ''.join(new_s[-len(sub):]) == sub:
                 count += 1
-                new_s.pop()
-            else:
-                new_s.append(cur)
-            
-            prev = cur
+                for _ in range(len(sub)):
+                    new_s.pop()
         
-        return (count, ''.join(new_s))
-
-
+        return count, ''.join(new_s)
