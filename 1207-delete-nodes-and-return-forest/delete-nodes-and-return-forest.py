4fbbbb17eb6752559deac610dc73377ea2980cb3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-17 01:04:14 -0700

    Time: 438 ms (5.22%) | Memory: 17.3 MB (7.26%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.res = None
+        self.found = False
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        self.res = [root]
+        for val in to_delete:
+            for i in range(len(self.res)):
+                rt = self.res[i]
+                self.res[i] = self.delNode(rt, val)
+
+                if self.found:
+                    self.found = False
+                    break
+
+        return [i for i in self.res if i is not None]
+        
+    def delNode(self, root, val):
+        if not root:
+            return root
+        
+        print(root.val, val)
+        
+        if root.val != val:
+            root.left = self.delNode(root.left, val)
+            root.right = self.delNode(root.right, val)
+            return root
+
+        # root.val == val
+        if root.left:
+            self.res.append(root.left)
+        if root.right:
+            self.res.append(root.right)
+        self.found = True
+        return None
