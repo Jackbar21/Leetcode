73723ef9f3a12bb390b658f88ca8042f711aa2ba
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-18 17:24:23 -0700

    Time: 111 ms (56.85%) | Memory: 16.8 MB (43.01%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -18,25 +18,18 @@ class Solution:
         return index
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
         # at most min(m,n)
-        lucky_contenders = []
+        lucky_contenders = set()
         for row_num in range(len(matrix)):
             row = matrix[row_num]
             col_num = self.getMinIndex(row)
-            lucky_contenders.append((row_num, col_num))
+            lucky_contenders.add((row_num, col_num))
         
-        print(lucky_contenders)
-        #  3  7  8
-        #  9 11 13
-        # 15 16 17
-        
-        res = set()
+        res = []
         for col_num in range(len(matrix[0])):
             col = [row[col_num] for row in matrix]
-            print(col)
             row_num = self.getMaxIndex(col)
-            print((row_num, col_num))
             if (row_num, col_num) in lucky_contenders:
-                res.add(matrix[row_num][col_num])
+                res.append(matrix[row_num][col_num])
         
         return list(res)
             
