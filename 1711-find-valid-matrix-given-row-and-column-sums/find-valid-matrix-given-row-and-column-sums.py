e2f775dcdf243936c8dea1502c5757aacdb33ff1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-19 22:31:41 -0700

    Time: 483 ms (74.73%) | Memory: 21.7 MB (53.07%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -11,10 +11,8 @@ class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
         matrix = [[0] * len(colSum) for i in range(len(rowSum))]
 
-        # cur_sum = 0
         row_sum = sum(rowSum)
         col_sum = sum(colSum)
-
         while row_sum + col_sum > 0:
             row_index = self.argMinNonZero(rowSum)
             col_index = self.argMinNonZero(colSum)
@@ -24,17 +22,9 @@ class Solution:
             rowSum[row_index] -= sum_to_add
             colSum[col_index] -= sum_to_add
             matrix[row_index][col_index] += sum_to_add
+            
+            # Loop Invariant
             row_sum -= sum_to_add
             col_sum -= sum_to_add
         
         return matrix
-        
-
-# matrix =
-# | 3  0 |
-# | 1  7 |
-
-# cur_sum = 3 + 1 + 7 = 11
-# total_sum = 3 + 8 + 4 + 7 = 11 + 
-# rowSum = [0, 0]
-# colSum = [0, 0]
