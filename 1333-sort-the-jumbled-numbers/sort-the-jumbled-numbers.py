13266089f8c732b16f862aa9f9bde327090e761d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-23 18:16:17 -0700

    Time: 1051 ms (63.59%) | Memory: 22.9 MB (76.63%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,8 +1,3 @@
 class Solution:
-    def __init__(self):
-        self.mapping = None
-    def mapNum(self, num):
-        return int(''.join(str(self.mapping[int(str_digit)]) for str_digit in str(num)))
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        self.mapping = mapping
-        return sorted(nums, key=lambda num:(self.mapNum(num)))
+        return sorted(nums, key=lambda num:int(''.join(str(mapping[int(str_digit)]) for str_digit in str(num))))
