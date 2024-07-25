7c5197e715a57b6eba0441f47a6471c381964a31
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-24 19:09:27 -0700

    Time: 1040 ms (70.68%) | Memory: 25.5 MB (31.08%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,13 +1,18 @@
 class Solution:
+    def __init__(self):
+        self.nums = None
+
     def sortArray(self, nums: List[int]) -> List[int]:
-        return self.mergeSort(nums, 0, len(nums)-1)
-    def mergeSort(self, nums, left, right):
+        self.nums = nums
+        return self.mergeSort(0, len(nums)-1)
+
+    def mergeSort(self, left, right):
         if left >= right:
-            return [nums[left]] if left == right else []
+            return [self.nums[left]] if left == right else []
         mid = (right+left) // 2
         
-        left_arr = self.mergeSort(nums, left, mid)
-        right_arr = self.mergeSort(nums, mid+1, right)
+        left_arr = self.mergeSort(left, mid)
+        right_arr = self.mergeSort(mid+1, right)
 
         l,r = 0,0
         res = []
