801f738da7b2ebef4263465cc321e01cb2f7be1e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-24 19:07:16 -0700

    Time: 1009 ms (72.81%) | Memory: 25.5 MB (31.08%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,26 +1,27 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        if len(nums) <= 1:
-            return nums
+        return self.mergeSort(nums, 0, len(nums)-1)
+    def mergeSort(self, nums, left, right):
+        if left >= right:
+            return [nums[left]] if left == right else []
+        mid = (right+left) // 2
         
-        mid = len(nums) // 2
-        left = self.sortArray(nums[:mid])
-        right = self.sortArray(nums[mid:])
+        left_arr = self.mergeSort(nums, left, mid)
+        right_arr = self.mergeSort(nums, mid+1, right)
 
         l,r = 0,0
-
         res = []
-        while l < len(left) and r < len(right):
-            if left[l] < right[r]:
-                res.append(left[l])
+        while l < len(left_arr) and r < len(right_arr):
+            if left_arr[l] < right_arr[r]:
+                res.append(left_arr[l])
                 l += 1
             else:
-                res.append(right[r])
+                res.append(right_arr[r])
                 r += 1
         
-        if l >= len(left):
-            res += right[r:]
+        if l >= len(left_arr):
+            res += right_arr[r:]
         else:
-            res += left[l:]
+            res += left_arr[l:]
         
         return res
