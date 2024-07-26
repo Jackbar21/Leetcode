2f850967d027b2d6c80c5e596e9d4d83fb8e93b2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-25 18:51:37 -0700

    Time: 691 ms (12.14%) | Memory: 19 MB (5.38%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,86 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        # E = edges
+        # V = {0,1,2,...,n} = {i : 0 <= i < n}
+        # L(u,v,k) = min. weight of u->v k path
+        # L(u,v,k) = min ({L(u,v,k-1)} U {L(u,w,k-1) + wt(w,v) : (w,v) \in E})
+        # u->v k-1, (w,v)
+
+        #           <= k-1          <= k-1
+        # p = i -------------k-------------------> j , k
+
+        # C(i,j,k) = min weight of an i->j k path (inf. if no such path)
+        #             forall i,j in V (where V = {0,1,...,n-1}), 
+        #             forall 0 <= k < n
+
+        # Case 1: k is *not* an intermediate node on p
+        #         C(i,j,k) = C(i,j,k-1)
+
+        # Case 2: k is intermediate node on p
+        #         C(i,j,k) = C(i,k,k-1) + C(k,j,k-1)
+
+        # (+) 
+        # C(i,j,k) = {
+        #    0,         if i==j and k = -1
+        #    wt(i,j),   if (i,j)\in edges and k = -1
+        #    INF,       if (i,j)\not\in edges and k = -1
+        #    min{ C(i,j,k-1), C(i,k,k-1) + C(k,j,k-1) }
+        # }
+
+        # FW(G,wt):
+        # initialize C(i,j,0) forall i,j\in V as in (+)
+        # for k := 1 to n do
+        #   for i := 1 to n do
+        #     for j := 1 to n do
+        #       C(i,j,k) := min(C(i,j,k-1), C(i,k,k-1) + C(k,j,k-1))
+        # return C(-,-,n)
+
+        # Running Time: O(n^3)
+        # Space Complexity: O(n^3) naively
+        #                   O(n^2) suffices (only store least two 3D "cds")
+
+        edge_dict = {}
+        for edge in edges:
+            u,v,cost = edge
+            edge_dict[(u,v)] = cost
+            edge_dict[(v,u)] = cost
+
+        # k == -1 setup
+        dp = [[float("inf")]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    dp[i][j] = 0
+                elif (i,j) in edge_dict:
+                    dp[i][j] = edge_dict[(i,j)]
+
+        new_dp = [[dp[i][j] for j in range(n)] for i in range(n)]
+        
+        for k in range(n): # k = 0
+            for i in range(n):
+                for j in range(n):
+                    case1 = dp[i][j] # i->j, k-1
+                    case2 = dp[i][k] + dp[k][j]
+                    new_dp[i][j] = min(case1, case2)
+            
+            # update dp
+            for i in range(n):
+                for j in range(n):
+                    assert new_dp[i][j] <= dp[i][j]
+                    dp[i][j] = new_dp[i][j]
+        
+        cities_dict = {i: set() for i in range(n)}
+        for i in range(n):
+            for j in range(n):
+                if dp[i][j] <= distanceThreshold:
+                    cities_dict[i].add(j)
+                    cities_dict[j].add(i)
+        
+        shortest_len = float("inf")
+        city = -1
+        for i in range(n):
+            if len(cities_dict[i]) <= shortest_len:
+                shortest_len = len(cities_dict[i])
+                city = i
+        
+        return city        
