e18333cb8b08cc378e66f867c66165c2bcd72761
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-28 22:53:58 -0700

    Time: 572 ms (40.98%) | Memory: 16.6 MB (66.67%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -4,9 +4,6 @@ class Solution:
         n = len(rating)
         for j in range(1,n-1):
             num_before_inc, num_before_dec = 0, 0
-            # Want to find elements in rating such that:
-            # (1) element occurs BEFORE j'th element in rating
-            # (2) rating value of that element is SMALLER than rating[j]
             for i in range(j):
                 if rating[i] < rating[j]:
                     num_before_inc += 1
@@ -14,9 +11,6 @@ class Solution:
                     num_before_dec += 1
             
             num_after_inc, num_after_dec = 0, 0
-            # Want to find elements in rating such. that:
-            # (1) element occurs AFTER j'th element in rating
-            # (2) rating value of that element is GREATER than rating[j]
             for k in range(j+1, n):
                 if rating[j] < rating[k]:
                     num_after_inc += 1
