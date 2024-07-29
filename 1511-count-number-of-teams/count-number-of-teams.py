506d2efe3dad1b33e40245fff87edb57f6d6d7ec
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-28 22:52:56 -0700

    Time: 568 ms (41.32%) | Memory: 16.6 MB (66.67%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -3,38 +3,27 @@ class Solution:
         res = 0
         n = len(rating)
         for j in range(1,n-1):
-            # How many elements exist in rating such that:
-            # (1) element occurs BEFORE index j
-            # (2) element's value is SMALLER than rating[j]
             num_before_inc, num_before_dec = 0, 0
+            # Want to find elements in rating such that:
+            # (1) element occurs BEFORE j'th element in rating
+            # (2) rating value of that element is SMALLER than rating[j]
             for i in range(j):
                 if rating[i] < rating[j]:
                     num_before_inc += 1
-                elif rating[i] > rating[j]:
+                if rating[i] > rating[j]:
                     num_before_dec += 1
             
-            # How many elements exist in rating such that:
-            # (1) element occurs AFTER index j
-            # (2) element's value is GREATER than rating[j]
             num_after_inc, num_after_dec = 0, 0
-            for k in range(j+1,n):
+            # Want to find elements in rating such. that:
+            # (1) element occurs AFTER j'th element in rating
+            # (2) rating value of that element is GREATER than rating[j]
+            for k in range(j+1, n):
                 if rating[j] < rating[k]:
                     num_after_inc += 1
-                elif rating[j] > rating[k]:
+                if rating[j] > rating[k]:
                     num_after_dec += 1
             
-            # Example:
-            # num_before = 2 [i1,i2] : rating[i1],rating[i2] < rating[j]
-            # num_after = 3  [k1,k2,k3] : rating[j]< rating[k1],rating[k2],rating[k3]
-            # 1. i1,j,k1 
-            # 2. i1,j,k2
-            # 3. i1,j,k3
-            # 4. i2,j,k1
-            # 5. i2,j,k2
-            # 6. i2,j,k3
-            # where 6 == 2 * 3
             res += num_before_inc * num_after_inc
             res += num_before_dec * num_after_dec
-        
+
         return res
-            
