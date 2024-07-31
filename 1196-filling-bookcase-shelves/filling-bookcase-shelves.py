2d974e671c3d7373e32b315daed020104f1222a5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-30 21:48:49 -0700

    Time: 51 ms (62.15%) | Memory: 17 MB (59.89%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.py
@@ -1,35 +1,24 @@
 class Solution:
     def __init__(self):
-        # HINT: Use dynamic programming: dp(i) will be the answer to the problem for books[i:].
-        self.memo = {} # (i, shelf_thickness, shelf_height)
+        self.memo = {}
         self.books = None
         self.shelfWidth = None
     def minHeightShelvesDp(self, i):
-        THICKNESS, HEIGHT = 0, 1
         if i in self.memo:
             return self.memo[i]
 
         if i >= len(self.books):
             return 0
-        
-        # book_thickness, book_height = self.books[i]
 
         count_books = 0
         tmp = 0
         while (
             (i + count_books) < len(self.books) and 
-            tmp + self.books[i + count_books][THICKNESS] <= self.shelfWidth
+            tmp + self.books[i + count_books][0] <= self.shelfWidth
         ):
-            tmp += self.books[i + count_books][THICKNESS]
+            tmp += self.books[i + count_books][0]
             count_books += 1
         
-        print(f"{count_books=}, {tmp=}")
-        
-        # 1,2
-        # 3--4
-        # for j in range(1,count_books+1):
-
-        # Case 1: only keep first book
         added_height = 0
         res = float("inf")
         for j in range(count_books): # TODO: Need the +1?
@@ -44,12 +33,6 @@ class Solution:
         return res
 
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        # Presetup
-        thickness, height = books[0]
-        total_height = height
-        cur_thickness = thickness
-        shelf_height = height
-        
         self.shelfWidth = shelfWidth
         self.books = books
         return self.minHeightShelvesDp(0)
