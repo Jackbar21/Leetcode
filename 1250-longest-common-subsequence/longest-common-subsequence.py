279c326eb3e22d1d379dbaab6f83911f04c2b59a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-31 22:59:40 -0700

    Time: 1328 ms (7.60%) | Memory: 184.4 MB (15.36%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -9,12 +9,11 @@ class Solution:
         if (i, j) in self.memo:
             return self.memo[(i, j)]
         
-        if i >= len(self.text1) or j >= len(self.text2):
-            return 0
-        
-        if self.text1[i] == self.text2[j]:
-            self.memo[(i, j)] = 1 + self.lcs(i + 1, j + 1)
-        else:
-            self.memo[(i, j)] = max(self.lcs(i + 1, j), self.lcs(i, j + 1))
+        self.memo[(i, j)] = (
+            0 if i >= len(self.text1) or j >= len(self.text2)
+            else 1 + self.lcs(i + 1, j + 1)
+            if self.text1[i] == self.text2[j]
+            else max(self.lcs(i + 1, j), self.lcs(i, j + 1))
+        )
 
         return self.memo[(i, j)]
