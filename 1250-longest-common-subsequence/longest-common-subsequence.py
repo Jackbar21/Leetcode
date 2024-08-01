3b3b80eaaf642f9f3cde8f5e4d7fdd281acc4f39
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-31 22:50:51 -0700

    Time: 2795 ms (5.02%) | Memory: 207.7 MB (14.63%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,45 +1,21 @@
 class Solution:
     def __init__(self):
-        self.s = None # smaller string
-        self.b = None # larger string
+        self.text1, self.text2 = None, None
         self.memo = {}
-    def isSubsequence(self, s, b):
-        if len(s) <= 0:
-            return True
-
-        i = 0
-        for letter in b:
-            if letter == s[i]:
-                i += 1
-                if i >= len(s):
-                    return True
-        return False
-
-
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        self.s, self.b = (text1, text2) if len(text1) < len(text2) else (text2, text1)
-        # want to find longest common subsequence of s inside b, since s is smaller
-        # This will be true for some s[i:j], where 0 <= i <= j <= len(s)
-        # We can use 2D Dynammic programming approach to solve this problem
-        # We know that for all i, s[i:i] is the empty string and a valid subset
-        # of length 0, meaning we could use that as our base for a bottom-up approach.
-        # However, I usually tend to prefer top-down since it's more intuitive to me.
-        # return self.longestComSub(0, len(s))
-        return self.longestComSub(0, 0)
-    def longestComSub(self, si, bi):
-        if si >= len(self.s) or bi >= len(self.b):
+        self.text1, self.text2 = text1, text2
+        return self.lcs(0, 0)
+    def lcs(self, i, j):
+        if i >= len(self.text1) or j >= len(self.text2):
             return 0
         
-        if (si, bi) in self.memo:
-            return self.memo[(si,bi)]
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
         
-        if self.s[si] == self.b[bi]:
-            self.memo[(si,bi)] = 1 + self.longestComSub(si+1,bi+1)
-        else:
-            self.memo[(si,bi)] = max(
-                self.longestComSub(si+1,bi),
-                self.longestComSub(si,bi+1)
-            )
+        res = 0
+        if self.text1[i] == self.text2[j]:
+            res = max(res, 1 + self.lcs(i + 1, j + 1))
         
-        return self.memo[(si,bi)]
-        
+        res = max(res, max(self.lcs(i + 1, j), self.lcs(i, j + 1)))
+        self.memo[(i, j)] = res
+        return self.memo[(i, j)]
