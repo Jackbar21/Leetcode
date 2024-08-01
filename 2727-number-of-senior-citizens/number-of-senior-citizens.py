3d4b9e5ea128d47e13fceda8893398347690cc73
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-07-31 17:39:33 -0700

    Time: 41 ms (86.50%) | Memory: 16.4 MB (75.00%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -3,9 +3,8 @@ class Solution:
         count = 0
 
         for detail in details:
-            assert len(detail) == 15
-            first, second = int(detail[-4]), int(detail[-3])
-            if first > 6 or first == 6 and second > 0:
+            first_digit, second_digit = int(detail[-4]), int(detail[-3])
+            if first_digit > 6 or first_digit == 6 and second_digit > 0:
                 count += 1
 
         return count
