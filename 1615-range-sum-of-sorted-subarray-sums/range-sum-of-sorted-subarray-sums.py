44f0196b50af7607f55bc1383114622135623e79
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-03 18:44:16 -0700

    Time: 333 ms (21.68%) | Memory: 36.2 MB (81.86%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,56 +1,39 @@
 class Solution:
     def __init__(self):
-        self.subarrays = []
+        self.subarray_sums = []
+        self.n = None
+        self.nums = None
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        prefix_sums = []
         magic_modulo_num = (10 ** 9) + 7
-        assert n == len(nums)
-        cur_sum = 0
-        for i in range(len(nums)):
-            cur_sum += nums[i]
-            prefix_sums.append(cur_sum)
-        # print(prefix_sums)
-        # return 0
+        self.n, self.nums = n, nums
 
-        d = {}
-        subarray_sums = []
-        for level in range(1,len(nums)+1):
-            # d[level] = self.populateSubarraySums(nums, level)
-            subarray_sums += self.populateSubarraySums(nums, level)
-        # print(d)
-        # return 0
-
-        
-        subarray_sums.sort()
+        for level in range(1, n + 1):
+            self.populateSubarraySums(level)
+        self.subarray_sums.sort()
 
         res = 0
-        for i in range(left-1,right):
-            res += subarray_sums[i]
+        for i in range(left - 1, right):
+            res += self.subarray_sums[i]
             res %= magic_modulo_num
         return res
-        
-        # print(d)
-        return 0
-    
-    def populateSubarraySums(self, nums, level):
-        if level > len(nums):
+
+    def populateSubarraySums(self, level):
+        n = self.n
+        if level > n:
             return
         
-        if level == len(nums):
-            return [sum(nums)]
-        
-        # level == 3
-        # 0-3
-        # 1-4
-        # 2-5
+        if level == n:
+            return self.subarray_sums.append(sum(self.nums))
 
+        # Setup first subarray of length level sum
         cur_sum = 0
         for i in range(level):
-            cur_sum += nums[i] # 0,1,2
-        res = [cur_sum]
-        for i in range(len(nums) - level + 0):
-            cur_sum -= nums[i]
-            cur_sum += nums[i + level]
-            res.append(cur_sum)
+            cur_sum += self.nums[i]
+        self.subarray_sums.append(cur_sum)
+
+        for i in range(n - level):
+            cur_sum -= self.nums[i]
+            cur_sum += self.nums[i + level]
+            self.subarray_sums.append(cur_sum)
         
-        return res
+        return
