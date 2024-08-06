0469728f502f9d9b6108245fe847df5e5570ac3b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-05 17:52:52 -0700

    Time: 214 ms (34.84%) | Memory: 17.6 MB (36.75%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -4,13 +4,12 @@ class Solution:
         for letter in word:
             d[letter] = d.get(letter, 0) + 1
 
-        arr = sorted(d.keys(), key=lambda key: d[key], reverse=True)
-
         number_slots = 8 # Constant
         multiplier = number_slots
         total_pushes = 0
 
-        for letter in arr:
+        # O(1)
+        for letter in sorted(d.keys(), key=lambda key: d[key], reverse=True):
             letter_freq = d[letter]
             total_pushes += letter_freq * (multiplier // number_slots)
             multiplier += 1
