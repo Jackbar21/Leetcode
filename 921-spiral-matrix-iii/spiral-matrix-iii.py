8790b76f99bf70a84eb968efe9e5df5ace22d42a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-07 22:15:36 -0700

    Time: 143 ms (5.08%) | Memory: 20.4 MB (7.42%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -209,7 +209,7 @@ class Solution:
 
             return self.visited_arr
         
-        if c == self.cols - 1:
+        if c == self.cols - 1 and r > 0:
             self.visited.add((rStart, cStart))
             self.visited_arr.append((rStart, cStart))
 
