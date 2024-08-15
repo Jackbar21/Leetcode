247a3156042f11ba710636889e3284344784bbec
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-14 17:17:27 -0700

    Time: 614 ms (71.87%) | Memory: 21.2 MB (15.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,37 +1,29 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        change = {
-            5: 0,
-            10: 0,
-            20: 0
-        }
+        five_bills, ten_bills = 0, 0
 
         for bill in bills:
-            assert bill in [5, 10, 20]
-
             # Case 1: customer pays $5, do nothing!
             if bill == 5:
-                change[5] += 1
+                five_bills += 1
             
             # Case 2: customer pays $10, check if have $5 to give back!
             elif bill == 10:
-                change[10] += 1
-                if change[5] > 0:
-                    change[5] -= 1  
+                ten_bills += 1
+                if five_bills > 0:
+                    five_bills -= 1
                 else:
-                    assert change[5] == 0
                     return False
             
             # Case 3: customer pays $20, check if can give back $10 + $5 or 3 x $5
             else:
-                assert bill == 20
-                change[20] += 1
-                if change[10] > 0 and change[5] > 0:
-                    change[10] -= 1
-                    change[5] -= 1
-                elif change[5] >= 3:
-                    change[5] -= 3
+                if ten_bills > 0 and five_bills > 0:
+                    ten_bills -= 1
+                    five_bills -= 1
+                elif five_bills >= 3:
+                    five_bills -= 3
                 else:
                     return False
         
+        # Broke exact change for every customer, so return true!
         return True
