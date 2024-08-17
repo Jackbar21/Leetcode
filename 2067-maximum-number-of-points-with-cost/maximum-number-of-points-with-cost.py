4ac087512e3a60131593cdec5c6fd51c8b97f70b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-16 22:41:13 -0700

    Time: 1542 ms (57.56%) | Memory: 50.2 MB (74.63%) - LeetSync

---
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m, n = len(points), len(points[0])
+        prev_row = points[0]
+
+        for r in range(1, m):
+            left_max, right_max, cur_row = [0] * n, [0] * n, [0] * n
+            left_max[0], right_max[-1] = prev_row[0], prev_row[-1]
+
+            for c in range(1, n):
+                left_max[c] = max(left_max[c-1] - 1, prev_row[c])
+
+            for c in range(n-2, -1, -1):
+                right_max[c] = max(right_max[c+1] - 1, prev_row[c])
+            
+            for i in range(n):
+                cur_row[i] = max(left_max[i], right_max[i]) + points[r][i]
+            
+            prev_row = cur_row
+        
+        return max(prev_row)
