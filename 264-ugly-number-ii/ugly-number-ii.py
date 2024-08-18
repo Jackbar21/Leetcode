20e3fed53ee5038929ab5165b089abdff51ee755
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-18 00:26:34 -0700

    Time: 123 ms (30.42%) | Memory: 16.9 MB (8.18%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,15 +1,3 @@
-#               1
-#       2       3       5
-#     4-6-10   6-9-15. 10-15-25
-#    8-12-20
-#    8
-
-# Ugly Numbers: 1 2 3 4 ... n'th ugly number
-# Candidates:   5 6 10 9 15 8 12 20
-
-# 1 2 3
-# Candidates: 2,3,5 --> 3,5,4,6,10 --> 5,4,6,10,9,15
-
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         candidates = [1] # min heap
@@ -28,4 +16,4 @@ class Solution:
                     seen.add(new_candidate)
                     heapq.heappush(candidates, new_candidate)
  
-        return ugly_nums[-1]
+        return ugly_nums[n - 1]
