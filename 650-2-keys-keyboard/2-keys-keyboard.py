158633b26b17916f958c5ee548e96d3e44165f2a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-18 19:23:45 -0700

    Time: 144 ms (48.29%) | Memory: 17.9 MB (27.33%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -3,31 +3,25 @@ class Solution:
         self.n = None
         self.memo = {}
     def minSteps(self, n: int) -> int:
+        self.n = n
         if n <= 1:
             return 0
-        
-        self.n = n
-        # return 
 
         # First step is copy step. This is unavoidable
-        steps = 1
-        cur_len = 1
-        paste_amount = 1 # Amount than you can paste at the moment
-        # just_copied = True # True if last action was 'copy', False if last action was 'paste'
+        # Hence we've already "taken" 1 step, with cur_len of 1
+        # since 1 'A' on screen, and paste_amount of 1 since we
+        # just copied the singular 'A' that is on screen :)
+        steps, cur_len, paste_amount = 1, 1, 1
         return steps + self.minStepsDp(cur_len, paste_amount)
     
-    def minStepsDp(self, cur_len: int, paste_amount: int) -> int:
-        assert self.n >= 1
-        if self.n <= 1:
-            return 0
-        
+    def minStepsDp(self, cur_len: int, paste_amount: int) -> int:        
         if cur_len >= self.n:
-            # return steps # TODO: consider removing steps altogether!
+            # If cur_len > self.n, doesn't matter how many times we copy and/or paste, we will
+            # NEVER reach a solution (since we can only grow the number of As, not shrink it!)
             return 0 if cur_len == self.n else float("inf")
         
         if (cur_len, paste_amount) in self.memo:
             return self.memo[(cur_len, paste_amount)]
-        
         
         # Case 1: Choose copy action
         assert cur_len >= paste_amount
@@ -38,10 +32,3 @@ class Solution:
 
         self.memo[(cur_len, paste_amount)] = 1 + min(case1, case2)
         return self.memo[(cur_len, paste_amount)]
-
-
-
-
-
-        
-        
