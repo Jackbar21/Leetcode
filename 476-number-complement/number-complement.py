0bc6361d68d4ae78ad99f4c036c505731c5b2f49
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-21 18:15:54 -0700

    Time: 31 ms (78.63%) | Memory: 16.4 MB (58.08%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,3 +1,3 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        return int(''.join('0' if d == '1' else '1' for d in bin(num)[2:]), 2)
+        return int(''.join(str(1 - int(d)) for d in bin(num)[2:]), 2)
