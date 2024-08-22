930b92e532b297aa91a5f47dadc214412a24acb4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-21 18:14:55 -0700

    Time: 34 ms (61.42%) | Memory: 16.5 MB (14.64%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,14 +1,4 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        # Convert num to binary (e.g. 5 --> '0b101')
-        bin_num = bin(num)
-
-        # Ignore first two characters (e.g. '0b101' --> '101')
-        bin_num = str(bin_num[2:])
-
-        # Flip all the digits (e.g. '101' --> '010')
-        flipped_bin_num = ''.join(str(1 - int(digit)) for digit in bin_num)
-        
-        # Convert binary into decimal (e.g. '010' --> 2)
-        return int(flipped_bin_num, 2)
+        return int(''.join(str(1 - int(d)) for d in bin(num)[2:]), 2)
         
