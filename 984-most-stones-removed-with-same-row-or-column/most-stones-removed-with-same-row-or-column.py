8c9a8524f392ecaaf86f003fe4f25f1f8914e02f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-28 20:30:38 -0700

    Time: 673 ms (21.74%) | Memory: 17.3 MB (71.81%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -15,9 +15,9 @@ class Solution:
         self.d = None
         self.num_stones = 0
     def removeStones(self, stones: List[List[int]]) -> int:
-        d = {
-            tuple(stone): set() for stone in stones
-        }
+        # d = {
+        #     tuple(stone): set() for stone in stones
+        # }
 
         groups = []
         # print(sorted(stones))
@@ -67,69 +67,69 @@ class Solution:
 
 
 
-        for i in range(len(stones)):
-            x, y = stones[i]
-            for stone in d:
-                if stone[0] == x or stone[1] == y:
-                    d[stone].add((x, y))
+    #     for i in range(len(stones)):
+    #         x, y = stones[i]
+    #         for stone in d:
+    #             if stone[0] == x or stone[1] == y:
+    #                 d[stone].add((x, y))
         
-        # Remove each stone from its own list of "touching" stones
-        for stone in d:
-            d[stone].remove(stone)
+    #     # Remove each stone from its own list of "touching" stones
+    #     for stone in d:
+    #         d[stone].remove(stone)
         
-        # If a stone isn't "touching" any other stone,
-        # it cannot ever be removed, and hence doesn't need
-        # to be considered
-        # for stone in d:
-        #     if d[stone] == 0:
-        #         del d[stone]
-        self.d = {
-            key: d[key] for key in d if len(d[key]) > 0
-        }
+    #     # If a stone isn't "touching" any other stone,
+    #     # it cannot ever be removed, and hence doesn't need
+    #     # to be considered
+    #     # for stone in d:
+    #     #     if d[stone] == 0:
+    #     #         del d[stone]
+    #     self.d = {
+    #         key: d[key] for key in d if len(d[key]) > 0
+    #     }
 
-        # No stones left to remove
-        if len(self.d.keys()) == 0:
-            return 0
+    #     # No stones left to remove
+    #     if len(self.d.keys()) == 0:
+    #         return 0
 
         
-        # for key in self.d:
-        #     print(f"{key=}: {d[key]=}")
-        # return 1
+    #     # for key in self.d:
+    #     #     print(f"{key=}: {d[key]=}")
+    #     # return 1
 
-        # Want to remove the stone with FEWEST number of "touching" stones.
-        # Where a stone i is touch stone j if and only if stone i and j are in the
-        # same row and/or i and j are in the same column.
-        # next_stone = min(self.d.keys(), key=lambda k: len(self.d[k]))
-        next_stone = self.nextStone()
+    #     # Want to remove the stone with FEWEST number of "touching" stones.
+    #     # Where a stone i is touch stone j if and only if stone i and j are in the
+    #     # same row and/or i and j are in the same column.
+    #     # next_stone = min(self.d.keys(), key=lambda k: len(self.d[k]))
+    #     next_stone = self.nextStone()
         
-        # Returns the stone that was removed, or None if no more stones can be removed
-        while next_stone is not None:
-            self.removeStone(next_stone)
-            # next_stone = min(self.d.keys(), key=lambda k: len(self.d[k])) # O(n)
-            next_stone = self.nextStone()
+    #     # Returns the stone that was removed, or None if no more stones can be removed
+    #     while next_stone is not None:
+    #         self.removeStone(next_stone)
+    #         # next_stone = min(self.d.keys(), key=lambda k: len(self.d[k])) # O(n)
+    #         next_stone = self.nextStone()
         
 
-        return self.num_stones
+    #     return self.num_stones
 
-    def nextStone(self):
-        best_stone, best_val = None, float("inf")
-        for stone in self.d:
-            if len(self.d[stone]) > 0 and len(self.d[stone]) < best_val:
-                best_val = len(self.d[stone])
-                best_stone = stone
+    # def nextStone(self):
+    #     best_stone, best_val = None, float("inf")
+    #     for stone in self.d:
+    #         if len(self.d[stone]) > 0 and len(self.d[stone]) < best_val:
+    #             best_val = len(self.d[stone])
+    #             best_stone = stone
         
-        return best_stone
+    #     return best_stone
 
-    def removeStone(self, del_stone):
-        # x, y = next_stone
-        assert del_stone in self.d
+    # def removeStone(self, del_stone):
+    #     # x, y = next_stone
+    #     assert del_stone in self.d
 
-        del self.d[del_stone]
-        self.num_stones += 1
+    #     del self.d[del_stone]
+    #     self.num_stones += 1
 
-        for stone in self.d:
-            if del_stone in self.d[stone]:
-                self.d[stone].remove(del_stone)
+    #     for stone in self.d:
+    #         if del_stone in self.d[stone]:
+    #             self.d[stone].remove(del_stone)
         
-        return del_stone
+    #     return del_stone
 
