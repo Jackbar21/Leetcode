cb26d72c36649dc921de3b23d5dc8effdb00fd2c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-30 17:57:27 -0700

    Time: 675 ms (5.64%) | Memory: 28.6 MB (47.81%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -1,121 +1,53 @@
 class Solution:
     def __init__(self):
-        self.n = None
-        self.prev = None
-        self.end_node = None
         self.adj_list = None
+        self.n = None
     def dijkstra(self, s):
-        # Dictionary mapping of shortest path (from s) for each node
+        # This is a special version of dijkstra, which returns the maximum-product
+        # path instead of the minimum-sum path.
         d = {
-            i: float("inf") for i in range(self.n)
+            i: 0 for i in range(self.n)
         }
-        d[s] = 0
+        d[s] = 1
 
-        min_heap = [(0, s)]
-        for i in range(self.n):
-            if i == s:
-                continue
-            min_heap.append((float("inf"), i))
-
-        self.prev = {
-            i: None for i in range(self.n)
-        }
-        self.prev[s] = s
-
+        max_heap = [(-1, s)] + [(0, i) for i in range(self.n) if i != s]
         visited = set()
+
         while len(visited) < self.n:
-            # Want to pop node with HIGHEST d-value that is NOT in visited
-            best_val, best_node = heapq.heappop(min_heap)
-            # Min heap will have at most n + m <= 3n elements inside
-            # of it at any given point in time. So the complexity will
-            # still be O((n+m)logn) = O(nlogn) [since m <= 2n], similar
-            # to the classical heap implementation of dijkstra with only
-            # at most n elements (without visited set) and frequent calls
-            # CHANGE-KEY (i.e. changing priority level of element in heap).
-            while best_node in visited:
-                best_val, best_node = heapq.heappop(min_heap)
+            best_val, best_node = heapq.heappop(max_heap)
+            if best_node in visited:
+                continue
             
-            # We only care about shortest path from s to self.end_node.
-            # So if we've computed the result ALREADY for self.end_node
-            # in d, then simply return d prematurely.
-            if best_node == self.end_node:
-                return d
+            # Since we're simulating a max heap using python's heapq min-heap
+            # implementation by multiplying all the values by -1, we must make
+            # sure to multiply by -1 again when we pop them from the heap (not just insert)
+            u = best_node
+            d[u] = -best_val
+            visited.add(u)
 
-            visited.add(best_node)
-            for v, prob in self.adj_list[best_node]:
-                u = best_node
-                # s --> best_node
-                # just "popped" best_node (== u)
-                # and for each edge (u, v) from adj_list:
-                # want s --> v to be min(s --> v, s --> u + cost(u,v)), 
-                #       where cost(u,v) is computed using "log trick"
-
-                mapped_prob = -math.log(prob)
-                new_cost = d[u] + mapped_prob
-                if new_cost < d[v]:
+            # We now have the BEST (maximum-product not minimum-sum) path from s to u,
+            # s ---> u. Now for every edge (u,v), we want to update s ---> v to be the
+            # better (in this case larger) between s ---> v and s ---> u (== d[u]) + cost(u,v)
+            for v, prob in self.adj_list[u]:
+                new_cost = d[u] * prob
+                if new_cost > d[v]:
                     d[v] = new_cost
-                    self.prev[v] = u
-                
-                    # When we get to v in some future iteration (if not yet seen),
-                    # the nice property is that even if there are many entries for
-                    # node v in the heap, by property of min heaps, only the best
-                    # (i.e. smallest) found candidate for v will be considered, and
-                    # thus this algorithm will still be correct & optimal.
-                    if v not in visited:
-                        heapq.heappush(min_heap, (d[v], v))
-
+                    heapq.heappush(max_heap, (-new_cost, v)) # -new_cost since max-heap!
+        
         return d
+
+
+
 
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         self.n = n
-        self.end_node = end_node
+        self.adj_list = {i: [] for i in range(self.n)}
         assert len(edges) == len(succProb)
-        # Idea: Leverage something like Dijkstra's (that returns smallest path where cost of
-        # a path is defined as SUM of its edges) by changing the value of each edge to the LOG
-        # of its value. This way, we can leverage properties of the log function, namely that
-        # log(X) + log(Y) = log(XY) for any X,Y>0, and that X > Y > 0 ==> log(X) > log(Y).
-
-        # Step 1: Graph reduction (change edge values, *as well as make graph directed)
-        # math.log from python is in base e (== 2.718281828...)
-        # reduced_edges = [math.log(edge) for edge in edges]
-
-        # Create adjaceny list of graph for faster edge population
-        # traversals inside dijkstra's algorithm call. We can afford
-        # this computation below efficiently since m is bounded by 2 * n :)
-        self.adj_list = { i: [] for i in range(self.n) }
         for i in range(len(edges)):
             u, v = edges[i]
             prob = succProb[i]
-            mapped_prob = -math.log(prob)
-
             self.adj_list[u].append((v, prob))
             self.adj_list[v].append((u, prob))
-        
-        # Naive array implementation: O(n^2)
-        # Heap implementation: O((n+m)logn) = O(nlogn) [since m <= 2n]
-        d = self.dijkstra(start_node)
 
-        # Now we have access to self.prev, which gives us the previous node
-        # in the shortest path with source node s to any destination node.
-        # So, traverse from end_node backwards to start_node, get the probability
-        # edge weights from succProb, and return the product of those probabilities.
-        # But first, if there is no path from start_node to end_node, return 0.
-        if self.prev[end_node] == None:
-            return 0
-        
-        # O(n)
-        probs = {}
-        for i in range(len(edges)):
-            u, v = edges[i]
-            probs[(u, v)] = succProb[i]
-            probs[(v, u)] = succProb[i]
-        
-        # O(n) work
-        res_prob = 1
-        cur_node = end_node
-        while cur_node != start_node:
-            prev_node = self.prev[cur_node]
-            res_prob *= probs[(prev_node, cur_node)]
-            cur_node = prev_node
-        
-        return res_prob
+        d = self.dijkstra(start_node)
+        return d[end_node]
