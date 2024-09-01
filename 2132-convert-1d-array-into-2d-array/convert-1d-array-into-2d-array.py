9dce7ff0ab889402ad62e64b9a399af2537ccb68
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-31 17:48:46 -0700

    Time: 714 ms (13.75%) | Memory: 24.6 MB (11.49%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -3,6 +3,14 @@ class Solution:
         if len(original) != m * n: # TODO: maybe only do this if len(original) < m * n ?
             return []
         
+        res = [
+            [
+                original[(n * j) + i] for i in range(n)
+            ] 
+            for j in range(m)
+        ]
+        return res
+        
         res = [[]]
         for num in original:
             if len(res[-1]) < n:
