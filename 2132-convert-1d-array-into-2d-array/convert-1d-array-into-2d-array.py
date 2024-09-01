b2df63fa922202eb012ed983af39b29a35f64b07
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-31 17:37:25 -0700

    Time: 693 ms (34.93%) | Memory: 23.7 MB (95.10%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if len(original) != m * n: # TODO: maybe only do this if len(original) < m * n ?
+            return []
+        
+        row_offset = 0
+        res = [[]]
+
+        for num in original:
+            if len(res[-1]) < n:
+                res[-1].append(num)
+            else:
+                res.append([num])
+        
+        return res
