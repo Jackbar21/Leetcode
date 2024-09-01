603b6165b590cae440c25b3de21d53685145a7af
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-08-31 17:39:03 -0700

    Time: 691 ms (38.14%) | Memory: 24.1 MB (46.05%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -3,10 +3,10 @@ class Solution:
         if len(original) != m * n: # TODO: maybe only do this if len(original) < m * n ?
             return []
         
-        row_offset = 0
         res = [[]]
-
-        for num in original:
+        # for num in original:
+        for i in range(m * n):
+            num = original[i]
             if len(res[-1]) < n:
                 res[-1].append(num)
             else:
