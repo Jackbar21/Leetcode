137cf98980768f90eb5ab7920bc9f12c4868b970
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-01 19:05:06 -0700

    Time: 561 ms (91.29%) | Memory: 30.6 MB (19.92%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,18 +1,10 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        n = len(chalk)
-
         k %= sum(chalk)
 
-        i = 0
-        # while chalk[i % n] >= k:
-        while chalk[i] <= k:
-            # print(i, k)
+        for i in range(len(chalk)):
+            if chalk[i] > k:
+                return i
             k -= chalk[i]
-            i += 1
-            assert i != n
-            if i >= n:
-                assert i == n
-                i = 0
-        
-        return i
+
+        raise Exception("Unreachable code")
