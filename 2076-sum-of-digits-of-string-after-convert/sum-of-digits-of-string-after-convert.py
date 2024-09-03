dbc55c1cafc8cfc3b6533f3f936499d4e7b7f7e8
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-02 18:26:29 -0700

    Time: 33 ms (85.81%) | Memory: 16.4 MB (73.84%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,22 +1,16 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        # In case you guys don't hear me well, I'll make sure
-        # to write out my steps & ideas as I go along. I'm currently
-        # on the train back to Seattle from my mini-trip to Vancouver :)
-
-        # Step 1: Convert the string 's' into the number
-        # First thing we need is a hashmap
         letters = 'abcdefghijklmnopqrstuvwxyz'
-        d = {letters[i]: (i+1) for i in range(26)} # letter to value mappings
+        d = {letters[i]: (i+1) for i in range(26)} # letter-to-value map
         transformed_s = ''.join(str(d[letter]) for letter in s)
 
-        # Transform up to k times (exit early if digit is 1 character long, since
-        # 8 --> 8 --> 8 .... --> 8 no matter how many times you repeat, and same for 1-9)
+        # Transform (up to) k times
         for _ in range(k):
-            if len(transformed_s) <= 1:
-                assert len(transformed_s) == 1
+            # If transformed string is only one character long, it doesn't matter
+            # how many more times we transform it, it will always remain the same.
+            if len(transformed_s) == 1:
                 break
-            
+
             transformed_s = str(sum(int(i) for i in transformed_s))
 
         return int(transformed_s)
