553a95476a9072315a84975e10807fa5231c789b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-05 17:37:16 -0700

    Time: 717 ms (64.33%) | Memory: 54.7 MB (81.13%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -7,15 +7,35 @@ class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         nums_set = set(nums)
 
-        new_head = ListNode(-1) # dummy node
-        last_node = new_head
-
-        while head:
-            if head.val not in nums_set:
-                last_node.next = ListNode(head.val)
-                last_node = last_node.next
-
-            # Loop Invariant
+        
+        # Keep moving head until it is either None or a VALID number (i.e. not in nums)
+        while head and head.val in nums_set:
             head = head.next
         
-        return new_head.next
+        # If head is None, no valid nums, hence return None
+        # if not head:
+        #     return None
+        
+        prev = head
+        assert prev is not None and prev.val not in nums_set # i.e. assert prev is valid node
+        cur = head.next
+
+        while cur:
+            # Delete cur if invalid (i.e. cur.val is in nums)
+            next_node = cur.next
+            if cur.val in nums_set:
+                # Delete cur
+                # FROM: prev -> cur -> next_node
+                # TO: prev -> next_node
+                prev.next = next_node
+
+                # Loop Invariant
+                # prev = next_node
+                # cur = next_node.next
+                cur = next_node
+            else:
+                # Loop Invariant
+                prev = cur
+                cur = cur.next
+
+        return head
