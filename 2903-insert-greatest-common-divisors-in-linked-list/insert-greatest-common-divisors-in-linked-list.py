e2f56bbd7ebd16183d44f1ffad7d1797ba9c2eed
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-09 18:06:11 -0700

    Time: 73 ms (47.04%) | Memory: 19.5 MB (97.91%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
@@ -6,15 +6,13 @@
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # If linked list has <= 1 nodes, return immediately
-        if not head or not head.next:
-            return head
+        # if not head or not head.next:
+        #     return head
         
-        prev = head     # 18
-        cur = head.next # 6
-        
-
+        prev = head
+        cur = head.next
         while cur:
-            node = ListNode(math.gcd(prev.val, cur.val)) # 6'
+            node = ListNode(math.gcd(prev.val, cur.val))
             prev.next = node
             node.next = cur
 
@@ -23,33 +21,3 @@ class Solution:
             cur = cur.next
         
         return head
-
-
-
-
-        # 18 -> 6 -> 10 -> 3
-        # 18 -> 6'
-        # 6' -> 6
-        # 18 -> 6' -> 6 -> 10 -> 3
-
-
-    def insertGreatestCommonDivisorsRec(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Case 1: Zero nodes left
-        if not head:
-            return head
-        
-        # Case 2: One node left
-        if not head.next:
-            return head
-
-        # Case 3: Two or more nodes left (recursive)
-        first, second, rest = head, head.next, head.next.next
-        gcd = math.gcd(first.val, second.val)
-        node = ListNode(gcd)
-        first.next = node
-        node.next = second
-        second = self.insertGreatestCommonDivisors(second)
-
-        return first
-
-
