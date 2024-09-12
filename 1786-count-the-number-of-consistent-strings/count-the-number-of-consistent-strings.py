fac21fd09c034ead8c6f9807308417d46c904bb5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-11 17:14:12 -0700

    Time: 209 ms (64.63%) | Memory: 18.4 MB (98.41%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -1,10 +1,6 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         allowed_letters = set(allowed)
-        # return len(list(
-        #     filter(lambda word: all([letter in allowed_letters for letter in word]), words)
-        # ))
-        
         res = 0
         for word in words:
             is_allowed = True
@@ -12,7 +8,7 @@ class Solution:
                 if letter not in allowed_letters:
                     is_allowed = False
                     break
-                
+
             res += is_allowed
         
         return res
