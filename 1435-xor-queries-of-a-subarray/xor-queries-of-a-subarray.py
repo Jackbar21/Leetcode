39a5c1f7828ce5ecfb51047abcb255cc76e89386
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-12 18:32:07 -0700

    Time: 348 ms (5.24%) | Memory: 31.6 MB (10.11%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -4,33 +4,21 @@ class Solution:
         cache = {0: arr[0]}
         for i in range(1, len(arr)):
             cache[i] = cache[i - 1] ^ arr[i]
-            # XOR ^= arr[i]
-            # cache[i] = XOR
+
         res = {}
         sorted_queries = sorted(queries)
-        # print(sorted_queries)
-        # print(queries)
-        prev_left = 0
+
+        prev_left = 1
         XOR = arr[0]
         for query in sorted_queries:
             left, right = query
-
-            # Base Case #1:
             if left == 0:
                 res[(left, right)] = cache[right]
                 continue
-            
-            # Base Case #2:
-            # if left == right:
-            #     res[(left, right)] = arr[left]
-            #     continue
 
             if prev_left < left:
                 # XOR is currently value from 0 to prev_left (not inclusive)
-                # for i in range(min_right, left):
-                #     del cache[i]
-                # XOR = arr[0]
-                for i in range(max(1, prev_left), left):
+                for i in range(prev_left, left):
                     XOR ^= arr[i]
                 # for key in cache:
                 #     cache[key] ^= XOR
