fb319f62c5685a88c2d15c6db986174b24d9fd9b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-12 17:49:08 -0700

    Time: 6283 ms (5.06%) | Memory: 31.6 MB (10.11%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -1,28 +1,40 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        res = []
         cache = {}
-        # print(queries[:10])
-        # print(sorted(queries)[:10])
-        # return []
-        for query in queries:
+        sorted_queries = sorted(queries)
+        prev_left = -1
+        for query in sorted_queries:
             left, right = query
             if (left, right) in cache:
-                res.append(cache[(left, right)])
                 continue
+            
+            # 
+            # if prev_left < left:
+            #     # left, right = (1, X)
+            #     # prev_left = 0
+            #     # want [1,X] == cache[(0,X)] ^ arr[0]
+            #     # If [0,X] in cache, we're done!
+            #     if (prev_left, right) in cache:
+            #         XOR = cache[(prev_left, right)] ^ arr[prev_left]
+            #         cache[(left, right)] = XOR
+            #         continue
+
+
+                # prev_left = left
+
+            
             XOR = arr[left]
             for i in range(left + 1, right + 1):
                 XOR = XOR ^ arr[i]
             cache[(left, right)] = XOR
-            res.append(XOR)
+            # d[(left, right)] = 
+            # res.append(XOR)
         
-        return res
+        # print(cache)
+        return [cache[tuple(query)] for query in queries]
 
-        # plaintext ^ key ^ key = plaintext
 
-        # a ^ a == 0
+        # [0,4], [0,5], [0,6], [1,3]
+        # [1,3] == 0 ^ [0,3]
 
-        # 0,3 --> 4,5 --> 0
-        1,5
-
-        # 1,000,000
+        # [0,4], [2,7]
