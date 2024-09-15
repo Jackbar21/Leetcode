ec0323c108e33090f2c669bd8807cf7038c86140
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-14 18:34:42 -0700

    Time: 1305 ms (5.01%) | Memory: 74.5 MB (5.01%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -19,32 +19,17 @@ class Solution:
         # Now want to find max{j-i+1 | self.isValidSubstring(i, j)}
         longest_count = 0
         for i in range(len(self.s)):
-            # for j in range(i, len(self.s)):
             for j in range(longest_count + i, len(self.s)):
-                if self.prefix_counts[j] ^ self.prefix_counts[i - 1] == 0:
+                # j starting from longest_count + i since only
+                # want to update longest_count on valid substring
+                # where j - i + 1 > longest_count <==> j >= longest_count + i
+                if self.isValidSubstring(i, j):
                     longest_count = j - i + 1
 
         return longest_count
-
-        # j > longest_count + i - 1
-    
-
-
-    # def evenVowelCounts(self, vowel_counts):
-    #     return all([value % 2 == 0 for value in vowel_counts.values()])
 
     def isValidSubstring(self, i, j):
         # Want even number of vowels between i and j in s.
         # One way to check this is count number of vowels between index 0 to j,
         # and subtract number of values between 0 and i.
         return 0 == self.prefix_counts[j] ^ self.prefix_counts[i - 1]
-
-        # vowel_counts = {vowel: 0 for vowel in 'aeiou'}
-        # for vowel, count in self.prefix_counts[j].items():
-        #     vowel_counts[vowel] += count
-        
-        # if i != 0:
-        #     for vowel, count in self.prefix_counts[i - 1].items():
-        #         vowel_counts[vowel] -= count
-        
-        # return self.evenVowelCounts(vowel_counts)
