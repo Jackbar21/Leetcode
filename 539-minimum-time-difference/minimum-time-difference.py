549cfcf5db5bf88d4da9d6f4899024f1f6c95b7f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-15 17:36:23 -0700

    Time: 68 ms (68.50%) | Memory: 19.6 MB (79.74%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -4,8 +4,9 @@ class Solution:
         return int(hour) * 60 + int(minute)
     def findMinDifference(self, timePoints: List[str]) -> int:
         sorted_minutes = sorted(map(self.convertToMinutes, timePoints))
-        #print(sorted_minutes)
-        # res = float("inf")
+
+        # Possible that first and last times are actually closest.
+        # (i.e. 12:00 AM and 11:59 PM are only one minute apart)
         res = self.getDifference(sorted_minutes[0], sorted_minutes[-1])
         for i in range(1, len(sorted_minutes)):
             time1, time2 = sorted_minutes[i - 1], sorted_minutes[i]
@@ -24,27 +25,12 @@ class Solution:
         # their literal (absolute-value) difference, OR when looping
         # back through midnight.
         big, small = max(time1, time2), min(time1, time2)
-        #print(big, small)
 
         # Case 1: go FORWARD in time from big to small
-        # (0 if big == 0 else 24 * 60 - big)
-        case1 = (24 * 60 - big) + small
+        # N.B. there are at most 24 hours == 1440 minutes in a day
+        case1 = (1440 - big) + small
 
         # Case 2: go FORWARD in time from small to big
         case2 = big - small
 
-        return min(case1, case2)
-
-
-        # case1 = abs(time1 - time2)
-        # case2 = abs(time1 - time2 + 24 * 60)
-
-        # Case1: go FORWARD in time from time1 to time2
-        #        <==> BACKWARD in time from time2 to time1
-
-        # Case2: go BACKWARD in time from time1 to time2
-        #       <==> FORWARD in time from time2 to time1
-
-
-        # #print(case1, case2)
         return min(case1, case2)
