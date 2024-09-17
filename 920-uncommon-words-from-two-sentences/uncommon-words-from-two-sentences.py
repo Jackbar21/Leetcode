b312b2df3285612916871350e06d9b3354746537
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-16 18:15:00 -0700

    Time: 31 ms (83.66%) | Memory: 16.5 MB (78.21%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
@@ -10,11 +10,11 @@ class Solution:
         for word in s2:
             d2[word] = d2.get(word, 0) + 1
 
-        for word in s1:
+        for word in d1:
             if d1[word] == 1 and word not in d2:
                 res.append(word)
         
-        for word in s2:
+        for word in d2:
             if d2[word] == 1 and word not in d1:
                 res.append(word)
         
