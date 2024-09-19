39c48badd1c45e158d31f8073fbb66395e822c8a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-18 20:18:01 -0700

    Time: 107 ms (5.06%) | Memory: 17 MB (8.08%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,51 +1,31 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val="", nxt=None):
-        self.val = val
-        self.next = nxt
-
 class Solution:
-    # Linked List to list/array
-    def ll_to_list(self, head):
-        res = []
-        while head:
-            res.append(head.val)
-            head = head.next
-        return res
     def diffWaysToCompute(self, expression: str) -> List[int]:
         # Setup expression as array with entries either being number
         # or operator, to make problem easier. Since expression.length <= 20,
         # this is only constant time :)
-        head = ListNode(-1)
-        cur = head
+        exp = expression
+        stack = []
         i = 0
-        while i < len(expression):
+        while i < len(exp):
             l = i
-            while i < len(expression) and expression[i] not in "+-*":
+            while i < len(exp) and exp[i] not in "+-*":
                 i += 1
 
-            cur.next = ListNode(expression[l:i]) # add number as string
-            cur = cur.next
-            if i < len(expression):
-                cur.next = ListNode(expression[i])   # add operator
-                cur = cur.next
+            stack.append(exp[l:i])  # add number as string
+            if i < len(exp):
+                stack.append(exp[i])   # add operator
             i += 1
-        
-        # print(ll_to_list(head.next))
-        # return []
 
         # Idea: for each operator, consider the case where it goes first. Apply the
         # operator, recursively get all the remaining sub-cases, and consider that
         # as one subset of possible values. Combine all subsets, to get your result.
-        stack = self.ll_to_list(head.next)
         results_as_strings = self.allPossibleValues(stack)
-        # results_as_strings = self.allPossibleValues(head.next)
         results_as_values = map(eval, results_as_strings)
         return results_as_values
 
     def allPossibleValues(self, stack):
         # Stack must be odd in length, since X operators & (X + 1)  
-        # numbers ---> 2X + 1 ---> odd number of elements in stac
+        # numbers ---> 2X + 1 ---> odd number of elements in stack
         assert len(stack) % 2 == 1
 
         # Base Case #1: Only one operator choice left
