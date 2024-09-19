40459914aa9b94d3fade7e97bb3270bc0b3b8d5f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-18 19:34:45 -0700

    Time: 269 ms (5.06%) | Memory: 21.3 MB (8.08%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -83,40 +83,11 @@ class Solution:
         assert len(stack) % 2 == 1
         # Base Case #1: Only one operator choice left
         if len(stack) <= 3:
+            # assert len(stack) == 3
             if len(stack) == 1:
                 return [stack[0]]
-            else:
-                assert len(stack) == 3
-                return [f"({''.join(stack)})"]
-            # res = stack[0]
-            # if len(stack) == 1:
-            #     assert False
-            #     return res
             
-            # if stack[1] == "*":
-            #     res *= stack[2]
-            # elif stack[1] == "+":
-            #     res += stack[2]
-            # else:
-            #     res -= stack[2]
-            
-            # return [res]
-
-        # Base Case #2: If have ONLY additions or ONLY multiplications,
-        # order DOES NOT MATTER, and should just directly evaluate result
-        # if op_counts["-"] <= 0:
-        #     assert op_counts["-"] == 0
-
-        #     # Only multiplication symbols
-        #     if op_counts["+"] == 0:
-        #         base = 1
-        #         for i in range(0, len(stack), 2):
-        #             base *= stack[i]
-        #         return [base]
-            
-        #     # Only addition symbols
-        #     if op_counts["*"] == 0:
-        #         return [sum(stack[i] for i in range(0, len(stack), 2))]
+            return [f"({''.join(stack)})"]
 
         # Operators are at odd indices. For each operator index,
         # consider the case where it is considered for evaluation first,
@@ -126,45 +97,12 @@ class Solution:
             # i-1,i,i+1 --> directly evaluated
             # 0:i-1     --> kept as same
             # i+2:      --> kept as same
-            op = stack[i]
-            op_counts[op] -= 1
             old_case = stack[:i-1] + self.allPossibleValues(stack[i-1:i+2]) + stack[i+2:]
-            # res.append(old_case)
-            str1 = ''.join(str(i) for i in stack[:i-1])
-            str2 = f"({''.join(str(i) for i in stack[i-1:i+2])})"
-            str3 = ''.join(str(i) for i in stack[i+2:])
-            corresponding_string = ''.join([str1, str2, str3])
-            self.strings.add(corresponding_string)
-        # return res
-            # tuple_case = tuple(old_case)
-            # if tuple_case in self.seen:
-            #     continue
-            # else:
-            #     self.seen.add(tuple_case)
-            
-
             case = self.allPossibleValues(old_case)
-            # print(f"{old_case=}, {case=}, {op_counts=}")
-            # seen = set("+*-")
-            # for num in case:
-            #     if num not in seen:
-            #         res.append(num)
-            #     seen.add(num)
-            # if len(case) == 1:
-            #     assert case[0] not in ['*', '-', '+']
-            #     res.extend(case[0])
+            print(f"{old_case=}, {case=}, {op_counts=}")
+            
             res.extend(case)
         
-        # print(f"Mapped res: {list(map(self.allPossibleValues, res))}")
         return res
         
-    def computeExpression(self, expression, ordering):
-        1-1-1-1-1-1-1-1-1-1-1-1-1-1-1
-
-# 2*3-4*5
-
-# [14, '*', 5]
-
-# [2, '*', 3, '-', 4, '*', 5]
-# --> ["(2*3)", '-', 4, '*', 5]
-# --> ["((2*3)-4)", '*', 5]
+    
