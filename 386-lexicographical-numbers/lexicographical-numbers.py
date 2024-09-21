b0b4a3dcb8dd892ba9c38a2040ad050f72a884cf
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-20 18:17:26 -0700

    Time: 72 ms (71.29%) | Memory: 22.3 MB (86.72%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -7,8 +7,11 @@ class Solution:
             return
         self.res.append(num)
         num *= 10
-        for i in range(10):
-            self.explore(num + i)
+        for _ in range(10):
+            if num > self.n:
+                return
+            self.explore(num)
+            num += 1
     def lexicalOrder(self, n: int) -> List[int]:
         self.n = n
         for starting_digit in range(1, 9 + 1):
