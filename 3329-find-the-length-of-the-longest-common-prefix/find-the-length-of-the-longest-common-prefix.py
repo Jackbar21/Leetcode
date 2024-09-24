35069e85ad081a362c0c5a000933930f240c72a1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-23 17:33:22 -0700

    Time: 1052 ms (20.36%) | Memory: 38.2 MB (6.23%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -2,16 +2,7 @@ class Solution:
     def __init__(self):
         self.trie1 = {}
         self.trie2 = {}
-    def getLongestCommonPrefix(self, str_x, str_y):
-        res = 0
-        for i in range(min(len(str_x), len(str_y))):
-            if str_x[i] != str_y[i]:
-                break
-            res += 1
-        return res
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        # arr1.sort()
-        # arr2.sort()
         arr1 = [str(num) for num in arr1]
         arr2 = [str(num) for num in arr2]
 
@@ -30,40 +21,25 @@ class Solution:
         
         # Step 2: For each num in arr1, get longest prefix in trie2.
         #         For each num in arr2, get longest prefix in trie1.
-        # print(self.trie1)
-        # print(self.trie2)
         res = 0
         for str_num in arr1:
             res = max(res, self.getPrefixLength(self.trie2, str_num))
-        
         for str_num in arr2:
             res = max(res, self.getPrefixLength(self.trie1, str_num))
 
         return res
-
-        
-        # res = 0
-        res = 0
-        for x in arr1:
-            for y in arr2:
-                res = max(res, self.getLongestCommonPrefix(x, y))
-        return res
     
     def insertNum(self, trie, str_num):
-        # trie = self.trie
         for digit in str_num:
             if digit not in trie:
                 trie[digit] = {}
             trie = trie[digit]
-        return
     
     def getPrefixLength(self, trie, str_num):
         res = 0
-        # trie = self.trie
         for digit in str_num:
             if digit not in trie:
-                return res
+                break
             res += 1
             trie = trie[digit]
-        assert res == len(str_num)
         return res
