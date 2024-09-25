6344c51993b87edb64698693588b0c497fb44c9f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-24 17:26:45 -0700

    Time: 1497 ms (89.31%) | Memory: 212.7 MB (91.03%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
@@ -1,7 +1,6 @@
 class Solution:
     def __init__(self):
-        self.trie = {} # prefix_count = INT
-        # self.d = {}
+        self.trie = {} # "pc": INT, for prefix counts
     def insertWord(self, word):
         trie = self.trie
         for letter in word:
@@ -10,8 +9,6 @@ class Solution:
 
             trie = trie[letter]
             trie["pc"] += 1
-        return
-    # def isPrefix(self, word):
     def getPrefixSum(self, word):
         res = 0
         trie = self.trie
@@ -21,35 +18,8 @@ class Solution:
             trie = trie[letter]
             assert "pc" in trie
             res += trie["pc"]
-            
-        
         return res
     def sumPrefixScores(self, words: List[str]) -> List[int]:
-        # Step 1: build a trie [O(n)]
         for word in words:
             self.insertWord(word)
-        
         return map(self.getPrefixSum, words)
-        # return [
-        #     self.getPrefixSum(word) for word 
-        # ]
-        print(self.trie)
-        print(self.getPrefixSum("abcd"))
-        return []
-        
-        answer = []
-        for word in words:
-            res = self.getPrefixSum(word)
-            answer.append(res)
-        
-        return answer
-
-
-        # 1 .. length
-
-        # 10^6
-        # 10^3
-
-        # for every word: # O(n)
-        #         go through every prefix of that word: (1 + 2 + ... + 1000)
-        #         # (1 + 2 + 3 + ... + n = n(n+1)/2 --> O(n^2))
