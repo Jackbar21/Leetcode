01d7759d7b619a44078bfa90faf0d90f4cc6c7fe
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-25 17:38:35 -0700

    Time: 279 ms (45.43%) | Memory: 17.3 MB (65.25%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,30 +1,69 @@
-class TreeNode:
+class ListNode:
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.left = None
-        self.right = None
+        self.next = None
 class MyCalendar:
     def __init__(self):
         self.calendar = []
-        self.tree = None
+        self.head = None
     
     def insertNode(self, start, end):
         # DON'T WANT: s1 < e2 and s2 < e1
         # i.e....     s1 < e2 < e1 < s2 or s1 < e2 < s2 < e1
-        if not self.tree:
-            self.tree = TreeNode(start, end)
+        # if not self.tree:
+        #     self.tree = TreeNode(start, end)
+        cur = self.head
+        prev = None
+        node = ListNode(start, end)
+        if not self.head:
+            self.head = ListNode(start, end)
+            return True
+
+        # sort by increasing finish times.
+        # start, end
+        # (s1,e1),(s2,e2),...,(s,e), (s',e') s.t. (start < e)
+        # now if s < end, then this is not bookable
+        # otherwise, insert the node here in place?
         
+        while cur and start >= cur.end:
+            # Loop Invariant
+            prev = cur
+            cur = cur.next
         
+        # Reached end of list, so interval must be here! Might as well check it is valid
+        if not cur:
+            assert prev
+            assert prev.end <= start
+            prev.next = node
+            return True
+        
+        # Not bookable
+        if cur.start < end:
+            return False
+        
+        # Bookable, so add right before cur
+        if not prev:
+            # Special Case: Add to very beginning of list
+            node.next = self.head
+            self.head = node
+        else:
+            prev.next = node
+            node.next = cur
+        return True
+        
+
+
 
 
     def book(self, start: int, end: int) -> bool:
-        for (other_start, other_end) in self.calendar:
-            if other_end > start and end > other_start:
-                return False
+        # for (other_start, other_end) in self.calendar:
+        #     if start < other_end and other_start < end:
+        #         return False
 
-        self.calendar.append((start, end))
-        return True
+        # self.calendar.append((start, end))
+        # return True
+        return self.insertNode(start, end)
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
