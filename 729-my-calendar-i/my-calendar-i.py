a7770fb06b51c42d68384e3e452e672c0035b10f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-25 17:49:57 -0700

    Time: 267 ms (45.68%) | Memory: 17.3 MB (65.25%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,18 +1,47 @@
-class ListNode:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.next = None
+# class ListNode:
+#     def __init__(self, start, end):
+#         self.start = start
+#         self.end = end
+#         self.next = None
 class MyCalendar:
     def __init__(self):
-        # self.calendar = []
-        self.head = None
+        self.calendar = []
+        # self.head = None
 
     def book(self, start: int, end: int) -> bool:
+        # [SOL1]
+        # self.insertNode(start, end)
+        # [SOL2]
         # for (other_start, other_end) in self.calendar:
         #     if start < other_end and other_start < end:
         #         return False
         # return True
+        # [SOL3]
+        i = 0
+        for (s, e) in self.calendar:
+            if start < e:
+                if s < end:
+                    return False
+                else:
+                    self.calendar.insert(i, (start, end))
+                    return True
+            i += 1
+
+        # List empty at start
+        self.calendar.append((start, end))
+        return True
+
+    # def getInsertIndex(self, start, end):
+    #     START, END = 0, 1
+    #     l, r = 0, len(self.calendar)
+    #     while l <= r:
+    #         mid = (l + r) // 2
+    #         if start < self.calendar[mid][END]:
+    #             r = mid
+    #         else:
+    #             l = mid
+    #     return l
+    def insertNode(self, start, end):
         cur = self.head
         prev = None
         node = ListNode(start, end)
