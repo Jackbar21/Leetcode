3fe2d2581493f4317cebef7bbf13f9c1e84073c2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-25 17:24:47 -0700

    Time: 309 ms (40.43%) | Memory: 17.3 MB (65.25%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,7 +1,22 @@
+class TreeNode:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
 class MyCalendar:
-
     def __init__(self):
         self.calendar = []
+        self.tree = None
+    
+    def insertNode(self, start, end):
+        # DON'T WANT: s1 < e2 and s2 < e1
+        # i.e....     s1 < e2 < e1 < s2 or s1 < e2 < s2 < e1
+        if not self.tree:
+            self.tree = TreeNode(start, end)
+        
+        
+
 
     def book(self, start: int, end: int) -> bool:
         for (other_start, other_end) in self.calendar:
@@ -11,14 +26,6 @@ class MyCalendar:
         self.calendar.append((start, end))
         return True
 
-
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
-
-# start1 <= start2 <= end1
-
-# s,e,start,end
-# start,end,s,e
-# s,start,e,end
-# start,s,e,end
