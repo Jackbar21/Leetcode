45d6361e4cf0c46fa0a87626ac6135cdcce354b7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-25 18:29:26 -0700

    Time: 181 ms (65.37%) | Memory: 17.4 MB (65.25%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,149 +1,33 @@
-START, END = 0, 1
 class MyCalendar:
     def __init__(self):
         self.calendar = []
 
     def book(self, start: int, end: int) -> bool:
-        # List empty at start
-        if len(self.calendar) == 0:
-            self.calendar.append((start, end))
-            return True
-
-        # Add to start of array
-        # if end <= self.calendar[0][START]:
-        #     self.calendar.insert(0, (start, end))
-        #     return True
-        
-        # Add to end of array
-        if self.calendar[-1][END] <= start:
+        # Base Case: Empty list, or largest start interval
+        if len(self.calendar) == 0 or self.calendar[-1][1] <= start:
             self.calendar.append((start, end))
             return True
         
-        # Two elements case
-        # if len(self.calendar) == 2:
-            # Already handled add to start & end cases, so only consider adding to middle
-        
-        # If valid, element will be sandwhiched between two others!
+        # Leftmost index in self.calendar such that start < self.calendar[index][1]
         index = self.leftmostBinarySearch(start, end)
-        if end > self.calendar[index][START]:
+        if end > self.calendar[index][0]:
             return False
         self.calendar.insert(index, (start, end))
         return True
 
     def leftmostBinarySearch(self, start, end):
-        
         l, r = 0, len(self.calendar)
         leftmost = len(self.calendar) - 1
 
         while l <= r:
             mid = (l + r) // 2
-            if start < self.calendar[mid][END]:
+            if start < self.calendar[mid][1]:
                 leftmost = min(leftmost, mid)
                 r = mid - 1
             else:
                 l = mid + 1
 
         return leftmost
-    def bookSearch(self, start, end, l, r):
-        # if start >= e, look in right sub-half
-        # if start < e, look in left subhalf
-        # if e' comes before e, and e' < start < e, then have found right index
-        # in which case return false if s < end, oterhwise append element
-        # between e' and e representative intervals
-        
-
-        if l >= r:
-            if l > r:
-                return False
-            if start < self.calendar[l][END]:
-                if self.calendar[l][START] < end:
-                    return False
-                self.calendar.insert(l, (start, end))
-                return True
-            elif start < self.calendar[l + 1][END]:
-                if self.calendar[l + 1][START] < end:
-                    return False
-                self.calendar.insert(l + 1, (start, end))
-                return True
-            elif start < self.calendar[l - 1][END]:
-                if self.calendar[l - 1][START] < end:
-                    return False
-                self.calendar.insert(l - 1, (start, end))
-                return True
-            else:
-                assert False
-            
-        
-
-        if l >= r:
-            print("SPECIAL",l, r, start, end, self.calendar)
-        else:
-            print(l, r, start, end, self.calendar)
-
-        mid = (l + r) // 2
-        if self.calendar[mid - 1][END] < start < self.calendar[mid][END]:
-            if self.calendar[mid][START] < end:
-                return False
-            else:
-                self.calendar.insert(mid, (start, end))
-                return True
-        
-        assert self.calendar[mid - 1][END] >= start or start >= self.calendar[mid][END]
-        if self.calendar[mid - 1][END] >= start:
-            # return self.bookSearch(start, end, mid, r)
-            return self.bookSearch(start, end, l, mid - 1)
-        else:
-            assert start >= self.calendar[mid][END]
-            return self.bookSearch(start, end, mid + 1, r)
-        
-    # def getInsertIndex(self, start, end):
-    #     
-    #     l, r = 0, len(self.calendar)
-    #     while l <= r:
-    #         mid = (l + r) // 2
-    #         if start < self.calendar[mid][END]:
-    #             r = mid
-    #         else:
-    #             l = mid
-    #     return l
-    def insertNode(self, start, end):
-        cur = self.head
-        prev = None
-        node = ListNode(start, end)
-        if not self.head:
-            self.head = ListNode(start, end)
-            return True
-
-        # sort by increasing finish times.
-        # start, end
-        # (s1,e1),(s2,e2),...,(s,e), (s',e') s.t. (start < e)
-        # now if s < end, then this is not bookable
-        # otherwise, insert the node here in place?
-        while cur and start >= cur.end:
-            # Loop Invariant
-            prev = cur
-            cur = cur.next
-        
-        # Reached end of list, so interval must be here! Might as well check it is valid
-        if not cur:
-            assert prev
-            assert prev.end <= start
-            prev.next = node
-            return True
-        
-        # Not bookable
-        if cur.start < end:
-            return False
-        
-        # Bookable, so add right before cur
-        if not prev:
-            # Special Case: Add to very beginning of list
-            node.next = self.head
-            self.head = node
-        else:
-            prev.next = node
-            node.next = cur
-        return True
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
