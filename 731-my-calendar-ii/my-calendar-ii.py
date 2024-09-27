09d39897f165a0a19e3f5d4b7a426f6f0df1f66f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-26 18:24:04 -0700

    Time: 263 ms (74.29%) | Memory: 17.3 MB (66.75%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.py
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -1,22 +1,15 @@
 class MyCalendarTwo:
     def __init__(self):
-        self.calendar = []
-
+        self.calendar = [] # (start, end) sorted by increasing end times
+   
     def book(self, start: int, end: int) -> bool:
-        # start,end | s,e
-        # start < e and end > s, then invalid
         conflicts = []
         for (s, e) in self.calendar:
-            # if not (start < end <= s < e or s < e <= start < end):
             if start < e and end > s:
-                # conflicts.append((s, e))
                 for (cs, ce) in conflicts:
                     if cs < e and ce > s:
                         return False
-                # interval = (start, e)
-                # conflicts += 1
-                # if conflicts >= 2:
-                #     return False
+
                 conflicts.append((s, e))
         
         self.calendar.append((start, end))
