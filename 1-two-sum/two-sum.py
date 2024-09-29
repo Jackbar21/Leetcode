894d6d2a87b84f50e5447eb094abf2d371efdacb
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-29 14:40:07 -0700

    Time: 1684 ms (20.51%) | Memory: 17.3 MB (77.13%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,21 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Want two nums num1 and num2 such that:
-        #   num1 + num2 = target
-        # For each num, if we keep track of nums we've seen, we
-        # can check if we've already found a number whose value
-        # is target - num, since of course:
-        #   num + (target - num) = target
-
-        seen = {} # num-to-index mappings
-        for index, num in enumerate(nums):
-            wanted_num = target - num
-
-            if wanted_num in seen:
-                prev_index = seen[wanted_num]
-                return (prev_index, index)
-            
-            # Don't have desired num, so add current one to "seen"
-            seen[num] = index
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return (i, j)
         
-        raise Exception("Error: Should not reach here.")
+        return (-1, -1)
