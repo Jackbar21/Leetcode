6bf3d695b88afbcdbf67da604b756e8b1152e996
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-29 14:36:41 -0700

    Time: 53 ms (34.95%) | Memory: 17 MB (51.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,14 +5,4 @@ class Solution:
             d[letter] = d.get(letter, 0) + 1
         return d
     def isAnagram(self, s: str, t: str) -> bool:
-        # return self.wordToDict(s) == self.wordToDict(t)
-        ds, dt = self.wordToDict(s), self.wordToDict(t)
-        for letter in ds:
-            if ds[letter] != dt.get(letter, 0):
-                return False
-        
-        for letter in dt:
-            if dt[letter] != ds.get(letter, 0):
-                return False
-        
-        return True
+        return self.wordToDict(s) == self.wordToDict(t)
