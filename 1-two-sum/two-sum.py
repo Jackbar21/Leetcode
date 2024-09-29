c26f44f4209bb1829b11ad5343c27a47b6fc855b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-29 14:50:55 -0700

    Time: 58 ms (68.27%) | Memory: 17.8 MB (33.75%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return (i, j)
+        # Want to find a pair (i,j) such that 0 <= i,j < len(nums), and 
+        # nums[i] + nums[j] == target
+        seen = {}
+        for i, num in enumerate(nums):
+            # Want a wanted_num such that num + wanted_num == target
+            # <--> wanted_num == target - num
+            wanted_num = target - num
+            if wanted_num in seen:
+                return (seen[wanted_num], i)
+
+            seen[num] = i
         
-        return (-1, -1)
+        raise Exception("Unreachable Code -- must exist at least one solution")
