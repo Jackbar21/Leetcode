bda64ad04ac6b26842cbd5547fcbdab91c678c7e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-09-30 18:25:34 -0700

    Time: 499 ms (62.44%) | Memory: 30.5 MB (57.83%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,49 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        d = {}
+        for i in range(len(arr)):
+            # arr[i] %= k
+            d[arr[i] % k] = d.get(arr[i] % k, 0) + 1
+        
+        # First step: check 0s (i.e. numbers who are direct multiples of k)
+        if 0 in d:
+            if d[0] % 2 != 0:
+                return False
+            del d[0]
+
+        # The keys in d represent the value of a for each index i in arr,
+        # where arr[i] == a + b * k, for some non-negative integers a, b.
+        # So now, as you can imagine, we want to match each key "key1" in d with
+        # another key "key2" in d (if available) such that key1 + key2 == k.
+        # In other words, in a spirit similar to a problem like Two Sum,
+        # given key1, want to find "key2" such that key2 == k - key1.
+        # A key like key2 is available if and only if key2's value in d
+        # is greater than 0.
+
+        for key in d:
+            # val = d[key]
+            # key2 = k - key
+            # print(d, k, arr)
+            if d[key] != d.get(k - key, 0):
+                return False
+        
+        return True
+
+
+            
+
+
+        
+
+        # arr.sort()
+        print(d)
+        return False
+
+        # For all i, arr[i] == a + b * k, for some numbers a, b
+        # Since the b part doesn't matter, the only "unique" values
+        # that remain are for 0 <= a < k
+
+        # (a, b)
+        # (a, b, c, d)
+        # (a, b) --> solution no longer possible with rest of elements
+        # k=5, a=1, b=4, c=9, 
