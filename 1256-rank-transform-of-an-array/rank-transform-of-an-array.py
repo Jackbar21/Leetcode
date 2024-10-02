e49a83f776e389a87ab7e88d05c230c17b5672ff
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-01 18:24:53 -0700

    Time: 250 ms (37.59%) | Memory: 41.2 MB (5.43%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -5,9 +5,14 @@ class Solution:
 
         d = {}
         rank = 1
-        for num in sorted(arr):
-            if num not in d:
-                d[num] = rank
-                rank += 1
+        for num in sorted(set(arr)):
+            # if num not in d:
+            d[num] = rank
+            rank += 1
                 
         return map(lambda num: d[num], arr)
+
+        # arr         == [37, 12, 28, 9, 100, 56, 80, 5, 12]
+        # sorted(arr) == [5, 9, 12, 12, 28, 28, 28, 28, 28, 28, 37, 56, 80, 100]
+        # d = {5: 1, 9: 2, 12: 3, 28: 4}
+        # rank = 5
