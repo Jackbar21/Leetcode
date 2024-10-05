95dab0666c0ff1ee37afbdc5f80b7542b3797fde
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-05 12:38:22 -0700

    Time: 48 ms (38.69%) | Memory: 17.8 MB (6.29%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def __init__(self):
+        self.res = True
         self.heights = {} # TreeNode to height
     def getHeight(self, node):
         if node in self.heights:
@@ -19,14 +20,30 @@ class Solution:
             self.getHeight(node.right)
         )
 
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    def getHeightAndUpdateRes(self, root):
         if not root:
-            return True
+            return 0
         
-        left_height = self.getHeight(root.left)
-        right_height = self.getHeight(root.right)
+        left_height = self.getHeightAndUpdateRes(root.left)
+        right_height = self.getHeightAndUpdateRes(root.right)
         diff = abs(left_height - right_height)
         if diff > 1:
-            return False
-        
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+            self.res = False
+
+        return 1 + max(left_height, right_height)
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.getHeightAndUpdateRes(root)
+        return self.res
+
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    #     if not root:
+    #         return True
+
+    #     left_height = self.getHeight(root.left)
+    #     right_height = self.getHeight(root.right)
+    #     diff = abs(left_height - right_height)
+    #     if diff > 1:
+    #         return False
+
+    #     return self.isBalanced(root.left) and self.isBalanced(root.right)
