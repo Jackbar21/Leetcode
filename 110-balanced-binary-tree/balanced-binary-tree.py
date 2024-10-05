8656f68f3bfb536c6474fb771aeef8017968c361
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-05 12:41:36 -0700

    Time: 47 ms (45.23%) | Memory: 17.7 MB (28.11%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,44 +6,22 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.res = True
-        self.heights = {} # TreeNode to height
-    def getHeight(self, node):
-        if node in self.heights:
-            return self.heights[node]
-        
-        if not node:
-            return 0
-        
-        return 1 + max(
-            self.getHeight(node.left),
-            self.getHeight(node.right)
-        )
+        self.is_balanced = True
+    
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.getHeightAndUpdateRes(root)
+        return self.is_balanced
 
     def getHeightAndUpdateRes(self, root):
-        if not root or self.res == False:
+        # If self.is_balanced is False, then end execution context
+        # as quickly as possible since answer will be False no matter what.
+        if not root or not self.is_balanced:
             return 0
         
         left_height = self.getHeightAndUpdateRes(root.left)
         right_height = self.getHeightAndUpdateRes(root.right)
         diff = abs(left_height - right_height)
         if diff > 1:
-            self.res = False
+            self.is_balanced = False
 
         return 1 + max(left_height, right_height)
-
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.getHeightAndUpdateRes(root)
-        return self.res
-
-    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
-    #     if not root:
-    #         return True
-
-    #     left_height = self.getHeight(root.left)
-    #     right_height = self.getHeight(root.right)
-    #     diff = abs(left_height - right_height)
-    #     if diff > 1:
-    #         return False
-
-    #     return self.isBalanced(root.left) and self.isBalanced(root.right)
