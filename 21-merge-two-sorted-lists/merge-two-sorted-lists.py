596020efc723ac95b1fa4112030d27b74ce02f9c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-04 17:09:39 -0700

    Time: 59 ms (5.50%) | Memory: 16.7 MB (5.97%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,8 +4,63 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def llToList(self, head):
+        res = []
+        while head:
+            res.append(head.val)
+        return res
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # while list1 or list2:
+        print('koala')
+        # Base Cases
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        if list1.val > list2.val:
+            return self.mergeTwoLists(list2, list1)
+
+        # head = list1 if list1.val <= list2.val else list2
+        # while list1.val > list2.val:
+        head = list1
+            
+
+        list1_prev = None
+        while list1 and list2:
+            if list1.val <= list2.val:
+                print('dsa')
+                list1_prev = list1
+                list1 = list1.next
+                continue
+            
+            assert list1.val > list2.val
+            print('kek')
+            # print(self.llToList(list1), self.llToList(list2))
+            # print(self.llToList(list2))
+            list2_node = list2
+            print(list2_node)
+            list2 = list2.next
+            # print(self.llToList(list2))
+            list2_node.next = None
+            print(list2_node)
+            # break
+        
+            # want to sandwchich list2_node between list1_prev and list1
+            list1_prev.next = list2_node
+            list2_node.next = list1
+            # print(self.llToList(list1), self.llToList(list2))
+
+            # list1_prev, list2_prev = list1, list2
+            list1_prev = list2_node
+
+        if not list1:
+            list1_prev.next = list2
+        # else:
+        #     list1.next = list2
+
+        return head
+
+
         head = ListNode(-1) # dummy node
         cur = head
 
