49075a3162900859025d01435d1317c0d230146d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-05 14:06:24 -0700

    Time: 83 ms (33.44%) | Memory: 16.7 MB (58.98%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,32 +6,17 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p or not q:
-            return not p and not q
-        
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return (not p and not q) if (not p or not q) else (
+            p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        )
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
-
-        if self.isSameTree(root, subRoot):
-            return True
-        
-        # if self.isSameTree(root.left, subRoot):
-        #     return True
-        
-        # if self.isSameTree(root.right, subRoot):
-        #     return True
-        if self.isSubtree(root.left, subRoot):
-            return True
-        
-        if self.isSubtree(root.right, subRoot):
-            return True
-        
-        return False
+        return False if not root else (
+            self.isSameTree(root, subRoot)
+            or self.isSubtree(root.left, subRoot)
+            or self.isSubtree(root.right, subRoot)
+        )
 
         
 
