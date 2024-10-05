64d7393ac42a8cc7e4b46ebb474ce573e8c2777a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-04 17:39:13 -0700

    Time: 56 ms (71.20%) | Memory: 16.8 MB (22.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,20 +1,32 @@
 class Solution:
-    def wordToDict(self, word):
-        d = {}
-        for letter in word:
-            d[letter] = d.get(letter, 0) + 1
-        return d
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s1) == 0:
+            return True
+
+        if len(s2) < len(s1):
             return False
-        if len(s1) == len(s2):
-            return self.wordToDict(s1) == self.wordToDict(s2)
+
+        dict_to_match = {}
+        for letter in s1:
+            dict_to_match[letter] = dict_to_match.get(letter, 0) + 1
         
-        d = self.wordToDict(s1)
-        k = len(s1)
-        for i in range(len(s2)-k+1):
-            if d.get(s2[i], 0) == 0:
-                continue
-            if self.wordToDict(s2[i:i+k]) == d:
-                return True
-        return False
+        d = {}
+        for i in range(len(s1)):
+            letter = s2[i]
+            d[letter] = d.get(letter, 0) + 1
+        
+        l, r = 0, len(s1)
+        while d != dict_to_match and r < len(s2):
+            letter_to_remove = s2[l]
+            letter_to_add = s2[r]
+
+            d[letter_to_remove] -= 1
+            if d[letter_to_remove] == 0:
+                del d[letter_to_remove]
+            d[letter_to_add] = d.get(letter_to_add, 0) + 1
+
+            l += 1
+            r += 1
+            # print(d)
+        
+        return d == dict_to_match
