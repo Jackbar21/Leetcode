35a3796b843a6ab931cd6447731ed07c1e5b1ff5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-04 20:11:33 -0700

    Time: 33 ms (98.25%) | Memory: 19.1 MB (24.56%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,11 +6,9 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return None
         tortoise = head
-        hare = head.next
-
+        hare = head
+        
         while hare and hare.next:
             tortoise = tortoise.next
             hare = hare.next.next
