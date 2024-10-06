979ddf337e484a19557ca2203da3855661f9a318
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-05 17:46:18 -0700

    Time: 39 ms (27.33%) | Memory: 16.6 MB (47.21%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -11,13 +11,17 @@ class Solution:
             small, big = s2, s1
         
         small, big = collections.deque(small), collections.deque(big)
-        while len(small) > 0 and (small[0] == big[0] or small[-1] == big[-1]):
+        while len(small) > 0:
             if small[0] == big[0]:
                 small.popleft()
                 big.popleft()
-            
-            if len(small) > 0 and small[-1] == big[-1]:
+                continue
+
+            if small[-1] == big[-1]:
                 small.pop()
                 big.pop()
+                continue
+            
+            break
         
         return len(small) == 0
