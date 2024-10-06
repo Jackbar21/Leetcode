a9f0b10114c3b1951f4dffe87fdb831a28e9a002
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-05 17:42:39 -0700

    Time: 35 ms (54.04%) | Memory: 16.5 MB (47.21%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split(" ")
+        s2 = sentence2.split(" ")
+        
+        if len(s1) == len(s2):
+            return s1 == s2
+        
+        small, big = s1, s2
+        if len(s1) > len(s2):
+            small, big = s2, s1
+        
+        small, big = collections.deque(small), collections.deque(big)
+        while len(small) > 0 and (small[0] == big[0] or small[-1] == big[-1]):
+            if small[0] == big[0]:
+                small.popleft()
+                big.popleft()
+            
+            elif small[-1] == big[-1]:
+                small.pop()
+                big.pop()
+        
+        return len(small) == 0
+            
+
+
+        
