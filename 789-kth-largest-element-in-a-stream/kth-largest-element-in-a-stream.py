5e28816d06cf5a6ee3aedde76466bcf66117ae3a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-06 11:16:44 -0700

    Time: 107 ms (30.50%) | Memory: 22.5 MB (10.48%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -8,6 +8,11 @@ class KthLargest:
             heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
+        # suppose have k (current largest) elements in stream
+        # Case 1: val is part of NEW largest k
+        #           there exists some element in heap such that val > element
+        if len(self.heap) == self.k and val < self.heap[0]:
+            return self.heap[0]
         heapq.heappush(self.heap, val)
         if len(self.heap) > self.k:
             heapq.heappop(self.heap)
