a06559604111b94daca8797a0c3c38263f162b13
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-06 12:55:20 -0700

    Time: 36 ms (45.12%) | Memory: 16.4 MB (92.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,15 +1,22 @@
 class Solution:
-    def __init__(self):
-        self.memo = {1: 1, 2: 2}
+    # Now realize this is just fibonacci sequence
     def climbStairs(self, n: int) -> int:
-        return self.climbStairsDp(n)
+        a, b = 1, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+    # def __init__(self):
+    #     self.memo = {1: 1, 2: 2}
+
+    # def climbStairs(self, n: int) -> int:
+    #     return self.climbStairsDp(n)
     
-    def climbStairsDp(self, i):
-        if i in self.memo:
-            return self.memo[i]
+    # def climbStairsDp(self, i):
+    #     if i in self.memo:
+    #         return self.memo[i]
         
-        self.memo[i] = self.climbStairsDp(i - 1) + self.climbStairsDp(i - 2)
-        return self.memo[i]
+    #     self.memo[i] = self.climbStairsDp(i - 1) + self.climbStairsDp(i - 2)
+    #     return self.memo[i]
         
 
         
