d0ffb699ee46d460dd57c3379bda0404dffad4c0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-06 22:39:51 -0700

    Time: 38 ms (48.73%) | Memory: 16.5 MB (76.54%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -2,10 +2,11 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         str_n = str(n)
         visited = set()
-        while str_n not in visited:
+        while str_n != "1":
+            if str_n in visited:
+                return False
+
             visited.add(str_n)
             str_n = str(sum(pow(int(digit), 2) for digit in str_n))
-            if str_n == "1":
-                return True
         
-        return False
+        return True
