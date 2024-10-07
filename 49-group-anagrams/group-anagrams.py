4564a7b933405d117fb5ba83495f1d13a06eb954
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-07 13:01:51 -0700

    Time: 99 ms (22.52%) | Memory: 22.1 MB (24.57%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,25 +1,16 @@
 class Solution:
-    def letterToIndex(self, letter):
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        return alphabet.find(letter)
     def wordToTuple(self, word):
         res = [0] * 26
         for letter in word:
-            index = self.letterToIndex(letter)
-            res[index] += 1
-        
+            res[ord(letter) - ord('a')] += 1
         return tuple(res)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # {'e': 1, 'a': 1, 't': 1}
-        # # abcdefghijklmnopqrstuvwxyz
-        # (1, 0, 0, 0, 1, ...)
         d = {}
-
         for word in strs:
             tuple_word = self.wordToTuple(word)
             if tuple_word in d:
                 d[tuple_word].append(word)
             else:
                 d[tuple_word] = [word]
-        
-        return [arr for arr in d.values()]
+
+        return d.values()
