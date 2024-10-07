91595ff004884d672314ccdb14d68b5e1c9dce8d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-07 01:02:39 -0700

    Time: 23 ms (99.20%) | Memory: 16.5 MB (50.63%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,3 +1,17 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        return int(('0'*32 + bin(n)[2:])[-32:][::-1], 2)
+        # return int(('0'*32 + bin(n)[2:])[-32:][::-1], 2)
+
+        # Step 1: Convert n into binary representation
+        bin_n = bin(n)[2:]
+
+        # Step 2: Reverse bits of n
+        bin_n = bin_n[::-1]
+
+        # Step 3: Pad with 0s until 32 bits long
+        len_padding = 32 - len(bin_n)
+        bin_n += '0' * len_padding
+
+        # Step 4: Convert back to int, and return
+        return int(bin_n, 2)
+        
