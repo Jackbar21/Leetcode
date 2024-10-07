58e6ca9a0792b989e07688c3345745e1f93c1b60
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-07 13:00:19 -0700

    Time: 102 ms (16.49%) | Memory: 21.3 MB (32.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,25 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Idea: use fixed array size of 26, since 26 letters
-        # in alphabet, each corresponding to count of that letter,
-        # to uniquely identify and group anagrams together
-        d = {}
-        for word in strs:
-            alphabet = [0] * 26
-            for letter in word:
-                alphabet[ord(letter) - ord('a')] += 1
-            
-            t = tuple(alphabet)
-            if t not in d:
-                d[t] = []
-            d[t].append(word)
+    def letterToIndex(self, letter):
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        return alphabet.find(letter)
+    def wordToTuple(self, word):
+        res = [0] * 26
+        for letter in word:
+            index = self.letterToIndex(letter)
+            res[index] += 1
         
-        return d.values()
+        return tuple(res)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # {'e': 1, 'a': 1, 't': 1}
+        # # abcdefghijklmnopqrstuvwxyz
+        # (1, 0, 0, 0, 1, ...)
+        d = {}
+
+        for word in strs:
+            tuple_word = self.wordToTuple(word)
+            if tuple_word in d:
+                d[tuple_word].append(word)
+            else:
+                d[tuple_word] = [word]
+        
+        return [arr for arr in d.values()]
