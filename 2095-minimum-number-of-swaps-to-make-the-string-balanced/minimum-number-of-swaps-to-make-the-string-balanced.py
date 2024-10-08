c26238b6bcbd95957a8cd2fb5f8d87ba5c03bda6
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-07 17:51:16 -0700

    Time: 249 ms (51.23%) | Memory: 30.4 MB (16.20%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,6 +1,5 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-
         stack = collections.deque(['dummy'])
         for bracket in s:
             if stack[-1] == "[" and bracket == "]":
@@ -8,36 +7,5 @@ class Solution:
             else:
                 stack.append(bracket)
         stack.popleft()
-        print(stack)
-        num_swaps = 0
+
         return math.ceil(len(stack) / 4)
-
-        if len(stack) == 0:
-            return 0
-        if len(stack) == 2:
-            return 1
-        return len(stack) // 2 - 1
-
-        queue = collections.deque(s)
-        num_swaps = 0
-        while len(queue) > 0:
-            if queue[0] == "[" and queue[-1] == "]":
-                queue.popleft()
-                queue.popright()
-                continue
-            
-        # Case 3: string is empty
-        return num_swaps
-
-
-        # '[', add ']' EASY
-        # '[', add '[' SWAP RIGHT
-
-        # ']', add '['
-        # ']', add ']'
-
-        # [[][][]]
-        # ][
-
-            # ']] [[' --> swap 0 with 4
-            # '[] []'
