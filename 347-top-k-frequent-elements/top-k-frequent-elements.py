c1073d6dfb3df20edbe162735381b31000477e68
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-08 23:48:39 -0700

    Time: 110 ms (5.87%) | Memory: 34.4 MB (8.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,45 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # BAD: O(nlogn)
-        d = {}
+        # Edge cases: 
+        #   (1) nums is same num n times, so min freq is len(nums)
+        #   (2) only unique num in nums, so max freq is 1
+        #   Hence, every number in nums will have a frequency between 1 and len(nums) [inclusive]
+        freq_dict = {i: set() for i in range(len(nums) + 1)} # freq to num
+
+        d = {} # num to freq
         for num in nums:
-            d[num] = d.get(num, 0) + 1
+            freq = d.get(num, 0) + 1
+            d[num] = freq
+            if num in freq_dict[freq - 1]:
+                freq_dict[freq - 1].remove(num)
+            assert num not in freq_dict[freq]
+            freq_dict[freq].add(num)
+            # freq = num_to_freq[num]
+            
+        assert len(freq_dict[0]) == 0
+        res = []
+        for freq in range(len(nums), 0, -1):
+            while k > 0 and len(freq_dict[freq]) > 0:
+                res.append(freq_dict[freq].pop())
+                k -= 1
+            if k == 0:
+                return res
+        
+        assert k == 0
+        return res
+
+
+
+
         
         # return sorted(set(nums), key=lambda num: d[num])[-k:]
         max_heap = []
         for num in d:
             heapq.heappush(max_heap, (-d[num], num))
         
+        # O(klogn), k <= n
         heapq.heapify(max_heap) # O(n)
         res = []
-        for _ in range(k):
-            res.append(heapq.heappop(max_heap)[1])
+        for _ in range(k): # O(k)
+            res.append(heapq.heappop(max_heap)[1]) # O(logn)
         return res
