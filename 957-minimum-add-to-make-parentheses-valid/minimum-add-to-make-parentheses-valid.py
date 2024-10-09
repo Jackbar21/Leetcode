923a4fe500220cc08723ddf7ab362b8e363d82e0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-08 17:53:18 -0700

    Time: 39 ms (26.49%) | Memory: 16.6 MB (30.64%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -4,13 +4,14 @@ class Solution:
         # closed_parentheses = 0
         # open_parentheses = 0
         i = 0
-        while i < len(s) and s[i] == ")":
-            i += 1
-        closed_parentheses = i
-        # i = 0
-        open_parentheses = 0
-        while i < len(s):
-            parenthese = s[i]
+        # while i < len(s) and s[i] == ")":
+        #     i += 1
+        # closed_parentheses = i
+        # # i = 0
+        # open_parentheses = 0
+        # while i < len(s):
+        open_parentheses, closed_parentheses = 0, 0
+        for parenthese in s:
             if parenthese == "(":
                 open_parentheses += 1
             else:
@@ -18,20 +19,5 @@ class Solution:
                     open_parentheses -= 1 # stack.pop()
                 else:
                     closed_parentheses += 1 # stack.append(")")
-            
-            # Loop Invariant
-            i += 1
         
         return closed_parentheses + open_parentheses
-
-
-        for parenthese in s:
-            if stack[-1] == "(" and parenthese == ")":
-                stack.pop()
-            else:
-                stack.append(parenthese)
-
-        return len(stack) - 1
-
-        # )))DD))()()()
-        #   p
