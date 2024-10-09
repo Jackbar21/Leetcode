e5826a300b9e06d851e6ca7f96dc51fe07349cbf
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-08 17:54:35 -0700

    Time: 46 ms (7.09%) | Memory: 16.5 MB (30.64%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,23 +1,13 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        # stack = ['dummy']
-        # closed_parentheses = 0
-        # open_parentheses = 0
-        i = 0
-        # while i < len(s) and s[i] == ")":
-        #     i += 1
-        # closed_parentheses = i
-        # # i = 0
-        # open_parentheses = 0
-        # while i < len(s):
         open_parentheses, closed_parentheses = 0, 0
         for parenthese in s:
             if parenthese == "(":
                 open_parentheses += 1
+            # parenthese == ")"
+            elif open_parentheses > 0:
+                open_parentheses -= 1
             else:
-                if open_parentheses > 0:
-                    open_parentheses -= 1 # stack.pop()
-                else:
-                    closed_parentheses += 1 # stack.append(")")
+                closed_parentheses += 1
         
         return closed_parentheses + open_parentheses
