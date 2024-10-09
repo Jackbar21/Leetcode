f3cd3f1c36047c9ec9859643b8d453dbbd30d4b3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-08 23:31:47 -0700

    Time: 86 ms (71.60%) | Memory: 21.2 MB (69.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,4 +5,13 @@ class Solution:
         for num in nums:
             d[num] = d.get(num, 0) + 1
         
-        return sorted(set(nums), key=lambda num: d[num])[-k:]
+        # return sorted(set(nums), key=lambda num: d[num])[-k:]
+        max_heap = []
+        for num in d:
+            heapq.heappush(max_heap, (-d[num], num))
+        
+        heapq.heapify(max_heap) # O(n)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(max_heap)[1])
+        return res
