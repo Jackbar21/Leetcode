4876faf41f2f1030bee9d54dc459bdb257127984
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-08 17:52:12 -0700

    Time: 40 ms (20.46%) | Memory: 16.7 MB (30.64%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,22 +1,37 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        stack = ['dummy']
+        # stack = ['dummy']
+        # closed_parentheses = 0
+        # open_parentheses = 0
+        i = 0
+        while i < len(s) and s[i] == ")":
+            i += 1
+        closed_parentheses = i
+        # i = 0
+        open_parentheses = 0
+        while i < len(s):
+            parenthese = s[i]
+            if parenthese == "(":
+                open_parentheses += 1
+            else:
+                if open_parentheses > 0:
+                    open_parentheses -= 1 # stack.pop()
+                else:
+                    closed_parentheses += 1 # stack.append(")")
+            
+            # Loop Invariant
+            i += 1
+        
+        return closed_parentheses + open_parentheses
+
+
         for parenthese in s:
             if stack[-1] == "(" and parenthese == ")":
                 stack.pop()
             else:
                 stack.append(parenthese)
-        
-        # print(''.join(stack[1:]))
-        # return -1
 
         return len(stack) - 1
 
-        # ))))))(
-
-        # () --> 0
-        # (( --> 2
-        # ))))))
-        # )(
-
-        # ))...)) ((...((
+        # )))DD))()()()
+        #   p
