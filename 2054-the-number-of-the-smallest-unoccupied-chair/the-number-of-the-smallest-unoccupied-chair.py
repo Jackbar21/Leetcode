d597e0cfa4d959097e0a1c597940402cc46d7517
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-10 19:46:36 -0700

    Time: 720 ms (18.92%) | Memory: 23 MB (48.65%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
@@ -87,13 +87,10 @@ class Solution:
         # when a friend releases a chair, it will be added back into the BST (O(logn) w/ AVL tree)
         # to indicate that it is available once again. We can also grab the smallest avaialble
         # chair by doing a DFS from root to leftmost node (O(logn) w/ AVL tree). This should
-        # theoretically allow us to solve the problem in O(nlogn) time w/ AVL tree, but technically
-        # O(n^2) time when unlucky with BST. To minimize this inconvenience, the tree will be
-        # initially balanced, but since I'm not writing a self-balancing tree algorithm, worst
-        # case is still TECHNICALLY O(n^2)
-    
-        # arrival_times = collections.deque(sorted(map(lambda i: (times[i][0], i), range(n))))
-        # leaving_times = collections.deque(sorted(map(lambda i: (times[i][1], i), range(n))))
+        # theoretically allow us to solve the problem in O(nlogn) time w/ AVL tree, but 
+        # technically O(n^2) time when unlucky with BST. To minimize this inconvenience, the tree 
+        # will be initially balanced, but since I'm not writing a self-balancing tree algorithm,
+        # worst case is still TECHNICALLY O(n^2).
         arrival_times = sorted(map(lambda i: (times[i][0], i), range(n)))
         leaving_times = sorted(map(lambda i: (times[i][1], i), range(n)))
         arrival_index, leaving_index = 0, 0
@@ -102,43 +99,28 @@ class Solution:
         while arrival_index < len(arrival_times) and leaving_index < len(leaving_times):
             next_arrival, friend_arrival = arrival_times[arrival_index]
             next_leaving, friend_leaving = leaving_times[leaving_index]
-            #print(next_arrival < next_leaving)
             
             if next_arrival < next_leaving:
-                # pass
                 # Delete smallest from BST
                 # and keep track that THIS FRIEND took THIS CHAIR
                 # assert self.bst is not None
                 smallest_val = self.getSmallestVal(self.bst) # i.e. smallest number chair
-                if friend_arrival == targetFriend:
-                    
-                    return smallest_val
-                # self.insertTreeNode(smallest_val, self.bst)
-
-                # self.inorder(self.bst)
-                #print("SA", smallest_val)
-                self.bst = self.deleteTreeNode(smallest_val, self.bst)
-                # self.inorder(self.bst)
-                #print(" \n")
-                # assert friend_arrival not in taken
-                taken[friend_arrival] = smallest_val
 
                 # IF the friend for next_arrival IS TARGET_FRIEND,
                 # THEN RETURN THIS CHAIR VALUE!!!
-                # arrival_times.popleft()
+                if friend_arrival == targetFriend:
+                    return smallest_val
+
+                self.bst = self.deleteTreeNode(smallest_val, self.bst)
+                # assert friend_arrival not in taken
+                taken[friend_arrival] = smallest_val
                 arrival_index += 1
             else:
-                # pass
                 # Add leaving friend's chair back to BST
                 # assert friend_leaving in taken
                 val = taken[friend_leaving]
                 self.bst = self.insertTreeNode(val, self.bst)
                 del taken[friend_leaving]
-                # leaving_times.popleft()
                 leaving_index += 1
 
-
-        # print(len(arrival_times))
-        # print(len(leaving_times))
-
-        return -1
+        raise Exception("Unreachable Code!")
