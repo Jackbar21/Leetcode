31b136b8d3c9a9b7904c05bcc8edf397bd4db8e0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-13 20:14:03 -0700

    Time: 306 ms (7.32%) | Memory: 25.7 MB (56.50%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,9 +1,49 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # Idea:
+        # Idea (Linear Space):
         # prefix_mults[i] = nums[0] * nums[1] * ... * nums[i]
         # suffix_mults[i] = nums[i] * nums[i+1] * ... * nums[len(nums) - 1]
         # answer[i] = prefix_mults[i-1] * suffix_mults[i+1]
+
+        # Idea (Constant Space):
+        # prefix = nums[0]
+        # suffix = nums[-1]
+        # Start off with answer = [1,1,1,1,1,...,1]
+        # We know that we want to multiply answer[i] with prefix_mults[i - 1]
+        # as well as suffix_mults[i + 1]. So as we build up our prefix value,
+        # start with prefix = nums[0], multiplying it by nums[1], then nums[2],
+        # so on and so on (and similarly for suffix in a secondary O(n) sweep),
+        # We can multiply the current value of prefix where after i iterations,
+        # prefix = nums[0] * ... * nums[i] to answer[i+1], and then after i
+        # iterations, suffix = nums[len(nums) - 1] * ... * nums[i] to answer[i - 1].
+
+        # Step 1: initialize answer array
+        answer = [1] * len(nums)
+
+        # TODO: Handle answer[0] and answer[-1] separately if need be
+        i = 0
+        prefix = nums[0]
+        # while i < len(nums):
+        while i + 1 < len(nums):
+            answer[i + 1] *= prefix
+
+            # Loop Invariant
+            i += 1
+            prefix *= nums[i]
+        
+        i = len(nums) - 1
+        suffix = nums[-1]
+        while i  - 1 >= 0:
+            answer[i - 1] *= suffix
+
+            # Loop Invariant
+            i -= 1
+            suffix *= nums[i]
+        
+        return answer
+
+
+
 
         # Step 1: build prefix_mults
         base = 1
