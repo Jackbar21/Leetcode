662d7c9583f83a84845c14fe9aeccba6d453cdf1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-13 20:01:08 -0700

    Time: 277 ms (36.68%) | Memory: 26.3 MB (10.98%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,33 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # prefix_mults[i] = nums[0] * nums[1] * ... * nums[i]
-
-        # prefix_mults[j] - prefix_mults[i] = (nums[0] * ... * nums[j]) - (nums[0])
-
+        # Idea:
         # prefix_mults[i] = nums[0] * nums[1] * ... * nums[i]
         # suffix_mults[i] = nums[i] * nums[i+1] * ... * nums[len(nums) - 1]
+        # answer[i] = prefix_mults[i-1] * suffix_mults[i+1]
 
-        # answer[i] = prefix_mults[i-1] + suffix_mults[i+1]
+        # Step 1: build prefix_mults
         base = 1
         prefix_mults = []
         for num in nums:
             base *= num
             prefix_mults.append(base)
         
+        # Step 2: build suffix_mults
         base = 1
-        suffix_mults = collections.deque([])
+        suffix_mults = []
         for i in range(len(nums) - 1, -1, -1):
             base *= nums[i]
-            suffix_mults.appendleft(base)
-        # suffix_mults = suffix_mults[::-1]
-        
-        # print(f"{prefix_mults}")
-        # print(f"{suffix_mults}")
-        # return []
-        answer = [suffix_mults[1]]
+            suffix_mults.append(base)
+
+        # Step 3: build answer
+        answer = [suffix_mults[-2]]
         for i in range(1, len(nums) - 1):
             answer.append(
-                prefix_mults[i - 1] * suffix_mults[i + 1]
+                prefix_mults[i - 1] * suffix_mults[(len(nums) - 1) - (i + 1)]
             )
         
         answer.append(prefix_mults[-2])
