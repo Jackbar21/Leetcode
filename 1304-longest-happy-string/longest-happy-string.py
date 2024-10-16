da8680adb1ba71a4007eb09316215b297e013ffb
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-15 21:45:05 -0700

    Time: 38 ms (37.42%) | Memory: 16.6 MB (68.60%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -4,8 +4,8 @@ class Solution:
         d = {'a': a, 'b': b, 'c': c}
         second_last, last = None, None
 
-        while sum(d.values()) > 0:
-            options = sorted([letter for letter in 'abc' if not (second_last == last == letter) and d[letter] > 0], key=lambda letter: d[letter])
+        while True:
+            options = sorted([letter for letter in 'abc' if not (second_last == last == letter) and d[letter] > 0], key=lambda letter: d[letter]) # O(1)
             if len(options) == 0:
                 break
             letter = options[-1]
