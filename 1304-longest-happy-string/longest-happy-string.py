aa57425b86c49db61b0aebd3bdccb0e6ed27dc55
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-15 21:51:47 -0700

    Time: 36 ms (51.75%) | Memory: 16.6 MB (68.60%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -3,22 +3,18 @@ class Solution:
         arr = []
         d = {'a': a, 'b': b, 'c': c}
         second_last, last = None, None
-        options = sorted([letter for letter in 'abc' if not (second_last == last == letter) and d[letter] > 0], key=lambda letter: d[letter])
 
-        while len(options) > 0:
-            # options = sorted([letter for letter in 'abc' if not (second_last == last == letter) and d[letter] > 0], key=lambda letter: d[letter]) # O(1)
-            # if len(options) == 0:
-            #     break
+        while True:
+            options = sorted([letter for letter in 'abc' if not (second_last == last == letter) and d[letter] > 0], key=lambda letter: d[letter]) # O(1)
+            if len(options) == 0:
+                break
             letter = options[-1]
             # assert d[letter] >= 1
-            d[letter] -= 1
             arr.append(letter)
+            d[letter] -= 1
 
             # Update last letters
             second_last = last
             last = letter
-
-            # Loop Invariant
-            options = sorted([letter for letter in 'abc' if not (second_last == last == letter) and d[letter] > 0], key=lambda letter: d[letter])
 
         return ''.join(arr)
