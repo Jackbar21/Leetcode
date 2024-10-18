fb098288068456d1b9c6b6a016403cfef25a1cf0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-17 19:47:39 -0700

    Time: 8 ms (100.00%) | Memory: 16.8 MB (30.08%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -3,11 +3,6 @@ class Solution:
         self.memo = {}
         self.max_bitwise_or = 0
         self.nums = None
-    def getBitwiseOr(self, nums):
-        bitwise_or = 0
-        for num in nums:
-            bitwise_or |= num
-        return bitwise_or
     
     def countDp(self, i, bitwise_or):
         if (i, bitwise_or) in self.memo:
@@ -27,30 +22,17 @@ class Solution:
 
 
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        # max_bitwise_or = 0
-        # for num in nums:
-        #     max_bitwise_or |= num
-        max_bitwise_or = self.getBitwiseOr(nums)
+        # Max bitwise or can simply be precomputed as bitwise or
+        # of all the numbers in nums!
+        max_bitwise_or = 0
+        for num in nums:
+            max_bitwise_or |= num
+
+        # Since all numbers have value >= 1, and empty-set solutions are invalid,
+        # there exist no plausible solutions, hence 0 is returned.
         if max_bitwise_or == 0:
             return 0
         
         self.nums = nums
         self.max_bitwise_or = max_bitwise_or
-        # return self.backtrack(0, 0)
         return self.countDp(0, 0)
-        count = 0
-        d = {nums[0]: 1}
-        for i in range(1, len(nums)):
-            bitwise_or = self.getBitwiseOr(d.keys())
-            count += bitwise_or == max_bitwise_or
-
-            # Loop Invariant
-            d[nums[i - 1]] -= 1
-            if d[nums[i - 1]] == 0:
-                del d[nums[i - 1]]
-            
-            d[nums[i]] = d.get(nums[i], 0) + 1
-
-        print(d, self.getBitwiseOr(d.keys()), max_bitwise_or)
-        return count
-        return max_bitwise_or
