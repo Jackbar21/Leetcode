a13f38519f80d207eb537d76b1d1acd9c5d3b5a3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-17 19:43:33 -0700

    Time: 15 ms (100.00%) | Memory: 17 MB (29.66%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -9,42 +9,23 @@ class Solution:
             bitwise_or |= num
         return bitwise_or
     
-    def backtrack(self, i, bitwise_or):
-        if i >= len(self.nums):
-            return bitwise_or == self.max_bitwise_or
-        
-        count = 0
-        # Case 1: Include index i
-        count += self.backtrack(i + 1, bitwise_or | self.nums[i])
-
-        # Case 2: Don't include index i
-        count += self.backtrack(i + 1, bitwise_or)
-
-        return count
-
-
     def countDp(self, i, bitwise_or):
         if (i, bitwise_or) in self.memo:
             return self.memo[(i, bitwise_or)]
-        
-        print(f"{i=}, {self.nums=}")
+
         if i >= len(self.nums):
-            return 0
-        
-        res = 0
+            return bitwise_or == self.max_bitwise_or
 
-        # Case 1: Include i
-        case1, bitwise_or1 = self.countDp(i + 1, bitwise_or | self.nums[i])
-        if bitwise_or1 == self.max_bitwise_or:
-            res += case1
-
-        # Case 2: Don't include i
-        case2, bitwise_or2 = self.countDp(i + 1, bitwise_or)
-        if bitwise_or2 == self.max_bitwise_or:
-            res += case2
+        # Case 1: Don't include index i
+        case1 = self.countDp(i + 1, bitwise_or)
         
-        self.memo[(i, bitwise_or)] = res
-        return res
+        # Case 2: Include index i
+        case2 = self.countDp(i + 1, bitwise_or | self.nums[i])
+
+        self.memo[(i, bitwise_or)] = case1 + case2
+        return case1 + case2
+
+
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         # max_bitwise_or = 0
         # for num in nums:
@@ -55,7 +36,7 @@ class Solution:
         
         self.nums = nums
         self.max_bitwise_or = max_bitwise_or
-        return self.backtrack(0, 0)
+        # return self.backtrack(0, 0)
         return self.countDp(0, 0)
         count = 0
         d = {nums[0]: 1}
