f189fb671f6342a6ebb2b348f16bb519e46fd0d7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-20 20:09:13 -0700

    Time: 443 ms (5.16%) | Memory: 16.7 MB (19.96%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -9,6 +9,14 @@ class Solution:
         l, r = 0, 0
         return self.maxUniqueSplitDp(set(), l, r)
     
+    def getSubstring(self, l, r):
+        # Returns self.s[l : r + 1]
+        if (l, r) in self.substrings:
+            return self.substrings[(l, r)]
+        
+        self.substrings[(l, r)] = self.s[l : r + 1]
+        return self.substrings[(l, r)]
+    
     def maxUniqueSplitDp(self, hset, l, r):
         # if (hset, l, r) in self.memo:
         #     return self.memo[(hset, l, r)]
@@ -16,7 +24,8 @@ class Solution:
         # Base Case: r >= len(self.s)
         if r >= len(self.s):
             assert r == len(self.s)
-            substring = self.s[l : r + 1]
+            # substring = self.s[l : r + 1]
+            substring = self.getSubstring(l, r)
             # print(f"{substring=}")
             if substring in hset:
                 return 0
@@ -29,7 +38,8 @@ class Solution:
 
         # Case 2: Split s at index r
         # case2 = self.maxUniqueSplitDp(hset, )
-        substring = self.s[l : r + 1]
+        # substring = self.s[l : r + 1]
+        substring = self.getSubstring(l, r)
         case2 = 0
         if substring not in hset:
             hset_copy = hset.copy()
@@ -43,58 +53,58 @@ class Solution:
         
 
     
-    def maxUniqueDp(self, bool_arr):
-        if len(bool_arr) == len(self.s):
-            return self.solver(bool_arr)
+    # def maxUniqueDp(self, bool_arr):
+    #     if len(bool_arr) == len(self.s):
+    #         return self.solver(bool_arr)
         
-        # if tuple(bool_arr) in self.memo:
-        #     return self.memo[tuple(bool_arr)]
+    #     # if tuple(bool_arr) in self.memo:
+    #     #     return self.memo[tuple(bool_arr)]
 
-        # Case 1: Add a 'True', representing that you will split the string at len(bool_arr) index
-        bool_arr.append(True)
-        case1 = self.maxUniqueDp(bool_arr.copy())
+    #     # Case 1: Add a 'True', representing that you will split the string at len(bool_arr) index
+    #     bool_arr.append(True)
+    #     case1 = self.maxUniqueDp(bool_arr.copy())
 
 
-        # Case 2: Add a 'False', representing you will NOT split the string at len(bool_arr) index
-        # assert bool_arr.pop() == True
-        bool_arr.pop()
-        bool_arr.append(False)
-        case2 = self.maxUniqueDp(bool_arr)
+    #     # Case 2: Add a 'False', representing you will NOT split the string at len(bool_arr) index
+    #     # assert bool_arr.pop() == True
+    #     bool_arr.pop()
+    #     bool_arr.append(False)
+    #     case2 = self.maxUniqueDp(bool_arr)
 
-        # return case1 + case2
-        # self.memo[tuple(bool_arr)] = max(case1, case2)
-        return max(case1, case2)
+    #     # return case1 + case2
+    #     # self.memo[tuple(bool_arr)] = max(case1, case2)
+    #     return max(case1, case2)
 
-    def solver(self, bool_arr):
-        l, r = 0, 0
-        arr = set()
-        assert len(bool_arr) == len(self.s)
+    # def solver(self, bool_arr):
+    #     l, r = 0, 0
+    #     arr = set()
+    #     assert len(bool_arr) == len(self.s)
         
-        for split_bool in bool_arr:
-            if not split_bool:
-                r += 1
-                continue
+    #     for split_bool in bool_arr:
+    #         if not split_bool:
+    #             r += 1
+    #             continue
             
-            # substring = None
-            # if (l, r) in self.substrings:
-            #     substring = self.substrings[(l, r)]
-            # else:
-            #     substring = self.s[l : r + 1]
-            #     self.substrings[(l, r)] = substring
-            # substring = self.s[l : r + 1]
+    #         # substring = None
+    #         # if (l, r) in self.substrings:
+    #         #     substring = self.substrings[(l, r)]
+    #         # else:
+    #         #     substring = self.s[l : r + 1]
+    #         #     self.substrings[(l, r)] = substring
+    #         # substring = self.s[l : r + 1]
 
-            # substring = self.substrings.get((l, r), self.s[l : r + 1])
-            # self.substrings[(l, r)] = substring
-            substring = self.s[l : r + 1]
+    #         # substring = self.substrings.get((l, r), self.s[l : r + 1])
+    #         # self.substrings[(l, r)] = substring
+    #         substring = self.s[l : r + 1]
 
-            # Invalid, so return 0
-            if substring in arr:
-                return 0 # Invalid!
+    #         # Invalid, so return 0
+    #         if substring in arr:
+    #             return 0 # Invalid!
 
-            arr.add(substring)
-            r += 1
-            l = r
+    #         arr.add(substring)
+    #         r += 1
+    #         l = r
 
-        # Valid, so return len(arr) [or count of "True" inside boolean array] as answer
-        return len(arr)
+    #     # Valid, so return len(arr) [or count of "True" inside boolean array] as answer
+    #     return len(arr)
 
