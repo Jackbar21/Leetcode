df5d342abca0e14b4796a402b6f40f620d14b046
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-21 18:34:04 -0700

    Time: 74 ms (98.12%) | Memory: 63.7 MB (5.64%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
@@ -9,10 +9,26 @@ class Solution:
         self.levels = {} # level: sum
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
         # Step 1: Calculate sum for each level
-        self.populateLevelSums(root, 1)
+        self.populateLevelSums(root, 1) # O(n)
 
         # Step 2: add each (sum, level) pair to max-heap, and get k'th largest
-        sorted_levels = sorted(self.levels.values(), reverse=True)
+        # Firstly, if there are less than k levels in total, then there is no solution.
+        if len(self.levels) < k:
+            return -1
+
+        # Otherwise, get k'th largest using a max heap :)
+        # This is done in O(klogn) time, instead of O(nlogn) time using sorting!
+        # This is because even though number of levels can be n in worst case.
+        max_heap = [-levelSum for levelSum in self.levels.values()]
+        heapq.heapify(max_heap) # O(h), where h is height of the tree (worst case h == n)
+        for _ in range(k - 1):
+            heapq.heappop(max_heap)
+        return -max_heap[0]
+
+        # Instead of heap can use sorting, but time complexity is O(hlogh), 
+        # where h is the height of the tree. In worst case, h = n, where n
+        # is the number of nodes in the tree, so worst case is actually O(nlogn).
+        sorted_levels = sorted(self.levels.values(), reverse=True) 
         return sorted_levels[k - 1] if (k - 1) < len(sorted_levels) else -1
     
     def populateLevelSums(self, root, depth):
