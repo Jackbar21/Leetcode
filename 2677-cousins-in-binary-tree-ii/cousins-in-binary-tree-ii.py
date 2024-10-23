3813fb8a6d97fa2c4fae679a20dfc3d353d3fe6c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-22 20:57:06 -0700

    Time: 418 ms (91.61%) | Memory: 98.1 MB (5.94%) - LeetSync

---
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.node_to_depth = {}
+        self.node_to_parent = {}
+        self.depth_sums = {}
+        self.node_to_replacement_value = {}
+
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # All we need for each node, is it's depth and parent.
+        # We also need to know level sums.
+        self.populateDepthsAndParents(root, 0, None)
+        # for node in self.node_to_depth:
+        #     depth = self.node_to_depth[node]
+        #     print(f"{node.val=}, {depth=}")
+        # print("DELIMITER")
+        # # print(self.node_to_parent)
+        # for node in self.node_to_parent:
+        #     parent = self.node_to_parent[node]
+        #     print(f"{node.val=}, parent.val={parent.val if parent else -1}")
+        # print("DELIMITER")
+        # print(self.depth_sums)
+        self.generateReplacementValues(root)
+        # for node in self.node_to_replacement_value:
+        #     replacement_value = self.node_to_replacement_value[node]
+        #     print(f"{node.val=}, {replacement_value=}")
+        # return None
+        return self.updateTree(root)
+    
+    def populateDepthsAndParents(self, root, depth, parent):
+        if not root:
+            return
+        
+        self.node_to_depth[root] = depth
+        self.node_to_parent[root] = parent
+        self.depth_sums[depth] = self.depth_sums.get(depth, 0) + root.val
+
+        self.populateDepthsAndParents(root.left, depth + 1, root)
+        self.populateDepthsAndParents(root.right, depth + 1, root)
+        return
+
+    def generateReplacementValues(self, root):
+        # Root node is special case, and value is always 0
+        self.node_to_replacement_value[root] = 0
+
+        if root.left:
+            self.generateReplacementValuesHelper(root.left)
+        if root.right:
+            self.generateReplacementValuesHelper(root.right)
+        return
+    
+    def generateReplacementValuesHelper(self, root):
+        if not root:
+            return
+        
+        depth = self.node_to_depth[root]
+        parent = self.node_to_parent[root]
+        
+        res = self.depth_sums[depth]
+        if parent.left:
+            res -= parent.left.val
+        if parent.right:
+            res -= parent.right.val
+        
+        self.node_to_replacement_value[root] = res
+
+        self.generateReplacementValuesHelper(root.left)
+        self.generateReplacementValuesHelper(root.right)
+        return
+    
+    def updateTree(self, root):
+        if not root:
+            return None
+        
+        root.val = self.node_to_replacement_value[root]
+        root.left = self.updateTree(root.left)
+        root.right = self.updateTree(root.right)
+        return root
