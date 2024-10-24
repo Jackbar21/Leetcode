6ba7f201a9703e89a233f7d85898db26625b4bd1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-23 19:28:38 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (16.61%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 or not root2:
+            return not root1 and not root2
+
+        if root1.val != root2.val:
+            return False
+        
+        # Never touch root2, only try to make root1 equal to root2. I.e. pick a
+        # tree to modify, and stick to that decision til the end.
+
+        # Case 1: Don't swap root1's children
+        case1 = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
+        if case1:
+            return True
+
+        # Case 2: Swap root1's children
+        # root1.left, root1.right = root1.right, root1.left
+        tmp_left = root1.left
+        root1.left = root1.right
+        root1.right = tmp_left
+
+        case2 = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
+        if case2:
+            return True
+        
+        # Neither case1 nor case2 was true, so no -- the trees are NOT flip equivalent!
+        return False
