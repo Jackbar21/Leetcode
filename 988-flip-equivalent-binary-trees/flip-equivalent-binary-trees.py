3ead618d85e26027c47c19ccda27ed38d0a20727
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-23 19:31:24 -0700

    Time: 0 ms (100.00%) | Memory: 16.5 MB (57.29%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        # If either root is None, return True if and only if both roots are None
         if not root1 or not root2:
             return not root1 and not root2
 
         if root1.val != root2.val:
             return False
         
-        # Never touch root2, only try to make root1 equal to root2. I.e. pick a
-        # tree to modify, and stick to that decision til the end.
+        # IDEA: Never touch root2, only try to make root1 equal to root2. 
+        # I.e. pick a tree to modify, and stick to that decision 'til the end.
 
         # Case 1: Don't swap root1's children
         case1 = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
@@ -21,9 +22,8 @@ class Solution:
             return True
 
         # Case 2: Swap root1's children
-        # root1.left, root1.right = root1.right, root1.left
-
-        case2 = self.flipEquiv(root1.right, root2.left) and self.flipEquiv(root1.left, root2.right)
+        root1.left, root1.right = root1.right, root1.left
+        case2 = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
         if case2:
             return True
         
