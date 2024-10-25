6bf8caea71f2c27a3d6810386d2307e379d6995c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-25 11:01:17 -0700

    Time: 3 ms (98.52%) | Memory: 17.5 MB (93.69%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left, right = 0, len(numbers) - 1
+        l, r = 0, len(numbers) - 1
 
-        while numbers[left] + numbers[right] != target:
-            if numbers[left] + numbers[right] > target:
-                # need to make sum smaller, so 
-                # decrement right pointer by 1
-                right -= 1
+        while l < r:
+            num1, num2 = numbers[l], numbers[r]
+
+            if num1 + num2 < target:
+                l += 1
+            elif num1 + num2 > target:
+                r -= 1
             else:
-                # need to make sum larger, so
-                # increment left pointer by 1
-                left += 1
+                assert num1 + num2 == target
+                return [l + 1, r + 1]
         
-        return (min(left, right) + 1, max(left, right) + 1)
+        raise Exception("Unreachable Code")
