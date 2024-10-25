aa58982c2391226e99f6d2f20f147931e6bee0d3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-24 19:45:59 -0700

    Time: 57 ms (81.38%) | Memory: 29.5 MB (93.10%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,21 +1,12 @@
 class Solution:
-    def isSubfolder(self, subfolder, folder):
-        # Check if folder is a PREFIX of subfolder
-        return folder[:len(subfolder)] == subfolder # TODO: Optimize this
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        # 4 * 10^4 * 100 == 4 * 10^4 * 10^2 == 4 * 10^6
         folders = set(folder)
         res = []
 
         for folder in folders:
-            slash_indices = []
-            for index in range(1, len(folder)):
-                if folder[index] == "/":
-                    slash_indices.append(index)
-
             is_subfolder = False
-            for slash_index in slash_indices:
-                if folder[:slash_index] in folders:
+            for i in range(1, len(folder)):
+                if folder[i] == "/" and folder[:i] in folders:
                     is_subfolder = True
                     break
             
