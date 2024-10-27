ac4b35ebad503540a38ac7fb7b952bd9a9637bc4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-26 18:54:58 -0700

    Time: 108 ms (31.88%) | Memory: 19.4 MB (15.31%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -18,22 +18,14 @@ class Solution:
                     offset = dimension - 1
                     is_square = True
                     for index in range(offset + 1):
-                        if matrix[i + index][j + offset] == 0:
-                            is_square = False
-                            break
-
-                    # if not is_square:
-                    #     break
-
-                    for index in range(offset):
-                        if matrix[i + offset][j + index] == 0:
+                        if (matrix[i + index][j + offset] == 0
+                            or matrix[i + offset][j + index] == 0
+                        ):
                             is_square = False
                             break
                         
-                    # count += is_square
-                    if is_square:
-                        count += 1
-                    else:
+                    count += is_square
+                    if not is_square:
                         # if n x n square fails, then (n + k) x (n + k)
                         # squares starting from same top-left index, for all k >= 1,
                         # will also be guaranteed to fail. Hence, exit early.
