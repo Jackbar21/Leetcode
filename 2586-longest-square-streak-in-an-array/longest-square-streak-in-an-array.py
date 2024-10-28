9e2bcfc66a15389f13d9f1db60602dbc2667b29f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-27 17:31:49 -0700

    Time: 176 ms (16.22%) | Memory: 41.3 MB (6.33%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -5,20 +5,18 @@ class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
         self.nums = set(nums)
 
-        # We now know there exists at least one square streak, so max value is AT LEAST 1
-        max_streak = 1
-
+        max_streak = 0
         for num in nums:
             streak = self.getSquareStreakFromNum(num)
             max_streak = max(max_streak, streak)
         
-        return max_streak if max_streak > 1 else -1
+        return max_streak if max_streak >= 2 else -1
     
     def getSquareStreakFromNum(self, num):
-        # if num in self.memo:
-        #     return self.memo[num]
+        if num in self.memo:
+            return self.memo[num]
         
         next_square = pow(num, 2)
         res = 1 + self.getSquareStreakFromNum(next_square) if next_square in self.nums else 1
-        # self.memo[num] = res
+        self.memo[num] = res
         return res
