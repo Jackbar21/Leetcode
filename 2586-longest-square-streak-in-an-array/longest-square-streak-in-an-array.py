a4a4221e2d34095ae454b9f795d1a9d25e3175a7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-27 17:27:21 -0700

    Time: 171 ms (16.22%) | Memory: 41.5 MB (6.33%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -15,14 +15,14 @@ class Solution:
         return max_streak if max_streak > 1 else -1
     
     def getSquareStreakFromNum(self, num):
-        # if num in self.memo:
-        #     return self.memo[num]
+        if num in self.memo:
+            return self.memo[num]
         
         next_square = pow(num, 2)
         if next_square not in self.nums:
-            # self.memo[num] = 1
+            self.memo[num] = 1
             return 1
         
         res = 1 + self.getSquareStreakFromNum(next_square)
-        # self.memo[num] = res
+        self.memo[num] = res
         return res
