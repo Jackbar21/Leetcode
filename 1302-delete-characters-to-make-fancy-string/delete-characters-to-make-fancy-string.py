d7e0a4aa8fecd22d4b5b36aa7d006711b2b8684c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-10-31 19:09:45 -0700

    Time: 359 ms (31.53%) | Memory: 18.8 MB (11.90%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -5,8 +5,7 @@ class Solution:
 
         letters = [s[0], s[1]]
         for i in range(2, len(s)):
-            if s[i] == s[i - 1] and s[i] == s[i - 2]:
-                continue
-            letters.append(s[i])
+            if s[i] != s[i - 1] or s[i] != s[i - 2]:
+                letters.append(s[i])
         
         return ''.join(letters)
