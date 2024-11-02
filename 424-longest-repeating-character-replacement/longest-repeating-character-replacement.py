f31d18baedafe95f4ec0c5f49981f8742d234971
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-01 20:27:48 -0700

    Time: 83 ms (32.46%) | Memory: 16.7 MB (55.89%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -8,12 +8,8 @@ class Solution:
             letter = s[r]
             d[letter] += 1
 
-            # count = sum(d.values())
-            # max_letter = max(d.values())
-            count, max_letter = 0, 0
-            for val in d.values():
-                count += val
-                max_letter = max(max_letter, val)
+            count = sum(d.values())
+            max_letter = max(d.values())
             while count - max_letter > k:
                 d[s[l]] -= 1
                 count -= 1
