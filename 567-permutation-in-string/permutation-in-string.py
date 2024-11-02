df15d151b100f5553006257bae9ef7382b4bda4f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-01 22:33:03 -0700

    Time: 16 ms (76.27%) | Memory: 16.7 MB (36.11%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,31 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) == 0:
-            return True
-
-        if len(s2) < len(s1):
+        if len(s1) > len(s2):
             return False
-
-        dict_to_match = {}
+        
+        # letter-to-frequency mappings for s1 & s2, respectively
+        d1, d2 = defaultdict(int), defaultdict(int)
         for letter in s1:
-            dict_to_match[letter] = dict_to_match.get(letter, 0) + 1
-        
-        d = {}
+            d1[letter] += 1
+
         for i in range(len(s1)):
-            letter = s2[i]
-            d[letter] = d.get(letter, 0) + 1
-        
-        l, r = 0, len(s1)
-        while d != dict_to_match and r < len(s2):
-            letter_to_remove = s2[l]
-            letter_to_add = s2[r]
+            d2[s2[i]] += 1
 
-            d[letter_to_remove] -= 1
-            if d[letter_to_remove] == 0:
-                del d[letter_to_remove]
-            d[letter_to_add] = d.get(letter_to_add, 0) + 1
-
+        l = 0
+        for r in range(len(s1), len(s2)):
+            # Return true if permutation of one another
+            if d1 == d2:
+                return True
+            
+            # Update window (fixed-size)
+            d2[s2[l]] -= 1
+            if d2[s2[l]] == 0:
+                del d2[s2[l]]
+            d2[s2[r]] += 1
             l += 1
-            r += 1
         
-        return d == dict_to_match
+        return d1 == d2
