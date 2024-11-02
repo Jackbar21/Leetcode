0503b6ede9e7544713d6b6ed8ec42d83b3fcfb5a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-01 17:11:29 -0700

    Time: 0 ms (100.00%) | Memory: 16.5 MB (45.68%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -1,14 +1,11 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
-        # print(f"{sentence=}")
         words = sentence.split(" ")
-        # print(f"{words=}")
         FIRST, LAST = 0, -1
 
         # Handle last case first (i.e. check first and last words)
-        # if words[FIRST][FIRST] != words[LAST][LAST]:
-        #     return False
-        words.append(words[FIRST])
+        if words[FIRST][FIRST] != words[LAST][LAST]:
+            return False
         
         # Now check for remaining elements (except last!)
         for i in range(len(words) - 1):
