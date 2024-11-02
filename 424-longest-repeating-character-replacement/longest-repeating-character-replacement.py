81e3af2adef5d4d10812db2803fdc47e45ae7f93
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-01 20:12:24 -0700

    Time: 1307 ms (5.02%) | Memory: 16.9 MB (17.69%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,29 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        res = 0
+        for letter in alphabet:
+            print(letter, self.charReplacementSolver(s, k, letter))
+            res = max(res, self.charReplacementSolver(s, k, letter))
+        
+        return res
+    
+    def charReplacementSolver(self, s, k, replace_letter):
+        num_to_replace = 0
+        res = k
+        
+        l = 0
+        for r in range(len(s)):
+            letter = s[r]
+            if letter != replace_letter:
+                num_to_replace += 1
+            
+            while l < r and num_to_replace > k:
+                if s[l] != replace_letter:
+                    num_to_replace -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+        
+        return res
