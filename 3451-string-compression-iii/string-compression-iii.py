5ff6cf6ef209c3b3224dd8e2e53ab449e2370609
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-03 21:28:18 -0800

    Time: 259 ms (45.39%) | Memory: 27.3 MB (16.78%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -6,16 +6,19 @@ class Solution:
         while i < len(word):
             letter = word[i]
             count = 1
+            i += 1
             for offset in range(1, 9):
-                index = i + offset
-                if index < len(word) and letter == word[index]:
+                # index = i + offset
+                if i < len(word) and letter == word[i]:
                     count += 1
+                    i += 1
                 else:
                     break
             
             res.append(f"{count}{letter}")
 
             # Loop Invariant
-            i += count
+            # i += count
+            # i += 1
 
         return ''.join(res)
