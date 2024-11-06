d2ae69f087ad438f8d3012a8842886bc709e77cc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-05 17:36:46 -0800

    Time: 31 ms (29.09%) | Memory: 16.7 MB (43.17%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,14 +1,11 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
         d = {} # num to #set-bits mappings
-
         for num in nums:
             d[num] = bin(num).count('1')
-        
-        index_dict = {} # index to desired index mappings
-        
-        # #print(d)
+
         INDEX, NUM = 0, 1
+        index_dict = {} # index to desired index mappings
         sorted_nums = sorted(enumerate(nums), key=lambda x: x[NUM])
         for desired_index in range(len(sorted_nums)):
             at_index = sorted_nums[desired_index][INDEX]
@@ -16,18 +13,13 @@ class Solution:
         
         for at_index in index_dict:
             desired_index = index_dict[at_index]
-            # if at_index == desired_index:
-            #     continue
-            
-            # if at_index < desired_index:
-            #     while at_index 
-            num = nums[at_index]
-            set_bits = d[num]
+            set_bits = d[nums[at_index]]
 
+            direction = 1 if at_index < desired_index else -1
             while at_index != desired_index:
-                next_index = (at_index + 1) if at_index < desired_index else (at_index - 1)
-                next_num = nums[next_index]
-                next_num_set_bits = d[next_num]
+                # next_index = (at_index + 1) if at_index < desired_index else (at_index - 1)
+                next_index = at_index + direction
+                next_num_set_bits = d[nums[next_index]]
                 if set_bits != next_num_set_bits:
                     return False
                 
