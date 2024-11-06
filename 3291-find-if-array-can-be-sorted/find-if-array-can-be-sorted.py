97cb88587b197eef92516e13666e673259f4619d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-05 17:37:55 -0800

    Time: 25 ms (32.73%) | Memory: 16.7 MB (43.17%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -17,7 +17,6 @@ class Solution:
 
             direction = 1 if at_index < desired_index else -1
             while at_index != desired_index:
-                # next_index = (at_index + 1) if at_index < desired_index else (at_index - 1)
                 next_index = at_index + direction
                 next_num_set_bits = d[nums[next_index]]
                 if set_bits != next_num_set_bits:
@@ -26,30 +25,4 @@ class Solution:
                 # Loop Invariant
                 at_index = next_index
 
-            # Case 1:
-            # while at_index < desired_index:
-            #     next_index = at_index + 1
-            #     next_num = nums[next_index]
-            #     next_num_set_bits = d[next_num]
-            #     if set_bits != next_num_set_bits:
-            #         return False
-                
-            #     # Loop Invariant
-            #     # at_index += 1
-            #     at_index = next_index
-            
-            # Redundant if statement
-            # if at_index == desired_index:
-            #     continue
-            
-            # Case 2:
-            # while at_index > desired_index:
-            #     next_index = at_index - 1
-            #     next_num = nums[next_index]
-            #     next_num_set_bits = d[next_num]
-            #     if set_bits != next_num_set_bits:
-            #         return False
-
-        #print(sorted_nums)
-        #print(index_dict)
         return True
