0e5b6a175804883c0a7e27cfb49e07e0463516d7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-06 17:46:08 -0800

    Time: 787 ms (13.75%) | Memory: 26.8 MB (81.82%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,30 +1,17 @@
 class Solution:
-    def __init__(self):
-        self.num_to_bin = {}
-
-    def isBitSet(self, num, bit_index):
-        # if num not in self.num_to_bin:
-        #     self.num_to_bin[num] = bin(num)
-        
-        # bin_num = self.num_to_bin[num]
-        bin_num = bin(num)
-        return -bit_index <= len(bin_num) and bin_num[bit_index] == '1'
-
-    
     def largestCombination(self, candidates: List[int]) -> int:
         max_num_bits = len(bin(max(candidates))) - 2
         
         res = 0
         for i in range(max_num_bits):
-            bit_index = -i - 1
-            # TODO: hash binary representation of each number to see if affects performance
+            bit_index = -1 - i
+
             count = 0
             for num in candidates:
-                # bin_num = bin(num)
-                # if -bit_index <= len(bin_num) and bin_num[bit_index] == '1':
-                #     count += 1
-                count += self.isBitSet(num, bit_index)
-    
+                bin_num = bin(num)
+                if -bit_index <= len(bin_num) and bin_num[bit_index] == '1':
+                    count += 1
+
             res = max(res, count)
 
         return res
