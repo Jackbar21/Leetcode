031ae378a0453b8230e485f821842844f2bbc304
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-07 18:01:40 -0800

    Time: 798 ms (5.36%) | Memory: 32.3 MB (88.95%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -17,17 +17,33 @@ class Solution:
         for num in nums:
             xor ^= num
         
-        max_k = 1 << maximumBit
-        best_k = [0] * maximumBit
+        # max_k = 1 << maximumBit
+        # best_k = [0] * maximumBit
 
         answer = []
         for i in range(len(nums) - 1, -1, -1):
-            # print(xor)
+            bin_xor = bin(xor)
+            length = len(bin_xor) - 2
+
+            num_to_take = min(maximumBit, length)
+            padding_len = max(0, maximumBit - num_to_take)
+
+            k = '1' * padding_len + ''.join(
+                '0' if c == '1' else '1' for c in bin_xor[-num_to_take:]
+            )
+
+            # k = '1' * (max(0, maximumBit - length)) # + ''.join(
+                # '1' * (maximumBit - len(bin_xor) + 2),  # padding
+                # ('0' if bin_xor[len(bin_xor) - 1 - i] == '1' else '1' for i in range(maximumBit))
+            # )
 
             # k = int(''.join('0' if c == '1' else '1' for c in bin(xor)[-maximumBit:]), 2)
-            k = int(''.join('0' if c == '1' else '1' for c in ('G' * maximumBit + bin(xor))[-maximumBit:]), 2)
+            # k = int(''.join('0' if c == '1' else '1' for c in ('G' * maximumBit + bin(xor))[-maximumBit:]), 2)
 
+            # 1 -> 0
+            # 0 -> 1
 
+            k = int(k, 2)
             answer.append(k)
 
             # "Delete" last number in nums
