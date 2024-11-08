d533a80ab645d7ec71e9de202673a3f9998cb04c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-07 18:07:15 -0800

    Time: 840 ms (5.36%) | Memory: 32.4 MB (84.59%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -5,20 +5,16 @@ class Solution:
         # X ^ X == 0
         # X ^ Y == Y ^ X
 
-        # want to maximize xor ^ k
-        # so want k to be INVERSE IN BITS of xor
-
-        # xor = 1110101010101010101
-        #  k =  0001010101010101010
-        #       ___________________
-        #       1111111111111111111
-
+        # Get xor of all numbers in nums
         xor = 0
         for num in nums:
             xor ^= num
         
-        # max_k = 1 << maximumBit
-        # best_k = [0] * maximumBit
+        # Want to maximize xor ^ k so want k to be INVERSE IN BITS of xor
+        # xor = 1110101010101010101
+        # k   = 0001010101010101010
+        #       ___________________
+        #       1111111111111111111
 
         answer = []
         for i in range(len(nums) - 1, -1, -1):
@@ -28,25 +24,19 @@ class Solution:
             num_to_take = min(maximumBit, length)
             padding_len = max(0, maximumBit - num_to_take)
 
-            k = '1' * padding_len + ''.join(
-                '0' if c == '1' else '1' for c in bin_xor[-num_to_take:]
-            )
-
-            # k = '1' * (max(0, maximumBit - length)) # + ''.join(
-                # '1' * (maximumBit - len(bin_xor) + 2),  # padding
-                # ('0' if bin_xor[len(bin_xor) - 1 - i] == '1' else '1' for i in range(maximumBit))
+            # bin_k = '1' * padding_len + ''.join(
+            #     '0' if c == '1' else '1' for c in bin_xor[-num_to_take:]
             # )
 
-            # k = int(''.join('0' if c == '1' else '1' for c in bin(xor)[-maximumBit:]), 2)
-            # k = int(''.join('0' if c == '1' else '1' for c in ('G' * maximumBit + bin(xor))[-maximumBit:]), 2)
+            k = int('1' * padding_len, 2) << num_to_take if padding_len > 0 else 0
+            k += int(''.join(
+                '0' if c == '1' else '1' for c in bin_xor[-num_to_take:]
+            ), 2)
 
-            # 1 -> 0
-            # 0 -> 1
-
-            k = int(k, 2)
+            # k = int(bin_k, 2)
             answer.append(k)
 
-            # "Delete" last number in nums
+            # Loop Invariant - "Delete" last number in nums
             xor ^= nums[i]
 
         return answer
