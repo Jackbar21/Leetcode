f1acd0225a88342615b99d1daaf8d048c1d5a276
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-08 19:09:13 -0800

    Time: 0 ms (100.00%) | Memory: 16.6 MB (33.33%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.py
+++ 3394-minimum-array-end/minimum-array-end.py
@@ -3,27 +3,16 @@ class Solution:
         bin_x = bin(x)[2:]
         bin_num = bin(n - 1)[2:]
 
-        # Add padding as needed
-        # if len(bin_num) > len(bin_x):
-        #     bin_x = '0' * (len(bin_num) - len(bin_x)) + bin_x
-
         x_index = len(bin_x) - 1
         num_index = len(bin_num) - 1
         arr = collections.deque()
         while num_index >= 0:
-            # All unset bits in x already taken care of, so
-            # add rest of nums in bin_num as needed
-            if x_index < 0:
-                arr.appendleft(bin_num[num_index])
-                num_index -= 1
-                continue
-
-            if bin_x[x_index] == '0':
-                arr.appendleft(bin_num[num_index])
-                num_index -= 1
-            else:
-                # assert bin_x[x_index] == '1'
+            if x_index >= 0 and bin_x[x_index] == '1':
+                # Not allowed to modify bit
                 arr.appendleft('1')
+            else:
+                arr.appendleft(bin_num[num_index])
+                num_index -= 1
 
             # Loop Invariant
             x_index -= 1
@@ -36,7 +25,7 @@ class Solution:
 #     01101100000101
 
 # 3 --> 11
-# 4 --> 100
+# 412738901273882197308912 --> 0b1010111011001101001111011100100010101000010110011110001011111010011010111110000
 
 # 1 0 1 == 1*2^2 + 0*2^1 + 1*2^0 == 5
 
