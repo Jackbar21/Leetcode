27c6512f960c36ff3031507f3b69e6e058813ef7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-08 19:02:40 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (33.33%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.py
+++ 3394-minimum-array-end/minimum-array-end.py
@@ -4,8 +4,8 @@ class Solution:
         bin_num = bin(n - 1)[2:]
 
         # Add padding as needed
-        if len(bin_num) > len(bin_x):
-            bin_x = '0' * (len(bin_num) - len(bin_x)) + bin_x
+        # if len(bin_num) > len(bin_x):
+        #     bin_x = '0' * (len(bin_num) - len(bin_x)) + bin_x
 
         x_index = len(bin_x) - 1
         num_index = len(bin_num) - 1
@@ -29,3 +29,21 @@ class Solution:
             x_index -= 1
 
         return x | int(''.join(arr), 2)
+
+# x = 01101100000101 == 6913
+# n = 01101100010101
+#     ______________
+#     01101100000101
+
+# 3 --> 11
+# 4 --> 100
+
+# 1 0 1 == 1*2^2 + 0*2^1 + 1*2^0 == 5
+
+# nums = [6913, 6915]
+
+# BITWISE AND:
+# 0 & 0 = 0
+# 0 & 1 = 0
+# 1 & 0 = 0
+# 1 & 1 = 1
