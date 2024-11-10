a3542a788347cd8ada47bbe1dbd0260987dadec9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-09 20:22:07 -0800

    Time: 4 ms (30.83%) | Memory: 18.5 MB (11.24%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -2,60 +2,29 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for token in tokens:
-            # print(stack)
             if token not in ["+", "-", "*", "/"]:
-                # Must be an integer between -200 and 200!
-                assert -200 <= int(token) <= 200
+                # Must be an integer! (between -200 and 200)
+                # assert -200 <= int(token) <= 200
                 stack.append(int(token))
                 continue
 
             rNum, lNum = stack.pop(), stack.pop()
-            # if lNum >= 0 and rNum < 0:
-            #     lNum *= -1
-            #     rNum *= -1
-            # if token == "/" and lNum < 0:
-            #     # Python rounds DOWN instead of towards 0 with division truncation
-            #     lNum += 1
-            if token == "/":
-                # if rNum < 0:
-                #     lNum = -lNum
-                #     rNum = -rNum
 
+            # Handle division case separately, since Python always 
+            # rounds DOWN instead of rounding towards 0.
+            if token == "/":
                 res = lNum // rNum
                 if res < 0 and res != lNum / rNum:
                     res += 1
-                # if res < 0:
-                #     res += 1
                 stack.append(res)
                 continue
-
-
+            
+            # Otherwise, straightforward operation!
             stack.append(
                 lNum + rNum if token == "+"
                 else lNum - rNum if token == "-"
-                else lNum * rNum if token == "*"
-                else lNum // rNum
+                else lNum * rNum
             )
-
-
-            # if token == "+":
-                
-            #     stack.append(lNum + rNum)
-            # elif token == "-":
-            #     rNum, lNum = stack.pop(), stack.pop()
-            #     stack.append(lNum - rNum)
-            # elif token == "*":
-            #     rNum, lNum = stack.pop(), stack.pop()
-            #     stack.append(lNum * rNum)
-            # elif token == "/":
-            #     rNum, lNum = stack.pop(), stack.pop()
-            #     assert lNum != 0
-            #     if lNum < 0:
-            #         div = -lNum // rNum
-            #         stack.append(-div)
-            #     else:
-            #         stack.append(lNum // rNum)
-                
         
-        assert len(stack) == 1
+        # assert len(stack) == 1
         return stack[0]
