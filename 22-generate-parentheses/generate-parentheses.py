724f7b2088eedee9bd854befef6be84131425495
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-09 21:28:03 -0800

    Time: 3 ms (34.43%) | Memory: 16.9 MB (21.43%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,12 +3,10 @@ class Solution:
         self.solution = []
     def generateParenthesis(self, n: int) -> List[str]:
         val = self.generateParenthesisHelper(n, n, [])
-        print(f"{val}")
         return self.solution
     
     def generateParenthesisHelper(self, open_parentheses, closed_parentheses, stack):
         if open_parentheses == 0:
-            # return 1
             self.solution.append(
                 ''.join(stack) + ')' * closed_parentheses
             )
@@ -17,15 +15,14 @@ class Solution:
         if open_parentheses >= closed_parentheses:
             assert open_parentheses == closed_parentheses
             stack.append('(')
-            return self.generateParenthesisHelper(open_parentheses - 1, closed_parentheses, stack)
+            return self.generateParenthesisHelper(open_parentheses-1, closed_parentheses, stack)
         
         assert open_parentheses >= 0 and open_parentheses < closed_parentheses
         # Here we have two options:
 
         # Case 1: add another open parenthese!
-        # stack.append('(')
-        case1 = self.generateParenthesisHelper(open_parentheses - 1, closed_parentheses, stack.copy() + ['('])
-        # stack.pop()
+        new_stack = stack.copy() + ["("]
+        case1 = self.generateParenthesisHelper(open_parentheses-1, closed_parentheses, new_stack)
 
         # Case 2: add a closed parenthese
         stack.append(')')
