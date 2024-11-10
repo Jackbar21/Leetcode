a7dbc804fc4d0edb1100b14c2c2fa017fcfe68f1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-10 12:33:08 -0800

    Time: 156 ms (39.61%) | Memory: 17.8 MB (82.56%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,8 +3,6 @@ class Solution:
         smallest_k = 1
         largest_k = max(piles)
 
-        best_k = float("inf")
-
         while smallest_k <= largest_k:
             k = (smallest_k + largest_k) // 2
 
@@ -12,17 +10,10 @@ class Solution:
             # I.e. figure out how many hours it takes to eat ALL the
             # bananas at a rate of k bananas-per-hour, and check if
             # it it <= h (which is when the guards come back)
-            num_hours = 0
-            for banana_pile in piles:
-                hours = math.ceil(banana_pile / k)
-                num_hours += hours
-            
-            is_true_index = num_hours <= h
-            if is_true_index:
-                best_k = min(best_k, k)
+            num_hours = sum(math.ceil(banana_pile / k) for banana_pile in piles)
+            if num_hours <= h:
                 largest_k = k - 1
             else:
                 smallest_k = k + 1
         
-        assert best_k != float("inf")
-        return best_k
+        return smallest_k
