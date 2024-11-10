7d1b83a6a0b1e8c7585eaf4952a1e911aebd84e9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-09 23:48:00 -0800

    Time: 131 ms (87.48%) | Memory: 38.3 MB (54.93%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,56 +1,25 @@
 class Solution:
-    def __init__(self):
-        self.target = None
-        self.pos_speed = None
-        self.car_to_moves = {} # number of moves it takes for car to reach target
-    
-    def getNumMoves(self, car_index):
-        # if car_index in self.car_to_moves:
-        #     return self.car_to_moves[car_index]
-        
-        POSITION, SPEED = 0, 1
-        # car_pos = self.pos_speed[car_index][POSITION]
-        # car_speed = self.pos_speed[car_index][SPEED]
-        car_pos, car_speed = self.pos_speed[car_index]
-
-        # res = math.ceil((self.target - car_pos) / car_speed)
-        res = (self.target - car_pos) / car_speed
-        # self.car_to_moves[car_index] = res
-        return res
-
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        assert len(position) == len(speed)
-        n = len(position)
+        # assert len(position) == len(speed)
         POSITION, SPEED = 0, 1
+        n = len(position)
+
         pos_speed = [(position[i], speed[i]) for i in range(n)]
         pos_speed.sort(key = lambda pair: pair[POSITION], reverse=True)
-        self.pos_speed = pos_speed
-        self.target = target
 
-        # car_to_fleet = {}
-        prev_car = None
+        # Skip first car, since it's part of first fleet
+        first_car_pos, first_car_speed = pos_speed[0]
+        handicap = (target - first_car_pos) / first_car_speed
         num_fleets = 1
-        handicap = self.getNumMoves(0)
-        # car_to_moves = {} 
-        # car_to_moves[0] = math.ceil((target - pos_speed[0][POSITION]) / pos_speed[0][SPEED])
-        for i, (pos, speed) in enumerate(pos_speed):
-            # Skip first car, since it's part of first fleet
-            if i == 0:
-                continue
 
+        for i in range(1, len(pos_speed)):
             # If current car can CATCH UP to previous car, it belongs in same fleet
             # Otherwise, it creates its own fleet, and at BEST all remaining cars
             # will join this new fleet or create their own (since they have smaller
             # starting positions, and passing a car is NOT allowed!)
-            prev_car = i - 1
-            prev_moves = self.getNumMoves(prev_car)
-
-            cur_moves = self.getNumMoves(i)
-            # if cur_moves <= prev_moves:
-            if cur_moves <= handicap:
-                # Joins same fleet, so nothing happens
-                pass
-            else:
+            car_pos, car_speed = pos_speed[i]
+            cur_moves = (target - car_pos) / car_speed
+            if cur_moves > handicap:
                 # Cannot catch up to car, so creates it's own fleet! And remember,
                 # remaining cars, can only perform AT BEST the same as this car now!
                 # Since again, surpassing cars is NOT allowed, and all remaining cars
@@ -59,14 +28,4 @@ class Solution:
                 num_fleets += 1
                 handicap = cur_moves
 
-        
-
-
-
-
         return num_fleets
-
-
-        
-        
-        
