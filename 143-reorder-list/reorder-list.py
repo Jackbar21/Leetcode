ea28dc7e09c173530b0614201787f29375773697
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-13 20:00:35 -0800

    Time: 2 ms (57.96%) | Memory: 22.3 MB (98.27%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -19,7 +19,7 @@ class Solution:
         
         # print(arr)
         l, r = 0, len(arr) - 1
-        while l < r:
+        while l <= r:
             cur_head = arr[l]
             cur_tail = arr[r]
 
@@ -31,30 +31,3 @@ class Solution:
             r -= 1
 
         return
-
-        
-        # l, r = 0, len(arr) - 1
-        # while l < r:
-    
-            
-        
-        # Step 2: Add elements to new array in re-ordered fashion
-        reordered_arr = []
-        # left_next = True
-        l, r = 0, len(arr) - 1
-        while l <= r:
-            # next_node = arr.popleft() if left_next else arr.pop()
-            # reordered_arr.append(next_node)
-            # left_next = not left_next
-            reordered_arr.append(arr[l])
-            l += 1
-            if l < r:
-                reordered_arr.append(arr[r])
-                r -= 1
-
-        reordered_arr.append(None)
-        for i in range(len(reordered_arr) - 1):
-            reordered_arr[i].next = reordered_arr[i + 1]
-        
-        # return arr[0]
-        
