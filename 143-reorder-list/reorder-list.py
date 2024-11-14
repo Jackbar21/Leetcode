d42db88333e971190fe1124f7d238413b45f9db0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-13 19:59:49 -0800

    Time: 7 ms (23.55%) | Memory: 22.5 MB (61.70%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -13,7 +13,25 @@ class Solution:
         arr = []
         while head:
             arr.append(head)
-            head = head.next
+            tmp_next = head.next
+            head.next = None
+            head = tmp_next
+        
+        # print(arr)
+        l, r = 0, len(arr) - 1
+        while l < r:
+            cur_head = arr[l]
+            cur_tail = arr[r]
+
+            cur_head.next = cur_tail
+            cur_tail.next = arr[l + 1] if l + 1 < r else None
+
+            # Loop Invariant
+            l += 1
+            r -= 1
+
+        return
+
         
         # l, r = 0, len(arr) - 1
         # while l < r:
