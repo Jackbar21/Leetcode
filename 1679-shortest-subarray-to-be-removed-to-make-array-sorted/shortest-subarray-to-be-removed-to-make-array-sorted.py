665966af48a565c54968bd70880588fcd87c22b5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-14 17:50:49 -0800

    Time: 84 ms (5.41%) | Memory: 30.3 MB (94.70%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,10 +1,5 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        # for each index, store:
-        #   (1) largest value preceding it
-        #   (2) smallest value succeeding it
-        #   (3) value at index itself (arr[i])
-
         i = len(arr) - 1
         while i > 0 and arr[i - 1] <= arr[i]:
             i -= 1
@@ -16,26 +11,20 @@ class Solution:
         # index k such that i <= k < len(arr), for every index j < i, such that arr[j] <= arr[k],
         # in which case you know you can create a sorted array by removing k - j + 1 elements!
         
-        # We know we only want to continue from a sorted left-half, so apply same principle
+        # We know we only want to continue from a sorted left-half, so apply same principle!
         j = 0
         while j < len(arr) - 1 and arr[j] <= arr[j + 1]:
             j += 1
         
         # If j and i cross each other, then array must already be sorted! 
         if j >= i:
-            assert j == len(arr) - 1 and i == 0 # Might be false at i == j ?
+            # assert j == len(arr) - 1 and i == 0
             return 0
-        
-        # Remove all but one element
-        # if min(arr[:j+1]) > max(arr[i:]):
-        #     return len(arr) - 1
 
-        res = len(arr) - 1 # Worst case, delete all but one element
-        assert j < i
-        # print(arr[i:])
+        # Worst case, delete all but one element!
+        res = len(arr) - 1 
+
         for index in range(j + 1):
-        # # print(i)
-        # for index in [1]:
             l, r = i, len(arr) - 1
             k = float("inf")
             while l <= r:
@@ -47,22 +36,16 @@ class Solution:
                 else:
                     l = mid + 1
             
-            # print(index, k, arr[:index + 1], (arr[k:] if k != float("inf") else []))
-            res = min(res, k - index + 1 - 2)
+            res = min(res, k - index - 1) # (k-index+1) - 2 since don't wanna include ends!
 
-        # Length of left subhalf
-        # length_left = len(arr[:j+1])
+        # Case 1: Check if removing all elements except left sorted part yields better results
         length_left = j + 1
+        res = min(res, len(arr) - length_left)
         
+        # Case 2: Check if removing all elements except right sorted part yields better results
         length_right = len(arr) - i
-        # print(f"{i=}")
-        return min(
-            res,
-            # len(arr) - (j+1) + 1, # sorted right-half
-            # len(arr) - (i + 1), # sorted left-half
-            len(arr) - length_left,
-            len(arr) - length_right
-        )
-
-
-        # [2,4,2,3,5]
+        res = min(res, len(arr) - length_right)
+        
+        # Optimal result!
+        return res
+        
