327715978a892fed9bc4655aa2df6a6289bd8584
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-15 17:38:36 -0800

    Time: 7 ms (57.45%) | Memory: 17 MB (5.94%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -22,20 +22,24 @@ class Solution:
         # sequence of elements STARTING at that index. Then from that point on, for each
         # index, we check if there is a sorted array of length AT LEAST k (and add its
         # power to answer result if so), or add -1 to answer array since not a power array.
-        # print([
-        #     self.getPowerArrayLengthFromIndex(i)
-        #     for i in range(len(nums))
-        # ])
+
+        # Change of plans: Make array that stores index of where current longest chain started
+        arr = [0]
+        prev = 0
+        for i in range(1, len(nums)):
+            if nums[i - 1] != nums[i] - 1:
+                prev = i
+            arr.append(prev)
+        # return arr
+        print(arr)
+
+        return [
+            nums[i]
+            if i - arr[i] + 1 >= k
+            else -1
+            for i in range(k - 1, len(nums))
+        ]
         
-        # answer = []
-        # n = len(nums)
-        # for i in range(n - k + 1):
-        #     length = self.getPowerArrayLengthFromIndex(i, nums)
-        #     if length >= k:
-        #         answer.append(nums[i] + k - 1)
-        #     else:
-        #         answer.append(-1)
-        # return answer
         return [
             (nums[i] + k - 1) 
             if self.getPowerArrayLengthFromIndex(i) >= k
