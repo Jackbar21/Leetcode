8f8271b302c555bf01252d87c813660bf2061889
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-15 17:30:15 -0800

    Time: 18 ms (34.04%) | Memory: 25.5 MB (5.94%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,23 +1,19 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-
-    def getPowerArrayLengthFromIndex(self, i, nums):
-        if i in self.memo:
-            return self.memo[i]
-
+    @cache
+    def getPowerArrayLengthFromIndex(self, i):
+        nums = self.nums
         if i >= len(nums) - 1:
             return 1
         
         res = 1
         if nums[i] == nums[i + 1] - 1:
-            res += self.getPowerArrayLengthFromIndex(i + 1, nums)
+            res += self.getPowerArrayLengthFromIndex(i + 1)
         
-        self.memo[i] = res
         return res
         
         
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        self.nums = nums
         # Idea: from each index, store the length of the LONGEST consecutive & sorted
         # sequence of elements STARTING at that index. Then from that point on, for each
         # index, we check if there is a sorted array of length AT LEAST k (and add its
@@ -38,27 +34,7 @@ class Solution:
         # return answer
         return [
             (nums[i] + k - 1) 
-            if self.getPowerArrayLengthFromIndex(i, nums) >= k
+            if self.getPowerArrayLengthFromIndex(i) >= k
             else -1
             for i in range(len(nums) - k + 1)
         ]
-
-
-        answer = []
-        n = len(nums)
-        for i in range(n - k + 1):
-            j = (i + k - 1)
-            # print(i, j, self.isSorted(i, j))
-            if self.isSorted(nums, i, j):
-                answer.append(nums[j])
-            else:
-                answer.append(-1)
-        
-        return answer
-    
-    def isSorted(self, nums, i, j):
-        for index in range(i, j):
-            if nums[index] != nums[index + 1] - 1:
-                return False
-        
-        return True
