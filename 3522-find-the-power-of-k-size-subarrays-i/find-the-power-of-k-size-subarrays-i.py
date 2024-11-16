8e0dcbfcdba51c8cedab89b04091a4f8d9c75266
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-15 17:50:29 -0800

    Time: 7 ms (57.45%) | Memory: 16.8 MB (21.46%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,28 +1,24 @@
 class Solution:      
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        # Base Case: k == 1 (each number represents power of its own length-1 array!)
+        if k == 1:
+            return nums
+
         # Idea: from each index, store the length of the LONGEST consecutive & sorted
         # sequence of elements STARTING at that index. Then from that point on, for each
         # index, we check if there is a sorted array of length AT LEAST k (and add its
         # power to answer result if so), or add -1 to answer array since not a power array.
 
         # Change of plans: Make array that stores index of where current longest chain started
-        if k == 1:
-            return nums
+        
+
         answer = []
-        arr = [0]
         prev = 0
         for i in range(1, len(nums)):
             if nums[i - 1] != nums[i] - 1:
                 prev = i
-            arr.append(prev)
 
             if i >= k - 1:
-                answer.append(nums[i] if (i - arr[-1] + 1) >= k else -1)
+                answer.append(nums[i] if (i - prev + 1) >= k else -1)
         
         return answer
-
-        return [
-            nums[i] if (i - arr[i] + 1) >= k else -1
-            for i in range(k - 1, len(nums))
-        ]
-        
