cf83133a530a051fedbe1bc18f2c8d4f8933336a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-15 17:31:01 -0800

    Time: 11 ms (39.89%) | Memory: 16.9 MB (5.94%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,6 +1,8 @@
 class Solution:
-    @cache
     def getPowerArrayLengthFromIndex(self, i):
+        if i in self.memo:
+            return self.memo[i]
+
         nums = self.nums
         if i >= len(nums) - 1:
             return 1
@@ -9,11 +11,13 @@ class Solution:
         if nums[i] == nums[i + 1] - 1:
             res += self.getPowerArrayLengthFromIndex(i + 1)
         
+        self.memo[i] = res
         return res
         
         
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         self.nums = nums
+        self.memo = {}
         # Idea: from each index, store the length of the LONGEST consecutive & sorted
         # sequence of elements STARTING at that index. Then from that point on, for each
         # index, we check if there is a sorted array of length AT LEAST k (and add its
