5c0c5633c355054f9e0ffe76324e55c1b9ff4abe
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-17 16:23:07 -0800

    Time: 4 ms (47.50%) | Memory: 16.8 MB (5.59%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -0,0 +1,74 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        if k == 0:
+            return [0] * len(code)
+        
+        return self.decryptPos(code, k) if k > 0 else self.decryptNeg(code, -k)
+    
+    def decryptPos(self, code, k):
+        new_code = []
+        cur_sum = 0
+        for i in range(k):
+            index = i % len(code)
+            cur_sum += code[i]
+
+        l, r = 0, ((k - 1) % len(code))
+        print(cur_sum, l, r)
+
+        for i in range(len(code)):
+            # Update cur_sum
+            print(f"{cur_sum=}")
+            cur_sum -= code[l]
+            print(f"{code[l]=}")
+            l += 1
+            l %= len(code)
+
+            r += 1
+            r %= len(code)
+            cur_sum += code[r]
+
+            # print(f"{cur_sum}")
+
+            # Modify with new value at index i
+            new_code.append(cur_sum)
+
+        return new_code
+
+
+
+
+    
+    def decryptNeg(self, code, k):
+        new_code = []
+        cur_sum = 0
+        index = float("inf")
+        for i in range(k):
+            index = (len(code) - 1) - (i % len(code))
+            cur_sum += code[index]
+
+        l, r = index, len(code) - 1
+        # l, r = len(code) - 1, len(code) - 1 - ((k + 1) % len(code))
+        # l, r = r, l
+        print(cur_sum, l, r)
+
+        for i in range(len(code)):
+            # Modify with new value at index i
+            new_code.append(cur_sum)
+            
+            # Update cur_sum
+            # print(f"{cur_sum=}")
+            cur_sum -= code[l]
+            # print(f"{code[l]=}")
+            l += 1
+            l %= len(code)
+
+            r += 1
+            r %= len(code)
+            cur_sum += code[r]
+
+            # print(f"{cur_sum}")
+
+            
+
+        return new_code
+
