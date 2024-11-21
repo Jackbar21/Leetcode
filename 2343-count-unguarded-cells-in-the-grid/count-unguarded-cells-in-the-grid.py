dc005838c02f11cb34a15f377739f74ff0ce656c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-20 19:08:24 -0800

    Time: 1678 ms (5.00%) | Memory: 67 MB (5.26%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -2,6 +2,7 @@ class Solution:
     def isValidPosition(self, x, y):
         return 0 <= x < self.m and 0 <= y < self.n and (x, y) not in self.walls_set
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        guards.sort(), walls.sort()
         guards_set = set((row, col) for row, col in guards)
         walls_set = set((row, col) for row, col in walls)
         self.walls_set = walls_set
