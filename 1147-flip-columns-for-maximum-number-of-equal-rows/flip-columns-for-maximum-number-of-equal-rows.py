978eceed868e8ee583152048b1fb96f29873eed6
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-21 17:03:24 -0800

    Time: 82 ms (32.41%) | Memory: 19.4 MB (32.16%) - LeetSync

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        # 0 ^ 1 == 1
+        # 1 ^ 1 == 0
+
+        # For each row, store:
+        # (1) its indices with value 0
+        # (2) its indices with value 1
+
+        # 0 0 0 |
+        # 0 0 1 |
+        # 1 1 0 |
+
+        # Always choose key where first digit/char is a '1'
+        d = defaultdict(int)
+
+        for row in matrix:
+            binary = (
+                ''.join(str(digit) for digit in row)
+                if row[0] == 1
+                else ''.join(str(1 - digit) for digit in row)
+            )
+            d[binary] += 1
+        
+        return max(d.values())
+
+
+
+
