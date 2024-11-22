a0ccc556b49038dd36f6093415742880856ad1b0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-21 17:07:39 -0800

    Time: 56 ms (72.22%) | Memory: 20.1 MB (23.12%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -9,8 +9,8 @@ class Solution:
                 if row[0] == 1
                 else tuple(digit ^ 1 for digit in row)
             )
-            new_val = d.get(binary, 0) + 1
-            res = max(res, new_val)
-            d[binary] = new_val
+            d[binary] = d.get(binary, 0) + 1
+            res = max(res, d[binary])
+            # d[binary] = new_val
         
         return res
