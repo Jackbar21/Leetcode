121dd74a205cc990aab402f3b8af210f51b12546
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-22 17:11:10 -0800

    Time: 1900 ms (58.54%) | Memory: 26.3 MB (100.00%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -0,0 +1,33 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        return zip(*[
+            self.applyHorizontalGravity(row)
+            for row in box
+        ][::-1])
+    
+    def applyHorizontalGravity(self, row):
+        queue = collections.deque()
+
+        STONE, OBSTACLE, EMPTY = '#', '*', '.'
+        for i in range(len(row) - 1, -1, -1):
+            item = row[i]
+            assert item in [STONE, OBSTACLE, EMPTY]
+            if item == EMPTY:
+                queue.append(i)
+                continue
+            
+            assert item in [STONE, OBSTACLE]
+            if item == OBSTACLE:
+                queue.clear() # TODO: check if re-initialization is faster
+                continue
+            
+            assert item == STONE
+            if len(queue) > 0:
+                index = queue.popleft()
+                assert row[index] == EMPTY
+                row[index] = STONE
+                assert row[i] == STONE
+                row[i] = EMPTY
+                queue.append(i)
+        
+        return row
