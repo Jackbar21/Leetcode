049ecfc67c4f885407df50537ad07687141d39ec
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-22 17:25:17 -0800

    Time: 1807 ms (99.68%) | Memory: 29.2 MB (19.83%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -1,17 +1,12 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        for row_index in range(len(box)):
-            self.applyHorizontalGravity(box, row_index)
-
-        return zip(*box[::-1])
         return zip(*[
-            self.applyHorizontalGravity(row)
-            for row in box
-        ][::-1])
+            self.applyHorizontalGravity(box[row_index])
+            for row_index in range(len(box) - 1, -1, -1)
+        ])
     
-    def applyHorizontalGravity(self, box, row_index):
+    def applyHorizontalGravity(self, row):
         queue = collections.deque()
-        row = box[row_index]
 
         STONE, OBSTACLE, EMPTY = '#', '*', '.'
         for i in range(len(row) - 1, -1, -1):
@@ -31,9 +26,47 @@ class Solution:
             if len(queue) > 0:
                 index = queue.popleft()
                 # assert row[index] == EMPTY
-                box[row_index][index] = STONE
+                row[index] = STONE
                 # assert row[i] == STONE
-                box[row_index][i] = EMPTY
+                row[i] = EMPTY
                 queue.append(i)
         
         return row
+
+
+# class Solution:
+#     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+#         for row_index in range(len(box)):
+#             self.applyHorizontalGravity(box, row_index)
+
+#         return zip(*box[::-1])
+
+    
+#     def applyHorizontalGravity(self, box, row_index):
+#         queue = collections.deque()
+#         row = box[row_index]
+
+#         STONE, OBSTACLE, EMPTY = '#', '*', '.'
+#         for i in range(len(row) - 1, -1, -1):
+#             item = row[i]
+#             # assert item in [STONE, OBSTACLE, EMPTY]
+#             if item == EMPTY:
+#                 queue.append(i)
+#                 continue
+            
+#             # assert item in [STONE, OBSTACLE]
+#             if item == OBSTACLE:
+#                 queue.clear() # TODO: check if re-initialization is faster
+#                 # queue = collections.deque()
+#                 continue
+            
+#             # assert item == STONE
+#             if len(queue) > 0:
+#                 index = queue.popleft()
+#                 # assert row[index] == EMPTY
+#                 box[row_index][index] = STONE
+#                 # assert row[i] == STONE
+#                 box[row_index][i] = EMPTY
+#                 queue.append(i)
+        
+#         return row
