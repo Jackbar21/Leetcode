1b04a898012100fd250135170ac5803d69430df3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-22 17:23:46 -0800

    Time: 1877 ms (76.58%) | Memory: 27.1 MB (96.90%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -1,34 +1,39 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        for row_index in range(len(box)):
+            self.applyHorizontalGravity(box, row_index)
+
+        return zip(*box[::-1])
         return zip(*[
             self.applyHorizontalGravity(row)
             for row in box
         ][::-1])
     
-    def applyHorizontalGravity(self, row):
+    def applyHorizontalGravity(self, box, row_index):
         queue = collections.deque()
+        row = box[row_index]
 
         STONE, OBSTACLE, EMPTY = '#', '*', '.'
         for i in range(len(row) - 1, -1, -1):
             item = row[i]
-            assert item in [STONE, OBSTACLE, EMPTY]
+            # assert item in [STONE, OBSTACLE, EMPTY]
             if item == EMPTY:
                 queue.append(i)
                 continue
             
-            assert item in [STONE, OBSTACLE]
+            # assert item in [STONE, OBSTACLE]
             if item == OBSTACLE:
                 queue.clear() # TODO: check if re-initialization is faster
                 # queue = collections.deque()
                 continue
             
-            assert item == STONE
+            # assert item == STONE
             if len(queue) > 0:
                 index = queue.popleft()
-                assert row[index] == EMPTY
-                row[index] = STONE
-                assert row[i] == STONE
-                row[i] = EMPTY
+                # assert row[index] == EMPTY
+                box[row_index][index] = STONE
+                # assert row[i] == STONE
+                box[row_index][i] = EMPTY
                 queue.append(i)
         
         return row
