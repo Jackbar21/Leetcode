56e702397f8a7134df675388eb83ae36bcb766b1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-24 15:52:18 -0800

    Time: 3 ms (71.12%) | Memory: 16.8 MB (7.87%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,22 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def listToNum(lst):
-            num, exponent = 0, 0
-            while lst:
-                num += lst.val * (pow(10, exponent))
-                lst = lst.next
-                exponent += 1
-            return num
-        
-        res = str(listToNum(l1) + listToNum(l2))[::-1]
-        cur = ListNode(0)
-        head = cur
-        for digit in res:
-            cur.next = ListNode(int(digit))
-            cur = cur.next
-        
-        return head.next
-                
+    def linkedListToNum(self, head):
+        res = 0
+        base = 1
+        while head:
+            res += head.val * base
+            base *= 10
 
+            # Loop Invariant
+            head = head.next
+        
+        return res
+    
+    def numToLinkedList(self, num):
+        str_num = str(num)
+
+        # Since we need the number to be backwards in the linked list,
+        # we can prepend the next digit to the list (instead of append)
+        # to obtain the final backward-digits order that we want :)
+        head = None
+        for digit in str_num:
+            node = ListNode(int(digit))
+            node.next = head
+            head = node
+        
+        return head
+
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1, num2 = self.linkedListToNum(l1), self.linkedListToNum(l2)
+        return self.numToLinkedList(num1 + num2)
