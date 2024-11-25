f1a06d4965ec46f2095dad06bbf49c51fc403d33
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-25 01:12:06 -0800

    Time: 155 ms (41.13%) | Memory: 77.3 MB (71.60%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,12 +1,11 @@
 class ListNode:
-    def __init__(self, key = 0, val = 0, next = None, prev = None):
+    def __init__(self, key = 0, val = 0):
         self.key = key
         self.val = val
-        self.next = next
-        self.prev = prev
+        self.next = None
+        self.prev = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         # Double linked list w/ dummy head & tail
         self.head = ListNode(-1, -1) # dummy
@@ -15,25 +14,22 @@ class LRUCache:
         self.tail.prev = self.head
 
         self.d = {} # maps keys to linked list nodes!
-        self.length = 0 # Length of linked list
         self.capacity = capacity # Maximum length
         
 
     def get(self, key: int) -> int:
-        # print(key, self.d)
-        # assert key in self.d
-        if key not in self.d:
+        node = self.d.get(key, None)
+        if not node:
             return -1
-        assert len(self.d) == self.length <= self.capacity
-        node = self.d[key]
+
         res = node.val
-        self.update(key) # update to MRU
+        self.update(key) # key is now most-recently-used!
         return res
         
 
     def put(self, key: int, value: int) -> None:
-        assert len(self.d) == self.length <= self.capacity
-        if key in self.d:
+        d = self.d
+        if key in d:
             node = self.d[key]
             node.val = value
             self.update(key)
@@ -41,46 +37,37 @@ class LRUCache:
 
         # Add node to tail & add to dict/increment length!
         node = ListNode(key, value)
-        last_node = self.tail.prev
+        tail = self.tail
+        last_node = tail.prev
         last_node.next = node
-        node.next = self.tail
-        self.tail.prev = node
+        node.next = tail
+        tail.prev = node
         node.prev = last_node
-        self.d[key] = node # Add node to dict!
-        self.length += 1
+        d[key] = node # Add node to dict!
 
         # Pop node from head if exceeds capacity
-        assert len(self.d) == self.length
-        if self.length > self.capacity:
-            assert self.length - 1 == self.capacity
+        if len(d) > self.capacity:
             first_node = self.head.next
-            first_node_key = first_node.key
+            del d[first_node.key]
             prev_node, next_node = first_node.prev, first_node.next
             prev_node.next = next_node
             next_node.prev = prev_node
-            assert first_node_key in self.d
-            del self.d[first_node_key]
-            self.length -= 1
-        
-        # self.update(key)
-    
+
+
     def update(self, key):
         # Update key to become most recently used inside linked list!
-        assert key in self.d
-        assert len(self.d) == self.length <= self.capacity
         node = self.d[key]
         prev_node, next_node = node.prev, node.next
         prev_node.next = next_node
         next_node.prev = prev_node
 
         # Now add it to end of linked list, i.e. as most-recently-used (MRU!)
-        last_node = self.tail.prev
+        tail = self.tail
+        last_node = tail.prev
         last_node.next = node
-        node.next = self.tail
-        self.tail.prev = node
+        node.next = tail
+        tail.prev = node
         node.prev = last_node
-        return
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
