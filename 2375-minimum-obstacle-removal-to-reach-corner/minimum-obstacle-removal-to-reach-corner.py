9d9a8f591b37a4e2fc5c50c2b4dfa1606e54d741
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-27 19:04:31 -0800

    Time: 1335 ms (67.90%) | Memory: 40.3 MB (90.30%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
@@ -1,12 +1,12 @@
 class Solution:
-    def minimumObstacles(self, grid: List[List[int]]) -> int:    
-        # O(m * n * log(m * n))
+    def minimumObstacles(self, grid: List[List[int]]) -> int:   
+        # UCS Algorithm - Time Complexity: O(m * n * log(m * n))
         m, n = len(grid), len(grid[0])
         GOAL_STATE = (m - 1, n - 1)
         EMPTY, OBSTACLE = 0, 1
         DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        # min heap / priority queue, (cost, (new_x, new_y))
+        # min heap / priority queue, (cost, position)
         fringe = [(0, (0, 0))]
         # Shoutout to Mr. 'notbalding' on YouTube for suggesting to use an array of
         # booleans (instead of a hashset) for keeping track of visited/unvisited nodes :)
@@ -28,4 +28,3 @@ class Solution:
                     heapq.heappush(fringe, (cost + is_obstacle, pos))
 
         # raise Exception("Unreachable Code")
-    
