748157ed09d95799a166a609df7ab6c3791d681c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-30 16:32:32 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,14 +1,16 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        seen = set(arr)
-        count_zeroes = arr.count(0)
+        # seen = set(arr)
+        # count_zeroes = arr.count(0)
+        seen = set()
+        count_zeroes = 0
+        for num in arr:
+            seen.add(num)
+            count_zeroes += num == 0
 
         for num in arr:
             double = 2 * num
-            if double in seen:
-                if double == 0 and count_zeroes <= 1:
-                    pass
-                else:
-                    return True
+            if double in seen and not (double == 0 and count_zeroes <= 1):
+                return True
         
         return False
