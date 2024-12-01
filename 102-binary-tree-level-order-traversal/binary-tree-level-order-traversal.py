e2e1be26346680ce98cfd1868e038b69b4504979
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-30 19:55:44 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.22%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -14,14 +14,14 @@ class Solution:
 
         queue = collections.deque([(root, 0)]) # (node, level)
         while len(queue) > 0:
-            node, level = queue.popleft()
+            node, level = queue.pop()
             level_to_values[level].append(node.val)
             if max_level < level:
                 max_level = level
             if node.left:
-                queue.append((node.left, level + 1))
+                queue.appendleft((node.left, level + 1))
             if node.right:
-                queue.append((node.right, level + 1))
+                queue.appendleft((node.right, level + 1))
 
 
         return [
