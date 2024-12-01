dc0aef0af3fba55a58b65a74b920be942135a971
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-30 22:53:56 -0800

    Time: 3 ms (35.41%) | Memory: 20.6 MB (8.26%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,32 +6,17 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # Idea: Simply do an inorder traversal from root, and return the k'th visited node's value!
-        # CHALLENGE: I'm gonna force myself to do this iteratively >:)
+        self.k = k
+        self.sorted_arr = []
+        self.inorder(root)
+        # print(f"{self.sorted_arr=}")
+        return self.sorted_arr[k - 1]
 
-        stack = [root]
-        nodes_visited = 0
-        while len(stack) > 0:
-            node = stack.pop()
-            left_node = node.left
-            if left_node:
-                # To allow for termination! 
-                # Another way to do this without modifying the tree
-                # would be by using a hash-set for one-time lookups :)
-                node.left = None 
+    def inorder(self, root):
+        if not root:
+            return
         
-                stack.append(node)
-                stack.append(left_node)
-                continue
-
-            # Inorder traversal!
-            assert not left_node
-
-            nodes_visited += 1
-            if nodes_visited == k:
-                return node.val
-            
-            if node.right:
-                stack.append(node.right)
-        
-        return -1
+        # Inorder Traversal!
+        self.inorder(root.left)
+        self.sorted_arr.append(root.val)
+        self.inorder(root.right)
