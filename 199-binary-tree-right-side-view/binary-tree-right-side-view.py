aa0ac4a78907202ee247d385aeea85a0cedde285
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-30 20:34:16 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (5.98%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -15,17 +15,21 @@ class Solution:
         queue = collections.deque([(root, 0)]) # (node, level)
         while len(queue) > 0:
             node, level = queue.popleft()
-            if node.left:
-                queue.append((node.left, level + 1))
             if node.right:
                 queue.append((node.right, level + 1))
+            if node.left:
+                queue.append((node.left, level + 1))
+            # if node.right:
+            #     queue.append((node.right, level + 1))
 
             # Since we add node.left before node.right for each node we pop
             # from the queue, whenever we traverse a node, we can be sure
             # it's the rightmost node we've seen AT ITS LEVEL! Hence, everytime
             # we see such a node (i.e. such as right now!), we will append the
             # value at key=level inside level_to_value dict with node's value :)
-            level_to_value[level] = node.val
+            # level_to_value[level] = node.val
+            if level not in level_to_value:
+                level_to_value[level] = node.val
             # if max_level < level:
             #     max_level = level
 
@@ -33,3 +37,10 @@ class Solution:
             level_to_value[level]
             for level in level_to_value
         ]
+
+# level=0: [1]
+# level=1: 3 [2,3]
+# level=2: [4]
+# level=3: [5]
+
+# [1, 3, 4, 5]
