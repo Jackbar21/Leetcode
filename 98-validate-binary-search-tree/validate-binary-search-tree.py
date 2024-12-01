c2b3455da9a36ca38eb5700e5b30492a2172e391
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-11-30 22:05:00 -0800

    Time: 3 ms (34.92%) | Memory: 19.3 MB (5.44%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,11 +6,9 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # return (self.isValidBSTHelper(root.left, root.val, root.val) 
-        # and self.isValidBSTHelper(root.right, root.val, root.val))
-        return self.isValidHelper(root, float("-inf"), float("inf"))
+        return self.isValid(root, float("-inf"), float("inf"))
     
-    def isValidHelper(self, root, lower_bound, upper_bound):
+    def isValid(self, root, lower_bound, upper_bound):
         if not root:
             return True
         
@@ -18,39 +16,6 @@ class Solution:
             return False
         
         return (
-            self.isValidHelper(root.left, lower_bound, min(upper_bound, root.val - 1))
-            and self.isValidHelper(root.right, max(lower_bound, root.val + 1), upper_bound)
+            self.isValid(root.left, lower_bound, min(upper_bound, root.val - 1))
+            and self.isValid(root.right, max(lower_bound, root.val + 1), upper_bound)
         )
-        
-    
-    def isValidBSTHelper(self, root, smallest, largest):
-        if not root:
-            return True
-        
-
-        
-        smallest = min(smallest, root.val)
-        largest = max(smallest, root.val)
-
-
-        
-        # if not root.left and not root.right:
-        #     return True
-        
-        if root.left:
-            if not (root.left.val < root.val):
-                return False
-            if root.left.val <= largest:
-                return False
-        
-        if root.right:
-            if not (root.right.val > root.val):
-                return False
-            if root.right.val >= smallest:
-                return False
-        
-        # Update smallest & largest
-        smallest = min(smallest, root.val)
-        largest = max(largest, root.val)
-        return (self.isValidBSTHelper(root.left, smallest, largest) 
-        and self.isValidBSTHelper(root.right, smallest, largest))
