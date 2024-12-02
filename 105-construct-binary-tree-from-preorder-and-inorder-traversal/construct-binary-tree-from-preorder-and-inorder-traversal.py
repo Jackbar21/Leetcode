4dbc0d7b5b3e9184afdcd2330574b2add1f896f1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-02 01:03:02 -0800

    Time: 95 ms (51.14%) | Memory: 18.3 MB (87.99%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,30 +8,24 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         self.preorder = collections.deque(preorder)
         self.inorder = inorder
-        return self.buildTreeHelper(0, inorder)
+        return self.buildTreeHelper(0, len(inorder) - 1)
     
-    def buildTreeHelper(self, preorder_index, inorder):
-        if len(self.preorder) == 0:
-            return None
-        
-        if len(inorder) == 0:
+    def buildTreeHelper(self, l, r):
+        # If either self.preorder or inorder is empty, then return None
+        if not self.preorder or l > r:
             return None
 
-        # preorder_val = self.preorder[preorder_index]
         preorder_val = self.preorder.popleft()
         node = TreeNode(preorder_val)
-        # print(f"{preorder_val=}")
-        # print(f"{inorder=}")
         
-        # assert preorder_val in self.inorder
-        index = inorder.index(preorder_val)
-        # print(f"{inorder[:index]=}")
-        # print(f"{inorder[index+1:]=}")
-        # print("")
-        # assert inorder_start <= index <= inorder_end
-        # assert index != float("inf")
-
+        # Find index of 'preorder_val' inside inorder from index 'l' to 'r' inclusive...
+        index = -1
+        for i in range(l, r + 1):
+            if self.inorder[i] == preorder_val:
+                index = i
+                break
+        # assert index != -1
         
-        node.left = self.buildTreeHelper(preorder_index + 1, inorder[:index])
-        node.right = self.buildTreeHelper(preorder_index + 2, inorder[index+1:])
+        node.left = self.buildTreeHelper(l, index - 1)
+        node.right = self.buildTreeHelper(index + 1, r)
         return node
