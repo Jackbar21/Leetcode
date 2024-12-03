4e3960742685eb94fcf025d957e33d154dce535b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-02 16:57:32 -0800

    Time: 91 ms (64.72%) | Memory: 50.7 MB (30.36%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
         chars = []
-        spaces = collections.deque(spaces)
+        # spaces = collections.deque(spaces)
+        spaces = spaces[::-1]
         cur_len = 0
 
         for i, char in enumerate(s):
-            if spaces[0] == i:
-                spaces.popleft()
+            if spaces[-1] == i:
+                spaces.pop()
                 chars.append(" ")
                 # Finish the problem early if no spaces left!
                 if len(spaces) == 0:
