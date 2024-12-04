86d5383075b7b9bfd6047f5c2271761b5db139f9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-03 19:59:36 -0800

    Time: 31 ms (98.61%) | Memory: 17.6 MB (31.28%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -8,62 +8,12 @@ class Solution:
             letter = alphabet[i]
             d[letter] = alphabet[i + 1]
         d['z'] = 'a'
-        # self.d = d
 
-        str1_index, str2_index = 0, 0
+        index, cur_letter, STR2_LENGTH = 0, str2[0], len(str2)
         for letter in str1:
-            if str2[str2_index] in [letter, d[letter]]:
-                str2_index += 1
-                if str2_index >= len(str2):
+            if cur_letter == letter or cur_letter == d[letter]:
+                index += 1
+                if index >= STR2_LENGTH:
                     return True
+                cur_letter = str2[index]
         return False
-
-
-        self.str1, self.str2 = str1, str2
-
-        self.is_subsequence = False
-        self.memo = {}
-        self.dp(0, 0)
-        return self.is_subsequence
-    
-    @cache
-    def dp(self, str1_index, str2_index):
-        # At any point if we've found a valid solution, terminate all dp calls!
-        if self.is_subsequence:
-            return True
-        
-        diff1 = len(self.str1) - str1_index
-        diff2 = len(self.str2) - str2_index
-        if diff1 < diff2:
-            return False
-
-        # if (str1_index, str2_index) in self.memo:
-        #     return self.memo[(str1_index, str2_index)]
-
-        if str2_index >= len(self.str2):
-            self.is_subsequence = True
-            return True
-        
-        if str1_index >= len(self.str1):
-            # self.memo[(str1_index, str2_index)] = False
-            return False
-
-        
-
-        # Case 2: Include character (only do this if ACTUALLY possible!)
-        case2 = False
-        letter = self.str1[str1_index]
-        if self.str2[str2_index] in [letter, self.d[letter]]:
-            if self.dp(str1_index + 1, str2_index + 1):
-                self.is_subsequence = True
-                return True
-        
-        # Case 1: Don't include character
-        case1 = self.dp(str1_index + 1, str2_index)
-        if case1:
-            self.is_subsequence = True
-            return True
-
-        
-        res = case1 or case2
-        return res
