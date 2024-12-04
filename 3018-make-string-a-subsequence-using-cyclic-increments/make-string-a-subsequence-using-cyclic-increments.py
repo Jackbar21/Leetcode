666b4513e7f8d2a953ced3eb71da3be83e47c862
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-03 20:02:26 -0800

    Time: 36 ms (98.61%) | Memory: 17.7 MB (31.28%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,14 +1,7 @@
 class Solution:
-    def getNextLetter(self, letter):
-        return self.d[letter]
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        d = {}
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        for i in range(len(alphabet) - 1):
-            letter = alphabet[i]
-            d[letter] = alphabet[i + 1]
-        d['z'] = 'a'
-
+        alphabet = "abcdefghijklmnopqrstuvwxyza" # Extra 'a' at end for cyclic property :)
+        d = {alphabet[i]: alphabet[i + 1] for i in range(len(alphabet) - 1)}
         index, cur_letter, STR2_LENGTH = 0, str2[0], len(str2)
         for letter in str1:
             if cur_letter == letter or cur_letter == d[letter]:
