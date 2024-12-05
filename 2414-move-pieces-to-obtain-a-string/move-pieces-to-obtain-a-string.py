95ae30f105a43a750b18adf66d3f61f4ed2e51c2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-04 16:58:18 -0800

    Time: 155 ms (49.19%) | Memory: 17.9 MB (37.72%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -1,5 +1,51 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
+        # Check same number of "L" and "R"s first!
+        start_count = len(start) - start.count("_")
+        target_count = len(target) - target.count("_")
+        if start_count != target_count:
+            return False
+        
+        # No letters to move!
+        if start_count == target_count == 0:
+            return True
+
+        l, r = 0, len(start) - 1
+        i, j = 0, len(target) - 1
+        while i <= j:
+            if l > r:
+                return False
+            
+            if start[l] == "_":
+                l += 1
+                continue
+            if start[r] == "_":
+                r -= 1
+                continue
+            if target[i] == "_":
+                i += 1
+                continue
+            if target[j] == "_":
+                j -= 1
+                continue
+            
+            s, t = start[l], target[i]
+            if s == t and ((s == "L" and l >= i) or (s == "R" and l <= i)):
+                l += 1
+                i += 1
+                continue
+            
+            s, t = start[r], target[j]
+            if s == t and ((s == "L" and r >= j) or (s == "R" and r <= j)):
+                r -= 1
+                j -= 1
+                continue
+            
+            # Permanently stuck!
+            return False
+
+        return True
+
         available = collections.deque((i, char) for i, char in enumerate(start) if char != "_")
         need = collections.deque((i, char) for i, char in enumerate(target) if char != "_")
         if len(available) != len(need):
