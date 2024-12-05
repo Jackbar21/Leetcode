53239202c2ff2d64cd613987d5d483ddff41ff76
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-04 16:43:58 -0800

    Time: 228 ms (16.13%) | Memory: 37.9 MB (14.37%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -2,18 +2,9 @@ class Solution:
     def canChange(self, start: str, target: str) -> bool:
         available = collections.deque((i, char) for i, char in enumerate(start) if char != "_")
         need = collections.deque((i, char) for i, char in enumerate(target) if char != "_")
-        # assert len(start) == len(target)
-        # n = len(start)
         if len(available) != len(need):
             return False
 
-        # for i in range(n):
-        #     if start[i] == target[i]:
-        #         continue
-        # # print(f"{available=}")
-        # # print(f"{need=}")
-
-        INDEX, PIECE = 0, 1
         while len(need) > 0:
             # See if can pop from left / right each time,
             # and at any point if stuck, then no solution!
@@ -41,5 +32,3 @@ class Solution:
 
             
         return True
-    
-    
