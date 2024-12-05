bc016da6d10a67cb18dc102f71997cec13807773
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-04 16:41:36 -0800

    Time: 383 ms (9.67%) | Memory: 42.5 MB (6.59%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -0,0 +1,48 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        available = collections.deque((i, char) for i, char in enumerate(start) if char != "_")
+        need = collections.deque((i, char) for i, char in enumerate(target) if char != "_")
+        # assert len(start) == len(target)
+        # n = len(start)
+        if len(available) != len(need):
+            return False
+
+        # for i in range(n):
+        #     if start[i] == target[i]:
+        #         continue
+        print(f"{available=}")
+        print(f"{need=}")
+
+        INDEX, PIECE = 0, 1
+        while len(need) > 0:
+            # See if can pop from left / right each time,
+            # and at any point if stuck, then no solution!
+            need_index, need_piece = need[0]
+            index, piece = available[0]
+            if (
+                (piece == need_piece and index == need_index) # Same pos!
+                or (piece == need_piece == "R" and index <= need_index) # Move Right!
+                or (piece == need_piece == "L" and need_index <= index) # Move Left!
+            ):
+                available.popleft()
+                need.popleft()
+                continue
+                # elif need_piece == "R" and index <= need_index:
+            
+            need_index, need_piece = need[-1]
+            index, piece = available[-1]
+            if (
+                (piece == need_piece and index == need_index) # Same pos!
+                or (piece == need_piece == "R" and index <= need_index) # Move Right!
+                or (piece == need_piece == "L" and need_index <= index) # Move Left!
+            ):
+                available.pop()
+                need.pop()
+                continue
+            
+            return False
+
+            
+        return True
+    
+    
