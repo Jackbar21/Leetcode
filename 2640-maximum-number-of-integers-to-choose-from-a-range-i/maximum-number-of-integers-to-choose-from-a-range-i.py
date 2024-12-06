96b3f70527fdd5edf3666195891292cddfd9fb56
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-05 16:38:41 -0800

    Time: 38 ms (88.16%) | Memory: 19.1 MB (7.35%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -4,9 +4,9 @@ class Solution:
 
         count = 0
         for num in range(1, n + 1):
-            if num > maxSum:
-                break
             if num not in banned_set:
+                if num > maxSum:
+                    break
                 maxSum -= num
                 count += 1
 
