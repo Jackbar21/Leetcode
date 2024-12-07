dc7e1f3f06f7b0666f90f5a668d17231e6f5a350
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-07 02:48:31 -0800

    Time: 163 ms (18.56%) | Memory: 19 MB (11.34%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -4,18 +4,18 @@ class Solution:
         for task in tasks:
             d[task] += 1
         
-        min_heap = [(-d[task], task) for task in d]
-        heapq.heapify(min_heap)
+        max_heap = [(-d[task], task) for task in d]
+        heapq.heapify(max_heap) # O(n)
 
         # res = []
         res = 0
         while len(d) > 0:
             new_tasks = []
             for _ in range(n + 1):
-                if len(min_heap) == 0:
+                if len(max_heap) == 0:
                     res += 1
                     continue
-                count, task = heapq.heappop(min_heap)
+                count, task = heapq.heappop(max_heap)
                 count = -count
                 # res.append(task)
                 res += 1
@@ -26,8 +26,8 @@ class Solution:
                         return res
                 else:
                     new_tasks.append((1 - count, task)) # -(count - 1) == 1 - count :)
-                    # heapq.heappush(min_heap, (1 - count, task))
+                    # heapq.heappush(max_heap, (1 - count, task))
             for item in new_tasks:
-                heapq.heappush(min_heap, item)
+                heapq.heappush(max_heap, item)
         
         return res
