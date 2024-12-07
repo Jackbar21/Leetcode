86f4c83eea320d76897935d6b46e5f533d7fb490
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-07 02:31:22 -0800

    Time: 381 ms (6.36%) | Memory: 21.4 MB (7.28%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.py
@@ -0,0 +1,36 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        d = defaultdict(int)
+        for task in tasks:
+            d[task] += 1
+        
+        min_heap = [(-d[task], task) for task in d]
+        heapq.heapify(min_heap)
+
+        res = []
+        while len(d) > 0:
+            print(f"{d=}")
+            print(f"{min_heap=}")
+            new_tasks = []
+            for _ in range(n + 1):
+                if len(min_heap) == 0:
+                    if len(d) == 0:
+                        break
+                    res.append("idle")
+                    continue
+                count, task = heapq.heappop(min_heap)
+                count = -count
+                res.append(task)
+                d[task] -= 1
+                if d[task] == 0:
+                    del d[task]
+                else:
+                    new_tasks.append((1 - count, task)) 
+            for item in new_tasks:
+                heapq.heappush(min_heap, item)
+        
+        print(f"{res=}")
+        return len(res)
+
+
+
