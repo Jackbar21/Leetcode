06bb8757f3dc553f445ba4c1bc183329cce0fdfb
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-06 16:42:54 -0800

    Time: 779 ms (40.53%) | Memory: 29.4 MB (26.36%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
@@ -6,20 +6,20 @@ class Solution:
         # larger the penalty, the less operations are needed and vice versa, we can simply 
         # binary search for the result :)
         l, r = 1, max(nums)
-        res = r # since 0 operations guaranteed to be <= maxOperations!
-        while l <= r:
+        # res = r # since 0 operations guaranteed to be <= maxOperations!
+        while l < r:
             mid = (l + r) // 2 # mid = penalty value itself!
             needed_operations = self.getNumOperationsToReachPenalty(nums, mid)
             if needed_operations <= maxOperations:
                 # Then we CAN indeed reach this penalty, so update our
                 # result, and now only search for potentially BETTER (i.e. smaller)
                 # penalties that might ALSO be valid!
-                res = mid
-                r = mid - 1
+                # res = mid
+                r = mid
             else:
                 l = mid + 1
 
-        return res
+        return r
     
     def getNumOperationsToReachPenalty(self, nums, max_penalty):
         # Want to compute how many times we'd need to split the bag to make it such that
