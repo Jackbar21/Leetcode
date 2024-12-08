be1ab56882f1ac9fc7515e74349fccada2316ef7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-07 17:51:45 -0800

    Time: 367 ms (27.67%) | Memory: 57.4 MB (61.44%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -3,28 +3,30 @@ class Solution:
         START, END, VALUE = 0, 1, 2
         events.sort(key = lambda event: event[END])
         prefix_values = []
-        max_value = float("-inf")
+        max_value = 0
         for event in events:
-            max_value = max(max_value, event[VALUE])
+            if event[VALUE] > max_value:
+                max_value = event[VALUE]
             prefix_values.append(max_value)
         
-        res = prefix_values[-1] # Exactly 1 interval case (since can pick AT MOST 2!)
+        res = max_value # Exactly 1 interval case (since can pick AT MOST 2!)
         for event in events:
-            max_value = float("-inf")
+            max_val = 0
             start, _, value = event
+            
             l, r = 0, len(events) - 1
             while l <= r:
                 mid = (l + r) // 2
                 # Want to find largest end time such that it is SMALLER than start,
                 # and grab its prefix_values to get max value out of all smaller
                 # non-overlapping intervals!
-                mid_event = events[mid]
-                if mid_event[END] < start:
-                    max_value = max(max_value, prefix_values[mid])
+                if events[mid][END] < start:
+                    max_val = prefix_values[mid]
                     l = mid + 1
                 else:
                     r = mid - 1
             
-            res = max(res, value + max_value)
+            if value + max_val > res:
+                res = value + max_val
 
         return res
