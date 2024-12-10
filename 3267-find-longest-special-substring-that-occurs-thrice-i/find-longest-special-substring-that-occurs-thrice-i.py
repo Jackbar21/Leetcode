845043ce44242b244f944581770df8a13c66dbf0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-09 19:15:29 -0800

    Time: 3 ms (99.73%) | Memory: 17.2 MB (17.44%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,59 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        # Example:  "zzzz"
+        # One       4-length
+        # Two       3-lenghts
+        # Three     2-lenghts
+        # Four      1-length
+
+        # "aaazzzzaaabaaa"
+
+        # [] (char (i.e. 'z'), length, count)
+        d = defaultdict(int) # (char, length): count
+
+        cur_letter = s[0]
+        l = 0
+        for r in range(len(s)):
+            if s[r] == cur_letter:
+                continue # keep incrementing r (if not at the end, i.e. if possible!)
+            
+            # Otherwise, the letters no longer match
+            # So, s[l..r-1] is of same letter, so we have
+            # (r-1)-l-1 == r - l many characters that are the same
+            # in a row, leaving us with 1 permutation of length r - l,
+            # two permutation of length r - l - 1, three permutation of length
+            # r - l - 2, ..., r - l permutations of length 1. So, add these all
+            # to a resulting dictionary!
+            length = r - l
+            count = 1
+            while l < r:
+                d[(cur_letter, length)] += count
+
+                # Loop Invariant
+                length -= 1
+                count += 1
+                l += 1
+            
+            # Update cur_letter to new letter!
+            cur_letter = s[r]
+        
+        # print(f"{l=}")
+        # print(f"{d=}")
+        # TODO: Might need one last iteration of l w/ r=len(nums)-1 here (in case of continue@end)
+        r = len(s) - 1
+        length = r - l + 1
+        count = 1
+        while l <= r: # <= here since last character counts!
+            d[(cur_letter, length)] += count
+
+            # Loop Invariant
+            length -= 1
+            count += 1
+            l += 1
+        
+        res = -1
+        for (_, length), value in d.items():
+            if value >= 3 and length > res:
+                res = length
+        return res
+
