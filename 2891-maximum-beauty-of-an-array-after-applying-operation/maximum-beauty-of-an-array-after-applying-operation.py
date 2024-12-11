b51e5a3cac673407d109230d6fd9580230e655c9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-10 18:10:50 -0800

    Time: 885 ms (14.52%) | Memory: 65.4 MB (5.09%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,22 +1,6 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-
-        # Base Case: if k == 0, then beauty is just current beauty in nums! 
-        # We can do a simple sliding window search to figure that out :)
-        # if k == 0:
-        #     res = 1
-        #     l = 0
-        #     cur_num = nums[0]
-        #     for r in range(len(nums)):
-        #         if nums[r] == cur_num:
-        #             continue
-                
-        #         # Otherwise, we have that only nums[l..r-1] is "beautiful". 
-        #         res = max(res, r - l)
-        #         l = r
-
-        #     return max(res, len(nums) - l)
-
+        START, END = 0, 1
         # Idea: Now that I realize this problem is asking for SUBSEQUENCE and not SUBARRAY,
         # this becomes a lot more intuitive :) This problem SCREAMS to me DP, but while I was
         # trying to come up with a solution for SUBARRAY version, one idea that came natural to
@@ -26,7 +10,6 @@ class Solution:
         # AT ALL in sorted the elements!
 
         # Step 1: convert nums into intervals!
-
         intervals = []
         lower, upper = float("inf"), float("-inf") # global interval bounds!
 
@@ -34,22 +17,14 @@ class Solution:
         for num in nums:
             left, right = num - k, num + k
             intervals.append((left - normal_scalar, right - normal_scalar))
-            # if left < lower:
-            #     lower = left
-            # if right > upper:
-            #     upper = right
-
-        START, END = 0, 1
         intervals.sort(key = lambda interval: interval[END])
 
+        # O(n + k)
         length = max(nums) + k - normal_scalar + 1
         line_sweep = [[0, 0] for _ in range(length)] # (start, end)
         for start, end in intervals:
             line_sweep[start][START] += 1
             line_sweep[end][END] += 1
-        
-        # print(f"{intervals=}")
-        # print(f"{line_sweep=}")
 
         res = 0
         cur_overlap = 0
@@ -58,7 +33,5 @@ class Solution:
             if cur_overlap > res:
                 res = cur_overlap
             cur_overlap -= end
-
-            # print(f"{start=}, {end=}, {cur_overlap=}, {res=}")
 
         return res
