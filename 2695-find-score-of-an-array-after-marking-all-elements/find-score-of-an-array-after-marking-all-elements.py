a86cb1e57860bda78feb2b38eb9c68349f79d393
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-12 16:37:59 -0800

    Time: 335 ms (83.64%) | Memory: 43.9 MB (27.65%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -1,14 +1,8 @@
 class Solution:
     def findScore(self, nums: List[int]) -> int:
         marked = set()
-        min_heap = [(num, i) for i, num in enumerate(nums)]
-
-        NUM, INDEX = 0, 1
-        sorted_nums = sorted(min_heap)
-        marked = set()
+        sorted_nums = sorted((num, i) for i, num in enumerate(nums))
         score = 0
-        i = 0
-        N = len(sorted_nums)
         for num, i in sorted_nums:
             if i in marked:
                 continue
@@ -22,22 +16,3 @@ class Solution:
             marked.add(i + 1)
 
         return score
-            
-            
-
-        heapq.heapify(min_heap)
-        score = 0
-        while len(min_heap) > 0:
-            num, i = heapq.heappop(min_heap)
-            if i in marked:
-                continue
-            score += num
-            for index in range(i - 1, i + 2):
-                marked.add(index)
-            # marked.add(i)
-            # marked.add(i - 1)
-            # marked.add(i + 1)
-            
-        return score
-
-        
