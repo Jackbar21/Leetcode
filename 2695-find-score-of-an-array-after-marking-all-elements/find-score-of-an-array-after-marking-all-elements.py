e313df3d4fa1dda3065fcf87e812ef2a1b4fabca
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-12 16:37:05 -0800

    Time: 332 ms (83.64%) | Memory: 44.5 MB (23.15%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -2,6 +2,29 @@ class Solution:
     def findScore(self, nums: List[int]) -> int:
         marked = set()
         min_heap = [(num, i) for i, num in enumerate(nums)]
+
+        NUM, INDEX = 0, 1
+        sorted_nums = sorted(min_heap)
+        marked = set()
+        score = 0
+        i = 0
+        N = len(sorted_nums)
+        for num, i in sorted_nums:
+            if i in marked:
+                continue
+            
+            # Add the value of the chosen integer to score
+            score += num
+            
+            # Mark the chosen element and its two adjacent elements if they exist
+            marked.add(i - 1)
+            marked.add(i)
+            marked.add(i + 1)
+
+        return score
+            
+            
+
         heapq.heapify(min_heap)
         score = 0
         while len(min_heap) > 0:
