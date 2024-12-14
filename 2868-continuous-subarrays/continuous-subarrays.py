510331dab63bbe90323336e86d65774421fc5abc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-13 21:27:35 -0800

    Time: 743 ms (37.95%) | Memory: 27.5 MB (28.65%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.py
+++ 2868-continuous-subarrays/continuous-subarrays.py
@@ -14,7 +14,6 @@ class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
         d = defaultdict(int)
         count = 0
-        double_count = 0
 
         l = 0
         for r, num in enumerate(nums):
@@ -23,25 +22,23 @@ class Solution:
             if self.isValidDict(d):
                 continue
             
-            double_count += 1
             # nums[l..r] is invalid, but nums[l..r-1] is valid! Therefore, it must be that
             # EVERY non-empty subarray within nums[l..r-1] is ALSO valid! We know the length
             # of this array is (r-1)-l+1 == r-l, and so total number of subarrays will be
             # r-l subarrays of length 1 + r-l-1 subarrays of length 2 + ... + 1 subarray of
             # length r - l == r-l + r-l-1 + ... + 1 == (r-l)(r-l+1)/2, by math :)
-            n = (r - l)
-            length = n
-            # count += (n * (n + 1)) // 2
+            # However, to avoid double counting, only append to count as we augment l!
+            length = r - l
             while not self.isValidDict(d):
                 l_num = nums[l]
                 d[l_num] -= 1
-                assert d[l_num] >= 0
+                # assert d[l_num] >= 0
                 if d[l_num] == 0:
                     del d[l_num]
                 l += 1
                 count += length
                 length -= 1
-                assert length >= 0
+                # assert length >= 0
 
             continue
         
