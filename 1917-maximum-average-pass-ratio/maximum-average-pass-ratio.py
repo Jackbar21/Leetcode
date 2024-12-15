597aa83298f670ce20a14ac016bb697ee87dd099
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-14 17:23:57 -0800

    Time: 1102 ms (56.00%) | Memory: 54.6 MB (23.46%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        # (avg, pass_i, total_i)
-        min_heap = [(-(((passing+1)/(total+1)) - (passing / total)), passing, total) for passing, total in classes] 
+        # (increase in avg if you pick this value next, pass, total)
+        min_heap = [((p/t) - ((p+1)/(t+1)), p, t) for p, t in classes]
         heapq.heapify(min_heap)
-        # print(f"{min_heap=}")
 
         for _ in range(extraStudents):
             _, passing, total = heapq.heappop(min_heap)
@@ -12,8 +11,6 @@ class Solution:
             total += 1
             new_ratio_increase = ((passing+1)/(total+1)) - (passing/total)
             heapq.heappush(min_heap, (-new_ratio_increase, passing, total))
-            # print(f"{min_heap=}")
         
-        # print(f"{min_heap}=")
-        return sum(item[1]/item[2] for item in min_heap) / len(min_heap)
+        return sum(passing/total for _, passing, total in min_heap) / len(min_heap)
         
