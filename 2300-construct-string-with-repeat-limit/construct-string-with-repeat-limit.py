6234895128cc93c7d3f71497ad90f725bcbc3919
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-16 17:11:01 -0800

    Time: 257 ms (42.39%) | Memory: 19.9 MB (6.99%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -1,5 +1,5 @@
 class Solution:
-    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+    def repeatLimitedStringOld(self, s: str, repeatLimit: int) -> str:
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         alphabet_backwards = alphabet[::-1]
         d = defaultdict(int)
@@ -51,4 +51,40 @@ class Solution:
             d[letter] -= 1
             cur_streak = 1
         
+        return "".join(res)
+
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        d = defaultdict(int)
+        for letter in s:
+            d[letter] += 1
+        
+        max_heap = [] # (-ord(letter), letter, count) --> first item used for comparisons!
+        for letter, count in d.items():
+            heapq.heappush(max_heap, (-ord(letter), letter, count))
+        
+        res = []
+        while len(max_heap) > 0:
+            comp_val, letter, count = heapq.heappop(max_heap)
+            add_count = min(count, repeatLimit)
+            res.append(letter * add_count)
+            count -= add_count
+            if count == 0:
+                continue # No more of that letter, so no need to fret about repeatLimit!
+
+            # Now need to add next largest letter. If none, then break!
+            if len(max_heap) == 0:
+                break
+            
+            other_comp_val, other_letter, other_count = heapq.heappop(max_heap)
+            res.append(other_letter)
+            other_count -= 1
+
+            # Add letters back into heap with new counts!
+            assert count > 0
+            heapq.heappush(max_heap, (comp_val, letter, count))
+            if other_count > 0:
+                heapq.heappush(max_heap, (other_comp_val, other_letter, other_count))
+
+        
+        print(f"{res=}")
         return "".join(res)
