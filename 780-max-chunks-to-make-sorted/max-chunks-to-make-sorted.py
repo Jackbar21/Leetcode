95754b6c07522698ef1764a856769c5c66fb183d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-18 16:43:41 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.45%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        prefix_max = []
-        max_num = float("-inf")
-        for num in arr:
-            max_num = max(max_num, num)
-            prefix_max.append(max_num)
+        # Needed for DP!
+        # prefix_max = []
+        # max_num = float("-inf")
+        # for num in arr:
+        #     max_num = max(max_num, num)
+        #     prefix_max.append(max_num)
         
         suffix_min = collections.deque()
         min_num = float("inf")
@@ -15,20 +16,25 @@ class Solution:
         # print(f"{prefix_max=}")
         # print(f"{suffix_min=}")
         # return 0
-        self.prefix_max = prefix_max
+        # self.prefix_max = prefix_max # Needed for DP!
         self.suffix_min = suffix_min
-        self.nums = arr
-        max_splits = self.dp(0, len(arr) - 1)
-        max_chunks = max_splits + 1
-        return max_chunks
-        return self.greedy(0)
-    
-    def greedy(i, cur_max = float("-inf")):
-        if i >= len(self.nums):
-            return 0
-    
-        if self.prefix_max[i] <= self.suffix_min[i + 1]:
-            pass
+        # max_splits = self.dp(0, len(arr) - 1)
+        # max_chunks = max_splits + 1
+        # return max_chunks
+        # return self.greedy(0)
+
+        # Greedy approach even better than DP!
+        res = 1 # Base case is just array itself as 1 and only chunk!
+        cur_max = float("-inf")
+        for i, num in enumerate(arr):
+            if i == len(arr) - 1:
+                continue
+
+            cur_max = max(cur_max, num)
+            if cur_max <= suffix_min[i + 1]:
+                res += 1
+                cur_max = float("-inf")
+        return res
     
     @cache
     def dp(self, i, j):
@@ -37,6 +43,5 @@ class Solution:
             if self.prefix_max[k] <= self.suffix_min[k + 1]:
                 valid_case = 1 + self.dp(i, k) + self.dp(k + 1, j)
                 res = max(res, valid_case)
-                return res
         
         return res
