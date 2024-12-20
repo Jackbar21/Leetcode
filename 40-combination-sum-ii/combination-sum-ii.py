0c2d81c9c781c9b6199224d646f955b051208add
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-19 18:26:31 -0800

    Time: 107 ms (5.05%) | Memory: 18.6 MB (8.86%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,73 +1,31 @@
-
-
-# [], target = 5
-
-# [], target = 5
-# [2], target = 3
-
-# [], target = 5
-# [5], target = 0 ding ding ding
-# [2], target = 3
-# [2,5], target = -2
 class Solution:
-    def __init__(self):
-        self.memo = {} # cache (i, target), i represents candidates[:i]
-        self.candidates = None
-        self.target = -1
-        self.solutions = {} # contains arrays mapping to sums
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        # self.candidates, self.target = candidates, target
-        # return self.combinationSumHelper(len(candidates), target)
-        candidates.sort()
-        solutions = set()
-        i = 0
-        current_solution = []
-        ban_list = defaultdict(int)
-        def backtrackHelper(i, current_sum):
-            if current_sum == target:
-                solutions.add(tuple(current_solution))
-            if i >= len(candidates) or current_sum > target:
-                return
-
-            # print(current_solution)
-
-            current_solution.append(candidates[i])
-            if current_sum + candidates[i] <= target and ban_list.get(candidates[i], 0) <= 0:
-                backtrackHelper(i + 1, current_sum + candidates[i])
-            current_solution.pop()
-            ban_list[candidates[i]] += 1
-            backtrackHelper(i + 1, current_sum)
-            ban_list[(candidates[i])] -= 1
-            return
-
-        backtrackHelper(0, 0)
-        return solutions
-
-
-
+        self.candidates = candidates
+        self.target = target
+        self.res = set()
+        self.memo = set()
+        self.backtrack(0, 0, [])
+        return list(self.res)
     
-
-    # def combinationSumHelper(self, i, target):
-    #     if target < 0:
-    #         return []
+    def backtrack(self, i: int, cur_sum: int, res: list) -> None:        
+        tuple_res = tuple(res)
+        if (tuple_res, cur_sum) in self.memo:
+            return
+        if cur_sum == self.target:
+            self.res.add(tuple(sorted(res)))
+            return
         
-    #     if target == 0:
-
+        # No more possible solutions!
+        if cur_sum > self.target or i >= len(self.candidates):
+            return
         
-    #     if (i, target) in self.memo:
-    #         return self.memo[(i, target)]
+        # Case 1: Include num at index i (CAN ONLY DO THIS ONCE!)
+        candidate = self.candidates[i]
+        res.append(candidate)
+        self.backtrack(i + 1, cur_sum + candidate, res)
+        res.pop()
 
-    #     # Case 1: Include the i'th element
-    #     #candidates[:i] which we've solved
-    #     target - candidates[i]
-    #     self.combinationSumHelper(i - 1)
+        # Case 2: Don't include num at index i
+        self.backtrack(i + 1, cur_sum, res)
 
-
-
-    #     # Case 2: Don't include the i'th element
-        
-
-    #     #candidates[:-1], target
-    #     #candidates[:-1], target - candidates[0]
-
-    #     return self.memo[(i, target)]
+        self.memo.add((tuple_res, cur_sum))
