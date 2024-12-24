5c93e1dfb0e0dda946df2f279748af68f389d83d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-24 15:12:15 -0800

    Time: 139 ms (5.04%) | Memory: 46.3 MB (5.05%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -10,13 +10,15 @@ class Solution:
             return True
 
         partitions = []
+        res = []
         def backtrack(i):
             # Base Case: If i >= len(s), then we want to check if every partition
             # is a palindrome!
             if i >= len(s):
                 strings = ["".join(partition) for partition in partitions]
                 if all(isPalindrome(string) for string in strings):
-                    yield strings
+                    # yield strings
+                    res.append(strings)
                 return
 
             letter = s[i]
@@ -25,12 +27,14 @@ class Solution:
             # (Can only do this if I have at least one partition in the first place!)
             if len(partitions) > 0:
                 partitions[-1].append(letter)
-                yield from backtrack(i + 1)
+                backtrack(i + 1)
                 partitions[-1].pop()
 
             # Case 2: add letter at index i into new partition
             partitions.append([letter])
-            yield from backtrack(i + 1)
+            backtrack(i + 1)
             partitions.pop()
         
-        return list(backtrack(0))
+        # return list(backtrack(0))
+        backtrack(0)
+        return res
