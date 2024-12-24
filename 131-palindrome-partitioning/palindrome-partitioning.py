00e9ec7c016903d949b719723942dfa500ac1f57
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-24 15:09:11 -0800

    Time: 178 ms (5.04%) | Memory: 41.7 MB (5.05%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+    def partition(self, s: str) -> List[List[str]]:
+        def backtrack(i, partitions = []):
+            # Base Case: If i >= len(s), then we want to check if every partition
+            # is a palindrome!
+            if i >= len(s):
+                strings = ["".join(partition) for partition in partitions]
+                if all(self.isPalindrome(string) for string in strings):
+                    yield strings
+                return
+
+            letter = s[i]
+
+            # Case 1: add letter at index i into last partition
+            # (Can only do this if I have at least one partition in the first place!)
+            if len(partitions) > 0:
+                partitions[-1].append(letter)
+                yield from backtrack(i + 1, partitions)
+                partitions[-1].pop()
+
+            # Case 2: add letter at index i into new partition
+            partitions.append([letter])
+            yield from backtrack(i + 1, partitions)
+            partitions.pop()
+        
+        def backtrackEnumerator():
+            yield from backtrack(0)
+        
+        return list(backtrackEnumerator())
