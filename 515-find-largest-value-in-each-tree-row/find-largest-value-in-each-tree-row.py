6d266f0ec0d074a0feb132526882f93944717783
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-24 16:20:46 -0800

    Time: 7 ms (6.59%) | Memory: 20 MB (6.38%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -11,16 +11,14 @@ class Solution:
 
         levels = {}
         queue = collections.deque([(root, 1)]) # (node, level)
-        max_level = 0
         while len(queue) > 0:
             node, level = queue.popleft()
-            max_level = max(max_level, level)
             levels[level] = max(node.val, levels.get(level, float("-inf")))
             if node.left:
                 queue.append((node.left, level + 1))
             if node.right:
                 queue.append((node.right, level + 1))
         
-        return list(levels[level] for level in range(1, max_level + 1))
+        return list(levels.values())
 
 
