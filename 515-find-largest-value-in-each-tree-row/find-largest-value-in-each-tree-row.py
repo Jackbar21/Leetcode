803728d06171e3148453f036914ffa36ac78d0e5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-24 16:14:54 -0800

    Time: 3 ms (48.85%) | Memory: 19.8 MB (8.63%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -5,31 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.d = {}
-        self.maxDepth = 0
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        # idea: create a dictionary of elements at each depth,
-        # and instead of constructing a minheap at each level
-        # and popping the smallest one from each depth for total
-        # complexity of O(nlogn), just keep a simple array and
-        # get max naively from each depth level which will be
-        # total of O(n). Sometimes, simple is better!
-        # EDIT: With inorder traversal, we know the max is the
-        # last of the array. Still O(n), but even better!!
-        # EDIT 2: Nevermind, this is not a BST lol...
         if not root:
             return []
-        self.generateValues(root, 0)
-        return [max(self.d[key]) for key in range(self.maxDepth+1)]
-    def generateValues(self, root, depth):
-        if not root:
-            return
+
+        levels = {}
+        queue = collections.deque([(root, 1)]) # (node, level)
+        max_level = 0
+        while len(queue) > 0:
+            node, level = queue.popleft()
+            max_level = max(max_level, level)
+            levels[level] = max(node.val, levels.get(level, float("-inf")))
+            if node.left:
+                queue.append((node.left, level + 1))
+            if node.right:
+                queue.append((node.right, level + 1))
         
-        # inorder traversal
-        self.generateValues(root.left, depth+1)
-        self.maxDepth = max(self.maxDepth, depth)
-        if depth not in self.d:
-            self.d[depth] = []
-        self.d[depth].append(root.val)
-        self.generateValues(root.right, depth+1)
+        return list(levels[level] for level in range(1, max_level + 1))
+
+
