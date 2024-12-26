891818e14df9f7c559f1a01e195f48fae2eeff49
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-25 16:29:24 -0800

    Time: 192 ms (30.41%) | Memory: 18.4 MB (58.81%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -2,10 +2,14 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         self.nums = nums
         self.target = target
+        self.memo = {}
         return self.dp(0, 0)
     
-    @cache
+    # @cache
     def dp(self, i, cur_sum):
+        if (i, cur_sum) in self.memo:
+            return self.memo[(i, cur_sum)]
+    
         if i >= len(self.nums):
             return cur_sum == self.target
         
@@ -16,5 +20,7 @@ class Solution:
 
         # Case 2: Add a '-' before num
         case2 = self.dp(i + 1, cur_sum - num)
-
-        return case1 + case2
+    
+        res = case1 + case2
+        self.memo[(i, cur_sum)] = res
+        return res
