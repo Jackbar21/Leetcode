70b347efdae8af6763e0a56a53668655cfb4bced
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-25 19:02:57 -0800

    Time: 647 ms (98.66%) | Memory: 52.6 MB (81.23%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,37 @@
+class WordDictionary:
+    def __init__(self):
+        self.trie = {}
+
+    def addWord(self, word: str) -> None:
+        trie = self.trie
+        for letter in word:
+            if letter not in trie:
+                trie[letter] = {}
+            trie = trie[letter]
+        trie["is_word"] = () # INTENTIONALLY EMPTY!!!
+        
+    def search(self, word: str) -> bool:
+        return self.searchWord(word, 0, self.trie)
+
+    def searchWord(self, word, i, trie):
+        if i >= len(word):
+            assert i == len(word)
+            return "is_word" in trie
+        
+        letter = word[i]
+        if letter != ".":
+            if letter not in trie:
+                return False
+            return self.searchWord(word, i + 1, trie[letter])
+        
+        assert letter == "."
+        for key in trie:
+            if self.searchWord(word, i + 1, trie[key]):
+                return True
+        return False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
