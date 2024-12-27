1157f1c4c0af054c8393494d156ce6d19ff5bb63
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-26 16:47:52 -0800

    Time: 320 ms (5.12%) | Memory: 24.5 MB (5.12%) - LeetSync

---
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        # max_val = float("-inf")
+        # for i in range(len(values)):
+        #     for j in range(i + 1, len(values)):
+        #         max_val = max(max_val , values[i] + values[j] + i - j)
+        # return max_val
+        # self.memo = {}
+        # return self.dp(0, len(values) - 1)
+
+        cur_max = float("-inf")
+        cur_dist = 0
+        suffix_max = collections.deque([cur_max])
+        for r in range(len(values) - 1, -1, -1):
+            cur_max = max(cur_max, values[r] - r)
+            suffix_max.appendleft(cur_max)
+
+        suffix_max.popleft()
+        print(f"{suffix_max=}")
+        # return 0
+
+        res = float("-inf")
+        for i in range(len(values)):
+            res = max(res, values[i] + i + suffix_max[i])
+            print(f"{res=}")
+        return res
+
+        max_val = float("-inf")
+        l, r = 0, len(values) - 1
+        while l < r:
+            max_val = max()
+    
+    # def dp(self, i, j):
+    #     if (i, j) in self.memo:
+    #         return self.memo[(i, j)]
+        
+
+        
+
+        return self.memo[(i, j)]
