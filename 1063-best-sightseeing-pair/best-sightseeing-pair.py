9fa3a366da07f8a9b82f63b2bf0811e40120a4db
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-26 17:06:09 -0800

    Time: 47 ms (95.37%) | Memory: 23.3 MB (20.49%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         cur_max = float("-inf")
-        suffix_max = collections.deque([])
+        suffix_max = []
         for j in range(len(values) - 1, 0, -1):
             new_val = values[j] - j
             if new_val > cur_max:
                 cur_max = new_val
-            suffix_max.appendleft(cur_max)
+            suffix_max.append(cur_max)
 
         res = float("-inf")
         for i in range(len(values) - 1):
-            new_val = values[i] + i + suffix_max[i]
+            new_val = values[i] + i + suffix_max[len(values) - 2 - i]
             if new_val > res:
                 res = new_val
         return res
