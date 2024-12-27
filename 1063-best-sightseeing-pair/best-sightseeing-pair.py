309e4200459f5adc4c99a0a48f8a283106618f0c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-26 16:50:02 -0800

    Time: 115 ms (6.10%) | Memory: 23 MB (31.71%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,26 +1,17 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        # max_val = float("-inf")
-        # for i in range(len(values)):
-        #     for j in range(i + 1, len(values)):
-        #         max_val = max(max_val , values[i] + values[j] + i - j)
-        # return max_val
-        # self.memo = {}
-        # return self.dp(0, len(values) - 1)
-
         cur_max = float("-inf")
-        cur_dist = 0
         suffix_max = collections.deque([cur_max])
         for r in range(len(values) - 1, -1, -1):
-            cur_max = max(cur_max, values[r] - r)
+            new_val = values[r] - r
+            if new_val > cur_max:
+                cur_max = new_val
             suffix_max.appendleft(cur_max)
-
         suffix_max.popleft()
-        # print(f"{suffix_max=}")
-        # return 0
 
         res = float("-inf")
         for i in range(len(values)):
-            res = max(res, values[i] + i + suffix_max[i])
-            # print(f"{res=}")
+            new_val = values[i] + i + suffix_max[i]
+            if new_val > res:
+                res = new_val
         return res
