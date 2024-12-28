9d9ee1c067aae986f85bd8c8ee90e4d7590c25c9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-27 17:43:50 -0800

    Time: 306 ms (6.11%) | Memory: 31.4 MB (12.50%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.py
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.py
@@ -19,39 +19,41 @@ class Solution:
         return self.fastDp(0, 3)
     
     def getSumFromIndices(self, indices):
-        # assert len(indices) == 3
         return sum(self.prefix_sums[index] for index in indices)
     
-    @cache
     def fastDp(self, i, indices_left):
         assert indices_left >= 0
         if indices_left == 0:
             return []
 
         if i > len(self.nums) - self.k:
-            return float("-inf") # since indices_left > 3
+            return None # since indices_left > 3
+        
+        if (i, indices_left) in self.memo:
+            return self.memo[(i, indices_left)]
         
         # Case 1: Choose index i
         case1 = self.fastDp(i + self.k, indices_left - 1)
-        case1_sum = case1
-        if case1 != float("-inf"):
+        case1_sum = 0
+        if case1 is not None:
             case1 = [i] + case1
             case1_sum = self.getSumFromIndices(case1)
 
         # Case 2: Skip index i
         case2 = self.fastDp(i + 1, indices_left)
-        case2_sum = case2 if case2 == float("-inf") else self.getSumFromIndices(case2)
+        case2_sum = 0 if case2 is None else self.getSumFromIndices(case2)
 
         if case1_sum > case2_sum:
-            # self.memo[(i, indices_left)] = case1
-            # return tuple(case1)
+            self.memo[(i, indices_left)] = case1
             return case1
         elif case2_sum > case1_sum:
-            # self.memo[(i, indices_left)] = case2
+            self.memo[(i, indices_left)] = case2
             return case2
         
         # Otherwise, since they are equal, we must return the lexicographically smallest one
-        return case1 if case1 < case2 else case2
+        res = case1 if case2 is None else case2 if case1 is None else case1 if case1 < case2 else case2
+        self.memo[(i, indices_left)] = res
+        return res
 
     
     def dp(self, i, indices):
