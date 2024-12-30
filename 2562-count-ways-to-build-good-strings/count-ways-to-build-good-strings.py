6f5b4afeb96ae94847e80d57c4b14c369cc92d0f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-29 17:46:09 -0800

    Time: 924 ms (22.15%) | Memory: 686.4 MB (6.17%) - LeetSync

---
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        MOD = pow(10, 9) + 7
+        self.low, self.high = low, high
+        self.zero, self.one = zero, one
+        self.memo = {}
+        return self.dp(0) % MOD
+    
+    def dp(self, cur_len):
+        if cur_len > self.high:
+            return 0
+        
+        if cur_len in self.memo:
+            return self.memo[cur_len]
+        
+        is_valid = self.low <= cur_len <= self.high
+        
+        # Case 1: Append '0' zero times
+        case1 = self.dp(cur_len + self.zero)
+
+        # Case 2: Append '1' one times
+        case2 = self.dp(cur_len + self.one)
+
+        res = is_valid + case1 + case2
+        self.memo[cur_len] = res
+        return res
