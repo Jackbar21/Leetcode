4eadf21c10c9bfade51c4249642602ac27da03c4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-01 16:25:52 -0800

    Time: 15 ms (93.12%) | Memory: 50.4 MB (15.48%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -3,12 +3,14 @@ class Solution:
         return word[0] in self.vowels and word[-1] in self.vowels
 
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        self.vowels = set(['a', 'e', 'i', 'o', 'u'])
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        # self.vowels = vowels
 
         prefix_sums = [] # prefix_sums[i] == sum(self.isValid(word) for word in words[0..i])
         count = 0
         for word in words:
-            count += self.isValid(word)
+            # count += self.isValid(word)
+            count += word[0] in vowels and word[-1] in vowels
             prefix_sums.append(count)
         
         # Since prefix_sums[i] == sum(self.isValid(word) for word in words[0..i]), notice that
@@ -20,9 +22,12 @@ class Solution:
         #      + sum(self.isValid(word) for word in words[i..j])
         #      - sum(self.isValid(word) for word in words[0..i-1])  
         #   == sum(self.isValid(word) for word in words[i..j]) 
-        ans = []
-        for l, r in queries:
-            answer = prefix_sums[r] - (prefix_sums[l - 1] if l > 0 else 0)
-            ans.append(answer)
-        
-        return ans
+        # ans = []
+        # for l, r in queries:
+        #     answer = prefix_sums[r] - (prefix_sums[l - 1] if l > 0 else 0)
+        #     ans.append(answer)
+        # return ans
+        return [
+            prefix_sums[r] - (prefix_sums[l - 1] if l > 0 else 0)
+            for l, r in queries
+        ]
