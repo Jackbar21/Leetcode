611bbc331e54366bd6c15e758ba01410a8f26c88
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-02 17:09:03 -0800

    Time: 63 ms (56.01%) | Memory: 33.6 MB (5.90%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -9,6 +9,7 @@ class Solution:
             cur_sum += num
             prefix_sums.append(cur_sum)
         self.prefix_sums = prefix_sums
+        nums_sum = cur_sum
 
         res = 0
         for i in range(len(nums) - 1):
@@ -17,6 +18,7 @@ class Solution:
             # if must be that nums[i+1..len(nums)-1] is non-empty, further implying
             # that i+1 must be less-than-or-equal-to len(nums)-1, i.e. i <= len(nums) - 2
             # Hence, why we don't consider the case for i being len(nums) - 1 directly.
-            res += self.getSubarraySum(0, i) >= self.getSubarraySum(i + 1, len(nums) - 1)
+            # res += self.getSubarraySum(0, i) >= self.getSubarraySum(i + 1, len(nums) - 1)
+            res += prefix_sums[i] >= nums_sum - (prefix_sums[i] if i+1 > 0 else 0)
         
         return res
