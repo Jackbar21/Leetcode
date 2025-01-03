4bd5c7f66ff55bbbcaf55c69c121fa6185d0d184
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-02 17:05:56 -0800

    Time: 179 ms (5.01%) | Memory: 33.4 MB (10.24%) - LeetSync

---
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getSubarraySum(self, i, j):
+        return self.prefix_sums[j] - (self.prefix_sums[i - 1] if i > 0 else 0)
+
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        prefix_sums = [] # prefix_sums[i] == sum(nums[0..i])
+        cur_sum = 0
+        for num in nums:
+            cur_sum += num
+            prefix_sums.append(cur_sum)
+        self.prefix_sums = prefix_sums
+
+        res = 0
+        for i in range(len(nums) - 1):
+            # split into nums[0..i] | nums[i+1..len(nums)-1]
+            # to guarantee that there is at least one element to the right of i,
+            # if must be that nums[i+1..len(nums)-1] is non-empty, further implying
+            # that i+1 must be less-than-or-equal-to len(nums)-1, i.e. i <= len(nums) - 2
+            # Hence, why we don't consider the case for i being len(nums) - 1 directly.
+            res += self.getSubarraySum(0, i) >= self.getSubarraySum(i + 1, len(nums) - 1)
+        
+        return res
