448396f294e4460a85c2f1bf73c7bbb68dd4edd5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-03 18:27:45 -0800

    Time: 302 ms (73.41%) | Memory: 127.5 MB (5.27%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -11,13 +11,10 @@ class Solution:
                 border_indices[letter][RIGHTMOST] = i
         
         prefix_freq = []
-        # freq_dict = {}
-        freq_arr = [0] * 26
+        freq_dict = {}
         for letter in s:
-            # freq_dict[letter] = freq_dict.get(letter, 0) + 1
-            # prefix_freq.append(freq_dict.copy()) # O(1) work, since at most 26 keys :)
-            freq_arr[ord(letter) - ORD_A] += 1
-            prefix_freq.append(freq_arr.copy())
+            freq_dict[letter] = freq_dict.get(letter, 0) + 1
+            prefix_freq.append(freq_dict.copy()) # O(1) work, since at most 26 keys :)
         
         res = 0
         for letter in alphabet:
@@ -28,12 +25,8 @@ class Solution:
             
             l = border_indices[letter][LEFTMOST] + 1
             r = border_indices[letter][RIGHTMOST] - 1
-            # di, dj = prefix_freq[l - 1], prefix_freq[r] # l > 0 always!!!
-            # for key in dj:
-            #     res += dj[key] > di.get(key, 0)
-            ai, aj = prefix_freq[l - 1], prefix_freq[r]
-            # res += sum(aj[idx] > ai[idx] for idx in range(26))
-            for idx in range(26):
-                res += aj[idx] > ai[idx]
+            di, dj = prefix_freq[l - 1], prefix_freq[r] # l > 0 always!!!
+            for key in dj:
+                res += dj[key] > di.get(key, 0)
 
         return res
