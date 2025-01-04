5d18e2cdeb58725ea7c27a133b560e01145b0536
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-03 18:26:17 -0800

    Time: 444 ms (72.09%) | Memory: 49.2 MB (5.27%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,5 +1,6 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
+        ORD_A = ord("a")
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         border_indices = {letter: [None, None] for letter in alphabet}
         LEFTMOST, RIGHTMOST = 0, 1
@@ -10,11 +11,14 @@ class Solution:
                 border_indices[letter][RIGHTMOST] = i
         
         prefix_freq = []
-        freq_dict = {}
+        # freq_dict = {}
+        freq_arr = [0] * 26
         for letter in s:
-            freq_dict[letter] = freq_dict.get(letter, 0) + 1
-            prefix_freq.append(freq_dict.copy()) # O(1) work, since at most 26 keys :)
-
+            # freq_dict[letter] = freq_dict.get(letter, 0) + 1
+            # prefix_freq.append(freq_dict.copy()) # O(1) work, since at most 26 keys :)
+            freq_arr[ord(letter) - ORD_A] += 1
+            prefix_freq.append(freq_arr.copy())
+        
         res = 0
         for letter in alphabet:
             if border_indices[letter][RIGHTMOST] is None:
@@ -24,8 +28,10 @@ class Solution:
             
             l = border_indices[letter][LEFTMOST] + 1
             r = border_indices[letter][RIGHTMOST] - 1
-            di, dj = prefix_freq[l - 1], prefix_freq[r] # l > 0 always!!!
-            for key in dj:
-                res += dj[key] > di.get(key, 0)
+            # di, dj = prefix_freq[l - 1], prefix_freq[r] # l > 0 always!!!
+            # for key in dj:
+            #     res += dj[key] > di.get(key, 0)
+            ai, aj = prefix_freq[l - 1], prefix_freq[r]
+            res += sum(aj[idx] > ai[idx] for idx in range(26))
 
         return res
