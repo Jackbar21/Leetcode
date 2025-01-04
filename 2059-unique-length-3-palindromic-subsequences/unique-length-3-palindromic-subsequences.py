e3fa8caf43315d2ea2f2dc169551ad2a29217145
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-03 18:22:46 -0800

    Time: 322 ms (73.19%) | Memory: 127.6 MB (5.27%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,19 +1,4 @@
 class Solution:
-    def getSubarrayFrequencies(self, i, j):
-        di, dj = self.prefix_freq[i - 1] if i > 0 else {}, self.prefix_freq[j]
-        return {
-            key: dj[key] - di.get(key, 0) # TODO: Consider not using defaultdict
-            for key in dj
-        }
-    
-    def getSubarrayFrequenciesCount(self, i, j):
-        di, dj = self.prefix_freq[i - 1], self.prefix_freq[j]
-        # return {
-        #     key: dj[key] - di.get(key, 0) # TODO: Consider not using defaultdict
-        #     for key in dj
-        # }
-        return sum(dj[key] > di.get(key, 0) for key in dj)
-
     def countPalindromicSubsequence(self, s: str) -> int:
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         border_indices = {letter: [None, None] for letter in alphabet}
@@ -29,7 +14,6 @@ class Solution:
         for letter in s:
             freq_dict[letter] = freq_dict.get(letter, 0) + 1
             prefix_freq.append(freq_dict.copy()) # O(1) work, since at most 26 keys :)
-        # self.prefix_freq = prefix_freq
 
         res = 0
         for letter in alphabet:
@@ -38,18 +22,10 @@ class Solution:
                 # 3 with it!!!
                 continue
             
-            # assert border_indices[letter][LEFTMOST] is not None
             l = border_indices[letter][LEFTMOST] + 1
             r = border_indices[letter][RIGHTMOST] - 1
-            # d = self.getSubarrayFrequencies(l, r)
-            # count = sum(val > 0 for val in d.values())
-            di, dj = prefix_freq[l - 1], prefix_freq[r]
-            # count = self.getSubarrayFrequenciesCount(l, r)
+            di, dj = prefix_freq[l - 1], prefix_freq[r] # l > 0 always!!!
             for key in dj:
                 res += dj[key] > di.get(key, 0)
-            # count = sum(dj[key] > di.get(key, 0) for key in dj)
-            # res += count
 
-        # print(f"{prefix_freq=}")
-        # # print(f"{border_indices=}")
         return res
