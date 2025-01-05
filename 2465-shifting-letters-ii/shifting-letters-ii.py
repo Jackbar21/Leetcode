d275bbe5193790cae4f58cc4381a366e7e15c7f3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-04 20:18:16 -0800

    Time: 55 ms (82.98%) | Memory: 41.4 MB (30.09%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,26 +1,19 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        ORD_A = ord("a")
-        mutateLetter = lambda letter, delta: alphabet[(ord(letter) - ORD_A + delta) % 26]
-        # val = self.mutateLetter("a", 0)
-        # print(f"{val=}")
-        # return ""
+        ALPHABET, ORD_A = "abcdefghijklmnopqrstuvwxyz", ord("a")
+        mutateLetter = lambda letter, delta: ALPHABET[(ord(letter) - ORD_A + delta) % 26]
         line_sweep = [0] * (len(s) + 1)
         for start, end, direction in shifts:
             l, r = (-1, 1) if direction == 0 else (1, -1)
             line_sweep[start] += l
             line_sweep[end + 1] += r
         
-        # print(f"{line_sweep=}")
         mutate = 0
-        new_s = []
+        new_s = [0] * len(s)
         for i in range(len(s)):
             mutate += line_sweep[i]
             mutated_letter = mutateLetter(s[i], mutate)
-            new_s.append(mutated_letter)
-
-
+            new_s[i] = mutated_letter
 
         # TODO: Modify new_s based on line_sweep
         return "".join(new_s)
