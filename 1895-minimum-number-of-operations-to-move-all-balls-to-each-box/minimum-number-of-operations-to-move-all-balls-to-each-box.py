2b4fdf593a58b3c6821cf129061ec462151927e4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-05 23:48:46 -0800

    Time: 10 ms (70.68%) | Memory: 18.2 MB (16.05%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -2,18 +2,13 @@ class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         N = len(boxes)
 
-        prefix_sums = [0]
-        count = 0
-        for digit in boxes:
-            count += digit == "1"
-            prefix_sums.append(prefix_sums[-1] + count)
-        # prefix_sums.pop()
-        
-        suffix_sums = [0]
-        count = 0
-        for digit in reversed(boxes):
-            count += digit == "1"
-            suffix_sums.append(suffix_sums[-1] + count)
-        # suffix_sums.pop()
+        prefix_sums, suffix_sums = [0], [0]
+        prefix_count, suffix_count = 0, 0
+        for i in range(N):
+            prefix_count += boxes[i] == "1"
+            prefix_sums.append(prefix_sums[-1] + prefix_count)
+
+            suffix_count += boxes[N - i - 1] == "1"
+            suffix_sums.append(suffix_sums[-1] + suffix_count)
 
         return [prefix_sums[i] + suffix_sums[N - 1 - i] for i in range(N)]
