af5c0fc297d29e223722fd1d1e64d9a7aef9cb8d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-05 23:49:26 -0800

    Time: 3 ms (99.54%) | Memory: 18.1 MB (23.30%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -4,11 +4,11 @@ class Solution:
 
         prefix_sums, suffix_sums = [0], [0]
         prefix_count, suffix_count = 0, 0
-        for i in range(N):
-            prefix_count += boxes[i] == "1"
+        for digit in boxes:
+            prefix_count += digit == "1"
             prefix_sums.append(prefix_sums[-1] + prefix_count)
-
-            suffix_count += boxes[N - i - 1] == "1"
+        for digit in reversed(boxes):
+            suffix_count += digit == "1"
             suffix_sums.append(suffix_sums[-1] + suffix_count)
 
         return [prefix_sums[i] + suffix_sums[N - 1 - i] for i in range(N)]
