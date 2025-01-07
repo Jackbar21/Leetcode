5af2c59ee45428219f03443f9c9feed19a4e69fd
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-06 17:34:54 -0800

    Time: 4 ms (28.54%) | Memory: 17.7 MB (19.52%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -3,8 +3,13 @@ class Solution:
         N = len(words)
         res = set()
         for i in range(N):
-            for j in range(N):
+            for j in range(i + 1, N):
+                if i == j:
+                    continue
                 word1, word2 = words[i], words[j]
-                if i != j and word1 in word2:
+                if word1 in word2:
                     res.add(word1)
+                if word2 in word1:
+                    res.add(word2)
+
         return list(res)
