218187eefea91b65a0032772345da343b0e6ec3b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-07 22:49:31 -0800

    Time: 40 ms (64.13%) | Memory: 18.1 MB (26.33%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -13,20 +13,20 @@ class Solution:
             return None
         val_to_node = {}
 
-        queue = collections.deque([node])
+        queue = [node]
         visited = set([node])
         while len(queue) > 0:
-            original_node = queue.popleft()
+            original_node = queue.pop()
             val = original_node.val
             if val not in val_to_node:
                 val_to_node[val] = Node(val)
             clone_node = val_to_node[original_node.val]
 
             for neighbor in original_node.neighbors:
-                val = neighbor.val
-                if val not in val_to_node:
-                    val_to_node[val] = Node(val)
-                clone_neighbor = val_to_node[neighbor.val]
+                neighbor_val = neighbor.val
+                if neighbor_val not in val_to_node:
+                    val_to_node[neighbor_val] = Node(neighbor_val)
+                clone_neighbor = val_to_node[neighbor_val]
                 clone_node.neighbors.append(clone_neighbor)
 
                 if neighbor not in visited:
