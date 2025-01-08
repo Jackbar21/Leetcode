4cfef56914ffca9626d9000575a00f4962c0e494
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-07 21:58:59 -0800

    Time: 15 ms (90.90%) | Memory: 17.8 MB (62.27%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -6,7 +6,6 @@ class Solution:
         WATER, LAND = 0, 1
 
         max_area = 0
-        # visited = set()
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == WATER:
@@ -15,7 +14,6 @@ class Solution:
                 area = 0
                 queue = collections.deque()
                 queue.append((i, j))
-                # visited.add((i, j))
                 grid[i][j] = WATER
                 while len(queue) > 0:
                     x, y = queue.popleft()
@@ -23,12 +21,9 @@ class Solution:
 
                     for dx, dy in DIRECTIONS:
                         new_x, new_y = x + dx, y + dy
-                        if (0 <= new_x < M and 
-                            0 <= new_y < N and 
-                            grid[new_x][new_y] == LAND #and 
-                            # (new_x, new_y) not in visited
+                        if (0 <= new_x < M and 0 <= new_y < N 
+                            and grid[new_x][new_y] == LAND
                         ):
-                            # visited.add((new_x, new_y))
                             grid[new_x][new_y] = WATER
                             queue.append((new_x, new_y))
                 
