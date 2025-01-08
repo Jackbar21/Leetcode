2188ce2de594f6a504ba4d51589cc2c6adb8db24
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-07 22:41:28 -0800

    Time: 51 ms (7.13%) | Memory: 18.4 MB (5.26%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,10 +11,11 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if node is None:
             return None
-
-        val_to_node = {
-            val: Node(val, set()) for val in range(0, 100 + 1)
-        }
+        
+        # val_to_node = {
+        #     val: Node(val, set()) for val in range(0, 100 + 1)
+        # }
+        val_to_node = {}
         
         # if node is not None:
         #     print(f"{node.val=}, {node.neighbors=}")
@@ -23,16 +24,32 @@ class Solution:
         # I will need to perform two traversals of this graph. One time to populate
         # all of the cloned nodes, and another time to populate all the appropriate neighbors!
 
-        # Step 1: 
+        # Step 1: Traverse the graph to figure out how many nodes there are. We can leverage
+        # # the fact that all of them are labeled 1, 2, ..., N, where N is the number of nodes!
+        # val_to_node = {
+        #     val: Node(val, set()) for val in range(0, 100 + 1)
+        # }
+        # val_to_node = {}
+        # queue = collections.deque([node])
+        # visited = set([node])
+        # while len(queue) > 0:
+        #     original_node = queue.popleft()
+
 
         # Step 2: Populate all the neighbors!
         queue = collections.deque([node])
         visited = set([node])
         while len(queue) > 0:
             original_node = queue.popleft()
+            val = original_node.val
+            if val not in val_to_node:
+                val_to_node[val] = Node(val, set())
             clone_node = val_to_node[original_node.val]
 
             for neighbor in original_node.neighbors:
+                val = neighbor.val
+                if val not in val_to_node:
+                    val_to_node[val] = Node(val, set())
                 clone_node.neighbors.add(val_to_node[neighbor.val])
                 if neighbor not in visited:
                     visited.add(neighbor)
