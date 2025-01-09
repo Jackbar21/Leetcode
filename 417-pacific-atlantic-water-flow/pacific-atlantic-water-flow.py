6b4511dc6b7a9238fc2cbc8a864c7a1180febffa
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-08 23:32:15 -0800

    Time: 36 ms (47.97%) | Memory: 18.8 MB (57.70%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -4,42 +4,38 @@ class Solution:
         RIGHT, DOWN, LEFT, UP = (0, 1), (1, 0), (0, -1), (-1, 0)
         DIRECTIONS = [RIGHT, DOWN, LEFT, UP]
         inBounds = lambda x, y: 0 <= x < M and 0 <= y < N
-
-        # Populate all the cells that can reach the Pacific Ocean
-        # First, we know all the cells where i == 0 or j == 0 will yield 'True'
-        # Then we want to do all the cells where i == 1 or j == 1, then i == 2 or j == 2
-        res = set()
-        for i in range(M):
-            for j in range(N):
-                # Do a BFS traversal and keep track of whenever a border-square is reached
-                # (indicating that an ocean can be reach)
-                reach_pacific, reach_atlantic = False, False
-                queue = collections.deque([(i, j)])
-                visited = set([(i, j)])
-                while len(queue) > 0:
-                    x, y = queue.popleft()
-
-                    if x == 0 or y == 0:
-                        reach_pacific = True
-                    
-                    if x == M - 1 or y == N - 1:
-                        reach_atlantic = True
-
-                    if reach_pacific and reach_atlantic:
-                        res.add((i, j))
-                        break
-                    
-                    height = heights[x][y]
-                    for dx, dy in DIRECTIONS:
-                        neigh_x, neigh_y = x + dx, y + dy
-
-                        if (inBounds(neigh_x, neigh_y)
-                            and (neigh_x, neigh_y) not in visited
-                            and heights[neigh_x][neigh_y] <= height
-                        ):
-                            visited.add((neigh_x, neigh_y))
-                            queue.append((neigh_x, neigh_y))
         
-        return list(res)
-                    
+        def bfs(queue):
+            visited = set(queue)
 
+            while len(queue) > 0:
+                x, y = queue.popleft()
+                height = heights[x][y]
+
+                for dx, dy in DIRECTIONS:
+                    neigh_x, neigh_y = x + dx, y + dy
+
+                    if (inBounds(neigh_x, neigh_y)
+                        and (neigh_x, neigh_y) not in visited
+                        and heights[neigh_x][neigh_y] >= height # INVERTING the problem!
+                    ):
+                        visited.add((neigh_x, neigh_y))
+                        queue.append((neigh_x, neigh_y))
+
+            return visited
+
+        pacific_queue = collections.deque()
+        for i in range(M):
+            pacific_queue.append((i, 0))
+        for j in range(N):
+            pacific_queue.append((0, j))
+        pacific_reachable = bfs(pacific_queue)
+
+        atlantic_queue = collections.deque()
+        for i in range(M):
+            atlantic_queue.append((i, N - 1))
+        for j in range(N):
+            atlantic_queue.append((M - 1, j))
+        atlantic_reachable = bfs(atlantic_queue)
+
+        return list(pacific_reachable.intersection(atlantic_reachable))
