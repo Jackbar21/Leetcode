6e0071b50985d63af22f8ed1cc51b0ad375d9fbe
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-10 17:12:06 -0800

    Time: 7 ms (26.35%) | Memory: 18.6 MB (8.04%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -25,7 +25,7 @@ class Solution:
             adj_list[v].remove(u)
 
             # Step 2: Pick a random node, perform dfs, and return edge if forms tree!
-            node = random.randint(1, N)
+            node = u
             stack = [node] # dfs
             visited = set([node])
             while len(stack) > 0:
