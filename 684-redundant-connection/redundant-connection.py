821c2b766fd68ed2787ed8f594119866b909e0f0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-10 17:08:54 -0800

    Time: 11 ms (19.87%) | Memory: 18.5 MB (13.98%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        return self.naive(edges)
+
+    def naive(self, edges):
+        adj_list = defaultdict(set)
+        for u, v in edges:
+            adj_list[u].add(v)
+            adj_list[v].add(u)
+        
+        # Since there are no repeated edges, and it is an UNDIRECTED graph,
+        # length of adj_list must be the number of nodes IN graph (i.e. each
+        # node is connected by at least ONE edge, and therefore in adj_list)
+        N = len(adj_list)
+
+        # Try removing the last edge in edges, and everytime checking if the graph
+        # forms a tree. We can check if the graph is a tree by picking a random node,
+        # performing a dfs/bfs traversal from that node, and checking if every other
+        # node was indeed reachable in the graph (in similar spirit to the "Number of 
+        # Connected Components In An Undirected Graph" problem).
+        for u, v in reversed(edges):
+            # Step 1: Remove the edge
+            adj_list[u].remove(v)
+            adj_list[v].remove(u)
+
+            # Step 2: Pick a random node, perform dfs, and return edge if forms tree!
+            node = random.randint(1, N)
+            stack = [node] # dfs
+            visited = set([node])
+            while len(stack) > 0:
+                node = stack.pop()
+                for neigh in adj_list[node]:
+                    if neigh not in visited:
+                        visited.add(neigh)
+                        stack.append(neigh)
+            if len(visited) == N:
+                return (u, v)
+
+            # Step 3: Add the edge back
+            adj_list[u].add(v)
+            adj_list[v].add(u)
+        
+        raise Exception("No Solution")
