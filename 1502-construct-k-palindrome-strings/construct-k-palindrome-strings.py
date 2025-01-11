d35a6a368a14b4c8ebf80b1c2de4e0b1900f1009
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-10 16:36:00 -0800

    Time: 51 ms (25.11%) | Memory: 18.1 MB (34.36%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,6 +1,5 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         d = defaultdict(int)
-        for letter in s:
-            d[letter] += 1
+        for letter in s: d[letter] += 1
         return len(s) >= k and sum(map(lambda freq: freq % 2, d.values())) <= k
