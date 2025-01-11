86e4038b7925c3ad9ce18b547e26dc56f4b2f5a4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-10 16:30:32 -0800

    Time: 43 ms (32.16%) | Memory: 18.1 MB (16.30%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,6 +1,15 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+
         ORD_A, frequencies = ord("a"), [0] * 26
         for letter in s:
             frequencies[ord(letter) - ORD_A] += 1
-        return len(s) >= k and sum(freq % 2 for freq in frequencies) <= k
+        
+        odd_count = 0
+        for freq in frequencies:
+            odd_count += freq % 2
+            if odd_count > k:
+                return False
+        return True
