306db442e08f10a99c2698602058126bf23ca040
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-10 16:36:51 -0800

    Time: 55 ms (20.26%) | Memory: 18 MB (43.17%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,5 +1,5 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        d = {letter: 0 for letter in "abcdefghijklmnopqrstuvwxyz"}
+        d = defaultdict(int)
         for letter in s: d[letter] += 1
         return len(s) >= k and sum(map(lambda freq: freq % 2, d.values())) <= k
