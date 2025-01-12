251946b8aa234773bcb2cadae1b9b870d4985593
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-11 22:03:54 -0800

    Time: 66 ms (33.50%) | Memory: 20.8 MB (5.22%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,25 +1,13 @@
 class Solution:
-    @cache
-    def dp(self, node, edges_left):
-        if node == self.dst:
-            return 0
-
-        if edges_left == 0:
-            return float("inf")
-        
-        res = float("inf")
-        for neigh, price in self.adj_list[node]:
-            path_cost = price + self.dp(neigh, edges_left - 1)
-            if path_cost < res:
-                res = path_cost
-        return res
-
-
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adj_list = defaultdict(list)
         for source, dest, price in flights:
             adj_list[source].append((dest, price)) # (neigh, price)
-        self.dst, self.adj_list = dst, adj_list
 
-        res = self.dp(src, k + 1)
+        @cache
+        def dp(node, edges_left):
+            return (0 if node == dst else float("inf") if edges_left == 0 else min(
+                (price + dp(neigh, edges_left - 1) for neigh, price in adj_list[node]), 
+                default = float("inf")))
+        res = dp(src, k + 1)
         return res if res != float("inf") else -1
