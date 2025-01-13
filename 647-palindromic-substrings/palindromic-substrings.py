4cef7518276f38394821cae5f186797fb8f332ad
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-13 01:11:23 -0800

    Time: 142 ms (40.14%) | Memory: 17.9 MB (29.06%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,14 +1,40 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        self.s, self.memo = s, {}
-        res = 0
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                res += self.dp(i, j)
+        # 2D Dynamic Programming Solution!
+        # self.s, self.memo = s, {}
+        # res = 0
+        # for i in range(len(s)):
+        #     for j in range(i, len(s)):
+        #         res += self.dp2D(i, j)
+        # return res
+
+        # 1D Dynamic Programming Solution!
+        res = 0 # Number of palindromic substrings!
+        N = len(s)
+        for center_index in range(N):
+            # Case 1: Odd length palindrome
+            l, r = center_index, center_index
+            res += 1
+            while l >= 1 and r < N - 1 and s[l - 1] == s[r + 1]:
+                l -= 1
+                r += 1
+                res += 1
+            
+            # Case 2: Even length palindrome
+            l, r = center_index, center_index + 1
+            if r == N or s[l] != s[r]:
+                continue
+            res += 1
+            while l >= 1 and r < N - 1 and s[l - 1] == s[r + 1]:
+                l -= 1
+                r += 1
+                res += 1
+        
         return res
+
     
-    # dp(i, j) returns True if and only if s[i..j] is a palindrome! Otherwise, returns False
-    def dp(self, i, j):
+    # dp2D(i, j) returns True if and only if s[i..j] is a palindrome! Otherwise, returns False
+    def dp2D(self, i, j):
         if (i, j) in self.memo:
             return self.memo[(i, j)]
         
@@ -23,6 +49,6 @@ class Solution:
         if self.s[i] != self.s[j]:
             return False
         
-        res = self.dp(i + 1, j - 1)
+        res = self.dp2D(i + 1, j - 1)
         self.memo[(i, j)] = res
         return res
