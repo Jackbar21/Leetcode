6dfc4c088dffb1cd5e419b4c7f00ba17b3897bdc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-13 01:06:48 -0800

    Time: 1605 ms (5.04%) | Memory: 112.6 MB (5.08%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,57 +1,28 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # SUPROBLEMS TO SOLVE:
-        """
-        For each i,j, 1 <= i <= j <= len(s), let:
-            L(i,j) = {
-                1,      if s[i:j+1] is a palindrome
-                0,      otherwise
-            }
-        """
-
-        # SOLVING ORIGINAL PROBLEM:
-        """
-        The answer to our original problem is simply:
-            sum(L(i,j) for i in range(len(s)) for j in range(len(s)))
-        """
-
-        # RECURSIVE FORMULA TO COMPUTE SUPROBLEMS:
-        """
-        L(i,j) = {
-            1,              if i == j
-            1,              if i < len(s), i+1==j, s[i]==s[j]
-            1,              if 1 <= i+1 <= j-1 <= len(s),
-                               L(i+1,j-1) == 1, s[i] == s[j]
-            0,              otherwise      
-        }
-        """
-        n = len(s)
-
-        # Initialize L(i,j) to 0 for all i,j in range(1,n)
-        L = [[0] * n for i in range(n)]
-
-        # All strings of length k = 1 are palindromes
-        for i in range(n):
-            L[i][i] = 1
+        self.s, self.memo = s, {}
+        res = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                res += self.dp(i, j)
+        return res
+    
+    # dp(i, j) returns True if and only if s[i..j] is a palindrome! Otherwise, returns False
+    def dp(self, i, j):
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
         
-        # All strings of length k = 2 whose characters
-        # are the same are palindromes
-        for i in range(n-1):
-            if s[i] == s[i+1]:
-                L[i][i+1] = 1
+        # Base Case 1: Even length palindrome
+        if i + 1 == j:
+            return self.s[i] == self.s[j]
         
-        # Find all palindromic substrings of s[0:n] of length k>2
-        for k in range(3,n+1):
-            for i in range(n-k+1):
-                j = i + k-1
-                if ((L[i+1][j-1] == 1) and (s[i] == s[j])):
-                    L[i][j] = 1
-            
-        num_palindromes = 0
-        for i in range(n):
-            for j in range(n):
-                num_palindromes += L[i][j]
+        # Base Case 2: Odd length palindrome
+        if i >= j:
+            return i == j
         
-        return num_palindromes
-
-
+        if self.s[i] != self.s[j]:
+            return False
+        
+        res = self.dp(i + 1, j - 1)
+        self.memo[(i, j)] = res
+        return res
