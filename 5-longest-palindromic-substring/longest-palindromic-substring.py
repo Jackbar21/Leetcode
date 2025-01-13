45fe85bdd63442123e1f16da99e26e1c0f172e35
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-12 22:28:16 -0800

    Time: 3338 ms (19.88%) | Memory: 69.7 MB (5.36%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,32 +1,33 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        self.s = s
-        self.memo = {}
-        best_i, best_j = 0, 0
-        longest = 1
+        self.s, self.memo = s, {}
+        longest, best_i, best_j = 0, 0, 0
         for i in range(len(s)):
             for j in range(i + 1, len(s)):
-                if self.dp(i, j) > longest:
-                    longest = self.dp(i, j)
-                    best_i = i
-                    best_j = j
-        return s[best_i:best_j+1]
-        return max((self.dp(i, j) for i in range(len(s)) for j in range(i + 1, len(s))), default=1)
-    
+                if j - i + 1 > longest and self.dp(i, j):
+                    best_i, best_j = i, j
+                    longest = j - i + 1
+        return s[best_i: best_j + 1]
+
     def dp(self, i, j):
         if (i, j) in self.memo:
             return self.memo[(i, j)]
-
-        if i >= j:
+        
+        # Base Case: Odd length palindrome
+        if i == j:
             return i == j
         
+        # Base Case: Even length palindrome
+        if i + 1 == j:
+            return self.s[i] == self.s[j]
+
+        # Base Case: Not a palindrome
+        if i > j:
+            return False
+        
         if self.s[i] != self.s[j]:
-            # self.memo[(i, j)] = False
-            return -1
+            return False
         
         res = self.dp(i + 1, j - 1)
-        res = res + 2 if res != -1 else -1
         self.memo[(i, j)] = res
         return res
-        
-
