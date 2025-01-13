0ea626bf296ffe6dcf88abb25e445836e80ea0a7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-12 17:10:49 -0800

    Time: 231 ms (28.25%) | Memory: 18.8 MB (32.70%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -3,7 +3,4 @@ class Solution:
         d = defaultdict(int)
         for letter in s:
             d[letter] += 1
-        print(f"{d=}")
-        # return -1
-
-        return len(d) + sum(d[letter] % 2 == 0 for letter in d)
+        return sum(2 if d[letter] % 2 == 0 else 1 for letter in d)
