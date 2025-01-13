e4125e9ebb17aaab76c632278e39170f2260d0e9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-12 17:18:09 -0800

    Time: 195 ms (33.33%) | Memory: 18.9 MB (14.92%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,6 +1,10 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        d = defaultdict(lambda: 1)
+        # d = {letter: 0 for letter in "abcdefghijklmnopqrstuvwxyz"}
+        # for letter in s:
+        #     d[letter] += 1
+        ORD_A = ord("a")
+        d = [0] * 26
         for letter in s:
-            d[letter] ^= 1 # Flip the bit!
-        return len(d) + sum(d.values())
+            d[ord(letter) - ORD_A] += 1
+        return sum(0 if freq == 0 else 2 if freq % 2 == 0 else 1 for freq in d)
