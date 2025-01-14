ed890c159cbbbcde2d1437e5ab8c3a8a4ce94746
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-13 18:18:52 -0800

    Time: 71 ms (10.16%) | Memory: 17.8 MB (32.03%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,11 +1,11 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         res = []
-        set_a = set()
-        set_b = set()
+        freqs_a = [False] * 51
+        freqs_b = [False] * 51
         assert len(A) == len(B)
         for i in range(len(A)):
-            set_a.add(A[i])
-            set_b.add(B[i])
-            res.append(len(set_a.intersection(set_b)))
+            freqs_a[A[i]] = True
+            freqs_b[B[i]] = True
+            res.append(sum(freqs_a[i] and freqs_b[i] for i in range(51)))
         return res
