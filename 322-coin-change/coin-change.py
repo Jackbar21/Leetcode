604bcf3cdfbf75a3cb6d08922a3099a96fb1281a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-13 23:47:55 -0800

    Time: 1155 ms (16.72%) | Memory: 20.5 MB (24.80%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -8,6 +8,10 @@ class Solution:
         if amount_left in self.memo:
             return self.memo[amount_left]
         
+        # Was able to reach the EXACT amount, so don't need ANY more coins!
+        if amount_left == 0:
+            return 0
+        
         # If amount_left < 0, this means we "overshot" the amount (since we're working
         # backwards from the original amount down to 0), so we return that we need "infinity"
         # coins to make this solution path work, obviously discouraging this solution from
@@ -15,13 +19,11 @@ class Solution:
         if amount_left < 0:
             return float("inf")
         
-        # Was able to reach the EXACT amount, so don't need ANY more coins!
-        if amount_left == 0:
-            return 0
-        
         res = float("inf")
         for coin in self.coins:
             res = min(res, 1 + self.dp(amount_left - coin))
+            if res == 1:
+                break
         
         self.memo[amount_left] = res
         return res
