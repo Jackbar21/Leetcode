f9149ea2cb656b1b0a9bb1298ab47d1f97c73ad3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-13 23:45:46 -0800

    Time: 1118 ms (19.54%) | Memory: 20.6 MB (24.56%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,23 +1,27 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount in self.memo:
-            return self.memo[amount]
+        self.coins, self.memo = coins, {}
+        res = self.dp(amount)
+        return res if res != float("inf") else -1
+    
+    def dp(self, amount_left):
+        if amount_left in self.memo:
+            return self.memo[amount_left]
         
-        if amount <= 0:
-            assert amount == 0
+        # If amount_left < 0, this means we "overshot" the amount (since we're working
+        # backwards from the original amount down to 0), so we return that we need "infinity"
+        # coins to make this solution path work, obviously discouraging this solution from
+        # being chosen... as this is a minimization problem :)
+        if amount_left < 0:
+            return float("inf")
+        
+        # Was able to reach the EXACT amount, so don't need ANY more coins!
+        if amount_left == 0:
             return 0
         
         res = float("inf")
-        for coin in coins:
-            if amount - coin >= 0:
-                val = self.coinChange(coins, amount - coin)
-                if val != -1:
-                    res = min(res, val+1)
+        for coin in self.coins:
+            res = min(res, 1 + self.dp(amount_left - coin))
         
-        if res == float("inf"):
-            res = -1
-        
-        self.memo[amount] = res
+        self.memo[amount_left] = res
         return res
