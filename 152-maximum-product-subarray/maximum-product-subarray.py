01bd6c17db6adf58469e0f9b0a03636ee20a9e0a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-14 21:51:44 -0800

    Time: 21 ms (5.97%) | Memory: 23.2 MB (5.12%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -2,8 +2,7 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         self.nums = nums
         self.memo = {}
-        self.dp(0)
-        return max(max_val for _, max_val in self.memo.values())
+
         res = float("-inf")
         for i in range(len(nums)):
             _, max_val = self.dp(i)
@@ -19,24 +18,14 @@ class Solution:
         # Base Case: If at last index, can only pick that number for product!
         if i == len(self.nums) - 1:
             val = self.nums[i]
-            self.memo[i] = (val, val)
             return (val, val)
         
         # Case 1: Only consider current element
         val = self.nums[i]
         min_val, max_val = self.dp(i + 1)
 
-        new_min_val = min(
-            val,
-            min_val * val,
-            max_val * val
-        )
-
-        new_max_val = max(
-            val,
-            min_val * val,
-            max_val * val
-        )
+        sorted_options = sorted([val, min_val * val, max_val * val])
+        new_min_val, _, new_max_val = sorted_options
 
         res = (new_min_val, new_max_val)
         self.memo[i] = res
