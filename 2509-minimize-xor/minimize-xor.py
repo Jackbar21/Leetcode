f6f6e215f8991cbbfee01a5b5858fd2163f0a69e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-14 21:03:47 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (23.20%) - LeetSync

--- 2509-minimize-xor/minimize-xor.py
+++ 2509-minimize-xor/minimize-xor.py
@@ -2,9 +2,6 @@ class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
         bin1, bin2 = bin(num1)[2:], bin(num2)[2:]
         bin1_count, bin2_count = bin1.count('1'), bin2.count('1')
-        print(f"{bin1=}, {bin1_count=}")
-        print(f"{bin2=}, {bin2_count=}")
-        # return -1
 
         # There are only three possible cases:
         # Case 1: bin1_count == bin2_count, in which case we can return num1
@@ -12,10 +9,10 @@ class Solution:
         if bin1_count == bin2_count:
             return num1
         
-        
-        
-        # Case 2: bin1_count < bin2_count, in which case we just set bin2_count
-        # least significant bits as 'set' and use that to xor with num1
+        # Case 2: bin1_count < bin2_count, in which case we will use bin1_count
+        # many bits to 'toggle' the currently set bits in num1, and activate the
+        # remaining bin2_count - bin1_count least significant bits that were NOT
+        # originally set in num1!
         if bin1_count < bin2_count:
             bin_num = [bit for bit in bin1]
             i = len(bin_num) - 1
@@ -26,19 +23,13 @@ class Solution:
                     bits_left -= 1
                 i -= 1
             return int("".join("1" for _ in range(bits_left)) + "".join(bin_num), 2)
-            # return int('1' * bin2_count, 2)
 
         # Case 3: bin1_count > bin2_count, in which case we want to pair the
         # bin2_count many bits we can set alongside the MOST significant bits
         # inside of num1, that we we can CANCEL them out (since 1 ^ 1 == 0!)
-        bin_num = ["0"] * len(bin1)
         # assert bin1_count > bin2_count
+        bin_num = ["0"] * len(bin1)
         num_bits = bin2_count
-        # index = -1
-        # while num_bits > 0:
-        #     num_bits -= bin1[index] == "1"
-        #     index -= 1
-        # index += 1
         i = 0
         while num_bits > 0:
             assert i < len(bin_num)
@@ -46,6 +37,4 @@ class Solution:
                 bin_num[i] = "1"
                 num_bits -= 1
             i += 1
-
         return int("".join(bin_num), 2)
-        print(f"{bin1[index:], int(bin1[index:], 2)=}")
