5e48262143c25d1047b372ea948563d900d0db6e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-18 01:01:56 -0800

    Time: 7 ms (44.07%) | Memory: 19.1 MB (24.18%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -13,20 +13,14 @@ class Solution:
         
         price = self.prices[i]
 
-        # Case 1: Buy the stock (can only do so if DON'T have stock!)
-        # case1 = self.dp(i + 1, True) - price if not have_stock else 0
+        # Case 1: Buy or Sell the stock
+        #   Must SELL stock if have stock, in which case we skip by two days 
+        #   and gain a profit of 'price', or must BUY stock if have no stock!
+        case1 = self.dp(i + 2, False) + price if have_stock else self.dp(i + 1, True) - price
         
-        # Case 2: Sell the stock (can only do so if DO have stock!)
-        # Remember, cannot sell on next day, so augment i by TWO instead of ONE!
-        # case2 = self.dp(i + 2, False) + price if have_stock else 0
+        # Case 2: Don't buy or sell the stock
+        case2 = self.dp(i + 1, have_stock)
 
-        res = self.dp(i + 2, False) + price if have_stock else self.dp(i + 1, True) - price
-        
-        # Case 3: Do nothing (don't buy or sell)!
-        case3 = self.dp(i + 1, have_stock)
-        if case3 > res:
-            res = case3
-
-        # res = max(case1, case2, case3)
+        res = case1 if case1 > case2 else case2
         self.memo[(i, have_stock)] = res
         return res
