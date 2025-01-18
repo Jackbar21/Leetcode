4e8a509553b2bee05454e28879cd1ca4da841743
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-18 00:59:30 -0800

    Time: 3 ms (75.92%) | Memory: 18.9 MB (31.48%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -14,15 +14,19 @@ class Solution:
         price = self.prices[i]
 
         # Case 1: Buy the stock (can only do so if DON'T have stock!)
-        case1 = self.dp(i + 1, True) - price if not have_stock else 0
+        # case1 = self.dp(i + 1, True) - price if not have_stock else 0
         
         # Case 2: Sell the stock (can only do so if DO have stock!)
         # Remember, cannot sell on next day, so augment i by TWO instead of ONE!
-        case2 = self.dp(i + 2, False) + price if have_stock else 0
+        # case2 = self.dp(i + 2, False) + price if have_stock else 0
+
+        res = self.dp(i + 2, False) + price if have_stock else self.dp(i + 1, True) - price
         
         # Case 3: Do nothing (don't buy or sell)!
         case3 = self.dp(i + 1, have_stock)
+        if case3 > res:
+            res = case3
 
-        res = max(case1, case2, case3)
+        # res = max(case1, case2, case3)
         self.memo[(i, have_stock)] = res
         return res
