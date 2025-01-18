953b51c51116de09ced93a985235ba9befdd9743
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-17 23:56:28 -0800

    Time: 1651 ms (5.51%) | Memory: 184.1 MB (19.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,19 +1,33 @@
 class Solution:
-    def __init__(self):
-        self.text1, self.text2 = None, None
-        self.memo = {}
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         self.text1, self.text2 = text1, text2
-        return self.lcs(0, 0)
-    def lcs(self, i, j):
+        self.memo = {}
+        return self.dp(0, 0)
+    
+    def dp(self, i, j):
         if (i, j) in self.memo:
             return self.memo[(i, j)]
         
-        self.memo[(i, j)] = (
-            0 if i >= len(self.text1) or j >= len(self.text2)
-            else 1 + self.lcs(i + 1, j + 1)
-            if self.text1[i] == self.text2[j]
-            else max(self.lcs(i + 1, j), self.lcs(i, j + 1))
-        )
+        # If we've "exhausted" one of the two strings,
+        # then we have no more possible matchings :(
+        if i >= len(self.text1) or j >= len(self.text2):
+            return 0
 
-        return self.memo[(i, j)]
+        letter1, letter2 = self.text1[i], self.text2[j]
+
+        # Case 1: letter1 == letter2, in which case you should ONLY consider this option!
+        if letter1 == letter2:
+            case1 = 1 + self.dp(i + 1, j + 1)
+            self.memo[(i, j)] = case1
+            return case1
+
+        # Case 2: Shift index i by one (no matching!)
+        case2 = self.dp(i + 1, j)
+
+        # Case 3: Shift index j by one (no matching!)
+        case3 = self.dp(i, j + 1)
+
+        res = max(case2, case3)
+        self.memo[(i, j)] = res
+        return res
+            
