05702941b07dd7587505d7d5dda8f5819e51b2b9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-18 15:29:10 -0800

    Time: 311 ms (49.20%) | Memory: 48.5 MB (42.96%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,14 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        self.coins, self.amount, self.memo = coins, amount, {}
-        return self.dp(0, 0)
+        # self.coins, self.amount, self.memo = coins, amount, {}
+        # return self.dp(0, 0)
 
         # Let's try a bottom up approach!
         N = len(coins)
-        # dp = [[0] * (amount + 1) for _ in range(N + 1)]
-        # dp[N][amount] = 1
+        dp = [[0] * (amount + 1) for _ in range(N + 1)]
+        dp[N][amount] = 1
 
-        # for i in range(N - 1, -1, -1):
+        for i in range(N - 1, -1, -1):
+            coin = coins[i]
+            for cur_amount in range(amount, -1, -1):
+                case1 = dp[i][cur_amount + coin] if cur_amount + coin <= amount else 0
+                case2 = dp[i + 1][cur_amount]
+                dp[i][cur_amount] = case1 + case2
+        return dp[0][0]
             
 
 
