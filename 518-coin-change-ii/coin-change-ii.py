0ffaa7a88dd555a97a7cba44a39415c0eba2c11c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-18 15:10:48 -0800

    Time: 6797 ms (5.01%) | Memory: 167 MB (19.20%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,20 +2,22 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         self.coins, self.memo = coins, {}
         return self.dp(0, amount)
-    
+
     def dp(self, i, amount):
         if (i, amount) in self.memo:
             return self.memo[(i, amount)]
         
-        if i >= len(self.coins):
-            return amount == 0
+        if i >= len(self.coins) or amount <= 0:
+            return int(amount == 0)
         
         coin = self.coins[i]
         res = 0
-        cur_amount = amount
-        while cur_amount >= 0:
+        # cur_amount = amount
+        # while cur_amount >= 0:
+        #     res += self.dp(i + 1, cur_amount)
+        #     cur_amount -= coin
+        for cur_amount in range(amount, -1, -coin):
             res += self.dp(i + 1, cur_amount)
-            cur_amount -= coin
 
         self.memo[(i, amount)] = res
         return res
