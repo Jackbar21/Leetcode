69ca08d6cdf9541bd896dd4cc1bea45c39d4e4be
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-19 12:36:34 -0800

    Time: 1842 ms (5.39%) | Memory: 35.1 MB (5.31%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -2,10 +2,49 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         self.s1, self.s2, self.s3 = s1, s2, s3
         self.memo = {}
-        return self.dp(0, 0, 0)
+        
+        # return self.dp3D(0, 0, 0)
 
-    # @cache
-    def dp(self, s1_index, s2_index, s3_index):
+        # Bottom Up Approach
+        S1, S2, S3 = len(s1), len(s2), len(s3)
+        dp = [[[False] * (S3 + 1) for _ in range((S2 + 1))] for _ in range((S1 + 1))]
+        dp[S1][S2][S3] = True
+        for s3_index in range(S3 - 1, -1, -1):
+            s3_letter = s3[s3_index]
+
+            for s2_index in range(S2, -1, -1):
+                # s2_letter = s2[s2_index]
+                s2_letter = None if s2_index >= len(self.s2) else self.s2[s2_index]
+                
+                for s1_index in range(S1, -1, -1):
+                    # s1_letter = s1[s1_index]
+                    s1_letter = None if s1_index >= len(self.s1) else self.s1[s1_index]
+                
+                    if s3_letter != s1_letter and s3_letter != s2_letter:
+                        continue # Keep as False!
+                    
+                    if s3_letter != s2_letter:
+                        dp[s1_index][s2_index][s3_index] = dp[s1_index + 1][s2_index][s3_index + 1]
+                        continue
+    
+                    if s3_letter != s1_letter:
+                        dp[s1_index][s2_index][s3_index] = dp[s1_index][s2_index + 1][s3_index + 1]
+                        continue
+                    
+                    dp[s1_index][s2_index][s3_index] = (
+                        dp[s1_index + 1][s2_index][s3_index + 1] or
+                        dp[s1_index][s2_index + 1][s3_index + 1]
+                    )
+        
+        return dp[0][0][0]
+    
+    # def dp2D(self, s1_index, s2_index)
+
+    # Time Complexity:
+    #   - O(len(s1) * len(s2) * len(s3)) subproblems
+    #   - O(1) time / suproblem
+    #   == O(S1 * S2 * S3), where S1 == len(s1), S2 == len(s2), S3 == len(s3)
+    def dp3D(self, s1_index, s2_index, s3_index):
         if (s1_index, s2_index, s3_index) in self.memo:
             return self.memo[(s1_index, s2_index, s3_index)]
         
@@ -16,25 +55,22 @@ class Solution:
         s2_letter = None if s2_index >= len(self.s2) else self.s2[s2_index]
         s3_letter = self.s3[s3_index]
         
-        # s1_letter, s2_letter, s3_letter = self.s1[s1_index], self.s2[s2_index], self.s3[s3_index]
-
         if s3_letter != s1_letter and s3_letter != s2_letter:
             return False
         
         if s3_letter != s2_letter:
-            res = self.dp(s1_index + 1, s2_index, s3_index + 1)
+            res = self.dp3D(s1_index + 1, s2_index, s3_index + 1)
             self.memo[(s1_index, s2_index, s3_index)] = res
             return res
         
         if s3_letter != s1_letter:
-            res = self.dp(s1_index, s2_index + 1, s3_index + 1)
+            res = self.dp3D(s1_index, s2_index + 1, s3_index + 1)
             self.memo[(s1_index, s2_index, s3_index)] = res
             return res
         
         res = (
-            self.dp(s1_index + 1, s2_index, s3_index + 1) 
-            or self.dp(s1_index, s2_index + 1, s3_index + 1)
+            self.dp3D(s1_index + 1, s2_index, s3_index + 1) 
+            or self.dp3D(s1_index, s2_index + 1, s3_index + 1)
         )
         self.memo[(s1_index, s2_index, s3_index)] = res
         return res
-
