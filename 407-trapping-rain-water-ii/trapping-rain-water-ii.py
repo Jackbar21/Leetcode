b57019a8af015b14a59b844f30deb8776519f369
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-18 23:06:12 -0800

    Time: 155 ms (31.60%) | Memory: 19.7 MB (81.26%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -6,18 +6,24 @@ class Solution:
         inBounds = lambda x, y: 0 <= x < M and 0 <= y < N
 
         fringe = [] # min-heap of borders
-        visited = set()
+        # visited = set()
+        # Instead of using a visited set, we can mark things visited
+        # by setting grid[x][y] as -1 :)
 
         for i in range(M):
             heapq.heappush(fringe, (grid[i][0], i, 0))
             heapq.heappush(fringe, (grid[i][N - 1], i, N - 1))
-            visited.add((i, 0))
-            visited.add((i, N - 1))
+            # visited.add((i, 0))
+            # visited.add((i, N - 1))
+            grid[i][0] = -1
+            grid[i][N - 1] = -1
         for j in range(N):
             heapq.heappush(fringe, (grid[0][j], 0, j))
             heapq.heappush(fringe, (grid[M - 1][j], M - 1, j))
-            visited.add((0, j))
-            visited.add((M - 1, j))
+            # visited.add((0, j))
+            # visited.add((M - 1, j))
+            grid[0][j] = -1
+            grid[M - 1][j] = -1
         
         res = 0
         while len(fringe) > 0:
@@ -25,11 +31,15 @@ class Solution:
 
             for dx, dy in DIRECTIONS:
                 neigh_x, neigh_y = x + dx, y + dy
-                if (neigh_x, neigh_y) in visited or not inBounds(neigh_x, neigh_y):
+                # if (neigh_x, neigh_y) in visited or not inBounds(neigh_x, neigh_y):
+                #     continue
+                # visited.add((neigh_x, neigh_y))
+                if not inBounds(neigh_x, neigh_y) or grid[neigh_x][neigh_y] == -1:
                     continue
-                visited.add((neigh_x, neigh_y))
+                
 
                 neigh_height = grid[neigh_x][neigh_y]
+                grid[neigh_x][neigh_y] = -1
                 diff = border_height - neigh_height if border_height > neigh_height else 0
 
                 res += diff
