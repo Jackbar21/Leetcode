41f0e1db99490209b7df984a90f6ee127dcff3cc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-19 17:41:41 -0800

    Time: 47 ms (81.95%) | Memory: 19.4 MB (80.35%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -8,37 +8,31 @@ class Solution:
         if (i, j) in self.memo:
             return self.memo[(i, j)]
         
-        # if i >= len(self.word1) and j >= len(self.word2):
-        #     return 0
-        
+        # Base Case 1: We've "exhausted" every letter in word2.
         if j >= len(self.word2):
             # Can only delete characters from this point onwards!
             return len(self.word1) - i
         
+        # Base Case 2: We've "exhausted" every letter in word1.
         if i >= len(self.word1):
             # Can only insert characters from this point onwards!
             return len(self.word2) - j
+
+        # Base Case 3: If both letters already match, no need for any edits!
+        if self.word1[i] == self.word2[j]:
+            res = self.dp(i + 1, j + 1)
+            self.memo[(i, j)] = res
+            return res
         
+        # Case 1: Insert a character
+        case1 = 1 + self.dp(i, j + 1)
 
-        letter1, letter2 = self.word1[i], self.word2[j]
+        # Case 2: Delete a character
+        case2 = 1 + self.dp(i + 1, j)
 
-        # Case 1: Do nothing (letters already match!)
-        case1 = float("inf")
-        if letter1 == letter2:
-            case1 = self.dp(i + 1, j + 1)
-            self.memo[(i, j)] = case1
-            return case1
-        
-        # Case 2: Insert a character
-        case2 = 1 + self.dp(i, j + 1)
+        # Case 3: Replace a character
+        case3 = 1 + self.dp(i + 1, j + 1)
 
-        # Case 3: Delete a character
-        case3 = 1 + self.dp(i + 1, j)
-
-        # Case 4: Replace a character
-        case4 = 1 + self.dp(i + 1, j + 1)
-
-        res = min(case1, case2, case3, case4)
+        res = min(case1, case2, case3)
         self.memo[(i, j)] = res
         return res
-        
