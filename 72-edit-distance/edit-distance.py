6a777c9612f617bde27b842e346bce6dbb00a216
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-19 17:36:12 -0800

    Time: 494 ms (5.00%) | Memory: 51.3 MB (5.40%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        self.word1, self.word2 = word1, word2
+        self.memo = {}
+        return self.dp(0, 0)
+
+    def dp(self, i, j):
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
+        
+        if i >= len(self.word1) and j >= len(self.word2):
+            return 0
+        
+        if j >= len(self.word2):
+            # Can only delete characters from this point onwards!
+            return 1 + self.dp(i + 1, j)
+        
+        if i >= len(self.word1):
+            # Can only insert characters
+            return 1 + self.dp(i, j + 1)
+        
+
+        letter1, letter2 = self.word1[i], self.word2[j]
+
+        # Case 1: Do nothing (letters already match!)
+        case1 = float("inf")
+        if letter1 == letter2:
+            case1 = self.dp(i + 1, j + 1)
+        
+        # Case 2: Insert a character
+        case2 = 1 + self.dp(i, j + 1)
+
+        # Case 3: Delete a character
+        case3 = 1 + self.dp(i + 1, j)
+
+        # Case 4: Replace a character
+        case4 = 1 + self.dp(i + 1, j + 1)
+
+        res = min(case1, case2, case3, case4)
+        self.memo[(i, j)] = res
+        return res
+        
