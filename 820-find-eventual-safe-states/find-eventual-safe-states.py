4ac4ee776d4fcf84f65b2702a062050a8c52a71f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-23 17:29:32 -0800

    Time: 59 ms (41.26%) | Memory: 25.4 MB (5.49%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -6,16 +6,16 @@ class Solution:
         # "loosening" the constraint for ANY nodes that POINT to 6. Hence, the first step
         # of the algorithm will be to create a hashmap 'd' of nodes to a set/list of nodes 
         # that POINT to them.
-        d = defaultdict(set) # node: {node's in-neighbors}, i.e. "reverse" adjacency list
+        N = len(graph)
+        d = [set() for _ in range(N)] # node: {node's in-neighbors}, i.e. "reverse" adjacency list
         for node, neighbors in enumerate(graph):
             for neigh in neighbors:
-                # node -> neigh, so node is neigh's "in-neighbor"
                 d[neigh].add(node)
         
         # print(f"{d=}")
         # return []
         outdegree = [len(neighbors) for neighbors in graph]
-        queue = collections.deque(node for node in range(len(graph)) if outdegree[node] == 0)
+        queue = collections.deque(node for node in range(N) if outdegree[node] == 0)
         topo_order = []
         while len(queue) > 0:
             node = queue.popleft()
@@ -28,15 +28,3 @@ class Solution:
 
         topo_order.sort() # The answer should be sorted in ascending order.
         return topo_order
-        
-        """
-        d = {
-            6: [] (0),
-            5: [] (0),
-            4: [5] (1),
-            3: [0] (1),
-            2: [5] (1),
-            1: [2,3] (2),
-            0: [1, 2] (2)
-        }
-        """
