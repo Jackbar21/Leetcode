730696eaba1919d8e32f58ebede119edec487769
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-23 17:47:08 -0800

    Time: 50 ms (51.61%) | Memory: 23.6 MB (65.86%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -8,7 +8,8 @@ class Solution:
         # that POINT to them.
         N = len(graph)
         d = [[] for _ in range(N)] # node: {node's in-neighbors}, i.e. "reverse" adjacency list
-        outdegree, stack = [], []
+        outdegree = []
+        good_nodes = [] # A node is "good" if it is terminal OR safe!
         for node, neighbors in enumerate(graph):
             for neigh in neighbors:
                 d[neigh].append(node)
@@ -16,16 +17,17 @@ class Solution:
             degree = len(neighbors)
             outdegree.append(degree)
             if degree == 0:
-                stack.append(node)
+                good_nodes.append(node) # Terminal node, hence "good"
         
         topo_order = []
-        while stack:
-            node = stack.pop()
+        while good_nodes:
+            node = good_nodes.pop()
             topo_order.append(node)
             for in_neigh in d[node]:
                 outdegree[in_neigh] -= 1
                 if outdegree[in_neigh] == 0:
-                    stack.append(in_neigh)
+                    # Node is safe, hence "good"
+                    good_nodes.append(in_neigh)
 
         topo_order.sort() # The answer should be sorted in ascending order.
         return topo_order
