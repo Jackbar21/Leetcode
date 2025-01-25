fa8fed5c949a6c1425a821f51e75b5e7b6b8622c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 15:55:41 -0800

    Time: 157 ms (5.59%) | Memory: 60 MB (13.74%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -8,23 +8,23 @@ class Solution:
         #   of each three values is indeed x, y, z!
         X, Y, Z = target
         set_x, set_y, set_z = set(), set(), set()
-        max_x, max_y, max_z = 0, 0, 0
+        found_x, found_y, found_z = False, False, False
         for x, y, z in triplets:
             triplet = (x, y, z)
             if x <= X:
                 set_x.add(triplet)
-                if x > max_x:
-                    max_x = x
+                if x == X:
+                    found_x = True
             if y <= Y:
                 set_y.add(triplet)
-                if y > max_y:
-                    max_y = y
+                if y == Y:
+                    found_y = True
             if z <= Z:
                 set_z.add(triplet)
-                if z > max_z:
-                    max_z = z
+                if z == Z:
+                    found_z = True
         
-        if max_x != X or max_y != Y or max_z != Z:
+        if not found_x or not found_y or not found_z:
             return False
         
         intersection = (set_x.intersection(set_y)).intersection(set_z)
