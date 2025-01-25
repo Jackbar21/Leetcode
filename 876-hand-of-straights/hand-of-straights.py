8b448f2d7525152673d6e2976fa0d20c4b236cce
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 15:31:08 -0800

    Time: 39 ms (40.06%) | Memory: 19.7 MB (8.95%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -26,8 +26,7 @@ class Solution:
 
             # We must form a group of 'groupSize' consecutive cards,
             # where the starting number is 'card' as it is of SMALLEST existing value!
-            d[card] -= 1
-            for group_card in range(card + 1, card + groupSize):
+            for group_card in range(card, card + groupSize):
                 if d[group_card] == 0:
                     return False
                 d[group_card] -= 1
