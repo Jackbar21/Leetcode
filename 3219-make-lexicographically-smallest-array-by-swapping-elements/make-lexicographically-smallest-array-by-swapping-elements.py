9316a2c9c3d0738c842e96d7218edd59ce37d9ae
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 00:37:42 -0800

    Time: 328 ms (63.20%) | Memory: 59.5 MB (18.06%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,29 +1,7 @@
 class Solution:
-    def intervalsOverlap(self, i1, i2):
-        s1, e1 = i1
-        s2, e2 = i2
-
-        assert s1 <= e1
-        assert s2 <= e2
-
-        if s1 > s2:
-            return self.intervalsOverlap(i2, i1)
-        
-        assert s1 <= s2
-        if s1 == s2:
-            return True
-        
-        assert s1 < s2
-        if e1 < s2:
-            return False
-        
-        return True
-
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         N = len(nums)
-        NUM, INDEX = 0, 1
         sorted_nums = sorted((num, i) for i, num in enumerate(nums))
-        connected_components = []
         d = {}
 
         i = 0
@@ -34,31 +12,21 @@ class Solution:
             i += 1
             connected_component = [num]
             d[num_index] = connected_component
-            # while i < N and self.intervalsOverlap((lower, upper), (sorted_nums[i][NUM] - limit, sorted_nums[i][NUM] + limit)):
             while i < N:
                 sorted_num, sorted_index = sorted_nums[i]
 
-                # Check if "overlapping"
                 if not (lower <= sorted_num <= upper):
                     break
                 
-                # TODO: maybe swap min/max functions? Probably not!
-                lower = min(lower, sorted_num - limit)
-                upper = max(upper, sorted_num + limit)
-
-                # connected_component.append(sorted_nums[i][NUM])
-                # d[sorted_nums[i][INDEX]] = connected_component
-                # i += 1
+                if sorted_num - limit < lower:
+                    lower = sorted_num - limit
+                if sorted_num + limit > upper:
+                    upper = sorted_num + limit
+    
                 connected_component.append(sorted_num)
-                d[sorted_index] = connected_component
+                d[sorted_index] = d[num_index]
                 i += 1
             
-            connected_component.reverse()
-            connected_components.append(connected_component)
+            connected_component.reverse() # Since can only pop() in arrays, and will need smallest values!
         
-        # print(f"{d=}")
-        # print(f"{connected_components=}")
-        # return []
         return [d[i].pop() for i in range(N)]
-
-                
