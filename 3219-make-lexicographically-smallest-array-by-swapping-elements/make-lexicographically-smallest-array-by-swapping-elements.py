13d656211706ed805704afe4d83ea07e5465479f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 00:51:31 -0800

    Time: 347 ms (57.64%) | Memory: 101.7 MB (5.56%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -2,15 +2,16 @@ class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         N = len(nums)
         sorted_nums = sorted((num, i) for i, num in enumerate(nums))
-        d = {}
+        d = {} # index to connected component mappings!
 
         i = 0
         while i < N:
             # Create new connected component
             num, num_index = sorted_nums[i]
             upper_bound = num + limit
+
             i += 1
-            connected_component = [num]
+            connected_component = collections.deque([num])
             d[num_index] = connected_component
             while i < N:
                 sorted_num, sorted_index = sorted_nums[i]
@@ -26,6 +27,6 @@ class Solution:
                 d[sorted_index] = d[num_index]
                 i += 1
             
-            connected_component.reverse() # Since can only pop() in arrays, and will need smallest values!
+            # connected_component.reverse() # Since can only pop() in arrays, and will need smallest values!
         
-        return [d[i].pop() for i in range(N)]
+        return [d[i].popleft() for i in range(N)]
