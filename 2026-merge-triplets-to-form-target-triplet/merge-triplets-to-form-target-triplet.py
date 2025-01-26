78e17f73bc56f6dca34fbd39cd9f4aea53d9bca5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 16:06:54 -0800

    Time: 0 ms (100.00%) | Memory: 55.2 MB (24.33%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,48 +1,20 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         # return self.mergeTripletsNaive(triplets, target)
-        X, Y, Z = target
+        TARGET_X, TARGET_Y, TARGET_Z = target
         found_x, found_y, found_z = False, False, False
         for x, y, z in triplets:
-            if not (x <= X and y <= Y and z <= Z):
+            if x > TARGET_X or y > TARGET_Y or z > TARGET_Z:
                 continue
             
-            if x == X:
+            if x == TARGET_X:
                 found_x = True
-            if y == Y:
+            if y == TARGET_Y:
                 found_y = True
-            if z == Z:
+            if z == TARGET_Z:
                 found_z = True
-        
-        return found_x and found_y and found_z
 
-    
-    def mergeTripletsNaive(self, triplets: List[List[int]], target: List[int]) -> bool:
-        # Idea: target == [x, y, z]. 
-        #   (1) Grab all the triplets whose x value is <= x
-        #   (2) Grab all the triplets whose y value is <= y
-        #   (3) Grab all the triplets whose z value is <= z
-        #   Get the intersection of these three sets, and figure out if the max
-        #   of each three values is indeed x, y, z!
-        X, Y, Z = target
-        set_x, set_y, set_z = set(), set(), set()
-        for x, y, z in triplets:
-            triplet = (x, y, z)
-            if x <= X:
-                set_x.add(triplet)
-            if y <= Y:
-                set_y.add(triplet)
-            if z <= Z:
-                set_z.add(triplet)
-        
-        intersection = [triplet for triplet in set_x if triplet in set_y and triplet in set_z]
-        found_x, found_y, found_z = False, False, False
-        for x, y, z in intersection:
-            if x == X:
-                found_x = True
-            if y == Y:
-                found_y = True
-            if z == Z:
-                found_z = True
-        
-        return found_x and found_y and found_z
+            if found_x and found_y and found_z:
+                return True
+
+        return False
