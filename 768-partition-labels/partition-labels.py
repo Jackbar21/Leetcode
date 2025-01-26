ea69671f1edb328dd3c394f19b32ee43480ebe7a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 23:33:15 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (37.74%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -11,18 +11,18 @@ class Solution:
         groups = []
         cur_size = 0
         for letter, leftmost_index in leftmost_indices.items():
-            rightmost_index = rightmost_indices[letter] + 1
+            rightmost_index = rightmost_indices[letter]
             if leftmost_index >= cur_size:
-                groups.append(rightmost_index - cur_size)
-                cur_size = rightmost_index
+                groups.append(rightmost_index + 1 - cur_size)
+                cur_size = rightmost_index + 1
                 continue
             
             # assert leftmost_index < cur_size
             # Since leftmost_index < cur_size, this letter must take part of a currently
             # existing group. Hence if its rightmost index is larger than (or equal to)
             # cur_size, then we must extend last group to include all of current letter's instances!
-            if rightmost_index > cur_size:
-                diff = rightmost_index - cur_size
+            if rightmost_index >= cur_size:
+                diff = rightmost_index + 1 - cur_size
                 groups[-1] += diff
                 cur_size += diff
         
