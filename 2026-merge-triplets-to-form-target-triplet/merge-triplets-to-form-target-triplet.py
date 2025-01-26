e5e30f0d37be2a85b2ca0652fde08ca0c9f33fc9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-25 16:13:06 -0800

    Time: 0 ms (100.00%) | Memory: 55.1 MB (61.15%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,35 +1,19 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         TARGET_X, TARGET_Y, TARGET_Z = target
-        # found_x, found_y, found_z = False, False, False
-        max_x, max_y, max_z = 0, 0, 0
+        found_x, found_y, found_z = False, False, False
         for x, y, z in triplets:
             if x > TARGET_X or y > TARGET_Y or z > TARGET_Z:
                 continue
             
-            # if x == TARGET_X:
-            #     found_x = True
-            # if y == TARGET_Y:
-            #     found_y = True
-            # if z == TARGET_Z:
-            #     found_z = True
-            # max_x = max(max_x, x)
-            # max_y = max(max_y, y)
-            # max_z = max(max_z, z)
-            if max_x < x:
-                max_x = x
-            if max_y < y:
-                max_y = y
-            if max_z < z:
-                max_z = z
-            
-            if max_x == TARGET_X and max_y == TARGET_Y and max_z == TARGET_Z:
+            if x == TARGET_X:
+                found_x = True
+            if y == TARGET_Y:
+                found_y = True
+            if z == TARGET_Z:
+                found_z = True
+
+            if found_x and found_y and found_z:
                 return True
 
-            
-
-            # if found_x and found_y and found_z:
-            #     return True
-
         return False
-        return max_x == TARGET_X and max_y == TARGET_Y and max_z == TARGET_Z
