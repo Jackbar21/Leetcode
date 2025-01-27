47dbf132c302855fc2e6ee259f202b281e407114
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 18:57:45 -0800

    Time: 30 ms (80.36%) | Memory: 20.6 MB (34.20%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -30,18 +30,13 @@ class Solution:
 
         # We know from constraints that prerequisites graph has NO cycles!!!
         # This means we have a DAG, which is perfect for something like DP!
-        memo = {}
+        @cache
         def dp(node):
-            if node in memo:
-                return memo[node]
-
             # Can add node as its own child, since constraints 
             # guarantee that u != v for any (u,v) in queries!
             children = set([node])
             for neigh in adj_list[node]:
                 children.update(dp(neigh))
-
-            memo[node] = children
             return children
 
         return [v in dp(u) for u, v in queries]
