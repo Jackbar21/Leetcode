caffa028b8c6f17f91c1842c748d7335b3c3c127
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 19:12:59 -0800

    Time: 22 ms (93.53%) | Memory: 20.8 MB (9.78%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -11,8 +11,9 @@ class Solution:
         def getChildren(node):
             # Can add node as its own child, since constraints 
             # guarantee that u != v for any (u,v) in queries!
-            children = set(adj_list[node])
+            children = set()
             for neigh in adj_list[node]:
+                children.add(neigh)
                 children.update(getChildren(neigh))
             return children
 
