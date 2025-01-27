e2081ba02e598964b69e72510db472f0c28f9db3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 17:02:21 -0800

    Time: 4766 ms (5.29%) | Memory: 66.1 MB (24.04%) - LeetSync

--- 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
@@ -1,95 +1,100 @@
 class Solution:
-    # SOLUTION FROM EDITORIAL... USING IT TO NOT LOSE MY STREAK!
     def maximumInvitations(self, favorite: List[int]) -> int:
-        n = len(favorite)
-        in_degree = [0] * n
-
-        # Calculate in-degree for each node
-        for person in range(n):
-            in_degree[favorite[person]] += 1
-
-        # Topological sorting to remove non-cycle nodes
-        q = deque()
-        for person in range(n):
-            if in_degree[person] == 0:
-                q.append(person)
-        depth = [1] * n  # Depth of each node
-
-        while q:
-            current_node = q.popleft()
-            next_node = favorite[current_node]
-            depth[next_node] = max(depth[next_node], depth[current_node] + 1)
-            in_degree[next_node] -= 1
-            if in_degree[next_node] == 0:
-                q.append(next_node)
-
-        longest_cycle = 0
-        two_cycle_invitations = 0
-
-        # Detect cycles
-        for person in range(n):
-            if in_degree[person] == 0:  # Already processed
-                continue
-
-            cycle_length = 0
-            current = person
-            while in_degree[current] != 0:
-                in_degree[current] = 0  # Mark as visited
-                cycle_length += 1
-                current = favorite[current]
-
-            if cycle_length == 2:
-                # For 2-cycles, add the depth of both nodes
-                two_cycle_invitations += depth[person] + depth[favorite[person]]
-            else:
-                longest_cycle = max(longest_cycle, cycle_length)
-
-        return max(longest_cycle, two_cycle_invitations)
-
-    def maximumInvitationsFailedAttempt(self, favorite: List[int]) -> int:
-        # For Disjoint Sets, we need a union function, as well as a find function.
-        # Initially, all nodes are in their own set
         N = len(favorite)
-        # parent = {node: node for node in range(N)}
         adj_list = defaultdict(list)
         for i in range(N):
+            # Reversed so we can chain connected-components with length 2 cycles!
             adj_list[favorite[i]].append(i)
-        # favorite = adj_list
-        print(f"{adj_list=}")
+        
+        res = 0
+        # length_2_cycle_chain = []
+        length_2_cycle_chain = 0
+        visited_length_2_cycle_nodes = set()
+        # visited_closed_cycle_nodes = set()
+        # visited = set()
+        unvisited = set(range(N))
+        while len(unvisited) > 0:
+            node = unvisited.pop()
+            node_to_dist = {}
+            cur_dist = 0
 
-        # Idea: In order for a group of employees to sit together, they MUST form a cycle!
-
-        max_len = 0
-        max_path = None
-        # Idea: run a dfs from each node, and keep track of longest len!
-        for i in range(N):
-            print(f"{i=}")
-            queue = collections.deque([(1, i, [i])]) # (cost, node, path)
-            available = defaultdict(lambda: 2)
-            # available[i] -= 1
-            while len(queue) > 0:
-                cost, node, path = queue.popleft()
-                if cost > max_len:
-                    max_len = cost
-                    max_path = path
+            already_seen_cycle = False
+            while node not in node_to_dist:
+                # if node in visited:
+                #     break
+                # visited.add(node)
+                node_to_dist[node] = cur_dist
+                node = favorite[node]
+                cur_dist += 1
+                if node not in unvisited:
+                    already_seen_cycle = True
+                    # break
                 
-                if node not in favorite:
-                    continue
-                neighbor = favorite[node]
-                if available[node] > 0 and available[neighbor] > 0:
-                    available[node] -= 1
-                    available[neighbor] -= 1
-                    queue.append((cost + 1, neighbor, path + [neighbor]))
-                    # visited.add(neighbor)
 
-            print(f"{max_len=}, {max_path=}")
-        
-        return max_len
+            
+            for visited_node in node_to_dist:
+                unvisited.discard(visited_node)
+            # unvisited = unvisited.difference(node_to_dist.keys())
+            # if already_seen_cycle:
+            #     continue
+            
+            
+            assert node in node_to_dist
+            cycle_length = cur_dist - node_to_dist[node]
 
-        
+            if cycle_length > 2:
+                # Forms a closed cycle, which CANNOT BE CHAINED!!!
+                # Hence, update res to max between res and length of this CLOSED cycle!
+                if res < cycle_length:
+                    res = cycle_length
+                continue
+            
+            # if already_seen_cycle:
+            #     continue
+            
+            assert cycle_length == 2
+            # Now, we will want to perform a BFS for longest path between each end of this length
+            # 2 cycle (not allowing to go through the other node in the length 2 cycle), and remember
+            # this result as it can be chained with OTHER components with length 2 cycles!!!
 
-# 0
-#    \
-# 1 <-> 2
-#    /
-# 3
+            # Step 1: we need the two nodes in the cycle, and mark them visited so as to NOT permit
+            # each one to go through the other!
+            # We know 'node' is part of the cycle, so the length 2 cycle must be node and favorite[node]!
+            cycle_nodes = [node, favorite[node]]
+            if any(cycle_node in visited_length_2_cycle_nodes for cycle_node in cycle_nodes):
+                continue
+            
+            length_2_cycle_component = 2 # initially 2 for the two cycle nodes themselves!
+            for cycle_node in cycle_nodes:
+                visited_length_2_cycle_nodes.add(cycle_node)
+                longest_branch = 0
+                # Want to find longest chain from this node, adding cycle nodes as visited first!
+                visited = set(cycle_nodes)
+                queue = collections.deque([(0, cycle_node)]) # (cost, node)
+                while len(queue) > 0:
+                    branch_length, node = queue.popleft()
+                    if longest_branch < branch_length:
+                        longest_branch = branch_length
+                    
+                    # This is when we want to consult the reversed-edges 'adj_list' we built earlier,
+                    # to find longest chains from cycle nodes from in-to-out favorite-wise!
+                    for neigh in adj_list[node]:
+                        if neigh in visited:
+                            continue
+                        
+                        queue.append((branch_length + 1, neigh))
+                        visited.add(neigh)
+                
+                length_2_cycle_component += longest_branch
+                unvisited = unvisited.difference(visited)
+                for visited_node in visited:
+                    unvisited.discard(visited_node)
+                
+
+            # Now, chain this length 2 cycle component onto the chain of length-2-cycle components!
+            # length_2_cycle_chain.append(length_2_cycle_component)
+            length_2_cycle_chain += length_2_cycle_component
+
+        if res < length_2_cycle_chain:
+            res = length_2_cycle_chain
+        return res
