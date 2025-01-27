9dd7fc5c098ac3978ab876b3220dc59875836e96
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 18:42:04 -0800

    Time: 54 ms (53.60%) | Memory: 20.4 MB (61.44%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -6,7 +6,7 @@ class Solution:
             adj_list[a].append(b)
 
         # Step 2: For each node, run a DFS from that node to find all the courses it is a prerequisite of
-        d = [set() for _ in range(numCourses)] # node is a prerequisite of every course in d[node]
+        d = [[] for _ in range(numCourses)] # node is a prerequisite of every course in d[node]
         for i in range(numCourses):
             queue = collections.deque([i])
             visited = set([i])
@@ -18,6 +18,6 @@ class Solution:
                         continue
                     visited.add(neigh)
                     queue.append(neigh)
-                    d[i].add(neigh)
+                    d[i].append(neigh)
 
         return list(v in d[u] for u, v in queries)
