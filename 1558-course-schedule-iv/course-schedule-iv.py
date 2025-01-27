782635b843ed8e6da0747c926de0a770a71b3407
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 18:53:38 -0800

    Time: 31 ms (78.82%) | Memory: 20.5 MB (47.37%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,5 +1,5 @@
 class Solution:
-    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+    def checkIfPrerequisiteNaive(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         # Step 1: Build an adjacency list
         adj_list = [[] for _ in range(numCourses)]
         for a, b in prerequisites:
@@ -15,8 +15,33 @@ class Solution:
 
                 for neigh in adj_list[node]:
                     if neigh not in visited:
-                        visited.add(neigh)
-                        stack.append(neigh)
-                        d[i].add(neigh)
+                        continue
+                    visited.add(neigh)
+                    stack.append(neigh)
+                    d[i].add(neigh)
 
         return [v in d[u] for u, v in queries]
+
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        # Step 1: Build an adjacency list
+        adj_list = [[] for _ in range(numCourses)]
+        for a, b in prerequisites:
+            adj_list[a].append(b)
+
+        # We know from constraints that prerequisites graph has NO cycles!!!
+        # This means we have a DAG, which is perfect for something like DP!
+        memo = {}
+        def dp(node):
+            if node in memo:
+                return memo[node]
+            children = set([node]) # Can do this since u_i != v_i for queries (from constraints!)
+            for neigh in adj_list[node]:
+                children.update(dp(neigh))
+            memo[node] = children
+            return children
+
+        res = []
+        for u, v in queries:
+            query = v in dp(u)
+            res.append(query)
+        return res
