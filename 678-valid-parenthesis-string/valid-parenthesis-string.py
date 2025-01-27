478fd7849fc62c63e74d5ff48b57d1b91c43a82f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 22:34:36 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (57.35%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -4,23 +4,19 @@ class Solution:
         star_indices = collections.deque()
         for i, parenthese in enumerate(s):
             if parenthese == "(":
-                # count_open += 1
                 open_indices.append(i)
                 continue
 
             if parenthese == "*":
-                # count_star += 1
                 star_indices.append(i)
                 continue
 
             assert parenthese == ")"
             if len(open_indices) > 0:
                 # Want to pop RIGHTMOST open parenthese!
-                # count_open -= 1 
                 open_indices.pop()
                 continue
             if len(star_indices) > 0:
-                # count_star -= 1
                 # Want to pop LEFTMOST stars!
                 star_indices.popleft()
                 continue
@@ -28,16 +24,12 @@ class Solution:
             # Nothing to match closed-parenthese with!
             return False
 
-        # print(f"{open_indices=}")
-        # print(f"{star_indices=}")
-        # return len(open_indices) == 0
-        while len(open_indices) > 0:
-            open_index = open_indices.pop()
-            # This must be to the left of a star. Hence, grab HIGHEST
-            # star index for it!
-            if len(star_indices) == 0:
-                return False
-            star_index = star_indices.pop()
-            if not (open_index < star_index):
-                return False
-        return True
+        # We now have the leftmost remaining open-parenthese indices, as well
+        # as the rightmost remaining star indices. Hence, want to make sure for
+        # each remaining open parenthese, that we have a star to the right of it!
+        # And once we've exhausted/closed ALL remaining open parentheses, we can
+        # convert all remaining stars to "" :)
+        return (
+            len(star_indices) >= len(open_indices) and 
+            all(open_indices[-i] < star_indices[-i] for i in range(1, len(open_indices) + 1))
+        )
