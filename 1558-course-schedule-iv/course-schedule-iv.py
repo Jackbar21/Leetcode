dd01890c0ade0bda507891fa348256c18d73c992
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-26 18:37:12 -0800

    Time: 46 ms (59.09%) | Memory: 20.5 MB (34.20%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        # Step 1: Build an adjacency list
+        adj_list = [[] for _ in range(numCourses)]
+        for a, b in prerequisites:
+            adj_list[a].append(b)
+
+        # Step 2: For each node, run a DFS from that node to find all the courses it is a prerequisite of
+        d = [set() for _ in range(numCourses)] # node is a prerequisite of every course in d[node]
+        for i in range(numCourses):
+            queue = collections.deque([i])
+            visited = set([i])
+            while len(queue) > 0:
+                node = queue.popleft()
+
+                for neigh in adj_list[node]:
+                    if neigh in visited:
+                        continue
+                    visited.add(neigh)
+                    queue.append(neigh)
+                    d[i].add(neigh)
+        
+        res = []
+        for u, v in queries:
+            res.append(v in d[u])
+        return res
