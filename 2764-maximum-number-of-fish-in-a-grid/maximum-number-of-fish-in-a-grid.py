c333791acf0956dbe7c8c7133210ce9e770d191d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-27 18:11:01 -0800

    Time: 46 ms (44.88%) | Memory: 17.7 MB (74.88%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        LAND = 0
+        M, N = len(grid), len(grid[0])
+        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        inBounds = lambda x, y: 0 <= x < M and 0 <= y < N
+        
+        # Once we visit / "consume" a cell, we will set it to a LAND cell to effectively
+        # mark it as "visited" :)
+        res = 0
+        for i in range(M):
+            for j in range(N):
+                # TODO: Use walrus operator
+                if grid[i][j] == LAND:
+                    continue
+                
+                num_fish = grid[i][j]
+                grid[i][j] = LAND
+                queue = collections.deque([(i, j)])
+                while len(queue) > 0:
+                    x, y = queue.popleft()
+                    # assert grid[x][y] > 0
+                    # num_fish += grid[x][y]
+
+                    for dx, dy in DIRECTIONS:
+                        neigh_x, neigh_y = x + dx, y + dy
+                        if not inBounds(neigh_x, neigh_y):
+                            continue
+                        if grid[neigh_x][neigh_y] == LAND:
+                            continue
+                        
+                        num_fish += grid[neigh_x][neigh_y]
+                        grid[neigh_x][neigh_y] = LAND
+                        queue.append((neigh_x, neigh_y))
+                
+                if res < num_fish:
+                    res = num_fish
+
+        return res
