711b20d01820128fbe3215c73f1dd19fa69bce52
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-29 22:07:40 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (50.83%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,14 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # Idea: transpose, then reverse rows
+        # Idea: Flip the rows in matrix, then transpose matrix!
+        N = len(matrix)
 
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i + 1, n):
+        # Step 1: Transpose the matrix (can only modify matrix)
+        for i in range(N):
+            for j in range(i + 1, N):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        for i in range(n):
-            matrix[i] = matrix[i][::-1]
-        
+        # Step 2: Flip the rows in matrix (can only modify matrix)
+        for i in range(N):
+            l, r = 0, N - 1
+            while l < r:
+                matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
+                l += 1
+                r -= 1
+
         return
