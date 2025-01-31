5d8e8f8a2cc37e2528816c3be5e220c3f0e5e67d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-30 19:29:15 -0800

    Time: 1402 ms (26.97%) | Memory: 81.6 MB (5.46%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,17 +1,53 @@
 class Solution:
-    def generateUniqueIslandId(self):
-        self.island_id += 1
-        return self.island_id - 1
-
     def largestIsland(self, grid: List[List[int]]) -> int:
-        self.pos_to_island = defaultdict(lambda: None)
-        self.island_to_area = {None: 0}
         self.island_id = 0
+        def generateUniqueIslandId():
+            self.island_id += 1
+            return self.island_id - 1
+        
         N = len(grid)
         WATER, LAND = 0, 1
         DIRECTIONS = [(0,1),(0,-1),(1,0),(-1,0)]
+        pos_to_island = defaultdict(lambda: None) # map (x, y) positions in grid to island id
+        island_to_area = {None: 0} # map island id to area / size of that island
+        
+        # Step 1: Get initial max island in grid, and populate 'pos_to_island'
+        # and 'island_to_area' dicitonaries in doing so!
+        res = 0
+        visited = set()
+        for i in range(N):
+            for j in range(N):
+                if grid[i][j] == WATER or (i, j) in visited:
+                    continue
 
-        res = self.maxAreaOfIsland(grid)
+                island_id = generateUniqueIslandId()                
+                area = 0
+                visited.add((i, j))
+                stack = [(i, j)]
+                while len(stack) > 0:
+                    x, y = stack.pop()
+                    pos_to_island[(x, y)] = island_id
+                    area += 1
+
+                    for dx, dy in DIRECTIONS:
+                        neigh_x, neigh_y = x + dx, y + dy
+                        if (0 <= neigh_x < N and 0 <= neigh_y < N
+                            and (neigh_x, neigh_y) not in visited 
+                            and grid[neigh_x][neigh_y] == LAND
+                        ):
+                            visited.add((neigh_x, neigh_y))
+                            stack.append((neigh_x, neigh_y))
+
+                island_to_area[island_id] = area
+                if res < area:
+                    res = area
+
+        # Step 2: Loop through all the water cells, and compute the new maximum island size
+        # if that water cell is turned into an island! This is done by looking at the water
+        # cells directly-adjacent neighbors, and getting those neighbor's corresponding island id
+        # (if they are in an island). Then, each found island id from these 4 neighbors can be joined
+        # together, so the new area will simply be 1 + sum of each directly reachable island size for
+        # every WATER cell in grid!
         for i in range(N):
             for j in range(N):
                 if grid[i][j] == LAND:
@@ -25,77 +61,14 @@ class Solution:
                 bordering_islands = set()
                 for di, dj in DIRECTIONS:
                     x, y = i + di, j + dj
-                    bordering_islands.add(self.pos_to_island[(x, y)])
+                    bordering_islands.add(pos_to_island[(x, y)])
 
                 area = 1
                 for island in bordering_islands:
-                    area += self.island_to_area[island]
+                    area += island_to_area[island]
                 
                 # Update max area!
                 if res < area:
                     res = area
 
         return res
-    
-    def maxAreaOfIsland(self, grid):
-        N = len(grid)
-        max_area = 0
-        visited = set()
-        WATER, LAND = 0, 1
-        DIRECTIONS = [(0,1),(0,-1),(1,0),(-1,0)]
-        inBounds = lambda x, y: 0 <= x < N and 0 <= y < N
-        for i in range(N):
-            for j in range(N):
-                if grid[i][j] == WATER or (i, j) in visited:
-                    continue
-
-                island_id = self.generateUniqueIslandId()                
-                area = 0
-                # visited = set()
-                visited.add((i, j))
-                stack = [(i, j)]
-                while len(stack) > 0:
-                    x, y = stack.pop()
-                    self.pos_to_island[(x, y)] = island_id
-                    area += 1
-
-                    for dx, dy in DIRECTIONS:
-                        neigh_x, neigh_y = x + dx, y + dy
-                        if not inBounds(neigh_x, neigh_y):
-                            continue
-                        if (neigh_x, neigh_y) in visited:
-                            continue
-                        if grid[neigh_x][neigh_y] == WATER:
-                            continue
-                        visited.add((neigh_x, neigh_y))
-                        stack.append((neigh_x, neigh_y))
-                
-
-                # if modify_perimiter:
-                #     for i, j in visited:
-                #         for di, dj in DIRECTIONS:
-                #             x, y = i + di, j + dj
-                #             if inBounds(x, y) and (x, y) not in visited and grid[x][y] == WATER:
-                #                 self.perimiter.add((x, y))
-                
-                # assert len(visited) == area
-                self.island_to_area[island_id] = area
-                max_area = max(max_area, area)
-                # visited.update(visited)
-
-                # For every visited position in the island, let's see how large of a BFS
-                # we can extend outwards from it!
-                # for i, j in visited:
-                #     for di, dj in DIRECTIONS:
-                #         x, y = i + di, j + dj
-                #         if inBounds(x, y) and (x, y) not in visited and grid[x][y] == WATER:
-                #             # Run a BFS from this x,y to catch as MANY visited land cells as possible!
-                #             stack = [(i, j)]
-                #             extra_visited = set([(i, j)])
-                #             isVisited = lambda pos: pos in visited or pos in extra_visited
-
-                #             while len(stack) > 0:
-                #                 x, y = stack.pop()
-
-        
-        return max_area
