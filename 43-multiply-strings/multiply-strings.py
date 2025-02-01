362f69c5b43a17e8d1a61495a169f7cf4f222282
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-31 18:55:50 -0800

    Time: 3 ms (65.32%) | Memory: 18 MB (19.72%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,16 +1,24 @@
 class Solution:
     def convertStringToNum(self, str_num):
         d = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
-        base = 1
+        base = 10 ** (len(str_num) - 1)
         res = 0
-        for i in range(len(str_num) - 1, -1, -1):
-            digit = str_num[i]
+        for digit in str_num:
             res += d[digit] * base
-            base *= 10
+            base //= 10
         return res
+    
+    def convertNumToString(self, num):
+        d = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        string_digits = []
+        while True:
+            string_digits.append(d[num % 10])
+            num //= 10
+            if num == 0:
+                break
+        return "".join(string_digits[i] for i in range(len(string_digits) - 1, -1, -1))
 
     def multiply(self, num1: str, num2: str) -> str:
         num1, num2 = self.convertStringToNum(num1), self.convertStringToNum(num2)
-        return str(num1 * num2)
-
+        return self.convertNumToString(num1 * num2)
         
