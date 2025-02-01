397be1e6ba47b98354a5387698ef04153debcaa3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-01-31 16:37:46 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.50%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,15 +1,17 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         M, N = len(matrix), len(matrix[0])
+        COUNT = M * N
         inBounds = lambda x, y: 0 <= x < M and 0 <= y < N
 
+        # This will be used for direction turning!
         DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)] # RIGHT, DOWN, LEFT, UP
         direction_index = 0
         
+        visited = set([(0, 0)])
+        res = [matrix[0][0]]
         i, j = 0, 0
-        visited = set([(i, j)])
-        res = [matrix[i][j]]
-        while len(visited) < M * N:
+        while len(res) < COUNT:
             di, dj = DIRECTIONS[direction_index]
             next_i, next_j = i + di, j + dj
             if not inBounds(next_i, next_j) or (next_i, next_j) in visited:
@@ -21,5 +23,3 @@ class Solution:
             res.append(matrix[i][j])
         
         return res
-        return [matrix[i][j] for i, j in visited]
-
