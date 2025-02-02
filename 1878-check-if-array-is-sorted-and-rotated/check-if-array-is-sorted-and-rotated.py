cf4388e46ecb0d5b0aa2912f5993e959f47628e5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-01 16:18:02 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.79%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -2,14 +2,17 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         N = len(nums)
 
-        # smallest_num = float("inf")
-        # smallest_index = None
-        # for i, num in enumerate(nums):
-        #     if num <= smallest_num:
-        #         smallest_num = num
-        #         smallest_index = i
+        smallest_num = float("inf")
+        smallest_indices = []
+        for i, num in enumerate(nums):
+            if num < smallest_num:
+                smallest_num = num
+                smallest_indices = [i]
+            elif num == smallest_num:
+                smallest_indices.append(i)
 
-        for index in range(N):
+        for smallest_index in smallest_indices:
+            index = smallest_index
             prev_num = nums[index]
             res = True
             for _ in range(N):
