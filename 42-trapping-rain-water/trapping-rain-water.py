634834041a24ecf83444ad7e8485cb3d0de7b886
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-02 15:07:12 -0800

    Time: 7 ms (89.11%) | Memory: 19.2 MB (79.18%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,13 +1,7 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # return self.trapSuffixHeights(height)
-
-        N = len(height)
-
-        l, r = 1, N - 2
-        prefix_max = height[0]
-        suffix_max = height[N - 1]
-        res = 0
+        l, r = 0, len(height) - 1
+        res, prefix_max, suffix_max = 0, 0, 0
         while l <= r:
             if prefix_max < suffix_max:
                 # Left wall is bottleneck. So let's compute water gained at
@@ -22,7 +16,7 @@ class Solution:
                     prefix_max = h
                 continue
             
-            assert suffix_max <= prefix_max
+            # assert suffix_max <= prefix_max
             h = height[r]
             water = suffix_max - h
             if water > 0:
@@ -31,33 +25,5 @@ class Solution:
             r -= 1
             if suffix_max < h:
                 suffix_max = h
-            continue
         
-        return res
-
-
-
-    def trapSuffixHeights(self, height: List[int]) -> int:
-        N = len(height)
-        suffix_max = []
-        suffix_height = 0
-        for i in range(N - 1, -1, -1):
-            suffix_height = max(suffix_height, height[i])
-            suffix_max.append(suffix_height)
-        suffix_max = suffix_max[::-1]
-        suffix_max.append(0)
-
-        res = 0
-        prefix_height = 0
-        for i, h in enumerate(height):
-            bottleneck_height = suffix_max[i + 1]
-            if prefix_height < bottleneck_height:
-                bottleneck_height = prefix_height
-
-            if bottleneck_height > h:
-                res += bottleneck_height - h
-            
-            if prefix_height < h:
-                prefix_height = h
-
         return res
