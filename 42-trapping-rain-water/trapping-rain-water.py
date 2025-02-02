08b4ffd26b4dfa93bee936e273d149a755e94326
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-02 14:54:41 -0800

    Time: 35 ms (14.82%) | Memory: 19.4 MB (34.06%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,17 +4,14 @@ class Solution:
         # prefix_max = []
         suffix_max = []
         prefix_height, suffix_height = 0, 0
-        for i in range(N):
-            # prefix_height = max(prefix_height, height[i])
-            # prefix_max.append(prefix_height)
-    
-            suffix_height = max(suffix_height, height[N - 1 - i])
+        for i in range(N - 1, -1, -1):
+            suffix_height = max(suffix_height, height[i])
             suffix_max.append(suffix_height)
-        suffix_max = suffix_max[::-1]
+        # suffix_max = suffix_max[::-1]
 
         res = 0
         prefix_height = height[0]
         for i in range(1, N - 1):
-            res += max(0, min(prefix_height, suffix_max[i + 1]) - height[i])
+            res += max(0, min(prefix_height, suffix_max[N - i - 1]) - height[i])
             prefix_height = max(prefix_height, height[i])
         return res
