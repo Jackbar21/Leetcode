75580d37ec85265cfdf296c52d695fa8f29abd21
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-02 16:17:49 -0800

    Time: 4 ms (24.03%) | Memory: 18 MB (11.65%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,5 +1,35 @@
 class Solution:
+    def longestIncreasingSlidingWindow(self, nums):
+        prev_num = float("-inf")
+        res = 0
+        count = 0
+        for num in nums:
+            if prev_num < num:
+                count += 1
+                prev_num = num
+                continue
+            
+            res = max(res, count)
+            count = 1
+            prev_num = num
+        
+        return max(res, count)
+
+
+
+
+
+
+
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        return max(
+            self.longestIncreasingSlidingWindow(nums),      # longest strictly INCREASING subarray
+            self.longestIncreasingSlidingWindow(nums[::-1]) # longest strictly DECREASING subarray
+        )
+        
+
+
+
         N = len(nums)
         res = 0
         # Find largest strictly increasing/decreasing subarray from index i, for 0 <= i < N
