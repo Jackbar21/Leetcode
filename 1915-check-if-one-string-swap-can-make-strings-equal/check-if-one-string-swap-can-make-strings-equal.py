dbdbfa3978a9b66857237b5e2819b7df92fe2d09
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-04 17:51:58 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.15%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -11,8 +11,8 @@ class Solution:
                 continue
             
             diff_indices.append(i)
-            # if len(diff_indices) > 2:
-            #     return False
+            if len(diff_indices) > 2:
+                return False
         
         count = len(diff_indices)
         # No string swaps needed!
@@ -21,8 +21,8 @@ class Solution:
 
         # Check if swapping the letters at the two diff. indices will work!
         if count == 2:
-            index1, index2 = diff_indices
-            return s1[index1] == s2[index2] and s1[index2] == s2[index1]
+            i, j = diff_indices
+            return s1[i] == s2[j] and s1[j] == s2[i]
 
         # Need exactly 2 (if not 0) characters different to make swap work!
         return False
