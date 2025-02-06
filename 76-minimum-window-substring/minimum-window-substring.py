8322c844d827b8d3dde0ccbb4b7bb6a3c6591027
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-05 20:37:22 -0800

    Time: 792 ms (5.00%) | Memory: 18.4 MB (15.18%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        all_letters = alphabet + alphabet.upper()
+
+        dt = {letter: 0 for letter in all_letters}
+        for letter in t:
+            dt[letter] += 1
+
+        isSubset = lambda freq_dict: all(freq_dict[letter] >= dt[letter] for letter in dt) # O(1)
+        d = {letter: 0 for letter in all_letters}
+        
+        min_length = float("inf")
+        best_index = -1
+
+        l = 0
+        for r, letter in enumerate(s):
+            d[letter] += 1
+            while isSubset(d):
+                if r - l + 1 < min_length:
+                    min_length = r - l + 1
+                    best_index = l
+                
+                # Loop Invariant
+                d[s[l]] -= 1
+                l += 1
+                # if l == r:
+                #     break
+        
+        # One last time, in case solution is at end of string!
+        if isSubset(d) and r - l + 1 < min_length:
+            min_length = r - l + 1
+            best_index = l
+            
+        return s[best_index: best_index + min_length] if best_index != -1 else ""
