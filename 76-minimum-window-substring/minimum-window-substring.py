e454d0bdda1102542bc8fba7f7cfc7e4d9b35baf
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-05 21:15:35 -0800

    Time: 50 ms (94.25%) | Memory: 17.9 MB (87.25%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,4 +1,67 @@
 class Solution: 
+    def minWindow(self, s: str, t: str) -> str:
+        # Idea: Unlike 'minWindowNaive', instead of using a dictionary for comparisons
+        # (which yes is O(1) because s and t are only English letters, but otherwise WOULDN'T be!)
+        # What we can do is grab the initially tightest window of s starting from the first index
+        # that includes every letter in t (by starting with all of them, and shaving off the last one
+        # as long as it doesn't become a subset!)
+        # Then, we can keep restricting the window until we're no longer a subset of t, and remember which
+        # letter we must FIND AGAIN to become a superset of t once more! This makes the algorithm O(m + n)!
+
+        # Step 1: Make frequency dict of s and t
+        ds, dt = defaultdict(int), defaultdict(int)
+        for letter in s:
+            ds[letter] += 1
+        for letter in t:
+            dt[letter] += 1
+
+        # Step 2: Return "" if no such substring (since currently have ALL letters in s!)
+        for letter in dt:
+            if ds[letter] < dt[letter]:
+                return ""
+        
+        # Step 2: Shave off last letter of s as long as still supserset of t!
+        index = len(s) - 1
+        letter = s[index]
+        while ds[letter] > dt[letter]:
+            ds[letter] -= 1
+            index -= 1
+            letter = s[index]
+        
+        # assert index >= 0
+        min_length = index + 1
+        best_index = 0
+
+        # Remove last letter to make no longer subset of t
+        ds[letter] -= 1
+        needed_letter = letter
+        
+        l = 0
+        for r in range(index, len(s)):
+            letter = s[r]
+            ds[letter] += 1
+            if letter != needed_letter:
+                continue
+            
+            # Found needed letter, so constrain the window size!
+            needed_letter = None
+            while needed_letter is None:
+                left_letter = s[l]
+                s_freq, t_freq = ds[left_letter], dt[left_letter]
+                # assert s_freq >= t_freq
+
+                if s_freq == t_freq:
+                    needed_letter = left_letter
+                    if r - l + 1 < min_length:
+                        min_length = r - l + 1
+                        best_index = l
+                
+                # Loop Invariant
+                ds[left_letter] -= 1
+                l += 1
+
+        return s[best_index: best_index + min_length] if best_index != -1 else ""
+
     def minWindowNaive(self, s: str, t: str) -> str:
         if len(s) < len(t):
             return ""
@@ -61,67 +124,4 @@ class Solution:
             min_length = r - l + 1
             best_index = l
             
-        return s[best_index: best_index + min_length] if best_index != -1 else ""
-
-    def minWindow(self, s: str, t: str) -> str:
-        # Idea: Unlike 'minWindowNaive', instead of using a dictionary for comparisons
-        # (which yes is O(1) because s and t are only English letters, but otherwise WOULDN'T be!)
-        # What we can do is grab the initially tightest window of s starting from the first index
-        # that includes every letter in t (by starting with all of them, and shaving off the last one
-        # as long as it doesn't become a subset!)
-        # Then, we can keep restricting the window until we're no longer a subset of t, and remember which
-        # letter we must FIND AGAIN to become a superset of t once more! This makes the algorithm O(m + n)!
-
-        # Step 1: Make frequency dict of s and t
-        ds, dt = defaultdict(int), defaultdict(int)
-        for letter in s:
-            ds[letter] += 1
-        for letter in t:
-            dt[letter] += 1
-
-        # Step 2: Return "" if no such substring (since currently have ALL letters in s!)
-        for letter in dt:
-            if ds[letter] < dt[letter]:
-                return ""
-        
-        # Step 2: Shave off last letter of s as long as still supserset of t!
-        index = len(s) - 1
-        letter = s[index]
-        while ds[letter] > dt[letter]:
-            ds[letter] -= 1
-            index -= 1
-            letter = s[index]
-        
-        assert index >= 0
-        min_length = index + 1
-        best_index = 0
-
-        # Remove last letter to make no longer subset of t
-        ds[letter] -= 1
-        needed_letter = letter
-        
-        l = 0
-        for r in range(index, len(s)):
-            letter = s[r]
-            ds[letter] += 1
-            if letter != needed_letter:
-                continue
-            
-            # Found needed letter, so constrain the window size!
-            needed_letter = None
-            while needed_letter is None:
-                left_letter = s[l]
-                s_freq, t_freq = ds[left_letter], dt[left_letter]
-                # assert s_freq >= t_freq
-
-                if s_freq == t_freq:
-                    needed_letter = left_letter
-                    if r - l + 1 < min_length:
-                        min_length = r - l + 1
-                        best_index = l
-                
-                # Loop Invariant
-                ds[left_letter] -= 1
-                l += 1
-
         return s[best_index: best_index + min_length] if best_index != -1 else ""
