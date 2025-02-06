a72ace33de0402b58e73e3a1376e733c642f948f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-05 21:19:54 -0800

    Time: 51 ms (94.02%) | Memory: 18 MB (80.95%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,6 +1,5 @@
 class Solution: 
     def minWindow(self, s: str, t: str) -> str:
-        return self.minWindowNaive(s, t)
         # Idea: Unlike 'minWindowNaive', instead of using a dictionary for comparisons
         # (which yes is O(1) because s and t are only English letters, but otherwise WOULDN'T be!)
         # What we can do is grab the initially tightest window of s starting from the first index
@@ -89,7 +88,6 @@ class Solution:
                 continue
             
             while l <= r:
-                assert isSubset(d)
                 left_letter = s[l]
                 s_freq, t_freq = d[left_letter], dt[left_letter]
                 if s_freq < t_freq:
