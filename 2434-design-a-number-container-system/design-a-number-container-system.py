aed29eb77ab8d1b479ba52cd18894dedc9fcab18
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-07 17:19:28 -0800

    Time: 99 ms (89.71%) | Memory: 76.2 MB (93.63%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -1,27 +1,19 @@
 class NumberContainers:
-
     def __init__(self):
         self.index_to_num = {}
-        self.num_to_indices = defaultdict(list)
-        
+        self.num_to_indices = defaultdict(list) 
 
     def change(self, index: int, number: int) -> None:
-        # if index not in self.index_to_num:
-        #     self.index_to_num[index] = number
-        #     return
-        
-        # # Otherwise, need to replace current number!
+        # Otherwise, need to replace current number!
         self.index_to_num[index] = number
         heapq.heappush(self.num_to_indices[number], index)
-        
 
     def find(self, number: int) -> int:
         min_heap = self.num_to_indices[number]
-        while len(min_heap) > 0 and self.index_to_num[min_heap[0]] != number:
+        d = self.index_to_num
+        while min_heap and d[min_heap[0]] != number:
             heapq.heappop(min_heap)
         return min_heap[0] if len(min_heap) > 0 else -1
-        
-
 
 # Your NumberContainers object will be instantiated and called as such:
 # obj = NumberContainers()
