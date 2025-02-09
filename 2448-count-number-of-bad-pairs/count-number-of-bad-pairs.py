385d9965bdeb93459a37c44c13a8f5be65fd6dae
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-08 17:27:24 -0800

    Time: 85 ms (48.57%) | Memory: 38.8 MB (59.29%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,32 +1,22 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
+        # Idea: Let N = len(nums). Then, there are N * (N - 1) / 2 total possible pairs. Since there are
+        # likely much fewer good pairs than there are bad, given we know there are k good pairs, the number
+        # of bad pairs is simply (N * (N - 1) / 2) - K.
+        # A pair of indices (i, j) is a GOOD PAIR if i >= j or j - i == nums[j] - nums[i]
+        # <==> j + nums[i] != i + nums[j]
+        # <==> nums[i] - i != nums[j] - j
+        # Hence for every single index i, we can map it to the value nums[i] - i, to use it for
+        # comparison with other indices in nums! Then if two indices map to the same value, we know
+        # those indices form ONE GOOD PAIR! Hence, we can add the frequency of each of these "values"
+        # to a hash map, then counting number of total good pairs, subtracting that from the total
+        # number of pairs in general, to finally obtain the number of BAD pairs.
         N = len(nums)
         d = defaultdict(int)
         for i, num in enumerate(nums):
             d[num - i] += 1
-        
-        good_pairs = 0
-        for k in d.values():
-            good_pairs += (k * (k - 1)) // 2
 
-        total_pairs = (N * (N - 1)) // 2
+        good_pairs = functools.reduce(lambda acc, k: acc + k * (k - 1) // 2, d.values(), 0)
+        total_pairs = N * (N - 1) // 2
         bad_pairs = total_pairs - good_pairs
         return bad_pairs
-        
-        print(f"{d=}")
-        res = 0
-        N = len(nums)
-        for i in range(N):
-            num_i = nums[i]
-            for j in range(i + 1, N):
-                res += (j - i != nums[j] - num_i)
-        return res
-        # return sum(j - i != nums[j] - nums[i] for i in range(len(nums)) for j in range(i + 1, len(nums)))
-
-        # Idea: Let N = len(nums). Then, there are N * (N - 1) / 2 total possible pairs. Since there are
-        # likely much fewer good pairs than there are bad, given we know there are k good pairs, the number
-        # of bad pairs is simply (N * (N - 1) / 2) - K.
-
-        # A pair of indices (i, j) is a GOOD PAIR if i >= j or j - i == nums[j] - nums[i]
-        # j + nums[i] != i + nums[j]
-        # nums[i] - i != nums[j] - j
