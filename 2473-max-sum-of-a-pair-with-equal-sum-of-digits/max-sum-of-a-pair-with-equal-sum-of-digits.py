964da9bea2834350a6002e1cfe6d45b43f5918c6
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-11 17:07:49 -0800

    Time: 266 ms (90.32%) | Memory: 34 MB (8.34%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -15,21 +15,23 @@ class Solution:
         for num in nums:
             # sum_digits = functools.reduce(lambda acc, x: acc + int(x), str(num), 0)
             sum_digits = convertNumToSumDigits(num)
-            lst = d[sum_digits]
-            lst.append(num)
-            if len(lst) <= 2:
-                continue
-            else:
-                # Get rid of smallest between three numbers!
-                # assert len(lst) == 3
-                lst.sort(reverse=True) # O(1)
-                lst.pop()
+            min_heap = d[sum_digits]
+            heapq.heappush(min_heap, num)
+            while len(min_heap) > 2:
+                heapq.heappop(min_heap)
+            # if len(lst) <= 2:
+            #     continue
+            # else:
+            #     # Get rid of smallest between three numbers!
+            #     # assert len(lst) == 3
+            #     lst.sort(reverse=True) # O(1)
+            #     lst.pop()
 
-        max_val = -1
+        max_val = -1 # default value if no results!
         for nums_with_same_digit_sum in d.values():
             if len(nums_with_same_digit_sum) > 1:
                 # assert len(nums_with_same_digit_sum) == 2
-                sum_nums = sum(nums_with_same_digit_sum)
+                sum_nums = nums_with_same_digit_sum[0] + nums_with_same_digit_sum[1]
                 if max_val < sum_nums:
                     max_val = sum_nums
         return max_val
