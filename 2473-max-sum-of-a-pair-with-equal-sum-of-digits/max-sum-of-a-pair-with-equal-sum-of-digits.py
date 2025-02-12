a14d304b1c744d0dca474ba92ba3e923996b02dd
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-11 16:59:29 -0800

    Time: 637 ms (7.41%) | Memory: 33.5 MB (78.99%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -9,9 +9,12 @@ class Solution:
         for num in nums:
             sum_digits = functools.reduce(lambda acc, x: acc + int(x), str(num), 0)
             d[sum_digits].append(num)
+            d[sum_digits].sort(reverse=True)
+            while len(d[sum_digits]) > 2:
+                d[sum_digits].pop()
         
         max_val = -1
         for nums_with_same_digit_sum in d.values():
             if len(nums_with_same_digit_sum) > 1:
-                max_val = max(max_val, sum(sorted(nums_with_same_digit_sum)[-2:]))
+                max_val = max(max_val, sum(nums_with_same_digit_sum))
         return max_val
