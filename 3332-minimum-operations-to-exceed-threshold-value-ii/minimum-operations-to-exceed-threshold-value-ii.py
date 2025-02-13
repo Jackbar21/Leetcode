3e53a9700e4ae53b3a08d770e0333d9dd59bc598
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-12 16:55:45 -0800

    Time: 223 ms (63.87%) | Memory: 35.4 MB (72.27%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -4,7 +4,7 @@ class Solution:
         count = 0
         while len(nums) >= 2 and nums[0] < k:
             x, y = heapq.heappop(nums), heapq.heappop(nums)
-            heapq.heappush(nums, min(x, y) * 2 + max(x, y))
+            heapq.heappush(nums, x * 2 + y if x < y else y * 2 + x)
             count += 1
         return count
         
