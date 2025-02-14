726d63c61b4154a668c20a0b2e53004626ec56dc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-13 16:48:44 -0800

    Time: 121 ms (11.72%) | Memory: 166.5 MB (5.63%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -7,7 +7,7 @@ class ProductOfNumbers:
     def add(self, num: int) -> None:
         is_zero = num == 0
         mult_val = 1 if is_zero else num
-        zero_val = is_zero
+        zero_val = int(is_zero)
 
         if self.N > 0:
             mult_val *= self.prefix_mults[-1]
@@ -32,3 +32,13 @@ class ProductOfNumbers:
 # obj = ProductOfNumbers()
 # obj.add(num)
 # param_2 = obj.getProduct(k)
+
+
+# nums, len(nums) == N
+# prefix_sums[i] = sum(nums[0..i])
+
+# sum(nums[i..j]) == sum(nums[0..j]) - sum(nums[0..i-1])
+#                 == prefix_sums[j] - (prefix_sums[i-1] if i - 1 >= 0 else 0)
+
+# prefix_mults[i] = product(nums[0..i])
+# product(nums[i..j]) == product(nums[0..j]) / product(nums[0..i-1])
