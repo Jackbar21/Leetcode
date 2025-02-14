cffd770a36fcd297b543819ce9e9f5500c8b57d2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-13 16:34:31 -0800

    Time: 79 ms (17.66%) | Memory: 166.6 MB (5.63%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -1,27 +1,32 @@
 class ProductOfNumbers:
-
     def __init__(self):
         self.prefix_mults = []
-        self.num_zeroes = []
+        self.prefix_zeroes = []
+        self.N = 0
 
     def add(self, num: int) -> None:
-        self.prefix_mults.append((1 if num == 0 else num) * (1 if len(self.prefix_mults) == 0 else self.prefix_mults[-1]))
-        self.num_zeroes.append((num == 0) + (0 if len(self.num_zeroes) == 0 else self.num_zeroes[-1]))
+        is_zero = num == 0
+        mult_val = 1 if is_zero else num
+        zero_val = int(is_zero)
+
+        if self.N > 0:
+            mult_val *= self.prefix_mults[-1]
+            zero_val += self.prefix_zeroes[-1]
+        
+        self.prefix_mults.append(mult_val)
+        self.prefix_zeroes.append(zero_val)
+        self.N += 1
 
     def getProduct(self, k: int) -> int:
-        # return functools.reduce(lambda acc, x: acc * x, self.queue[-k:], 1)
-        # print(f"{self.prefix_mults=}")
-        numerator = self.prefix_mults[-1]
-        denominator = (self.prefix_mults[len(self.prefix_mults) - 1 - k] if len(self.prefix_mults) - 1 - k >= 0 else 1)
+        index = self.N - 1 - k
+        in_bounds = index >= 0
 
-        num_zeroes = self.num_zeroes[-1] - (self.num_zeroes[len(self.num_zeroes) - 1 - k] if len(self.prefix_mults) - 1 - k >= 0 else 0)
-        assert num_zeroes >= 0
+        num_zeroes = self.prefix_zeroes[-1] - (0 if not in_bounds else self.prefix_zeroes[index])
         if num_zeroes > 0:
             return 0
-        # if denominator == 0:
-        #     return 0
-        return numerator // denominator
-
+        
+        res = self.prefix_mults[-1] // (1 if not in_bounds else self.prefix_mults[index])
+        return res
 
 # Your ProductOfNumbers object will be instantiated and called as such:
 # obj = ProductOfNumbers()
