4ab8e0523d6184a19b9ba64cb7bfed0acf4aaf72
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-16 17:45:00 -0800

    Time: 26 ms (77.51%) | Memory: 17.7 MB (96.07%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,24 +1,24 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        res = []
+        self.res = 0
 
         d = {}
         for tile in tiles:
             d[tile] = d.get(tile, 0) + 1
 
-        arr = []
+        # arr = []
         def backtrack():
-            res.append("".join(arr))
+            self.res += 1
             for letter, freq in d.items():
                 # assert freq >= 0
                 if freq == 0:
                     continue
                 
-                arr.append(letter)
+                # arr.append(letter)
                 d[letter] -= 1
                 backtrack()
                 # assert arr.pop() == letter
                 d[letter] += 1
         
         backtrack()
-        return len(res) - 1 # Exclude empty string from results!
+        return self.res - 1 # Exclude empty string from results!
