ed80a6fc69ea24385e48e78e05a81e336a451c96
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-16 17:54:13 -0800

    Time: 22 ms (85.77%) | Memory: 18 MB (49.32%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,26 +1,25 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        res = []
+        self.res = 0
 
+        # Setup "options" tile-to-frequency mappings
         d = {}
         for tile in tiles:
             d[tile] = d.get(tile, 0) + 1
 
-        arr = []
         def backtrack():
-            res.append("".join(arr))
+            self.res += 1
             for letter, freq in d.items():
-                # assert freq >= 0
                 if freq == 0:
                     continue
                 
-                arr.append(letter)
+                # Backtrack logic
                 d[letter] -= 1
                 backtrack()
-                assert arr.pop() == letter
                 d[letter] += 1
         
+        # Initial backtrack call
         backtrack()
 
         # Exclude empty string from results!
-        return len(res) - 1
+        return self.res - 1
