cf7a1ab1810bffbdf31fe7a3da442dddbfa8c02e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-17 19:06:28 -0800

    Time: 552 ms (6.21%) | Memory: 17.9 MB (38.51%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -5,13 +5,11 @@ class Solution:
                 return "".join(num)
     
     def isValid(self, num: tuple, pattern: str) -> bool:
-        assert len(num) == len(pattern) + 1
-        N = len(num)
-
-        for i in range(N - 1):
-            if pattern[i] == "I" and not (num[i] < num[i + 1]):
+        # assert len(num) == len(pattern) + 1
+        for i in range(len(num) - 1):
+            cur_num, next_num = num[i], num[i + 1]
+            if pattern[i] == "I" and not (cur_num < next_num):
                 return False
-            elif pattern[i] == "D" and not (num[i] > num[i + 1]):
+            elif pattern[i] == "D" and not (cur_num > next_num):
                 return False
-
         return True
