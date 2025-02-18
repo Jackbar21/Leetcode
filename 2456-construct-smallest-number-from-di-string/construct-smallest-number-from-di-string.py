408f3635eebf7fa107db05deac8707d6221b6c57
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-17 19:25:18 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.51%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,7 +1,5 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        return self.naive(pattern)
-
         # Instead of raw brute force, we can also try backtracking!
         # Since we want lexicographically SMALLEST result, we can enumerate
         # the possible strings in order of smallest to largest lexicographically,
@@ -32,21 +30,3 @@ class Solution:
         
         backtrack(0)
         return "".join(res)
-
-
-    def naive(self, pattern: str) -> str:
-        for num in itertools.permutations("123456789", len(pattern) + 1):
-            if self.isValid(num, pattern):
-                return "".join(num)
-
-   
-    def isValid(self, num: tuple, pattern: str) -> bool:
-        for i in range(len(num) - 1):
-            cur_num, next_num = num[i], num[i + 1]
-            if (
-                (pattern[i] == "I" and cur_num >= next_num) or 
-                (pattern[i] == "D" and cur_num <= next_num)
-            ):
-                return False
-
-        return True
