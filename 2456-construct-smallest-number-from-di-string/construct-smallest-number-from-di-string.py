34cc0684c40c2423d5a7d0ec484b2f1a3cbb73fd
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-17 19:21:43 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.23%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -9,12 +9,11 @@ class Solution:
         available_nums = set("123456789")
         res = []
         def backtrack(i):
-            sorted_nums = sorted(available_nums)
-            prev = res[-1] if len(res) > 0 else float("-inf")
-            
             if i == len(pattern) + 1:
                 return True
 
+            sorted_nums = sorted(available_nums)
+            prev = res[-1] if len(res) > 0 else float("-inf")
             for num in sorted_nums:
                 if (i > 0) and (
                     (pattern[i - 1] == "I" and prev >= num) or 
@@ -34,17 +33,13 @@ class Solution:
         backtrack(0)
         return "".join(res)
 
-    
-
-
-
-
 
     def naive(self, pattern: str) -> str:
         for num in itertools.permutations("123456789", len(pattern) + 1):
             if self.isValid(num, pattern):
                 return "".join(num)
-    
+
+   
     def isValid(self, num: tuple, pattern: str) -> bool:
         for i in range(len(num) - 1):
             cur_num, next_num = num[i], num[i + 1]
