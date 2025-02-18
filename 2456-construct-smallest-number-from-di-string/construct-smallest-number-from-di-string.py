5df76a2818db900839d383e7ba9dd636aa2b5fee
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-17 19:19:39 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.18%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,7 +1,46 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        return self.naive(pattern)
+        # return self.naive(pattern)
+
+        # Instead of raw brute force, we can also try backtracking!
+        # Since we want lexicographically SMALLEST result, we can enumerate
+        # the possible strings in order of smallest to largest lexicographically,
+        # and stop as soon as we find one that is valid :)
+
+        available_nums = set("123456789")
+        res = []
+        def backtrack(i):
+            sorted_nums = sorted(available_nums)
+            prev = res[-1] if len(res) > 0 else float("-inf")
+            
+            if len(res) == len(pattern) + 1:
+                return True
+
+            for num in sorted_nums:
+                if (i > 0) and (
+                    (pattern[i - 1] == "I" and prev >= num) or 
+                    (pattern[i - 1] == "D" and prev <= num)
+                ):
+                    continue
+                
+                res.append(num)
+                available_nums.remove(num)
+                if backtrack(i + 1):
+                    return True
+                available_nums.add(num)
+                assert res.pop() == num
+
+            return False
+        
+        backtrack(0)
+        return "".join(res)
+
     
+
+
+
+
+
     def naive(self, pattern: str) -> str:
         for num in itertools.permutations("123456789", len(pattern) + 1):
             if self.isValid(num, pattern):
