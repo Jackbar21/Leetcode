ed35a878b940cfbf89007779df22f53b6a8571ac
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-17 19:09:51 -0800

    Time: 556 ms (6.21%) | Memory: 18 MB (17.18%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,16 +1,19 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        def isValid(num: tuple, pattern: str) -> bool:
-            for i in range(len(num) - 1):
-                cur_num, next_num = num[i], num[i + 1]
-                if (
-                    (pattern[i] == "I" and cur_num >= next_num) or 
-                    (pattern[i] == "D" and cur_num <= next_num)
-                ):
-                    return False
-            return True
+        return self.naive(pattern)
+    
+    def naive(self, pattern: str) -> str:
         for num in itertools.permutations("123456789", len(pattern) + 1):
-            if isValid(num, pattern):
+            if self.isValid(num, pattern):
                 return "".join(num)
     
-    
+    def isValid(self, num: tuple, pattern: str) -> bool:
+        for i in range(len(num) - 1):
+            cur_num, next_num = num[i], num[i + 1]
+            if (
+                (pattern[i] == "I" and cur_num >= next_num) or 
+                (pattern[i] == "D" and cur_num <= next_num)
+            ):
+                return False
+
+        return True
