836677a0dd3d705c29e8393576f096f5ae31a7ee
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-18 18:33:34 -0800

    Time: 6 ms (71.55%) | Memory: 17.9 MB (56.02%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,5 +1,39 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
+        # return self.yielding(n, k)
+
+        arr = []
+        self.count = 0
+        self.res = ""
+        def backtrack():
+            if len(arr) == n:
+                self.count += 1
+                if self.count == k:
+                    self.res = "".join(arr)
+                return
+            
+            # If found solution, then stop!
+            if self.res != "":
+                return
+            
+            prev_char = arr[-1] if len(arr) > 0 else None
+            for char in ["a", "b", "c"]:
+                if char == prev_char:
+                    continue
+
+                arr.append(char)
+                backtrack()
+                arr.pop()
+
+                # If found solution, then stop!
+                if self.res != "":
+                    return
+        
+        backtrack()
+        return self.res
+
+    
+    def yielding(self, n: int, k: int) -> str:
         arr = []
         def backtrack():
             if len(arr) == n:
