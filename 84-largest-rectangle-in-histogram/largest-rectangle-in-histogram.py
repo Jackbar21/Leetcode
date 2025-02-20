dcc04adbc1ee7428f3c32b596204054e591b8de3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-19 23:17:39 -0800

    Time: 259 ms (10.26%) | Memory: 37.4 MB (6.63%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -14,8 +14,47 @@ class Solution:
             index = j
 
         return max_area
+    
+    def neetcode(self, heights: List[int]) -> int:
+        print(f"{heights=}")
+        N = len(heights)
+        INDEX, HEIGHT = 0, 1
+
+        next_smallest = [-1] * N
+        stack = [(None, float("-inf"))]
+        for index, height in enumerate(heights):
+            while height < stack[-1][HEIGHT]:
+                prev_index, _ = stack.pop()
+                next_smallest[prev_index] = index
+            stack.append((index, height))
+        
+        prev_smallest = [-1] * N
+        stack = [(None, float("-inf"))]
+        for index in range(N - 1, -1, -1):
+            height = heights[index]
+            # print(f"{stack=}, {index=}, {height=}")
+            while height < stack[-1][HEIGHT]:
+                next_index, _ = stack.pop()
+                prev_smallest[next_index] = index
+            stack.append((index, height))
+        # prev_smallest = prev_smallest[::-1]
+        
+        # print(f"{next_smallest=}")
+        # print(f"{prev_smallest=}")
+
+        max_area = 0
+        for index, height in enumerate(heights):
+            left = prev_smallest[index] + 1 if prev_smallest[index] != -1 else 0
+            right = next_smallest[index] - 1 if next_smallest[index] != -1 else N - 1
+            area = (right - left + 1) * height
+            if max_area < area:
+                max_area = area
+
+        return max_area
 
     def largestRectangleArea(self, heights: List[int]) -> int:
+        return self.neetcode(heights)
+
         suffix_min = []
         min_height = float("inf")
         for height in reversed(heights):
