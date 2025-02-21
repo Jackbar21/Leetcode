7c0f98fcf0ef0b7ab7362800010f5986a41275d1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-20 17:45:46 -0800

    Time: 6 ms (70.59%) | Memory: 22.3 MB (37.43%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -6,15 +6,25 @@
 #         self.right = right
 class FindElements:
     def __init__(self, root: Optional[TreeNode]):
-        self.nums = set()
-        def inorder(treeNode, x):
-            if treeNode is None:
-                return
+        # self.nums = set()
+        # def inorder(treeNode, x):
+        #     if treeNode is None:
+        #         return
             
-            inorder(treeNode.left, 2 * x + 1)
-            self.nums.add(x)
-            inorder(treeNode.right, 2 * x + 2)
-        inorder(root, 0)
+        #     inorder(treeNode.left, 2 * x + 1)
+        #     self.nums.add(x)
+        #     inorder(treeNode.right, 2 * x + 2)
+        # inorder(root, 0)
+        nums = set()
+        stack = [(root, 0)]
+        while stack:
+            treeNode, x = stack.pop()
+            nums.add(x)
+            if treeNode.left:
+                stack.append((treeNode.left, 2 * x + 1))
+            if treeNode.right:
+                stack.append((treeNode.right, 2 * x + 2))
+        self.nums = nums
         
     def find(self, target: int) -> bool:
         return target in self.nums    
