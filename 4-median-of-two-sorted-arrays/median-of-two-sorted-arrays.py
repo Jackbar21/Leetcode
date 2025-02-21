52cbe13da8ffe4696893e7ebd3ccc9bb69419e0d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-20 19:48:45 -0800

    Time: 1 ms (66.02%) | Memory: 18.3 MB (20.52%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,30 +1,36 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         # O((M+N)), where N = len(nums1), M = len(num2)
-        N, M = len(nums1), len(nums2)
-        i1, i2 = 0, 0
-        nums = []
-        while i1 < N and i2 < M:
-            num1, num2 = nums1[i1], nums2[i2]
-            if num1 < num2:
-                nums.append(num1)
-                i1 += 1
-            else:
-                nums.append(num2)
-                i2 += 1
-        nums.extend(nums1[i1:] if i1 < N else nums2[i2:])
-        L = len(nums)
+        # N, M = len(nums1), len(nums2)
+        # i1, i2 = 0, 0
+        # nums = []
+        # while i1 < N and i2 < M:
+        #     num1, num2 = nums1[i1], nums2[i2]
+        #     if num1 < num2:
+        #         nums.append(num1)
+        #         i1 += 1
+        #     else:
+        #         nums.append(num2)
+        #         i2 += 1
+        # nums.extend(nums1[i1:] if i1 < N else nums2[i2:])
+        # L = len(nums)
+        # if L % 2 == 1:
+        #     return nums[L // 2]
+        # return (nums[L // 2] + nums[-1 + L // 2]) / 2
+
+        L = len(nums1) + len(nums2)
+        self.L, self.nums1, self.nums2 = L, nums1, nums2
+        
         if L % 2 == 1:
-            return nums[L // 2]
-        return (nums[L // 2] + nums[-1 + L // 2]) / 2
+            return self.helper(L // 2)
+        return (self.helper(L // 2) + self.helper(L // 2 - 1)) / 2
 
-        # For debugging purposes!
-        # EXPECTED = sorted(nums1 + nums2)
+    # Returns number at 'target_index' between nums1 and nums2 in O(log (m+n)) time!
+    def helper(self, target_index):
+        L, nums1, nums2 = self.L, self.nums1, self.nums2
+        assert 0 <= target_index < L
+        return sorted(nums1 + nums2)[target_index]
 
-        # # Define a helper function that can find the number at some target index in
-        # # O(log (m+n)) time!
-        # def helper(nums1, nums2, target_index):
-        #     return sorted(nums1 + nums2)
         
         # # Get to index (N//2)-1
         # N = len(nums1) + len(nums2)
