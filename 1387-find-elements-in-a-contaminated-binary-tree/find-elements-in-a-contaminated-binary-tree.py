9e7557307c585beb185ff215c7fc1ecc830a9ff1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-20 17:38:22 -0800

    Time: 3 ms (93.05%) | Memory: 22.2 MB (66.31%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -5,25 +5,19 @@
 #         self.left = left
 #         self.right = right
 class FindElements:
-
     def __init__(self, root: Optional[TreeNode]):
         self.nums = set()
         def inorder(treeNode, x):
             if treeNode is None:
                 return
             
-            self.nums.add(x)
             inorder(treeNode.left, 2 * x + 1)
+            self.nums.add(x)
             inorder(treeNode.right, 2 * x + 2)
-            
-
         inorder(root, 0)
         
-
     def find(self, target: int) -> bool:
         return target in self.nums
-        
-
 
 # Your FindElements object will be instantiated and called as such:
 # obj = FindElements(root)
