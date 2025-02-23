c9eded1be4b2eac2f72e2dd2cb70e8ed55817b8d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-22 20:40:25 -0800

    Time: 7 ms (9.15%) | Memory: 17.8 MB (71.53%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -11,8 +11,8 @@ class Solution:
 
         pre_index, post_index = 0, 0
         val_to_node = {}
-        print(f"{preorder=}")
-        print(f"{postorder=}")
+        #print(f"{preorder=}")
+        #print(f"{postorder=}")
 
         
         root = TreeNode(preorder[0])
@@ -21,26 +21,27 @@ class Solution:
         visited = set() # values, not nodes!
         visited.add(root.val)
         while post_index < len(postorder):
-            print(f"{visited=}")
-            print(f"{post_index=}, {postorder[post_index]=}")
+            #print(f"{visited=}")
+            #print(f"{post_index=}, {postorder[post_index]=}")
             if (val := postorder[post_index]) in visited:
                 post_index += 1
                 while stack[-1].val != val: # all values unique!
                     stack.pop()
-                assert stack.pop().val == val
+                #assert stack.pop().val == val
+                stack.pop()
                 continue
         # for i in range(1):
-            print(f"{pre_index=}, {post_index=}")
+            #print(f"{pre_index=}, {post_index=}")
             while (val := preorder[pre_index]) != postorder[post_index]:
                 node = TreeNode(val)
-                assert val not in visited
+                #assert val not in visited
                 visited.add(val)
                 prev_node = stack[-1]
-                # assert not prev_node.left
+                # #assert not prev_node.left
                 if not prev_node.left:
                     prev_node.left = node
                 else:
-                    assert not prev_node.right
+                    #assert not prev_node.right
                     prev_node.right = node
                 # stack[-1].left = node
                 stack.append(node)
@@ -48,14 +49,14 @@ class Solution:
                 pre_index += 1
             # One more time
             node = TreeNode(val)
-            assert val not in visited
+            #assert val not in visited
             visited.add(val)
             # stack[-1].left = node
             prev_node = stack[-1]
             if not prev_node.left:
                 prev_node.left = node
             else:
-                assert not prev_node.right
+                #assert not prev_node.right
                 prev_node.right = node
             stack.append(node)
             pre_index += 1
@@ -65,14 +66,14 @@ class Solution:
             # pre_index += 1
             # post_index += 1
 
-            # print(f"{val_to_node=}")
-            # print(f"{stack=}")
-            print("STACK START")
-            for line in stack:
-                print(f"node={line}")
-            print("STACK END")
-            print(f"{preorder[pre_index:]=}")
-            print(f"{postorder[post_index:]=}")
+            # #print(f"{val_to_node=}")
+            # #print(f"{stack=}")
+            #print("STACK START")
+            # for line in stack:
+                #print(f"node={line}")
+            #print("STACK END")
+            #print(f"{preorder[pre_index:]=}")
+            #print(f"{postorder[post_index:]=}")
 
             stack.pop()
         
