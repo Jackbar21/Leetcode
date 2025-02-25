ea32820a2ac3b23a41243e0e84c593dcc913d3e2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-24 23:13:45 -0800

    Time: 395 ms (5.11%) | Memory: 190.7 MB (5.11%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -5,29 +5,31 @@ class Solution:
 
         memo = {}
         def dp(i):
+            # Returns (NUM_EVEN_SUBARRAYS, NUM_ODD_SUBARRAYS)
             if i in memo:
                 return memo[i]
-            # Returns (NUM_EVEN_SUBARRAYS, NUM_ODD_SUBARRAYS)
-            # if i == len(arr) - 1:
-            #     return (arr[i] % 2 == 0, arr[i] % 2 == 1)
+
+            # Base Case: End of subarray, empty subarray => no even / odd sum subarrays!
             if i >= len(arr):
                 return (0, 0)
             
+            # Get number of even & odd subarrays from index i+1 onwards recursively!
             num_even, num_odd = dp(i + 1)
-            # if arr[i] % 2 == 0:
-            #     # Since we have an even number, just by itself it forms a length 1 even subarray!
-            #     return (num_even + 1, num_odd)
+
+            # If number is odd, then it will form a length 1 odd subarray, and all even subarrays from
+            # index i+1 onwards will now become odd subarrays after including number at index i, and
+            # similarly all odd subarrays will now become even subarrays!
+            if arr[i] & 1:
+                memo[i] = (num_odd, num_even + 1)
+                return memo[i]
             
-            # # Since we have an odd number, just by itself it forms a length 1 odd subarray!
-            # # Since we have an odd number, adding it to any even subarray will make it odd,
-            # # and adding it to any odd subarray will make it even, hence we swap the two values!
-            # # But of course, we increase number of odd subarrays by one as well, for the singular
-            # # element :)
-            # return (num_odd, num_even + 1)
-            memo[i] = (num_even + 1, num_odd) if arr[i] % 2 == 0 else (num_odd, num_even + 1)
+            # If number is even, then it will form a length 1 even subarray, and all even subarrays
+            # from index i+1 onwards will remain even after including number at index i, and similarly
+            # for all odd subarrays!
+            memo[i] = (num_even + 1, num_odd)
             return memo[i]
 
         res = 0
         for i in range(N):
-            res += dp(i)[1]
+            res += dp(i)[1] # Index 1 is for odd sum counts!
         return res % MOD
