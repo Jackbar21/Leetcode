1f908d88bdddbdfc5c37f87903ef82a96b6593d0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-24 23:06:13 -0800

    Time: 449 ms (5.11%) | Memory: 48.1 MB (5.11%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -17,11 +17,13 @@ class Solution:
 
         self.isOddSubarray = isOddSubarray
         self.arr = arr
+        self.memo = {}
         EVEN, ODD = 0, 1
         return sum(self.dp(i)[ODD] for i in range(N)) % MOD
 
-    @cache
     def dp(self, i):
+        if i in self.memo:
+            return self.memo[i]
         # Returns (NUM_EVEN_SUBARRAYS, NUM_ODD_SUBARRAYS)
         # if i == len(self.arr) - 1:
         #     return (self.arr[i] % 2 == 0, self.arr[i] % 2 == 1)
@@ -29,16 +31,19 @@ class Solution:
             return (0, 0)
         
         num_even, num_odd = self.dp(i + 1)
-        if self.arr[i] % 2 == 0:
-            # Since we have an even number, just by itself it forms a length 1 even subarray!
-            return (num_even + 1, num_odd)
+        # if self.arr[i] % 2 == 0:
+        #     # Since we have an even number, just by itself it forms a length 1 even subarray!
+        #     return (num_even + 1, num_odd)
         
-        # Since we have an odd number, just by itself it forms a length 1 odd subarray!
-        # Since we have an odd number, adding it to any even subarray will make it odd,
-        # and adding it to any odd subarray will make it even, hence we swap the two values!
-        # But of course, we increase number of odd subarrays by one as well, for the singular
-        # element :)
-        return (num_odd, num_even + 1)
+        # # Since we have an odd number, just by itself it forms a length 1 odd subarray!
+        # # Since we have an odd number, adding it to any even subarray will make it odd,
+        # # and adding it to any odd subarray will make it even, hence we swap the two values!
+        # # But of course, we increase number of odd subarrays by one as well, for the singular
+        # # element :)
+        # return (num_odd, num_even + 1)
+        res = (num_even + 1, num_odd) if self.arr[i] % 2 == 0 else (num_odd, num_even + 1)
+        self.memo[i] = res
+        return res
         
 
         
