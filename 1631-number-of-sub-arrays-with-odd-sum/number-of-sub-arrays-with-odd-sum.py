ef527a493059fa02b998f638d9ff2c0e12bb123a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-24 23:08:11 -0800

    Time: 395 ms (5.11%) | Memory: 191 MB (5.11%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -3,49 +3,32 @@ class Solution:
         N = len(arr)
         MOD = pow(10, 9) + 7
 
-        # Compute prefix sums to be able to obtain sum of
-        # ANY subarray in arr in O(1) time
-        # prefix_sums = []
-        # cur_sum = 0
-        # for num in arr:
-        #     cur_sum += num
-        #     prefix_sums.append(cur_sum)
+        memo = {}
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            # Returns (NUM_EVEN_SUBARRAYS, NUM_ODD_SUBARRAYS)
+            # if i == len(arr) - 1:
+            #     return (arr[i] % 2 == 0, arr[i] % 2 == 1)
+            if i >= len(arr):
+                return (0, 0)
+            
+            num_even, num_odd = dp(i + 1)
+            # if arr[i] % 2 == 0:
+            #     # Since we have an even number, just by itself it forms a length 1 even subarray!
+            #     return (num_even + 1, num_odd)
+            
+            # # Since we have an odd number, just by itself it forms a length 1 odd subarray!
+            # # Since we have an odd number, adding it to any even subarray will make it odd,
+            # # and adding it to any odd subarray will make it even, hence we swap the two values!
+            # # But of course, we increase number of odd subarrays by one as well, for the singular
+            # # element :)
+            # return (num_odd, num_even + 1)
+            res = (num_even + 1, num_odd) if arr[i] % 2 == 0 else (num_odd, num_even + 1)
+            memo[i] = res
+            return res
 
-        # getSubarraySum = lambda i, j: prefix_sums[j] - (prefix_sums[i - 1] if i > 0 else 0) # O(1)
-        # isOddSubarray = lambda i, j: getSubarraySum(i, j) & 1 # O(1)
-        # return sum(isOddSubarray(i, j) for i in range(N) for j in range(i, N)) # O(N^2)
-
-        # self.isOddSubarray = isOddSubarray
-        self.arr = arr
-        self.memo = {}
-        EVEN, ODD = 0, 1
-        return sum(self.dp(i)[ODD] for i in range(N)) % MOD
-
-    def dp(self, i):
-        if i in self.memo:
-            return self.memo[i]
-        # Returns (NUM_EVEN_SUBARRAYS, NUM_ODD_SUBARRAYS)
-        # if i == len(self.arr) - 1:
-        #     return (self.arr[i] % 2 == 0, self.arr[i] % 2 == 1)
-        if i >= len(self.arr):
-            return (0, 0)
-        
-        num_even, num_odd = self.dp(i + 1)
-        # if self.arr[i] % 2 == 0:
-        #     # Since we have an even number, just by itself it forms a length 1 even subarray!
-        #     return (num_even + 1, num_odd)
-        
-        # # Since we have an odd number, just by itself it forms a length 1 odd subarray!
-        # # Since we have an odd number, adding it to any even subarray will make it odd,
-        # # and adding it to any odd subarray will make it even, hence we swap the two values!
-        # # But of course, we increase number of odd subarrays by one as well, for the singular
-        # # element :)
-        # return (num_odd, num_even + 1)
-        res = (num_even + 1, num_odd) if self.arr[i] % 2 == 0 else (num_odd, num_even + 1)
-        self.memo[i] = res
-        return res
-        
-
-        
-        
-
+        res = 0
+        for i in range(N):
+            res += dp(i)[1]
+        return res % MOD
