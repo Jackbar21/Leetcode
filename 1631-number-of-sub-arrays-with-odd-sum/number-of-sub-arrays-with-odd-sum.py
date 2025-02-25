2294e30f2c26df3d45c363bbafa8e5556bd2a965
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-24 23:21:22 -0800

    Time: 158 ms (11.83%) | Memory: 32.1 MB (5.11%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        return self.numOfSubarraysTopDown(arr)
+        # return self.numOfSubarraysTopDown(arr)
         N = len(arr)
         MOD = pow(10, 9) + 7
+        EVEN, ODD = 0, 1
 
+        dp = [(0, 0)] * (N + 1)
+        res = 0
+        for i in range(N - 1, -1, -1):
+            num_even, num_odd = dp[i + 1]
+            dp[i] = (num_odd, num_even + 1) if arr[i] & 1 else (num_even + 1, num_odd)
+            res += dp[i][ODD]
+        
+        return res % MOD
         
 
     def numOfSubarraysTopDown(self, arr: List[int]) -> int:
@@ -40,5 +49,4 @@ class Solution:
         res = 0
         for i in range(N):
             res += dp(i)[1] # Index 1 is for odd sum counts!
-            res %= MOD
-        return res
+        return res % MOD
