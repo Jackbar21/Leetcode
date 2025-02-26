a50e44c75331a72acaad59c6080632f5a0af8465
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-25 17:07:31 -0800

    Time: 25 ms (88.54%) | Memory: 28.8 MB (21.46%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,15 +1,5 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        # N = len(nums)
-        # prefix_sums = []
-        # cur_sum = 0
-        # for num in nums:
-        #     cur_sum += num
-        #     prefix_sums.append(cur_sum)
-        
-        # getSubarraySum = lambda i, j: prefix_sums[j] - (prefix_sums[i - 1] if i > 0 else 0)
-        # return max(abs(getSubarraySum(i, j)) for i in range(N) for j in range(i, N))
-
         # Idea: We want largest absolute subarray sum. Why not get largest subarray sum, smallest 
         # subarray sum, and take the larger of the two values (absolute value'd of course)
         return max(
