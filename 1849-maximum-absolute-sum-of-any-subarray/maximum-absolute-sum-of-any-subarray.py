371a9a17894b0dc6b9af557971d88c3b6eda1f73
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-25 17:11:53 -0800

    Time: 15 ms (96.10%) | Memory: 28.5 MB (34.63%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -7,19 +7,25 @@ class Solution:
         #     abs(self.maxSubarraySum([-num for num in nums]))
         # )
 
+        # One pass solution O(N)
         cur_pos_sum, cur_neg_sum = 0, 0
         res = 0 # Since we can always take ABSOLUTE VALUE of any subarray, 0 is smallest possible result!
         for num in nums:
             cur_pos_sum += num
             cur_neg_sum -= num
+
+            # Update res
             if res < cur_pos_sum:
                 res = cur_pos_sum
             if res < cur_neg_sum:
                 res = cur_neg_sum
+
+            # Reset current sums
             if cur_pos_sum < 0:
                 cur_pos_sum = 0
             if cur_neg_sum < 0:
                 cur_neg_sum = 0
+
         return res
     
     def maxSubarraySum(self, nums):
