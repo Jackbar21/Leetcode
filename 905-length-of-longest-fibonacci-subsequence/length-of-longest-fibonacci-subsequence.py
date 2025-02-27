9af18c9c88b791976306fc6dda577dff1451f04b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-26 17:51:43 -0800

    Time: 1995 ms (20.20%) | Memory: 18.2 MB (64.57%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -1,9 +1,5 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        # Map frequency of each number in arr[i:] for every i
-        suffix_freqs = []
-        d = {}
-
         # Since arr is STRICTLY increasing, that means every number in arr is UNIQUE.
         # Hence, we can map each number to its index, and use that whenever we "need"
         # a number in one of our DP calls!
@@ -18,30 +14,15 @@ class Solution:
         res = 0
         for i in range(N):
             for j in range(i + 1, N):
-                res = max(res, 2 + self.dp(i, j))
-        # return max(2 + self.dp(i, j) for i in range(N) for j in range(i + 1, N))
+                length = 2 + self.dp(i, j)
+                if res < length:
+                    res = length
 
         return res if res >= 3 else 0
 
-        # print(f"{suffix_freqs=}")
-
-        N = len(arr)
-        self.arr = arr
-        self.memo = {}
-        res = 0
-        for i, arr_i in enumerate(arr):
-            for j in range(i + 1, N):
-                base = arr_i + arr[j]
-                chain = self.dp(j + 1, base)
-                # if chain > 0 and res < base + 2:
-                if res < chain + 2:
-                    res = chain + 2
-        print(f"{self.memo=}")
-        return res if res >= 3 else 0
-    
     def dp(self, i, j):
-        if (i, j) in self.memo:
-            return self.memo[(i, j)]
+        # if (i, j) in self.memo:
+        #     return self.memo[(i, j)]
 
         if j >= len(self.arr):
             return 0        
@@ -52,5 +33,5 @@ class Solution:
         if index > j:
             res = 1 + self.dp(j, index)
 
-        self.memo[i] = res
+        # self.memo[(i, j)] = res
         return res
