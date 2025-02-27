f43cea03ed383571e2247d712bbe3caa24ac3c8b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-26 17:52:57 -0800

    Time: 1472 ms (36.76%) | Memory: 17.8 MB (92.38%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -3,35 +3,40 @@ class Solution:
         # Since arr is STRICTLY increasing, that means every number in arr is UNIQUE.
         # Hence, we can map each number to its index, and use that whenever we "need"
         # a number in one of our DP calls!
-        num_to_index = {}
-        for i, num in enumerate(arr):
-            num_to_index[num] = i
-        self.num_to_index = num_to_index
+        # num_to_index = {}
+        # for i, num in enumerate(arr):
+        #     num_to_index[num] = i
+        # self.num_to_index = num_to_index
+        self.nums = set(arr)
 
         N = len(arr)
         self.memo = {}
         self.arr = arr
         res = 0
-        for i in range(N):
+        for i, x in enumerate(arr):
             for j in range(i + 1, N):
-                length = 2 + self.dp(i, j)
+                y = arr[j]
+                length = 2 + self.dp(x, y)
                 if res < length:
                     res = length
 
         return res if res >= 3 else 0
 
-    def dp(self, i, j):
-        # if (i, j) in self.memo:
-        #     return self.memo[(i, j)]
+    def dp(self, x, y):
+        # if (x, y) in self.memo:
+        #     return self.memo[(x, y)]
 
-        if j >= len(self.arr):
+        if y > (self.arr[-1]):
             return 0        
 
         res = 0
-        base = self.arr[i] + self.arr[j]
-        index = self.num_to_index.get(base, -1)
-        if index > j:
-            res = 1 + self.dp(j, index)
+        # base = self.arr[i] + self.arr[j]
+        # index = self.num_to_index.get(base, -1)
+        if (x + y) in self.nums:
+            res = 1 + self.dp(y, x + y)
 
-        # self.memo[(i, j)] = res
+        # if index > j:
+        #     res = 1 + self.dp(j, index)
+
+        # self.memo[(x, y)] = res
         return res
