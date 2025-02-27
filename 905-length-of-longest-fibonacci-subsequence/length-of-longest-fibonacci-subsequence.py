18f5f85429284fb25f1f8b5819621a0a906022eb
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-02-26 17:59:48 -0800

    Time: 1109 ms (42.39%) | Memory: 19.5 MB (36.09%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -1,34 +1,27 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         # Since arr is STRICTLY increasing, that means every number in arr is UNIQUE.
-        # Hence, we can map each number to its index, and use that whenever we "need"
-        # a number in one of our DP calls!
-        # num_to_index = {}
-        # for i, num in enumerate(arr):
-        #     num_to_index[num] = i
-        # self.num_to_index = num_to_index
-        self.nums = set(arr)
+        # Hence, just keep a hash set of the numbers in arr to lookup if needed value
+        # exists, as its existence will be unique and the array is STRICTLY INCREASING,
+        # meaning existence of such a number is guaranteed to occur later in the array,
+        # making a valid subsequence order-wise!
+        nums = set(arr)
+        dp = lambda x, y: 1 + dp(y, x + y) if (x + y) in nums else 0
 
         N = len(arr)
-        self.memo = {}
-        self.arr = arr
         res = 0
         for i, x in enumerate(arr):
             for j in range(i + 1, N):
                 y = arr[j]
-                length = self.dp(x, y)
+                length = dp(x, y)
                 if res < length:
                     res = length
 
         return res + 2 if res >= 1 else 0
 
-    def dp(self, x, y):
+    # def dp(self, x, y):
         # if (x, y) in self.memo:
         #     return self.memo[(x, y)]
         # res = 1 + self.dp(y, x + y) if (x + y) in self.nums else 0
         # self.memo[(x, y)] = res
         # return res
-
-        # For some reason, caching as per solution above makes things slower!
-        # Hence, just return the raw result :)
-        return 1 + self.dp(y, x + y) if (x + y) in self.nums else 0
