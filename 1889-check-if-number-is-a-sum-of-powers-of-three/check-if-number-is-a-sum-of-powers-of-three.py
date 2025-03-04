1aaeccc01c700c38653dc70b8f704b8691bd8395
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-03 18:40:34 -0800

    Time: 347 ms (5.58%) | Memory: 18 MB (18.33%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,5 +1,27 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
+        self.cur_sum = 0
+        def backtrack(power_of_three):
+            if self.cur_sum == n:
+                return True
+
+            if max(power_of_three, self.cur_sum) > n:
+                return False
+            
+            next_power = power_of_three * 3
+            
+            # Case 1: Add power_of_three
+            self.cur_sum += power_of_three
+            if backtrack(next_power):
+                return True
+            self.cur_sum -= power_of_three
+
+            # Case 2: Ignore power_of_three
+            return backtrack(next_power)
+        
+        # return backtrack(pow(3, 0))
+        return backtrack(1)
+
         solutions = set([0])
         max_expo = math.floor(math.log(n, 3))
         power_of_three = pow(3, 0)
