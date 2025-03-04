dcde8fa951df7fdaee969cead7900ebfe30debc3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-03 18:35:30 -0800

    Time: 47 ms (9.36%) | Memory: 21.7 MB (18.33%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,34 +1,14 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        self.memo = {}
-        return self.dp(n, 1)
-
         solutions = set([0])
         max_expo = math.floor(math.log(n, 3))
-        power_of_three = pow(3, max_expo)
+        power_of_three = pow(3, 0)
         for _ in range(max_expo + 1):
             solutions.update([solution + power_of_three for solution in solutions])
 
             # Loop Invariant
-            power_of_three //= 3
+            power_of_three *= 3
             if n in solutions:
                 return True
         
         return n in solutions
-
-    def dp(self, n, expo):
-        if (n, expo) in self.memo:
-            return self.memo[(n, expo)]
-
-        if n <= 0:
-            return n == 0
-        
-        if expo >= n:
-            return expo == n
-        
-        next_expo = expo * 3
-        res = self.dp(n - expo, next_expo)
-        if not res:
-            res = self.dp(n, next_expo)
-        self.memo[(n, expo)] = res
-        return res
