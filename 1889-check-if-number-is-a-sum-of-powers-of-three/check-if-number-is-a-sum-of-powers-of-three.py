94e8aaa69923317cc772cf35046db705348c6954
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-03 18:27:40 -0800

    Time: 53 ms (9.16%) | Memory: 21.8 MB (18.33%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -5,12 +5,12 @@ class Solution:
 
         solutions = set([0])
         max_expo = math.floor(math.log(n, 3))
-        power_of_three = pow(3, 0) # 1
+        power_of_three = pow(3, max_expo)
         for _ in range(max_expo + 1):
             solutions.update([solution + power_of_three for solution in solutions])
 
             # Loop Invariant
-            power_of_three *= 3
+            power_of_three //= 3
             if n in solutions:
                 return True
         
