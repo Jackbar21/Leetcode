4d4d773407c962050ce1157512964c24e0baed5e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-05 19:21:14 -0800

    Time: 7 ms (72.80%) | Memory: 18.3 MB (23.37%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,15 +1,13 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         N = len(grid)
-        s = set()
+        s = set(range(1, pow(N, 2) + 1))
         a, b = None, None
         for row in grid:
             for num in row:
-                if num in s:
+                if num not in s:
                     a = num
-                s.add(num)
-        for num in range(1, pow(N, 2) + 1):
-            if num not in s:
-                b = num
-                break
+                else:
+                    s.remove(num)
+        b = s.pop()
         return a, b
