63e0aa49f9d37fa86fbb3d66ed3322c124fc34e1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-06 19:05:07 -0800

    Time: 1997 ms (33.44%) | Memory: 35.3 MB (9.38%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -1,6 +1,63 @@
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-        primes = set(filter(lambda num: num & 1, range(left, right + 1)))
+
+        ### FROM WIKIPEDIA (START) ###
+        """
+        algorithm Sieve of Eratosthenes is
+        input: an integer n > 1.
+            output: all prime numbers from 2 through n.
+
+            let A be an array of Boolean values, indexed by integers 2 to n,
+            initially all set to true.
+
+            for i = 2, 3, 4, ..., not exceeding √n do
+                if A[i] is true
+                    for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n do
+                        set A[j] := false
+
+            return all i such that A[i] is true.
+        """
+        ### FROM WIKIPEDIA (END) ###
+        A = [True] * (right + 1)
+        i = 2
+        MAX = math.sqrt(right)
+        while i <= MAX:
+            if A[i]:
+                j = i * 2
+                while j <= right:
+                    A[j] = False
+                    j += i
+
+            # Loop Invariant
+            i += 1
+        
+        sorted_primes = []
+        i = max(left, 2)
+        while i <= right:
+            if A[i]:
+                sorted_primes.append(i)
+            i += 1
+        
+        ans = [-1, -1]
+        cur_diff = float("inf")
+        # sorted_primes = sorted(primes)
+        prev = float("-inf")
+        for cur in sorted_primes:
+            if cur - prev < cur_diff:
+                cur_diff = cur - prev
+                ans = [prev, cur]
+            
+            # Loop Invariant
+            prev = cur
+
+        # print(f"{primes=}")
+        return ans
+
+
+
+
+        ### WORKING SOLUTION (SLOW) WITHOUT USING ANY EXTERNAL RESOURCES: ###
+        primes = set(range(left + (left % 2 == 0), right + 1, 2))
         primes.discard(1) # 1 is NOT a prime number!
         if left <= 2:
             primes.add(2) # 2 IS a prime number!
