3ba7a61cb66b7f22f59064df844f2cfd8cc0f1c7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-08 18:43:12 -0800

    Time: 712 ms (75.75%) | Memory: 21.1 MB (90.70%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.py
+++ 3483-alternating-groups-ii/alternating-groups-ii.py
@@ -1,18 +1,10 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
         N = len(colors)
-        
-        res = 0
-        streak = 0 # want this to be k
-        index = 0
-        prev_color = None
-        # while index < N + k - 1:
-        for index in range(N + k - 1):
-            # # TODO: Fix this up
-            # if streak == k:
-            #     streak -= 1
-            #     res += 1
 
+        res, index, streak = 0, 0, 0
+        prev_color = None
+        for index in range(N + k - 1):
             color = colors[index % N]
             if prev_color != color:
                 # Valid case, continue the streak
@@ -21,12 +13,14 @@ class Solution:
                 # Same color, restart the streak
                 streak = 1
             
+            # If streak of size k, we found one valid solution, so make sure
+            # to COUNT it and to set ourselves up to check whether next k-contiguous
+            # tiles are also alternating :)
             if streak == k:
-                streak = k - 1
+                streak -= 1
                 res += 1
 
             # Loop Invariant
             prev_color = color
-            # index += 1
         
         return res
