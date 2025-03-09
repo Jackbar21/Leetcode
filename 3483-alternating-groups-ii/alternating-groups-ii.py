61b6a70a9cee17ab252e331f1114cad611953629
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-08 18:38:54 -0800

    Time: 744 ms (37.54%) | Memory: 21.7 MB (39.54%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        RED, BLUE = 0, 1
+        N = len(colors)
+        colors += colors[:k - 1]
+        
+        res = 0
+        streak = 0 # want this to be k
+        index = 0
+        # opposite_color = lambda color: RED if color == BLUE else BLUE
+        # prev_color = opposite_color[colors[index]]
+        prev_color = None
+        while index < len(colors):
+            # # TODO: Fix this up
+            # if streak == k:
+            #     streak -= 1
+            #     res += 1
+
+            color = colors[index]
+            if prev_color != color:
+                # Valid case, continue the streak
+                streak += 1
+            else:
+                # Same color, restart the streak
+                streak = 1
+            
+            if streak == k:
+                streak = k - 1
+                res += 1
+
+            # Loop Invariant
+            prev_color = color
+            index += 1
+        
+        # print(f"{streak=}")
+        return res #+ (streak == k)
+
+
+#        i
+# [0,1,0,0,1,0,1], k = 6
+
+# streak = 3
+# index = 3
+# res = 0
