cb40411cdd408ec130b041cd94b6546cfa20e591
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-13 18:32:37 -0700

    Time: 668 ms (9.17%) | Memory: 29.8 MB (18.92%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -2,17 +2,26 @@ class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
         l, r = 1, max(candies)
         # Can always give each child 0 candy, but maybe not always max(candies) for each child.
-        # Think of this as an array of booleans (0 or more True's followed by 0 or more False's),
+        # Think of this as an array of booleans (1 or more True's followed by 0 or more False's),
         # want to find rightmost 'True' index (i.e. LARGEST VALID value)
         res = 0 # Max candy
         while l <= r:
             mid = (l + r) // 2
 
+            # mid == 3
+            # 5 --> 3,2
+            # [3,2]
+
+            # 8 --> 5,3 --> 3,3,2
+
+            # 6 --> 3,3
+
             # Can we feed 'mid' amount of candy to each child?
             # Let's see how many children we can feed 'mid' candy
             can_feed = 0
             for candy_count in candies:
-                can_feed += candy_count // mid
+                # can_feed += candy_count // mid
+                can_feed += math.floor(candy_count / mid)
             
             if can_feed >= k:
                 # Valid solution, update res and look for potentially BETTER ONES in right subhalf!
