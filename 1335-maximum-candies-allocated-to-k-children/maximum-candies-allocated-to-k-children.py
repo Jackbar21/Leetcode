58e46ad5c529560169a28b15d941d3991e1099a8
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-13 18:18:53 -0700

    Time: 341 ms (66.37%) | Memory: 29.8 MB (54.00%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -1,6 +1,10 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-        l, r = 1, max(candies)
+        max_candy = 1
+        for candy in candies:
+            if max_candy < candy:
+                max_candy = candy
+        l, r = 1, max_candy
         # Can always give each child 0 candy, but maybe not always max(candies) for each child.
         # Think of this as an array of booleans (0 or more True's followed by 0 or more False's),
         # want to find rightmost 'True' index (i.e. LARGEST VALID value)
@@ -10,19 +14,16 @@ class Solution:
 
             # Can we feed 'mid' amount of candy to each child?
             # Let's see how many children we can feed 'mid' candy
-            # can_feed = 0
-            # for candy_count in candies:
-            #     can_feed += candy_count // mid
-            can_feed = sum(candy_count // mid for candy_count in candies)
-
-            is_valid = can_feed >= k
-            if is_valid:
+            can_feed = 0
+            for candy_count in candies:
+                can_feed += candy_count // mid
+            
+            if can_feed >= k:
                 # Valid solution, update res and look for potentially BETTER ONES in right subhalf!
                 res = mid
                 l = mid + 1
             else:
+                # Invalid solution, look for potentially valid ones in left subhalf (giving LESS candy!)
                 r = mid - 1
         
         return res
-                
-        
