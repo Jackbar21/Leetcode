e34f28c23abc9df860977aabf9c5e652607258f4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-14 19:50:11 -0700

    Time: 327 ms (58.82%) | Memory: 28.5 MB (81.09%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.py
+++ 2690-house-robber-iv/house-robber-iv.py
@@ -2,7 +2,6 @@ class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
         N = len(nums)
         l, r = 1, max(nums)
-        rightmost = r
         while l <= r:
             mid = (l + r) // 2
 
@@ -21,7 +20,6 @@ class Solution:
             
             if stolen_homes >= k:
                 # Valid solution, look for even BETTER ones on the left-hand side!
-                rightmost = mid
                 r = mid - 1
             else:
                 # Invalid solution, look for potentially VALID ones on right-hand side!
