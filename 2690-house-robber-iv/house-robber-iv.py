18364cca94f8c2c20ebee8ad8940a696871cc9c5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-14 19:50:31 -0700

    Time: 380 ms (35.50%) | Memory: 28.7 MB (19.96%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.py
+++ 2690-house-robber-iv/house-robber-iv.py
@@ -18,7 +18,7 @@ class Solution:
                     # Don't steal the home (as would go OVER capability of 'mid')!
                     i += 1
             
-            if stolen_homes >= k:
+            if stolen_homes == k:
                 # Valid solution, look for even BETTER ones on the left-hand side!
                 r = mid - 1
             else:
