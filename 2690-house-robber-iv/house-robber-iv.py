8708712ee04a6cc8eaaeea8532f364287847f04b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-14 19:49:23 -0700

    Time: 335 ms (55.88%) | Memory: 28.6 MB (50.00%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        l, r = 1, max(nums)
+        rightmost = r
+        while l <= r:
+            mid = (l + r) // 2
+
+            # Want to check if can reach a capability of mid
+            i = 0
+            stolen_homes = 0
+            while i < N: # and stolen_homes < k:
+                num = nums[i]
+                if num <= mid:
+                    # Steal the home!
+                    stolen_homes += 1
+                    i += 2
+                else:
+                    # Don't steal the home (as would go OVER capability of 'mid')!
+                    i += 1
+            
+            if stolen_homes >= k:
+                # Valid solution, look for even BETTER ones on the left-hand side!
+                rightmost = mid
+                r = mid - 1
+            else:
+                # Invalid solution, look for potentially VALID ones on right-hand side!
+                l = mid + 1
+
+        return rightmost
