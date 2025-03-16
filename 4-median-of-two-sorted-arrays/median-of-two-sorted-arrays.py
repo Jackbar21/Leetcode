708a47eaf9f88d2510056d61247c1901e9f4218f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-16 15:59:26 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (60.04%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -4,6 +4,7 @@ class Solution:
         # Set nums1 and nums2 to be smaller and larger arrays in length, respectively
         nums1, nums2 = (nums1, nums2) if len(nums1) < len(nums2) else (nums2, nums1)
         M, N = len(nums1), len(nums2)
+        # assert M <= N
         L = M + N
         half = L // 2
 
@@ -12,26 +13,26 @@ class Solution:
         magic_x = None
         while l <= r:
             x = (l + r) // 2
-            a_max_left = nums1[x - 1] if x - 1 >= 0 else float("-inf")
-            a_min_right = nums1[x] if x < M else float("inf")
-            b_max_left = nums2[half - x - 1] if half - x - 1 >= 0 else float("-inf")
-            b_min_right = nums2[half - x] if half - x < N else float("inf")
+            nums1_max_left = nums1[x - 1] if x - 1 >= 0 else float("-inf")
+            nums1_min_right = nums1[x] if x < M else float("inf")
+            nums2_max_left = nums2[half - x - 1] if half - x - 1 >= 0 else float("-inf")
+            nums2_min_right = nums2[half - x] if half - x < N else float("inf")
             
-            if a_max_left <= b_min_right and b_max_left <= a_min_right:
+            if nums1_max_left <= nums2_min_right and nums2_max_left <= nums1_min_right:
                 magic_x = x
                 break
             
-            if a_max_left <= b_min_right:
-                # assert not (b_max_left <= a_min_right)
+            if nums1_max_left <= nums2_min_right:
+                # assert not (nums2_max_left <= nums1_min_right)
                 l = x + 1
                 continue
             
-            # assert not (a_max_left <= b_min_right)
+            # assert not (nums1_max_left <= nums2_min_right)
             r = x - 1
             continue
 
+        # assert magic_x is not None
         x = magic_x
-        # assert x is not None
         min_right = min(
             nums1[x] if x < M else float("inf"),
             nums2[half - x] if half - x < N else float("inf")
