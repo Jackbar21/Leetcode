7198d483cee9694316eb9e17c21916f431b8db43
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-16 17:03:21 -0700

    Time: 3 ms (70.86%) | Memory: 17.8 MB (73.03%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        d = defaultdict(int)
+        d = {}
         for num in nums:
-            d[num] += 1
+            d[num] = d.get(num, 0) + 1
         return all(val % 2 == 0 for val in d.values())
