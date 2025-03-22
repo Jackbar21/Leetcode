a0156901e78bbc5043d2aa79363f6edb75b63650
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-21 22:35:29 -0700

    Time: 81 ms (82.21%) | Memory: 21.6 MB (20.92%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -22,14 +22,9 @@ class Codec:
             queue.append(node.left)
             queue.append(node.right)
 
-        # print(f"{preorder=}")
-        # content = ",".join(preorder)
-        # res = f"[{','.join(preorder)}]"
-        # print(f"{res=}")
         while preorder and preorder[-1] == "null":
             preorder.pop()
         res = ",".join(preorder)
-        print(f"Serialize: {res=}")
         return res
             
 
@@ -45,54 +40,32 @@ class Codec:
         des = data.split(",")
         N = len(des)
         
-        index_to_node = defaultdict(lambda: None)
+        index_to_node = {}
         for index, value in enumerate(des):
-            # if value == "null":
-            #     index_to_node[index] = None
-            #     continue
             if value == "null":
+                index_to_node[index] = None
                 continue
-            
+
             node = TreeNode(int(value))
             index_to_node[index] = node
         
-        print(f"{N=}, {len(des)=}, {len(index_to_node)=}")
-        print(f"{index_to_node=}")
         queue = collections.deque([index_to_node[0]])
-        queue_index = 0
         queue_left = True
         for index in range(1, N):
             node = index_to_node[index]
-            queue_node = queue[queue_index]
+            queue_node = queue[0]
             if queue_left:
                 queue_node.left = node
                 queue_left = False
             else:
                 queue_node.right = node
-                queue_index += 1
+                queue.popleft()
                 queue_left = True
 
-            if not node:
-                continue
-            queue.append(node)
+            if node is not None:
+                queue.append(node)
         
-        return queue[0]
-
-        for index in range(N):
-            node = index_to_node[index]
-            if not node:
-                continue
-            left_child = index_to_node[2 * index + 1]
-            right_child = index_to_node[2 * index + 2]
-
-            node.left = left_child
-            node.right = right_child
-        
-        # print(f"{index_to_node}")
-        res = index_to_node[0]
-        print(f"Deserialize: {res}")
-        return res
-        # [1,2,3,null,null,4,5,6,7]
+        return index_to_node[0]
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
