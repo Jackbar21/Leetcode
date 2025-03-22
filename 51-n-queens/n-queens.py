64bd1352281a9f6b43968e8a40b21a5290282f0d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-22 14:22:13 -0700

    Time: 11 ms (74.39%) | Memory: 18.3 MB (35.48%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,56 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # Rows: use row index (e.g. i, 0 <= i < n)
+        # Columns: use col index (e.g. j, 0 <= j < n)
+        # Negative Diagonals: use col - row index (e.g. j - i)
+        # Positive Diagonals: use row + col
+
+        # Already used up indices!
+        # rows = set() # Can ignore
+        cols = set()
+        neg_diags = set()
+        pos_diags = set()
+
+        # There will only be one queen per row, so might as well backtrack through row indices!
+        arr = [["."] * n for _ in range(n)]
+        #print(f"{arr=}")
+        def backtrack(i):
+            #print(f"{i=}, {arr=}")
+            if i == n:
+                #print(f"SOL: {arr=}")
+                # yield arr.copy() # TODO: Remove copy if unneeded
+                sol = ["".join(arr[i]) for i in range(n)]
+                #print(f"{sol=}")
+                yield sol
+                return
+
+            for j in range(n):
+                if j in cols or (j - i) in neg_diags or (i + j) in pos_diags:
+                    continue
+                
+                # Prepare add
+                cols.add(j)
+                neg_diags.add(j - i)
+                pos_diags.add(i + j)
+                assert arr[i][j] == "."
+                arr[i][j] = "Q"
+
+                # Get more solutions
+                yield from backtrack(i + 1)
+
+                # Undo (for backtracking!)
+                assert arr[i][j] == "Q"
+                arr[i][j] = "."
+                cols.remove(j)
+                neg_diags.remove(j - i)
+                pos_diags.remove(i + j)
+
+        
+        res = list(backtrack(0))
+        #print(f"{res=}")
+        return res
+
+
+        
+        
+
