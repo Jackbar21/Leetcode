8c77ca7a1176de89dbda08265b925d3164a3f4b1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-22 20:17:42 -0700

    Time: 1239 ms (5.62%) | Memory: 34 MB (5.31%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -1,11 +1,43 @@
 class Solution:
-    # Step 2: Simple UCS to find optimal cost from 'source' to GOAL_NODE
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        self.n = n
+        MOD = pow(10, 9) + 7
+
+        # Step 1: Build adjacency list!
+        adj_list = {node: [] for node in range(n)}
+        for u, v, time in roads:
+            adj_list[u].append((v, time))
+            adj_list[v].append((u, time))
+        self.adj_list = adj_list
+
+        # Step 2: Compute number of ways to get from 0 to goal node in 'optimal_cost' time!
+        optimal_cost = self.ucs(0)
+        return self.dp(0, optimal_cost) % MOD
+    
+    @cache
+    def dp(self, node, budget):
+        if budget <= 0:
+            # assert budget == 0
+            GOAL_NODE = self.n - 1
+            return node == GOAL_NODE
+        
+        shortest_path_cost = self.ucs(node)
+        if budget < shortest_path_cost:
+            return 0
+
+        res = 0
+        for neigh, time in self.adj_list[node]:
+            if budget - time >= 0:
+                res += self.dp(neigh, budget - time)
+        
+        return res
+
+    # Simple UCS to find optimal cost from 'source' to GOAL_NODE
     @cache
     def ucs(self, source):
         GOAL_NODE = self.n - 1
         fringe = [(0, source)] # (cost, node) | min-heap/priority-queue (i.e. UCS)
         visited = set()
-        optimal_cost = float("inf")
         while len(fringe) > 0:
             cost, node = heapq.heappop(fringe)
             if node in visited:
@@ -21,41 +53,3 @@ class Solution:
                     heapq.heappush(fringe, (cost + time, neigh))
         
         raise Exception("No path found!")
-
-    def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        self.n = n
-        MOD = pow(10, 9) + 7
-
-        # Step 1: Build adjacency list!
-        adj_list = {node: [] for node in range(n)}
-        for u, v, time in roads:
-            adj_list[u].append((v, time))
-            adj_list[v].append((u, time))
-        self.adj_list = adj_list
-
-        # Step 2: Simple UCS to find optimal cost
-        # self.shortest_paths = {node: self.ucs(node) for node in range(n)}
-        self.dp_memo = {}
-        return self.dp(0, self.ucs(0)) % MOD
-    
-    # @cache
-    def dp(self, node, cost):
-        if (node, cost) in self.dp_memo:
-            return self.dp_memo[(node, cost)]
-
-        if cost <= 0:
-            # assert cost == 0
-            GOAL_NODE = self.n - 1
-            return node == GOAL_NODE
-        
-        shortest_path = self.ucs(node)
-        if shortest_path > cost:
-            return 0
-
-        res = 0
-        for neigh, time in self.adj_list[node]:
-            if cost - time >= 0:
-                res += self.dp(neigh, cost - time)
-        
-        self.dp_memo[(node, cost)] = res
-        return res
