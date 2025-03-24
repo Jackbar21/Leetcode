4c66c051befb5f3fe726a8e37db05fbb38fc087c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-23 20:15:41 -0700

    Time: 156 ms (72.79%) | Memory: 57.9 MB (8.31%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -4,23 +4,20 @@ class Solution:
         for start, end in meetings:
             line_sweep[start] += 1
             line_sweep[end + 1] -= 1
-        
-        sorted_line_sweep = {key: line_sweep[key] for key in sorted(line_sweep.keys())}
-        # sorted_days = sorted(line_sweep.keys())
+
         res = 0
         prev_day = 1
         active_intervals = 0
-        # print(f"{line_sweep=}")
-        print(f"{sorted_line_sweep=}")
-        for key, val in sorted_line_sweep.items():
-            day = key
-            assert active_intervals >= 0
+        sorted_days = sorted(line_sweep.keys())
+        for day in sorted_days:
+            # assert active_intervals >= 0
             if active_intervals == 0:
                 res += day - prev_day
+
+            # Loop Inavriant
+            active_intervals += line_sweep[day] # Can be negative!
             prev_day = day
-            # active_intervals += line_sweep[day]
-            active_intervals += val
         
-        # res += days - prev_day
-        res += days - max(line_sweep.keys()) + 1
+        # Last amount of days also matters
+        res += days - day + 1
         return res
