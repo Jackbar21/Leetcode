8334c7b91f9af165e5e2f640b7551277e436b9e7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-25 16:43:53 -0700

    Time: 191 ms (58.60%) | Memory: 19.9 MB (49.98%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -24,27 +24,31 @@ class Solution:
         return self.END_WORD in trie and trie[self.END_WORD] == word
     
     def getSimilarWords(self, word):
-        return self.getSimilarWordsHelper(word, 0, self.trie, 1)
+        self.sol = []
+        self.getSimilarWordsHelper(word, 0, self.trie, 1)
+        return self.sol
 
     def getSimilarWordsHelper(self, word, index, trie, can_change):
         if index == len(word):
             if self.END_WORD in trie:
-                yield trie[self.END_WORD]
+                # yield trie[self.END_WORD]
+                self.sol.append(trie[self.END_WORD])
             return
 
         word_letter = word[index]
         if word_letter in trie:
-            yield from self.getSimilarWordsHelper(word, index + 1, trie[word_letter], can_change)
+            # yield from self.getSimilarWordsHelper(word, index + 1, trie[word_letter], can_change)
+            self.getSimilarWordsHelper(word, index + 1, trie[word_letter], can_change)
 
         if can_change > 0:
             for letter in trie:
                 if letter != word_letter:
-                    yield from self.getSimilarWordsHelper(word, index + 1, trie[letter], can_change - 1)
+                    # yield from self.getSimilarWordsHelper(word, index + 1, trie[letter], can_change - 1)
+                    self.getSimilarWordsHelper(word, index + 1, trie[letter], can_change - 1)
 
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         self.trie = {}
         self.END_WORD = "."
-        # self.addWord(beginWord)
         for word in wordList:
             self.addWord(word)
 
