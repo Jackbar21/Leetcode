0fec30b668f29d27697715b1ca6473bc29bc8c2a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-25 16:44:24 -0700

    Time: 204 ms (57.57%) | Memory: 19.9 MB (49.79%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -24,26 +24,23 @@ class Solution:
         return self.END_WORD in trie and trie[self.END_WORD] == word
     
     def getSimilarWords(self, word):
-        self.sol = []
+        self.res = []
         self.getSimilarWordsHelper(word, 0, self.trie, 1)
-        return self.sol
+        return self.res
 
     def getSimilarWordsHelper(self, word, index, trie, can_change):
         if index == len(word):
             if self.END_WORD in trie:
-                # yield trie[self.END_WORD]
-                self.sol.append(trie[self.END_WORD])
+                self.res.append(trie[self.END_WORD])
             return
 
         word_letter = word[index]
         if word_letter in trie:
-            # yield from self.getSimilarWordsHelper(word, index + 1, trie[word_letter], can_change)
             self.getSimilarWordsHelper(word, index + 1, trie[word_letter], can_change)
 
         if can_change > 0:
             for letter in trie:
                 if letter != word_letter:
-                    # yield from self.getSimilarWordsHelper(word, index + 1, trie[letter], can_change - 1)
                     self.getSimilarWordsHelper(word, index + 1, trie[letter], can_change - 1)
 
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
