33589a5c8d504004964e2a0c4e63c5056a5cb8da
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-25 16:09:23 -0700

    Time: 9323 ms (5.01%) | Memory: 19.1 MB (53.52%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,31 +1,37 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if (endWord not in wordList 
-            or len(beginWord) != len(endWord) 
-            or beginWord == endWord
-        ):
-            return 0
+        wordList.append(beginWord)
+        N = len(wordList)
+        WORD_LENGTH = len(beginWord)
 
-        queue = collections.deque([(beginWord, 0)])
-        visited = set([beginWord])
-        wordList = set(wordList)
-
+        adj_list = {word: [] for word in wordList}
+        for i, word_i in enumerate(wordList):
+            for j in range(i + 1, len(wordList)):
+                word_j = wordList[j]
+                diff_count = 0
+                
+                # assert len(word_i) == len(word_j) == WORD_LENGTH
+                for index in range(WORD_LENGTH - 1, -1, -1):
+                    diff_count += word_i[index] != word_j[index]
+                    if diff_count > 1:
+                        break
+                
+                if diff_count == 1:
+                    adj_list[word_i].append(word_j)
+                    adj_list[word_j].append(word_i)
+        
+        queue = collections.deque([(1, beginWord)]) # (cost, word)
+        visited = set()
         while len(queue) > 0:
-            word, path_len = queue.popleft()
+            cost, node = queue.popleft()
+            if node == endWord:
+                return cost
 
-            if len(word) != len(beginWord):
-                continue
-            
-            if word == endWord:
-                return path_len + 1
+            for neigh in adj_list[node]:
+                if neigh not in visited:
+                    queue.append((cost + 1, neigh))
+                    visited.add(neigh)
 
-            for i in range(len(beginWord)):
-                for letter in 'qwertyuiopasdfghjklzxcvbnm':
-                    new_word = word[:i] + letter + word[i+1:]
-                    if letter == word[i] or new_word not in wordList:
-                        continue
-                    if new_word not in visited:
-                        queue.append((new_word, path_len + 1))
-                        visited.add(new_word)
-
+        # No such sequence exists!
         return 0
+
