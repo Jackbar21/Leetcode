1ff2362c14fb53663dce0b51901d46be065e1b70
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-25 17:50:57 -0700

    Time: 190 ms (39.03%) | Memory: 38.8 MB (78.95%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,63 +1,26 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        #print(f"{x=}")
         M, N = len(grid), len(grid[0])
-        first_val = grid[0][0]
-        res = float("inf")
 
-        # Step 1: Get the average
-        # avg = sum(grid[i][j] for i in range(M) for j in range(N)) / (M * N)
+        # Step 1: Get the median
         nums = sorted(grid[i][j] for i in range(M) for j in range(N))
         L = len(nums)
-        assert L == M * N
-        median = nums[L // 2] if L%2 else (nums[(L//2)-1]+nums[L//2])/2
-        avg = median
-        #print(f"{avg=}")
+        # assert L == M * N
+        median = nums[L // 2] if L % 2 == 1 else (nums[(L//2) - 1] + nums[L//2]) / 2
 
-        # Step 2: Figure out closest number to average you can turn any number in
+        # Step 2: Figure out closest number to median you can turn any number in
         #         grid into (i.e. grid[0][0]) with just +x or -x operations.
         i, j = random.randint(0, M - 1), random.randint(0, N - 1)
-        # num = grid[i][j]
-        num = min(grid[i][j] for i in range(M) for j in range(N))
-        #print(f"{i=}, {j=}, {num=}")
-        steps = abs(avg - num) / x
-        #print(f"IMPORTANT: {steps=}")
-        steps = round(steps)
-        target = num + (steps * x) if num < avg else num - (steps * x)
-        # #print(f"{steps=}, {target=}")
+        num = nums[0] # smallest num, so need +x operations!
+        assert num <= median
+        steps = round((median - num) / x)
+        target = num + (steps * x)
 
+        # Step 3: Figure out how many steps to get to target for each num, or return
+        #         -1 if not possible for ANY num in grid!
         res = 0
-        for i in range(M):
-            for j in range(N):
-                num = grid[i][j]
-                if abs(num - target) % x != 0:
-                    return -1 # Not possible!
-                else:
-                    #print(f"{res=}, {abs(num - target)=}, {abs(num - target) // x=}")
-                    res += abs(num - target) / x
-        return round(res)
-
-        # for target in [first_val - x, first_val, first_val + x]:
-        #     count = self.solver(grid, x, target)
-        #     #print(f"{count=}")
-        #     if res > count:
-        #         res = count
-
-        # return res if res != float("inf") else -1
-
-    # def solver(self, grid, x, target):
-    #     M, N = len(grid), len(grid[0])
-    #     res = 0
-    #     for i in range(M):
-    #         for j in range(N):
-    #             val = grid[i][j]
-    #             if target == val:
-    #                 continue
-    #             if target == (val - x) or target == (val + x):
-    #                 res += 1
-    #                 continue
-
-    #             # Not possible!
-    #             return -1
-        
-    #     return res
+        for num in nums:
+            if abs(num - target) % x != 0:
+                return -1
+            res += abs(num - target) // x
+        return res
