3946431bab4e180f14801d13c1d66e93ac61d362
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-26 21:55:04 -0700

    Time: 39 ms (39.96%) | Memory: 18.5 MB (58.81%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -8,9 +8,7 @@ class Solution:
         fringe = [(grid[0][0], 0, 0)] # min-heap | (cost, i, j)
         visited = set()
         while len(fringe) > 0:
-            #print(f"{fringe=}")
             cost, i, j = heapq.heappop(fringe)
-            #print(f"{cost, i, j=}")
             
             if (i, j) in visited:
                 continue
@@ -24,10 +22,9 @@ class Solution:
                 neigh_i, neigh_j = i + di, j + dj
                 if not inBounds(neigh_i, neigh_j):
                     continue
-                #print(f"{cost, grid[neigh_i][neigh_j], neigh_i, neigh_j=}, {(neigh_i, neigh_j) in visited=}")
                 if (neigh_i, neigh_j) in visited:
                     continue
-                # if cost < (neigh_cost := grid[neigh_i][neigh_j]):
-                #     cost = neigh_cost
-                heapq.heappush(fringe, (max(cost, grid[neigh_i][neigh_j]), neigh_i, neigh_j))
-
+                neigh_cost = max(cost, grid[neigh_i][neigh_j])
+                heapq.heappush(fringe, (neigh_cost, neigh_i, neigh_j))
+        
+        raise Exception("No solution found!")
