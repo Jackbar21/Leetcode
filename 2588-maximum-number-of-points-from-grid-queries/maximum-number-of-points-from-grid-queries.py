c0299b6053cb86cfa242d032d071fcef50ac6e4b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-27 18:48:39 -0700

    Time: 980 ms (24.18%) | Memory: 43.8 MB (10.46%) - LeetSync

---
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        M, N = len(grid), len(grid[0])
+        DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        inBounds = lambda i, j: 0 <= i < M and 0 <= j < N
+
+        sorted_queries = sorted(queries)
+        solutions = {}
+        # shortest_paths = {}
+
+        fringe = [(grid[0][0], 0, 0)] # (grid[i][j], i, j)
+        visited = set()
+        for query in sorted_queries:
+            if query in solutions:
+                continue
+            
+            while fringe and fringe[0][0] < query:
+                cost, i, j = heapq.heappop(fringe)
+                if (i, j) in visited:
+                    continue
+                visited.add((i, j))
+                # shortest_paths[(i, j)] = cost
+
+                for di, dj in DIRECTIONS:
+                    neigh_i, neigh_j = i + di, j + dj
+                    if not inBounds(neigh_i, neigh_j) or (neigh_i, neigh_j) in visited:
+                        continue
+                    neigh_cost = max(cost, grid[neigh_i][neigh_j])
+                    heapq.heappush(fringe, (neigh_cost, neigh_i, neigh_j))
+            
+            solutions[query] = len(visited)
+        
+        # print(f"{solutions=}")
+        return [solutions[query] for query in queries]
+        
