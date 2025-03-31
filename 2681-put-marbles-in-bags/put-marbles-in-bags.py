94db50a853b2e908991a0852c8f296655d21111f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-30 21:37:17 -0700

    Time: 291 ms (10.74%) | Memory: 33.3 MB (8.68%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -1,8 +1,27 @@
-# Okay, I will not see if I am able to troubleshoot the wifi...
-# Will cut the video here, and resume as soon as I have wifi and was able to hit run & submit!!
-# See you in just one second :)
-# OKAY I'M BACK - my phone is at low battery, but I'm using my hotspot and I think it's BARELY enough...
-# LET'S RUN ITTTTTTTT OMGGGG
+# Okok so as always in all my videos I do a quick recap of I how I was able to solve the problem!
+# This problem I was struggling because I was really trying to force the subarrays, and if the
+# constraints said that 2 * k <= len(weights)... I would have been BING CHILLING. My idea was
+# essentially grab the 2 * k largest indices, since then you could have all partitions you need
+# (now I think about it, might not cover everything). I first did DP which works but is too slow obviously.
+# I then thought "okay what if I grab index N-1 and then the k-1 largest indics in value besides N-1". Then
+# I could sort them, i.e having indices i1,i2,...,i_{k-1},i_k where i_k == N - 1. Then I could make my
+# partitions weights[0..i1], weights[i1+1..i2], ..., weighs[i_{k-1}..N-1]. But even that was incorrect.
+# I read the hints, and they were not helpful AT ALL. I then (during one of the breaks in Vancouver-Seattle
+# bus ride) decided to scroll through discussions to see what people were saying about this problem. I found
+# an "actually helpful hints" comment, and saw a clever idea for solving this problem: USE the fact that
+# since we must make k non-overlapping subarrays, we can treat this as making k-1 cuts, where each cut will
+# have the elements adjacent to the cut contribute to the overall score! Since they will denote the end &
+# start of their own separate partitions, respectively! Hence we grab the score from making k-1 cuts, as
+# well as weights[0] and weights[N - 1] as those are baked in by default. To find max solution, we simply
+# grab largest k-1 cuts. To find min solution, we simply grab smallest k-1 cuts. Then we get their
+# difference and voila, we have our solution!! For "grabbing cuts" logic, I simply created a 'cut_profit'
+# array where cut_profit[i] == profit from splitting into nums[..i], nums[i+1..] == nums[i] + nums[i+1].
+# Then I turn cut_profit into a max_heap that I pop from k-1 times to get max solution, and similarly
+# into a min_heap that I pop from k-1 times as well to get min solution. Again, grab the difference
+# between these results, and BADA-BING-BADA-BOOM you got a nice and working solution!!
+# But otherwise, that's pretty much it for this video, if you did find it useful or enjoyable in any
+# way-shape-or-form, I'd highly appreciate it if you left a #LIKE or a #COMMENT, but otherwise... I'll
+# catch you in the next one! So until then, chao chao my friend. Bye bye :)
 class Solution:
     def putMarbles(self, weights: list[int], k: int) -> int:
         N = len(weights)
