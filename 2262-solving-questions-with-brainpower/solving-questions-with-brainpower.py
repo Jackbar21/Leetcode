c085a7dac78f15d1b8534ba1da1a454d49442bf3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-31 17:05:29 -0700

    Time: 179 ms (20.71%) | Memory: 140.1 MB (5.72%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,11 +1,12 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         N = len(questions)
-        memo = {}
+        # memo = {}
 
+        @cache
         def dp(i):
-            if i in memo:
-                return memo[i]
+            # if i in memo:
+            #     return memo[i]
 
             if i >= N:
                 return 0
@@ -18,8 +19,9 @@ class Solution:
             # Case 2: Skip question i.
             case2 = dp(i + 1)
 
-            res = case1 if case1 > case2 else case2
-            memo[i] = res
-            return res
+            # res = case1 if case1 > case2 else case2
+            # memo[i] = res
+            # return res
+            return max(case1, case2)
         
         return dp(0)
