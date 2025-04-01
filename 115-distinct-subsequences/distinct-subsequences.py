23653f44822055b6d2575d87d445b58341b22c43
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-03-31 20:09:14 -0700

    Time: 998 ms (11.94%) | Memory: 225.8 MB (22.49%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,30 +1,28 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        self.s, self.t = s, t
-        self.memo = {}
-        res = self.dp(0, 0)
-        return res
-    
-    def dp(self, i, j):
-        if (i, j) in self.memo:
-            return self.memo[(i, j)]
-        
-        # Base Case: Found a subsequence!
-        if j >= len(self.t):
-            return 1
+        memo = {}
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            # Base Case: Found a subsequence!
+            if j >= len(t):
+                return 1
 
-        # Base Case: No letters left to use in s, and idn't find a subsequence!
-        if i >= len(self.s):
-            return 0
+            # Base Case: No letters left to use in s, and idn't find a subsequence!
+            if i >= len(s):
+                return 0
 
-        s_letter, t_letter = self.s[i], self.t[j]
+            s_letter, t_letter = s[i], t[j]
 
-        # Case 1: If s_letter == t_letter, then have option of using it for new unique subsequence!
-        case1 = self.dp(i + 1, j + 1) if s_letter == t_letter else 0
+            # Case 1: If s_letter == t_letter, then have option of using it for new unique subsequence!
+            case1 = dp(i + 1, j + 1) if s_letter == t_letter else 0
 
-        # Case 2: Whether are not s_letter == t_letter, can simply skip s_letter!
-        case2 = self.dp(i + 1, j)
+            # Case 2: Whether are not s_letter == t_letter, can simply skip s_letter!
+            case2 = dp(i + 1, j)
 
-        res = case1 + case2
-        self.memo[(i, j)] = res
-        return res
+            res = case1 + case2
+            memo[(i, j)] = res
+            return res
+
+        return dp(0, 0)
