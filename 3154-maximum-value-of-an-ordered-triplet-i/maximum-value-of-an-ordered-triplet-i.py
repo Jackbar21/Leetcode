52a64ab594e756a9ad0ccd54ad09cfb44898f187
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-01 22:53:50 -0700

    Time: 3 ms (78.55%) | Memory: 17.8 MB (54.86%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -1,8 +1,13 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        # return max(0, max(((nums[i] - nums[j]) * nums[k]) for i in range(len(nums)) for j in range(i + 1, len(nums)) for k in range(j + 1, len(nums))))
         N = len(nums)
-
+        ### O(N^3) solution (one line!) ###
+        """
+        return max(0, max(((nums[i] - nums[j]) * nums[k]) for i in range(len(nums)) for j in range(i + 1, len(nums)) for k in range(j + 1, len(nums))))
+        """
+        
+        ### O(N^2) solution ##
+        """
         prefix_max = [] # prefix_max[i] == max(nums[i:])
         cur_max = float("-inf")
         for num in reversed(nums):
@@ -43,4 +48,30 @@ class Solution:
                 if res < val:
                     res = val
 
+        return res
+        """
+
+        ### O(N) solution ###
+        prefix_max = [] # prefix_max[i] == max(nums[i:])
+        cur_max = float("-inf")
+        for num in reversed(nums):
+            if cur_max < num:
+                cur_max = num
+            prefix_max.append(cur_max)
+        prefix_max = prefix_max[::-1]
+
+        suffix_max = [] # suffix_max[i] == max(nums[0..i])
+        cur_max = float("-inf")
+        for num in nums:
+            if cur_max < num:
+                cur_max = num
+            suffix_max.append(cur_max)
+        
+        res = 0
+        for j in range(1, N - 1):
+            best_i = suffix_max[j - 1]
+            best_k = prefix_max[j + 1]
+            val = (best_i - nums[j]) * best_k
+            if res < val:
+                res = val
         return res
