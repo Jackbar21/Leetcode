9db06670efc77dedb54174ce415b0091b7f2a7d7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-02 17:16:35 -0700

    Time: 133 ms (91.47%) | Memory: 30.2 MB (37.88%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -10,16 +10,17 @@ class Solution:
             prefix_max.append(cur_max)
         prefix_max = prefix_max[::-1]
 
-        suffix_max = [] # suffix_max[i] == max(nums[0..i]) == max(nums[:i+1])
-        cur_max = float("-inf")
-        for num in nums:
-            if cur_max < num:
-                cur_max = num
-            suffix_max.append(cur_max)
+        # suffix_max = [] # suffix_max[i] == max(nums[0..i]) == max(nums[:i+1])
+        # cur_max = float("-inf")
+        # for num in nums:
+        #     if cur_max < num:
+        #         cur_max = num
+        #     suffix_max.append(cur_max)
         
         res = 0
+        best_i = float("-inf")
         for j in range(1, N - 1):
-            best_i = suffix_max[j - 1]
+            best_i = max(best_i, nums[j - 1])
             best_k = prefix_max[j + 1]
             val = (best_i - nums[j]) * best_k
             if res < val:
