ebc77b7835777099e4aac161f9f41a5e52aca533
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-02 17:24:59 -0700

    Time: 88 ms (92.83%) | Memory: 30.2 MB (26.28%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         N = len(nums)
 
-        # Compute prefix maxes for all of nums!
+        # Compute prefix maxes for all indices in nums!
         prefix_max = [] # prefix_max[i] == max(nums[i:])
         cur_max = float("-inf")
         for num in reversed(nums):
@@ -10,14 +10,19 @@ class Solution:
                 cur_max = num
             prefix_max.append(cur_max)
         prefix_max = prefix_max[::-1]
+
+        # Compute suffix maxes for all indices in nums!
+        suffix_max = []
+        cur_max = float("-inf")
+        for num in nums:
+            if cur_max < num:
+                cur_max = num
+            suffix_max.append(cur_max)
         
         res = 0
-        best_i = float("-inf")
         for j in range(1, N - 1):
             # Get largest value of nums[i], such that i < j
-            prev_val = nums[j - 1]
-            if best_i < prev_val:
-                best_i = prev_val
+            best_i = suffix_max[j - 1]
 
             # Get largest value of nums[k], such that j < k
             best_k = prefix_max[j + 1]
