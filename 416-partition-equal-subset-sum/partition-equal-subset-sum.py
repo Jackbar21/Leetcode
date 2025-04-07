45864b47db03595fa5f9d57d21a9468f4c89f546
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-06 17:14:42 -0700

    Time: 986 ms (41.70%) | Memory: 229.5 MB (9.53%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -2,63 +2,28 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         sum_nums = sum(nums)
         if sum_nums % 2 == 1:
+            # Sum is odd, 100% CANNOT partition it into two equal subsets in sum!
             return False
-        self.target = sum_nums // 2
-
-        # Sorting the numbers somehow makes code even faster lol...
-        # Probably based on the tests themselves :P
-        nums.sort()
-
-        self.nums, self.memo = nums, {}
-        # return self.dp2d(0, self.target)
-        return self.target in self.dp(0)
+        
+        self.nums = nums
+        target_sum = sum_nums // 2
+        return self.dp(0, target_sum)
     
-    # dp(i) == set of ALL possible subset sums in nums[i:] (unless target in there!)
-    def dp(self, i):
-        if i in self.memo:
-            return self.memo[i]
-        
-        nums, target = self.nums, self.target
-
-        if i >= len(nums):
-            return set()
-        
-        subset_sums = self.dp(i + 1)
-        if target in subset_sums:
-            self.memo[i] = subset_sums
-            return subset_sums
-        
-        num = nums[i]
-        new_candidates = set([num])
-
-        for subset_sum in subset_sums:
-            new_subset_sum = num + subset_sum
-            if new_subset_sum <= target:
-                new_candidates.add(new_subset_sum)
-
-        res = new_candidates.union(subset_sums)
-        self.memo[i] = res
-        return res
-
-
-    def dp2d(self, i, target):
-        if (i, target) in self.memo:
-            return self.memo[(i, target)]
-        
-        if target <= 0:
-            return target == 0
-        
-        if i >= len(self.nums):
-            return False
+    @cache
+    def dp(self, i, sum_left):
+        if sum_left <= 0 or i >= len(self.nums):
+            return sum_left == 0
         
         num = self.nums[i]
 
-        # Case 1: Include num in subset
-        case1 = self.dp2d(i + 1, target - num)
+        # Case 1: Include num
+        case1 = self.dp(i + 1, sum_left - num)
+        if case1:
+            return True
 
-        # Case 2: Don't include num in subset
-        case2 = self.dp2d(i + 1, target)
+        # Case 2: Don't include num
+        case2 = self.dp(i + 1, sum_left)
+        if case2:
+            return True
 
-        res = case1 or case2
-        self.memo[(i, target)] = res
-        return res
+        return False
