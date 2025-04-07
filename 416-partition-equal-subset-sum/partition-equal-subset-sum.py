1b547f2f47eaab7beb3deff4eda21d17174120e3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-06 17:15:29 -0700

    Time: 731 ms (50.76%) | Memory: 143.4 MB (17.94%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -6,11 +6,14 @@ class Solution:
             return False
         
         self.nums = nums
+        self.memo = {}
         target_sum = sum_nums // 2
         return self.dp(0, target_sum)
     
-    @cache
     def dp(self, i, sum_left):
+        if (i, sum_left) in self.memo:
+            return self.memo[(i, sum_left)]
+
         if sum_left <= 0 or i >= len(self.nums):
             return sum_left == 0
         
@@ -19,11 +22,14 @@ class Solution:
         # Case 1: Include num
         case1 = self.dp(i + 1, sum_left - num)
         if case1:
+            self.memo[(i, sum_left)] = True
             return True
 
         # Case 2: Don't include num
         case2 = self.dp(i + 1, sum_left)
         if case2:
+            self.memo[(i, sum_left)] = True
             return True
 
+        self.memo[(i, sum_left)] = False
         return False
