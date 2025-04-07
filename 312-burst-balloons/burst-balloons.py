e76fb221736b9cecc092a0ae09e7565aeb7ef50c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-06 20:38:15 -0700

    Time: 10263 ms (5.02%) | Memory: 27.1 MB (49.78%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -2,13 +2,15 @@ class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
         self.nums = nums
+        self.memo = {}
         N = len(nums)
         return self.dp(1, N - 2)
 
-    @cache
     def dp(self, i, j):
-        nums = self.nums
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
 
+        nums = self.nums
         left_val = nums[i - 1]
         right_val = nums[j + 1]
         # getVal = lambda index: left_val * nums[index] * right_val
@@ -19,7 +21,9 @@ class Solution:
             return getVal(i) if i == j else 0
         
         # We assume index k will be LAST balloon to get popped!
-        return max(
+        res = max(
             self.dp(i, k - 1) + getVal(k) + self.dp(k + 1, j)
             for k in range(i, j + 1)
         )
+        self.memo[(i, j)] = res
+        return res
