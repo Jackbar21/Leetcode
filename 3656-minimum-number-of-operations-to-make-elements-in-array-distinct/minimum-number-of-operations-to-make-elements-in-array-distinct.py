5d798286e4e15d4d54b2e6dc72b083d8971f5838
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-07 18:36:45 -0700

    Time: 4 ms (51.86%) | Memory: 18 MB (9.05%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,31 +1,18 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        self.nums = nums
-        self.memo = {}
-        return self.dp(0, len(nums) - 1)
-
-    
-    def dp(self, i, j):
-        if (i, j) in self.memo:
-            return self.memo[(i, j)]
-        
-        nums = self.nums
-        # Base Case: Check if nums is already distinct!
+        # Loop through nums in REVERSE order, until you reach the FIRST
+        # index i that is maps to a duplicate number! Then you know you
+        # must delete EVERY element in nums[0..i], deleting 3 elements
+        # at a time, with a total of i - 0 + 1 == i + 1 elements.
+        # Hence, given this index i, minimum number of operations
+        # will simply be math.ceil((i + 1) / 3)
         seen = set()
-        is_distinct = True
-        for index in range(i, j + 1):
-            num = nums[index]
+        index = None
+        for i in range(len(nums) - 1, -1, -1):
+            num = nums[i]
             if num in seen:
-                is_distinct = False
+                index = i
                 break
             seen.add(num)
-        if is_distinct:
-            return 0
         
-        # Case 1: Pop 3 elements from the LEFT
-        res = 1 + self.dp(i + 3, j)
-
-        # Case 2: Pop 3 elements from the RIGHT
-
-        self.memo[(i, j)] = res
-        return res
+        return 0 if index is None else math.ceil((i + 1) / 3)
