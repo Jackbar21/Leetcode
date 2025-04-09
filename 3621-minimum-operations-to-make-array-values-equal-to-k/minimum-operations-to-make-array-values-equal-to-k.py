d3ddaa86bcd3f3b947c4b3adcf3f3e6c641fa665
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-08 17:35:27 -0700

    Time: 66 ms (61.09%) | Memory: 17.6 MB (95.75%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,7 +1,19 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        # For fun, here is a one line solution!
+        # return -1 if min(nums) < k else len(set(filter(lambda num: num > k, nums)))
+
+        # We can only make numbers SMALLER. So to make every number equal to k,
+        # it cannot be that ANY number is smaller than k inside of nums!
         if min(nums) < k:
             return -1
+        
+        # Want to find the number of UNIQUE numbers that are larger than k! Since if
+        # the only unique numbers we have larger than k are X < Y < Z, then we must:
+        # (1) Convert Z into Y
+        # (2) Convert Y into X
+        # (3) Convert X into k
+        # So the number of unique numbers larger than k is the solution to this problem!
         larger_nums = set()
         for num in nums:
             if num > k:
