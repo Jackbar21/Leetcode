71c1bb1d020eb2a496acf99fd72424a199257360
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-12 18:01:19 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.38%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -2,6 +2,6 @@ class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = pow(10, 9) + 7
         num_even = math.ceil(n / 2)
-        num_prime = n // 2
+        num_odd = n // 2
         EVEN_CHOICES, PRIME_CHOICES = 5, 4
-        return (pow(EVEN_CHOICES, num_even, MOD) * pow(PRIME_CHOICES, num_prime, MOD)) % MOD
+        return (pow(EVEN_CHOICES, num_even, MOD) * pow(PRIME_CHOICES, num_odd, MOD)) % MOD
