3b0fcd85fab183f337d68b177a30eca9f36137ce
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-18 12:41:07 -0700

    Time: 7 ms (69.28%) | Memory: 18.1 MB (29.54%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,10 +1,13 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         self.s, self.p = s, p
+        self.memo = {}
         return self.dp(0, 0)
     
-    @cache
     def dp(self, i, j):
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
+
         N, M = len(self.s), len(self.p)
         # i represents current index in input string 's'
         # j represents current index in pattern str. 'p'
@@ -15,10 +18,13 @@ class Solution:
             # one or more '<char>*' patterns, where <char> is any character including '.'
             while j < M:
                 if not (j + 1 < M and self.p[j + 1] == "*"):
+                    self.memo[(i, j)] = False
                     return False
                 j += 2
+            self.memo[(i, j)] = True
             return True  
         elif j >= M:
+            self.memo[(i, j)] = False
             return False
         
         letter_s, letter_p = self.s[i], self.p[j]
@@ -30,14 +36,17 @@ class Solution:
 
             # Case 1(a): Pick 0 letters
             if self.dp(i, next_j_index):
+                self.memo[(i, j)] = True
                 return True
             
             # Case 1(b): Pick 1 or more letters
             index = i
             while index < N and (self.s[index] == letter_p or letter_p == "."):
                 if self.dp(index + 1, next_j_index):
+                    self.memo[(i, j)] = True
                     return True
                 index += 1
         
         # Case 2: index j + 1 does not map to '*', which is simplest case
-        return (letter_s == letter_p or letter_p == ".") and self.dp(i + 1, j + 1)
+        self.memo[(i, j)] = (letter_s == letter_p or letter_p == ".") and self.dp(i + 1, j + 1)
+        return self.memo[(i, j)]
