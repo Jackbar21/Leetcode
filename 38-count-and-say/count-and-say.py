2fb6a7ad8ed948de96e07c0a0c44579d3945324f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-17 19:14:06 -0700

    Time: 3 ms (97.10%) | Memory: 18.1 MB (20.58%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,24 +1,23 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        assert n >= 1
-        if n == 1:
-            return "1"
-        
-        digits = self.countAndSay(n - 1)
+        digits = "1"
 
-        prev_digit = digits[0]
-        streak = 0
-        res = []
-        for digit in digits:
-            if digit == prev_digit:
-                streak += 1
-                continue
+        for _ in range(n - 1):
+            prev_digit = digits[0]
+            streak = 0
+            res = []
+            for digit in digits:
+                if digit == prev_digit:
+                    streak += 1
+                    continue
+                
+                res.append(f"{streak}{prev_digit}")
+                prev_digit = digit
+                streak = 1
             
-            res.append(f"{streak}{prev_digit}")
-            prev_digit = digit
-            streak = 1
+            if streak > 0:
+                res.append(f"{streak}{prev_digit}")
+            
+            digits = "".join(res)
         
-        if streak > 0:
-            res.append(f"{streak}{prev_digit}")
-        
-        return "".join(res)
+        return digits
