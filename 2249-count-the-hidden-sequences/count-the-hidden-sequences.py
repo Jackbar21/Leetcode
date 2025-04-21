e5198eb79964acd3ca1b92468232ef6ad14d2ed1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-20 18:03:01 -0700

    Time: 32 ms (94.95%) | Memory: 32.3 MB (57.58%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.py
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.py
@@ -2,27 +2,20 @@ class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
         # For every num in 'hidden', it must be that lower <= num <= upper
         delta = 0
-        deltas = []
+        min_delta, max_delta = 0, 0
         for diff in differences:
             delta += diff
-            deltas.append(delta)
+            if delta < min_delta:
+                min_delta = delta
+            if delta > max_delta:
+                max_delta = delta
 
         # num + min_delta >= lower
         # <==> num >= lower - min_delta
-        min_delta = min(deltas)
-        if min_delta > 0:
-            min_delta = 0
+        lowest = lower - min_delta
 
         # num + max_delta <= upper
         # <==> num <= upper - max_delta
-        max_delta = max(deltas)
-        if max_delta < 0:
-            max_delta = 0
-
-        lowest = lower - min_delta
         highest = upper - max_delta
 
         return highest - lowest + 1 if lowest <= highest else 0
-
-# hidden = [h1, h2, h3, h4, ..., h_n, h_(n+1)]
-# diffs  = [h2-h1, h3-h2, h4-h3, ..., h_(n+1)-h_n]
