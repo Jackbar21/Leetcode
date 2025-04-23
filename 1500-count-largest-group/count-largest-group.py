94b2453b37b5f997b594a578b1fd1e15d7d40f2d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-22 17:07:29 -0700

    Time: 23 ms (81.75%) | Memory: 17.7 MB (72.25%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -2,7 +2,10 @@ class Solution:
     def countLargestGroup(self, n: int) -> int:
         groups = defaultdict(int)
         for num in range(1, n + 1):
-            digit_sum = sum(map(int, str(num)))
+            digit_sum = 0
+            while num > 0:
+                digit_sum += (num % 10)
+                num //= 10
             groups[digit_sum] += 1
         max_group_size = max(groups.values())
         return sum(group_size == max_group_size for group_size in groups.values())
