70031813c172c9d99f8e3c74e527483e08421fa5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-23 17:16:48 -0700

    Time: 728 ms (11.19%) | Memory: 18 MB (46.50%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        N = len(nums)
+        distinct_count = len(set(nums))
+
+        res = 0
+        for i in range(len(nums)):
+            distinct = set()
+            for j in range(i, N):
+                distinct.add(nums[j])
+                # if len(distinct) == distinct_count:
+                #     res += N - j
+                #     break
+                res += len(distinct) == distinct_count
+        return res
+            
+        
+
+        # O(N) - but incorrect
+        l = 0
+        d = {}
+        res = 0
+        for r, num in enumerate(nums):
+            d[num] = d.get(num, 0) + 1
+
+            while len(d) == distinct_count:
+                res += N - r
+                for idx in range(r, N):
+                    print(f"{nums[l:idx+1]}")
+                l_num = nums[l]
+                d[num] -= 1
+                if d[num] == 0:
+                    del d[num]
+                l += 1
+       
+        return res
