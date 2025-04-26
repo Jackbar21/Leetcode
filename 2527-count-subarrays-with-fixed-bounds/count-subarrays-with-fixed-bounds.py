2ffe26ac0053acbeed27c9473cd69c95add3226e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-25 17:37:23 -0700

    Time: 477 ms (5.23%) | Memory: 34 MB (7.60%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -1,12 +1,30 @@
 class Solution:
+
+    # I will write down my solution in case you cannot hear me on the bus!
+    # Essentially, we want to count how many subarrays have minimum value EXACTLY minK
+    # and maximum value EXACTLY maxK. So, we can first grab ALL the invalid indices inside
+    # of nums, i.e. ones whose value is smalelr than minK or larger than maxK -- as subarrays
+    # containing any of those indices are INVALID. 
+    #
+    # Once that's done, we can do a simple sliding window technique where we use a frequency
+    # hash map: a hash map that maps numbers to their current frequencies in the current sliding
+    # window. At ANY point, if we come accross an INVALID number (i.e. one at one of those invalid
+    # indices), then we must RESTART a NEW sliding window array as the current one would become invalid!
+    #
+    # Now with that, we know our current window will NEVER contain values SMALLER than minK nor
+    # LARGER than maxK. Since we're using a frequency hash map, we can check if there's at least
+    # one occurrance of both minK and maxK in our sliding window. If so, then the current window
+    # is VALID (i.e. nums[l..r]). We also know that it is valid for any index R, where r <= R,
+    # as long as every index in range [r,R] maps to a VALID index in nums! Hence, we can check
+    # for the next invalid index after index r using leftmost binary search on our initial
+    # 'invalid_indices' array! Then, we can take that index and decrement one from it to correctly
+    # find index R (i.e. the last valid index is the one right before the first invalid index after r!)
+    # We use that to compute the number of remaining valid windows that start at index l, and then
+    # increment index l (with remembering to decrement the frequency of the number at index l by
+    # one in our current window frequency hash-map!) 
+
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
         N = len(nums)
-        # min_indices = [i for i in range(N) if nums[i] == minK]
-        # max_indices = [i for i in range(N) if nums[i] == maxK]
-
-        # print(f"{min_indices=}")
-        # print(f"{max_indices=}")
-
         invalid_indices = [i for i in range(N) if (nums[i] < minK or nums[i] > maxK)]
 
         # Since its now about equal to counts, maybe sliding window is perfect here?
@@ -39,55 +57,18 @@ class Solution:
                 
                 # If leftmost is still None, that means there's no more invalid indices!
                 if leftmost is None:
-                    assert len(invalid_indices) == 0 or invalid_indices[-1] < l <= r
+                    # assert len(invalid_indices) == 0 or invalid_indices[-1] < l <= r
                     res += N - r
                 else:
                     last_valid = invalid_indices[leftmost] - 1
-                    assert l <= r <= last_valid
-                    # res += N - last_valid
+                    # assert l <= r <= last_valid
                     res += last_valid - r + 1
-                
 
-                # res += N - r # Invalid, since can be invalid elements to the right of us...
+                # Increment 'l' pointer!
                 l_num = nums[l]
                 d[l_num] -= 1
                 if d[l_num] == 0:
                     del d[l_num]
                 l += 1
+
         return res
-
-            
-
-        
-        # I thought the question was count of subarrays where all the numbers
-        # are greater-than-or-equal-to minK and less-than-or-equal-to maxK...
-        l = 0
-        res = []
-        r = 0
-        while r < len(nums):
-            if (nums[r] < minK or nums[r] > maxK):
-                if l <= r - 1:
-                    res.append((l, r - 1))
-                else:
-                    print("ALERT!")
-                r += 1
-                while r < len(nums) and (nums[r] < minK or nums[r] > maxK):
-                    r += 1
-                l = r
-
-            r += 1
-            
-        # for r, num in enumerate(nums):
-        #     if num < minK or num > maxK:
-        #         if l <= r - 1:
-        #             res.append((l, r))
-        assert l <= r - 1
-        res.append((l, r - 1))
-
-        print(f"{res=}, {l=}, {r=}")
-        ans = 0
-        for l, r in res:
-            count = r - l + 1
-            ans += (count * (count + 1)) // 2
-        return ans
-        # return sum(lambda )
