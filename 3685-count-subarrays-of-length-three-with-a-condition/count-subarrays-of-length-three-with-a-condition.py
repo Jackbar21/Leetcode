56fc44005a7f4976239fa6d8dc43bb0c3cdc5aa0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-26 22:38:48 -0700

    Time: 13 ms (13.27%) | Memory: 18 MB (7.35%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(N - 2):
             first, second, third = nums[i], nums[i + 1], nums[i + 2]
             # if (first + third) == second / 2:
-            if 2 * (first + third) == second:
-                res += 1
+            # if 2 * (first + third) == second:
+            #     res += 1
+            res += (first + third) * 2 == second
         return res
