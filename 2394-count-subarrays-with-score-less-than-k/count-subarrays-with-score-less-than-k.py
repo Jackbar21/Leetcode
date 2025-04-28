d8bf7737034e95a8f96b3e4de1a43ed48630bb1e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-04-27 23:05:32 -0700

    Time: 113 ms (79.47%) | Memory: 30.8 MB (14.30%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
@@ -1,5 +1,36 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
+        # return self.countSubarraysLazy(nums, k)
+        N = len(nums)
+
+        # Realization: Nums only consists of POSITIVE integers. So, as soon as we get to a sum
+        # that's too large, we know we must stop. The number of subarrays whose score is strictly
+        # less than k, is simply the total number of possible subarrays MINUS the amount of subarrays
+        # whose score is >= k! Computing the latter is much simpler, as we can use sliding window trick
+        # to get the job done (i.e. once nums[l..r] is valid, since all nums are positive, then
+        # nums[l..r'] is also valid for all r <= r' < N).
+
+        # Step 1: Compute total number of possible subarrays
+        total_subarrays = (N * (N + 1)) // 2
+
+        # Step 2: Count all subarrays whose score is >= k (in variable 'res')
+        l = 0
+        invalid_subarrays = 0
+        cur_sum = 0
+        for r, num in enumerate(nums):
+            cur_sum += num
+            
+            while (cur_sum * (r - l + 1)) >= k:
+                invalid_subarrays += N - r
+                cur_sum -= nums[l]
+                l += 1
+        
+        # Step 3: Compute valid result using computed values from steps 1 & 2
+        valid_subarrays = total_subarrays - invalid_subarrays
+        return valid_subarrays
+
+
+    def countSubarraysLazy(self, nums: List[int], k: int) -> int:
         N = len(nums)
         prefix_sums = []
         cur_sum = 0
@@ -9,14 +40,6 @@ class Solution:
         
         getSubarraySum = lambda i, j: prefix_sums[j] - (prefix_sums[i - 1] if i - 1 >= 0 else 0)
         getScore = lambda i, j: (j - i + 1) * getSubarraySum(i, j)
-
-        # res = 0
-        # for i in range(N):
-        #     for j in range(i, N):
-        #         score = getScore(i, j)
-        #         # print(f"getScore({i}, {j})={score}")
-        #         res += score < k
-        # return res
 
         # Realization: Nums only consists of POSITIVE integers. So, as soon as we get to a sum
         # that's too large, we know we must stop. The number of subarrays whose score is strictly
