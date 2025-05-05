e3c3e68f7e28edf135a5c53073bd3d04f3f3e0da
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-04 19:40:37 -0700

    Time: 10708 ms (5.01%) | Memory: 210.4 MB (5.05%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,54 +1,48 @@
 class Solution:
     def numTilings(self, n: int) -> int:
         MOD = pow(10, 9) + 7
+        self.MOD = MOD
         self.n = n
         self.memo = {}
         res = self.dp(0, 0)
-        #print(f"{self.memo=}")
         return res % MOD
     
     def dp(self, i, j):
         if (i, j) in self.memo:
             return self.memo[(i, j)]
-        N = self.n
+        N, MOD = self.n, self.MOD
 
         if i >= N and j >= N:
-            assert i == N and j == N # TODO: Return 1 IF AND ONLY IF i == N and j == N
-            # return 1
-            return i == N and j == N
+            # assert i == N and j == N # TODO: Return 1 IF AND ONLY IF i == N and j == N
+            return 1
         
         if i >= N:
-            assert i == N
+            # assert i == N
             # Can only put horizontal dominoes on bottom
             return (N - j) % 2 == 0
 
         if j >= N:
-            assert j == N
+            # assert j == N
             # Can only put horizontal dominoes on bottom
             return (N - i) % 2 == 0
-        
-        assert i < N and j < N
-        
+
         # Case 1: Put a Domino vertically
         case1 = 0
         # Only possible if i and j are the same, as otherwise there's a collision!
-        # if i == j and i < N and j < N:
         if i == j:
             case1 = self.dp(i + 1, j + 1)
-            #print(f"{case1=}")
-            assert case1 >= 0
 
         # Case 2: Put a Domino horizontally (either top or bottom!)
         case2 = 0
         # We can put a domino horizontally on either the top or the bottom.
         # When doing so, we must ensure we're not going out of bounds (i.e. i, j <= N)
-        if i + 2 <= N:
-            case2 += self.dp(i + 2, j)
-        if j + 2 <= N:
-            case2 += self.dp(i, j + 2)
         if i + 2 <= N and j + 2 <= N:
-            case2 -= self.dp(i + 2, j + 2) # Do not double count!
-        assert case2 >= 0
+            case2 = self.dp(i + 2, j) + self.dp(i, j + 2) - self.dp(i + 2, j + 2)
+            case2 %= MOD
+        elif i + 2 <= N:
+            case2 = self.dp(i + 2, j)
+        elif j + 2 <= N:
+            case2 = self.dp(i, j + 2)
 
         # Case 3: Put a tromino with 'r' shape
         case3 = 0
@@ -70,6 +64,8 @@ class Solution:
         if i == j + 1:
             case6 = self.dp(i + 1, j + 2)
 
-        res = case1 + case2 + case3 + case4 + case5 + case6
+        res = 0
+        for case in [case1, case2, case3, case4, case5, case6]:
+            res = (res + case) % MOD
         self.memo[(i, j)] = res
         return res
