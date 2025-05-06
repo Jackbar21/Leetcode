44c4fee0da4144d42bb2c15420649d678b04cc1d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-05 17:05:40 -0700

    Time: 3 ms (44.72%) | Memory: 17.9 MB (84.60%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,3 +1,3 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return list(nums[nums[index]] for index in range(len(nums)))
+        return [nums[nums[index]] for index in range(len(nums))]
