43d56e7d96151a892ab1838325e3bb99c743da1d
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-06 17:22:42 -0700

    Time: 129 ms (66.76%) | Memory: 18.7 MB (19.35%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -6,13 +6,14 @@ class Solution:
         DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
         inBounds = lambda x, y: 0 <= x < N and 0 <= y < M
 
-        visited = set([(0, 0)])
+        # visited = set([(0, 0)])
+        visited = set()
         while len(fringe) > 0:
             time, x, y = heapq.heappop(fringe)
             # assert (x, y) not in visited
-            # if (x, y) in visited:
-            #     continue # Already saw this position with a BETTER time!
-            # visited.add((x, y))
+            if (x, y) in visited:
+                continue # Already saw this position with a BETTER time!
+            visited.add((x, y))
 
             if (x, y) == GOAL_STATE:
                 return time
@@ -26,6 +27,6 @@ class Solution:
                 
                 time_to_reach_neigh = max(time, moveTime[neigh_x][neigh_y]) + 1
                 heapq.heappush(fringe, (time_to_reach_neigh, neigh_x, neigh_y))
-                visited.add((neigh_x, neigh_y))
+                # visited.add((neigh_x, neigh_y))
         
         raise Exception("No solution found!")
