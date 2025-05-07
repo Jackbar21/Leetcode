d30b65a4ffff7a682cc4e54a130074fc1402ad28
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-06 17:24:10 -0700

    Time: 156 ms (26.16%) | Memory: 18.4 MB (53.13%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -1,32 +1,28 @@
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        fringe = [(0, 0, 0)] # (time, x, y)
         N, M = len(moveTime), len(moveTime[0])
         GOAL_STATE = (N - 1, M - 1)
         DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
         inBounds = lambda x, y: 0 <= x < N and 0 <= y < M
 
-        # visited = set([(0, 0)])
         visited = set()
+        fringe = [(0, 0, 0)] # (time, x, y)
         while len(fringe) > 0:
             time, x, y = heapq.heappop(fringe)
-            # assert (x, y) not in visited
+
+            # If we've reached GOAL_STATE, we know we've reached
+            # it w/ OPTIMAL time thanks to UCS!
+            if (x, y) == GOAL_STATE:
+                return time
+
             if (x, y) in visited:
                 continue # Already saw this position with a BETTER time!
             visited.add((x, y))
-
-            if (x, y) == GOAL_STATE:
-                return time
             
             for dx, dy in DIRECTIONS:
                 neigh_x, neigh_y = x + dx, y + dy
-                if not inBounds(neigh_x, neigh_y):
-                    continue
-                if (neigh_x, neigh_y) in visited:
-                    continue
-                
-                time_to_reach_neigh = max(time, moveTime[neigh_x][neigh_y]) + 1
-                heapq.heappush(fringe, (time_to_reach_neigh, neigh_x, neigh_y))
-                # visited.add((neigh_x, neigh_y))
-        
+                if inBounds(neigh_x, neigh_y) and (neigh_x, neigh_y) not in visited:
+                    time_to_reach_neigh = max(time, moveTime[neigh_x][neigh_y]) + 1
+                    heapq.heappush(fringe, (time_to_reach_neigh, neigh_x, neigh_y))
+
         raise Exception("No solution found!")
