13e11f607754ffb000061f97c70e44fe76d039f5
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-06 17:30:59 -0700

    Time: 145 ms (34.60%) | Memory: 18.3 MB (71.12%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.py
@@ -5,21 +5,24 @@ class Solution:
         DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
         inBounds = lambda x, y: 0 <= x < N and 0 <= y < M
 
-        visited = set([(0, 0)])
         fringe = [(0, 0, 0)] # (time, x, y)
-        while len(fringe) > 0:
+        visited = set()
+        while fringe:
             time, x, y = heapq.heappop(fringe)
 
             # If we've reached GOAL_STATE, we know we've reached
             # it w/ OPTIMAL time thanks to UCS!
             if (x, y) == GOAL_STATE:
                 return time
+            
+            if (x, y) in visited:
+                continue # Already visited this position with SMALLER time before!
+            visited.add((x, y))
 
             for dx, dy in DIRECTIONS:
                 neigh_x, neigh_y = x + dx, y + dy
                 if inBounds(neigh_x, neigh_y) and (neigh_x, neigh_y) not in visited:
                     time_to_reach_neigh = max(time, moveTime[neigh_x][neigh_y]) + 1
                     heapq.heappush(fringe, (time_to_reach_neigh, neigh_x, neigh_y))
-                    visited.add((neigh_x, neigh_y))
 
         raise Exception("No solution found!")
