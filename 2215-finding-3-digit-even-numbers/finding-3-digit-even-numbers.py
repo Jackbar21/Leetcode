9d21dfd1be312c5229025873ddee6d10cc53b9fa
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-11 17:51:39 -0700

    Time: 4234 ms (10.22%) | Memory: 18.1 MB (28.25%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        N = len(digits)
+        res = set()
+
+        is_valid = lambda x, y, z: x != 0 and z % 2 == 0
+        get_number = lambda x, y, z: (x * 100) + (y * 10) + z
+        
+        for i in range(N):
+            digit_i = digits[i]
+            for j in range(i + 1, N):
+                digit_j = digits[j]
+                for k in range(j + 1, N):
+                    digit_k = digits[k]
+                    
+                    # Given digit_i, digit_j, digit_k, there are 6 ways
+                    # we can rearrange these digits. Append all valid
+                    # ways to res
+                    for x, y, z in [
+                        (digit_i, digit_j, digit_k),
+                        (digit_i, digit_k, digit_j),
+                        (digit_j, digit_i, digit_k),
+                        (digit_k, digit_i, digit_j),
+                        (digit_j, digit_k, digit_i),
+                        (digit_k, digit_j, digit_i)
+                    ]:
+                        if is_valid(x, y, z):
+                            res.add(get_number(x, y, z))
+        
+        return sorted(res)
