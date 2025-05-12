56c8bb5b4d636301d5a84148799b1c798ddb3f78
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-11 17:54:11 -0700

    Time: 2882 ms (22.70%) | Memory: 17.9 MB (46.03%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -6,8 +6,7 @@ class Solution:
         is_valid = lambda x, y, z: x != 0 and z % 2 == 0
         get_number = lambda x, y, z: (x * 100) + (y * 10) + z
         
-        for i in range(N):
-            digit_i = digits[i]
+        for i, digit_i in enumerate(digits):
             for j in range(i + 1, N):
                 digit_j = digits[j]
                 for k in range(j + 1, N):
@@ -24,7 +23,9 @@ class Solution:
                         (digit_j, digit_k, digit_i),
                         (digit_k, digit_j, digit_i)
                     ]:
-                        if is_valid(x, y, z):
-                            res.add(get_number(x, y, z))
+                        # if is_valid(x, y, z):
+                        #     res.add(get_number(x, y, z))
+                        if x != 0 and z % 2 == 0:
+                            res.add((x * 100) + (y * 10) + z)
         
         return sorted(res)
