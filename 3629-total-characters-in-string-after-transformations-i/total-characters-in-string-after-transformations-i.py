f44f7cb01262fd5c6af79ae21ae65ef0e00f6b54
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-12 18:39:01 -0700

    Time: 3558 ms (35.95%) | Memory: 708.1 MB (5.23%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
@@ -1,31 +1,31 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-        self.MAX_MOD = pow(10, 9) + 7
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        MOD = pow(10, 9) + 7
+        res = 0
+        for char in s:
+            offset_from_a = ord(char) - ord("a")
+            res += self.dp(t + offset_from_a)
+        return res % MOD
+    
+    # Return letter after applying k transformations on letter 'a'
+    @cache
+    def dp(self, t):
+        assert t >= 0
+        print(f"{t=}")
 
-    def ASolver(self, t):
-        if t < 26:
+        if t <= 25:
+            # It takes at minimum 26 transformations to increase length of
+            # string that was originally just 'a'
             return 1
         
-        if t in self.memo:
-            return self.memo[t]
-        
-        # After 26 iterations, a turns into "ab"
-        # We can solve the "a" recursively. "b"
-        # is just "a" after 1 transformation, 
-        # so can also solve that recursively :)
-        res = (self.ASolver(t - 26) + self.ASolver(t - 26 + 1)) % self.MAX_MOD
-        self.memo[t] = res
-        return res
-
-
-    def lengthAfterTransformations(self, s: str, t: int) -> int:
-        count = 0
-        for letter in s:
-            offset = ord(letter) - ord('a')
-            count += self.ASolver(t + offset)
-            count %= self.MAX_MOD
-        
-        return count
-
-        
+        # Otherwise, on the 26'th operation, we will have the string "ab"
+        # We can then conclude the length will be dictated from running
+        # t - 26 more operations on the first letter a, and t - 26 more
+        # operations on the second letter b.
+        # One thing to denote is that running k transformations on the
+        # letter b is the same thing as running k + 1 transformations
+        # on the letter a (since a transforms into b in one step!)
+        # Therefore, we can recursively define the resulting length
+        # to simply be dp(t - 26) for the 'a' character in "ab", as well
+        # as dp(t - 26 + 1) == dp(t - 25) for the 'b' character
+        return self.dp(t - 26) + self.dp(t - 25)
