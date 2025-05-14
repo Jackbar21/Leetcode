c654bed9732f1cd2807003f1edb0ba43ea8ce7e1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-13 18:50:46 -0700

    Time: 3162 ms (46.59%) | Memory: 20.6 MB (54.55%) - LeetSync

--- 3630-total-characters-in-string-after-transformations-ii/total-characters-in-string-after-transformations-ii.py
+++ 3630-total-characters-in-string-after-transformations-ii/total-characters-in-string-after-transformations-ii.py
@@ -41,24 +41,53 @@ class Solution:
 ### Solution after checking out 5'th editorial solution for this problem: 
 ### https://leetcode.com/problems/knight-dialer/
 class Solution:
-    # Helper method to multiply two matrices, taken from Knight Dialer Editorial!
-    # (I.e. did not write this on my own...)
-    # TODO: Write my own matrix multiplier function, just to show understanding!
-    # But for now, using it so that I don't fail tests because of matrix multiplication bugs :P
     def multiply(self, A, B):
-        result = [[0] * len(B[0]) for _ in range(len(A))]
-        # print(f"{result=}, {A=}, {B=}")
-        for i in range(len(A)):
-            for j in range(len(B[0])):
-                for k in range(len(B)):
-                    result[i][j] = (result[i][j] + A[i][k] * B[k][j]) % self.MOD
-        return result
+        # Let A be of dimensions ixj. Then B must be of dimensions jxk.
+        # The resulting matrix will be of size ixk.
+        #     [ a_11 a_12 . . . a_1j ]     [ b_11 b_12 . . . b_1k ]
+        #     [ a_21 a_22 . . . a_2j ]     [ b_21 b_22 . . . b_2k ]
+        # A = [ .                    ]  x  [ .                    ] = B
+        #     [ .                    ]     [ .                    ]
+        #     [ .                    ]     [ .                    ]
+        #     [ a_i1 a_i2 . . . a_ij ]     [ b_j1 b_j2 . . . b_jk ]
+        #
+        # <==>
+        #
+        #     [ --------a_1--------- ]     [  |    | . . .     |  ]
+        #     [ --------a_2--------- ]     [  |    | . . .     |  ]
+        # A = [ .                    ]  x  [ b_1  b_2  . . .  b_j ] = B
+        #     [ .                    ]     [  |    |           |  ]
+        #     [ .                    ]     [  |    |           |  ]
+        #     [ --------a_i--------- ]     [  |    | . . .     |  ]
+        #
+        # <==>
+        #         [ (a_1 * b_1) (a_1 * b_2) . . . (a_1 * b_j) ]
+        #         [ (a_2 * b_1) (a_2 * b_2) . . . (a_2 * b_j) ]
+        # A x B = [ .                                         ], where '*' represents DOT-PRODUCT.
+        #         [ .                                         ]
+        #         [ .                                         ]
+        #         [ (a_i * b_1) (a_i * b_2) . . . (a_i * b_j) ]
+        B_cols = self.getColumns(B)
+        res = [[0] * len(B_cols) for _ in range(len(A))]
+        for i, a_vector in enumerate(A):
+            for j, b_vector in enumerate(B_cols):
+                res[i][j] = self.dotProduct(a_vector, b_vector)
+        return res
+    
+    def getColumns(self, matrix):
+        return [[matrix[ri][ci] for ri in range(len(matrix))] for ci in range(len(matrix[0]))]
+
+    def dotProduct(self, v1, v2):
+        assert len(v1) == len(v2)
+        return sum(map(lambda tup: tup[0] * tup[1], zip(v1, v2))) % self.MOD
+
 
     def lengthAfterTransformations(self, s: str, t: int, nums: List[int]) -> int:
         MOD = pow(10, 9) + 7
         self.MOD = MOD
         ALPHABET = "abcdefghijklmnopqrstuvwxyz"
         ORD_A = ord("a")
+        self.memo = {}
 
         # Step 1: Build Actions Matrix
         matrix = [[0] * 26 for _ in range(26)]
@@ -71,8 +100,6 @@ class Solution:
         self.matrix = matrix
 
         
-        # for i, row in enumerate(matrix):
-        #     # print(f"{ALPHABET[i]}: {row}")
         freq_dict = defaultdict(int)
         for letter in s:
             freq_dict[letter] += 1
@@ -83,26 +110,32 @@ class Solution:
             self.matrixExp(t)
         )[0]) % MOD
 
-    @cache
     def matrixExp(self, n):
+        if n in self.memo:
+            return self.memo[n]
+
         # self.matrix should already be defined. This will return value
         # of self.matrix to the power of 'n'
-        matrix = self.matrix.copy()
+        matrix = self.matrix
 
-        assert n >= 1
+        # assert n >= 1
         if n == 1:
             return matrix
         
         if n % 2 == 1:
-            return self.multiply(
+            res = self.multiply(
                 matrix,
                 self.matrixExp(n - 1)
             )
+            self.memo[n] = res
+            return res
         
         half_exp_matrix = self.matrixExp(n // 2)
-        return self.multiply(
+        res = self.multiply(
             half_exp_matrix,
-            half_exp_matrix.copy()
+            half_exp_matrix
         )
+        self.memo[n] = res
+        return res
             
 
