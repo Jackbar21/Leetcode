a18ffbe98543e9c20e0f0ea74b6dd37977bd2dac
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-15 18:25:16 -0700

    Time: 802 ms (67.04%) | Memory: 18.2 MB (75.00%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -2,9 +2,6 @@ class Solution:
     def isValidHammingDistance(self, i: int, j: int) -> bool:
         # Returns true if and only if hamming distance between words
         # at indices i and j is 1.
-        if (i, j) in self.hamming_memo:
-            return self.hamming_memo[(i, j)]
-
         words = self.words
         N = len(words)
         # assert 0 <= i < N
@@ -12,7 +9,6 @@ class Solution:
         # assert i < j
         word_i, word_j = words[i], words[j]
         if len(word_i) != len(word_j):
-            self.hamming_memo[(i, j)] = False
             return False
         
         hamming_distance = 0
@@ -21,17 +17,13 @@ class Solution:
             if letter_i != letter_j:
                 hamming_distance += 1
                 if hamming_distance > 1:
-                    self.hamming_memo[(i, j)] = False
                     return False
-        res = (hamming_distance == 1)
-        self.hamming_memo[(i, j)] = res
-        return res
+        return hamming_distance == 1
 
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
         N = len(words)
         self.words, self.groups = words, groups
         self.memo = {}
-        self.hamming_memo = {}
 
         best_length = self.dp(0)
         best_index = 0
@@ -43,25 +35,22 @@ class Solution:
         
         res = [words[best_index]]
         want_length = best_length - 1
-        for index in range(best_index + 1, N):
+        prev_index = best_index
+        for index in range(prev_index + 1, N):
             if self.dp(index) == want_length:
                 # Make sure it's valid!
-                if groups[best_index] == groups[index]:
+                if groups[prev_index] == groups[index]:
                     continue
-                if not self.isValidHammingDistance(best_index, index):
+                if not self.isValidHammingDistance(prev_index, index):
                     continue
 
                 res.append(words[index])
                 want_length -= 1
                 
                 # Update best_index for next searching!
-                best_index = index
+                prev_index = index
 
-                # if want_length == 0:
-                #     break
-        #print(f"{self.words=}")
-        #print(f"{self.groups=}")
-        sorted_memo = {i: self.memo[i] for i in sorted(self.memo.keys())}
+        # sorted_memo = {i: self.memo[i] for i in sorted(self.memo.keys())}
         #print(f"self.memo={sorted_memo}")
         # return res
         # res.sort(key = lambda word: self.original_indices[word])
