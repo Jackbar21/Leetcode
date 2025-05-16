a1f3bfd6460e460e5fe4f4e81ba76d3fa8c67979
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-15 18:34:21 -0700

    Time: 687 ms (86.36%) | Memory: 18 MB (93.18%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -1,24 +1,18 @@
 class Solution:
-    def isValidHammingDistance(self, i: int, j: int) -> bool:
+    def isValidHammingDistance(self, word1: str, word2: str) -> bool:
         # Returns true if and only if hamming distance between words
         # at indices i and j is 1.
-        words = self.words
-        N = len(words)
-        # assert 0 <= i < N
-        # assert 0 <= j < N
-        # assert i < j
-        word_i, word_j = words[i], words[j]
-        if len(word_i) != len(word_j):
+        if len(word1) != len(word2):
             return False
         
         hamming_distance = 0
-        for index, letter_i in enumerate(word_i):
-            letter_j = word_j[index]
+        for index, letter_i in enumerate(word1):
+            letter_j = word2[index]
             if letter_i != letter_j:
                 hamming_distance += 1
                 if hamming_distance > 1:
                     return False
-        return hamming_distance == 1
+        return True # Since all words are distinct, cannot be that hamming_distance == 0
 
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
         N = len(words)
@@ -36,19 +30,23 @@ class Solution:
         res = [words[best_index]]
         want_length = best_length - 1
         prev_index = best_index
+        prev_word = words[prev_index]
         for index in range(prev_index + 1, N):
             if self.dp(index) == want_length:
+                word = words[index]
+
                 # Make sure it's valid!
                 if groups[prev_index] == groups[index]:
                     continue
-                if not self.isValidHammingDistance(prev_index, index):
+                if not self.isValidHammingDistance(prev_word, word):
                     continue
 
-                res.append(words[index])
+                res.append(word)
                 want_length -= 1
                 
                 # Update best_index for next searching!
                 prev_index = index
+                prev_word = word
 
         return res
 
@@ -65,9 +63,10 @@ class Solution:
         # Return longest subsequence starting from index i
         res = 1
         group = groups[i]
+        word = words[i]
 
         for j in range(i + 1, N):
-            if group != groups[j] and self.isValidHammingDistance(i, j):
+            if group != groups[j] and self.isValidHammingDistance(word, words[j]):
                 length = 1 + self.dp(j)
                 if res < length:
                     res = length
