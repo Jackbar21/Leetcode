8ad4d439de066ea10855740201dae3851ad11f57
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-17 21:17:10 -0700

    Time: 2625 ms (13.33%) | Memory: 87.9 MB (14.44%) - LeetSync

--- 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.py
+++ 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.py
@@ -8,22 +8,30 @@ class Solution:
         M, N = self.M, self.N
 
         if col_index >= N:
-            assert col_index == N
+            # assert col_index == N
             return 1 # We found a valid solution!
 
         res = 0
         for permutation in self.permutations:
             # Valid if and only if each index has a different color from previous column!
-            assert len(prev_column) == len(permutation) == M
-            is_valid = all(prev_column[i] != permutation[i] for i in range(M))
+            # assert len(prev_column) == len(permutation) == M
+            is_valid = True
+            for i in range(M):
+                if prev_column[i] == permutation[i]:
+                    is_valid = False
+                    break
+
             if is_valid:
                 res += self.dp(col_index + 1, permutation)
+                res %= self.MOD
+
         return res
 
     def colorTheGrid(self, m: int, n: int) -> int:
-        MOD = pow(10, 9) + 7
+        self.MOD = pow(10, 9) + 7
         RED, GREEN, BLUE, NO_COLOR = 0, 1, 2, 3
-        DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        COLOR_OPTIONS = [RED, GREEN, BLUE]
+
         M, N = m, n
         self.M, self.N = M, N
 
@@ -31,17 +39,15 @@ class Solution:
         permutations = []
 
         def backtrack(i):
-            assert i > 0
-            prev_color = arr[i - 1]
-
+            # assert i > 0
             if i == M:
                 permutations.append(tuple(arr))
                 return
 
-            for color in [RED, GREEN, BLUE]:
+            prev_color = arr[i - 1]
+            for color in COLOR_OPTIONS:
                 if color == prev_color:
                     continue
-                
                 arr.append(color)
                 backtrack(i + 1)
                 arr.pop()
@@ -49,18 +55,14 @@ class Solution:
             arr = [color] # to always have a prev_color!
             backtrack(1)
         
-        assert len(permutations) == 3 * pow(2, M - 1)
-        print(f"{len(permutations)=}")
-        print(f"{permutations=}")
+        # assert len(permutations) == 3 * pow(2, M - 1)
         self.permutations = permutations
-        # return 1
-        return self.dp(0, tuple([NO_COLOR] * M)) % MOD
+        return self.dp(0, tuple([NO_COLOR] * M)) % self.MOD
         
-            
-            
 
         ### BACKTRACKING SOLUTION (START) ### -- Too slow!
         # Let's give backtracking a shot, even though there's nearly 100% chance this TLEs!
+        DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
         self.res = 0
         self.grid = [[NO_COLOR] * N for _ in range(M)]
         self.inBounds = lambda i, j: 0 <= i < M and 0 <= j < N
@@ -99,50 +101,5 @@ class Solution:
             return
         
         backtrack(0, 0)
-        return self.res % MOD
+        return self.res % self.MOD
         ### BACKTRACKING SOLUTION (END) ### -- Too slow!
-
-        # self.M, self.N = M, N
-        # self.memo = {}
-        # return self.dp(0, 0, {})
-        # dp = [[NO_COLOR] * N for _ in range(M)]
-        # for i in range(M):
-        #     for j in range(N):
-            
-
-
-
-
-
-    
-    
-    # @cache
-    # def dp(self, i, j, d):
-    #     cache_key = (i, j, tuple(key, d[key] for key in sorted(d.keys())))
-    #     if cache_key in self.memo:
-    #         return self.memo[cache_key]
-
-    #     RED, GREEN, BLUE, NO_COLOR = 0, 1, 2, 3
-    #     M, N = self.M, self.N
-
-    #     # Valid colors represents available options to us!
-    #     valid_colors = set([RED, GREEN, BLUE])
-    #     for color in [left, up, right, down]:
-    #         valid_colors.discard(color)
-        
-    #     assert self.inBounds(i, j)
-        
-    #     # Termination case!
-    #     if len(valid_colors) == 0:
-    #         # We've hit a dead end (ran out of options), so no solution here!
-    #         return 0
-
-    #     if i == M - 1 and j == N - 1:
-    #         # Got to the last cell, and len(valid_colors) > 0, so this is a plausible solution!
-    #         return 1
-        
-    #     for color in valid_colors:
-            
-        
-
-        
