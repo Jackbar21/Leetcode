bad8ab5e190253126a92f1ace3eb65c75752a45a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-19 17:13:35 -0700

    Time: 146 ms (10.60%) | Memory: 61.1 MB (5.11%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -1,14 +1,12 @@
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
         N = len(nums)
-        line_sweep = {i: 0 for i in range(N + 1)}
+        # line_sweep = {i: 0 for i in range(N + 1)}
+        line_sweep = defaultdict(int)
         for start, end in queries:
             line_sweep[start] += 1
             line_sweep[end + 1] -= 1
         
-        # print(f"{line_sweep=}")
-        # print(f"{nums=}")
-        # print(f"{queries=}")
         cur_delta = 0
         for i, num in enumerate(nums):
             cur_delta += line_sweep[i]
