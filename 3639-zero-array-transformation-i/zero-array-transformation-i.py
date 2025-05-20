2b6e3f0eb30866107ca18db891349061479d2fc1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-19 17:12:54 -0700

    Time: 138 ms (12.38%) | Memory: 61.1 MB (5.11%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        N = len(nums)
+        line_sweep = {i: 0 for i in range(N + 1)}
+        for start, end in queries:
+            line_sweep[start] += 1
+            line_sweep[end + 1] -= 1
+        
+        # print(f"{line_sweep=}")
+        # print(f"{nums=}")
+        # print(f"{queries=}")
+        cur_delta = 0
+        for i, num in enumerate(nums):
+            cur_delta += line_sweep[i]
+            if num - cur_delta > 0:
+                return False
+        return True
