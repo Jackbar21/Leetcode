0b38cc436ec2df0a7cbd9f86bca887166da1828e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-20 17:21:23 -0700

    Time: 2 ms (78.44%) | Memory: 18.6 MB (17.00%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,61 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # return self.setZeroesLinearSpace(matrix)    # O(M * N) time, O(M + N) space
-        return self.setZeroesConstantSpace(matrix) # O((M * N)^2) time, O(1) space
-    
-    def setZeroesLinearSpace(self, matrix):
         M, N = len(matrix), len(matrix[0])
+        zero_rows = set()
+        zero_cols = set()
 
-        # O(M + N) space!
-        row_indices = set()
-        col_indices = set()
-
-        # O(M * N)
         for i in range(M):
             for j in range(N):
-                if matrix[i][j] == 0:
-                    row_indices.add(i)
-                    col_indices.add(j)
+                num = matrix[i][j]
+                if num == 0:
+                    zero_rows.add(i)
+                    zero_cols.add(j)
         
-        # O(M * N)
-        for row_index in row_indices:
-            for col_index in range(N):
-                matrix[row_index][col_index] = 0
-        
-        # O(M * N)
-        for col_index in col_indices:
-            for row_index in range(M):
-                matrix[row_index][col_index] = 0
-        
-        return matrix
-
-    
-    def setZeroesConstantSpace(self, matrix: List[List[int]]) -> None:
-        M, N = len(matrix), len(matrix[0])
-
-        # Idea: In order to use constant space, whenever a 0 is detected, set ALL of the values
-        # in that row & column to some impossible value such as 'None'. This is because even something
-        # as simple as tracking the row & column INDICES that need to be set to 0s will require O(M + N)
-        # space!
-        # N.B. Any found 0 should NOT be set to None so that it can be looped over in the future so
-        # that its column and row related values also become None or 0 later on!
-        for row_index in range(M):
-            for col_index in range(N):
-                if matrix[row_index][col_index] != 0:
-                    continue
-                
-                # This makes runtime no longer O(mn) tho :/
-                for i in range(M):
-                    if matrix[i][col_index] != 0:
-                        matrix[i][col_index] = None
-                for j in range(N):
-                    if matrix[row_index][j] != 0:
-                        matrix[row_index][j] = None
-        
-        for row_index in range(M):
-            for col_index in range(N):
-                if matrix[row_index][col_index] == None:
-                    matrix[row_index][col_index] = 0
+        for i in range(M):
+            for j in range(N):
+                if i in zero_rows or j in zero_cols:
+                    matrix[i][j] = 0
         
         return matrix
