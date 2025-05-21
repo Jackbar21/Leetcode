c816ae41a86ada46bd574aad898cf66b428724d8
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-20 17:50:21 -0700

    Time: 11 ms (7.82%) | Memory: 19.1 MB (6.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,6 +4,76 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         M, N = len(matrix), len(matrix[0])
+
+        top_left_commands = set()
+
+        for i in range(M):
+            for j in range(N):
+                num = matrix[i][j]
+                if num == 0:
+                    # matrix[i][j] = None
+                    matrix[i][0] = None
+                    # matrix[i][-1] = None
+                    matrix[0][j] = None
+                    # matrix[-1][j] = None
+
+                    if i == 0:
+                        top_left_commands.add("R")
+                    if j == 0:
+                        top_left_commands.add("C")
+        
+        for row in matrix:
+            print(row)
+        
+        # for i in range(M):
+        #     for j in range(N):
+        #         num = matrix[i][j]
+        #         if num == None:
+        #             matrix[i][j] = 0
+
+        # Handle first row (zero columns!)
+        for j in range(1, N):
+            if matrix[0][j] is None:
+                for i in range(M):
+                    if matrix[i][j] is not None:
+                        matrix[i][j] = 0
+
+        # Handle first column (zero rows!)
+        for i in range(1, M):
+            if matrix[i][0] is None:
+                for j in range(N):
+                    if matrix[i][j] is not None:
+                        matrix[i][j] = 0
+        
+        # Wipe top row!
+        if "R" in top_left_commands:
+            for j in range(N):
+                matrix[0][j] = 0
+        
+        # Wipe first col!
+        if "C" in top_left_commands:
+            for i in range(M):
+                matrix[i][0] = 0
+        
+
+
+        print(f"\nPRE-CLEANUP:\n")
+        for row in matrix:
+            print(row)
+
+        
+        # Cleanup!
+        for i in range(M):
+            for j in range(N):
+                if matrix[i][j] is None:
+                    matrix[i][j] = 0
+        
+        return matrix
+
+
+        # O(M + N) space solution
+        """
+        M, N = len(matrix), len(matrix[0])
         zero_rows = set()
         zero_cols = set()
 
@@ -20,3 +90,9 @@ class Solution:
                     matrix[i][j] = 0
         
         return matrix
+        """
+
+#  N  N  3  N
+#  N  0  7  8
+#  N 10 11 12
+#  N 14 15  0
