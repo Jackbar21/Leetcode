e5f0231c9a3b8cea3330e20e28f27fd6c8873b33
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-22 18:07:17 -0700

    Time: 327 ms (8.62%) | Memory: 27.4 MB (17.24%) - LeetSync

---
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -0,0 +1,143 @@
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        N, M = len(nums), len(edges)
+        print(f"{sum(nums)=}")
+
+        # We leverage the fact that since this is a TREE, that means EVERY two nodes
+        # are connected, which means we can apply on operation on ANY pair of two nodes
+        # (u, v) in the graph, NOT just the ones in edges!
+        getDelta = lambda num: (num ^ k) - num
+        max_heap = [(-getDelta(num), num) for num in nums] # (delta, num)
+        heapq.heapify(max_heap)
+        for num in nums:
+            print(f"getDelta({num})={getDelta(num)}, num % k ={num ^ k}, num={num}")
+
+        # pos_deltas = []
+        # for num in nums:
+        #     delta = getDelta(num)
+        #     if delta >= 0:
+        #         pos_deltas.append(num)
+        
+        # min_delta = 0 if len(pos_deltas) % 2 == 0 else min(pos_deltas)
+        # print(f"{min_delta=}")
+        # print(f"{sorted(pos_deltas)=}")
+        # return sum(nums) + sum(pos_deltas) - min_delta
+
+
+
+        res = sum(nums)
+        while len(max_heap) >= 2:
+            delta1, num1 = heapq.heappop(max_heap)
+            delta2, num2 = heapq.heappop(max_heap)
+            delta1 = -delta1
+            delta2 = -delta2
+            delta = delta1 + delta2
+
+            print(f"{delta1, delta2=}")
+
+            # if delta < 0:
+            #     break
+            
+            # res += delta
+            if delta > 0:
+                res += delta
+        
+        print(f"{res=}, {max_heap=}")
+        return res
+
+
+
+    # My attempt at solving this problem before checking out Hints & Discussions!
+    def maximumValueSumAttempt(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        N, M = len(nums), len(edges)
+        adj_list = {i: set() for i in range(N)}
+
+        def getEdgeCost(u, v):
+            res = 0
+
+            old_u, old_v = nums[u], nums[v]
+            new_u, new_v = nums[u] ^ k, nums[v] ^ k
+
+            res += (new_u - old_u)
+            res += (new_v - old_v)
+            return res
+        
+        for u, v in edges:
+            edge_cost = getEdgeCost(u, v)
+            adj_list[u].add((v, edge_cost))
+            adj_list[v].add((u, edge_cost))
+        
+        # Want to find path with MAXIMAL sum in Graph!
+        # return sum(nums) + sum(max(0, getEdgeCost(u, v)) for u, v in edges)
+        res = sum(nums)
+
+        max_heap = [(-getEdgeCost(u, v), u, v, False)] # (edge_cost, u, v, reseen)
+        heapq.heapify(max_heap) # O(n)
+
+        while max_heap:
+            edge_cost, u, v, reseen = heapq.heappop(max_heap)
+            edge_cost = -edge_cost
+
+            if edge_cost != getEdgeCost(u, v):
+                heapq.heappush(max_heap, (-getEdgeCost(u, v), u, v, reseen))
+                continue
+            
+            if edge_cost <= 0:
+                found_larger = False
+                for i, (cost, x, y, seen) in max_heap:
+                    cost = getEdgeCost(x, y)
+                    max_heap[i] = (-cost, x, y, seen)
+                    if cost > 0:
+                        found_larger = True
+                        break
+                if not found_larger:
+                    break
+                else:
+                    continue
+
+                # if reseen:
+                #     break
+                
+                # break # Nothing good for us anymore!
+                heapq.heappush(max_heap, (-getEdgeCost(u, v), u, v, True))
+            
+            res += edge_cost
+        
+        return res
+
+
+
+        ### BATCKTRACK SOLUTION (TLE) ###
+        """
+        N, M = len(nums), len(edges)
+        res = float("-inf")
+        cur_sum = sum(nums)
+        def backtrack(i):
+            nonlocal res
+            nonlocal cur_sum
+            if i >= M:
+                # res = max(res, sum(nums))
+                res = max(res, cur_sum)
+                return
+            
+            # Case 1: Don't apply edge
+            backtrack(i + 1)
+
+            # Case 2: Apply edge
+            u, v = edges[i]
+            old_u, old_v = nums[u], nums[v]
+            nums[u] ^= k
+            nums[v] ^= k
+            cur_sum += (nums[u] - old_u)
+            cur_sum += (nums[v] - old_v)
+            backtrack(i + 1)
+            # nums[u] ^= k
+            # nums[v] ^= k
+            cur_sum -= (nums[u] - old_u)
+            cur_sum -= (nums[v] - old_v)
+            nums[u] = old_u
+            nums[v] = old_v
+
+        backtrack(0)
+        return res
+        """
