04128f85a94d37302f57997f21e4c5c5e078de4c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-27 17:45:59 -0700

    Time: 1873 ms (40.94%) | Memory: 18.9 MB (36.22%) - LeetSync

---
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.py
@@ -0,0 +1,62 @@
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+        # We're going to apply the same operation for every node i, 0 <= i < n, in the first tree.
+        # Hence, let i be arbitrary.
+        # 
+        # Since we're able to add an edge from i to some other arbitrary node i' in the second tree,
+        # we can simply connect it to the node in the second tree that has the maximal amount of
+        # target nodes with value k - 1 (since one out of k edges is reserved from i to i').
+        # We can do this computation once in O(m^2) time in the second tree, and use it for
+        # every node in the first tree.
+        N, M = len(edges1) + 1, len(edges2) + 1
+
+        adj_list1 = [[] for _ in range(N)]
+        for a, b in edges1:
+            adj_list1[a].append(b)
+            adj_list1[b].append(a)
+
+        adj_list2 = [[] for _ in range(M)]
+        for u, v in edges2:
+            adj_list2[u].append(v)
+            adj_list2[v].append(u)
+
+        # Step 1: Get node in second tree with maximal number of target nodes with value k - 1.
+        max_reachability = max(self.getNodeReachabilities(M, adj_list2, k - 1))
+
+        # Step 2: For each node in first tree, figure out its reachability 
+        reachable = self.getNodeReachabilities(N, adj_list1, k)
+
+        # Step 3: Add max reachability to each node in tree1's already set reachability!
+        return list(map(lambda reachability: reachability + max_reachability, reachable))
+
+    
+    def getNodeReachabilities(self, node_count, adj_list, max_distance):
+        if max_distance == 0:
+            return [1] * node_count # Just the node itself!
+        elif max_distance < 0:
+            return [0] * node_count
+
+        reachable = []
+        for i in range(node_count):
+            queue = collections.deque([(i, 0)])
+            visited = set([i])
+            while queue:
+                node, distance = queue.popleft()
+
+                #print(f"{distance, max_distance=}")
+                assert distance <= max_distance
+                if distance == max_distance:
+                    continue # No more exploration!
+                
+                for neigh in adj_list[node]:
+                    if neigh in visited:
+                        continue
+                    visited.add(neigh)
+                    queue.append((neigh, distance + 1))
+            
+            # #print(f"{i=}, {visited=}")
+            reachable.append(len(visited))
+        
+        # #print(f"{max(reachable)=}")
+        # max_reachable = max(reachable)
+        return reachable
