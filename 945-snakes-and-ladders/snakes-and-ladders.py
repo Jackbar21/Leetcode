d8cacdda88692abb1f37131265a3bd960692d9cc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-05-30 20:09:22 -0700

    Time: 240 ms (5.11%) | Memory: 18.1 MB (15.72%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -0,0 +1,67 @@
+class Solution:
+    def getSquareCoordinates(self, square):
+        board, N = self.board, self.N
+        assert 1 <= square <= pow(N, 2)
+
+        row_index_backwards = (square // N) - (square % N == 0)
+        row_index = (N - 1) - row_index_backwards
+
+        # Column index gets affected by "Boustrophedon style", hence
+        # need to check if base row index (i.e. 'row_index_backwards')
+        # is odd or even!
+        start_square_of_row = row_index_backwards * N + 1
+        column_offset = square - start_square_of_row
+
+        column_index = (
+            column_offset
+            if row_index_backwards % 2 == 0
+            else N - column_offset - 1
+        )
+
+        return row_index, column_index
+
+
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        N = len(board)
+        self.board, self.N = board, N
+        GOAL_STATE = pow(N, 2)
+        fringe = [(0, 1)] # (cost, curr_square) | UCS, i.e. min_heap
+        # best_cost = defaultdict(lambda: float("inf")) # visited set | keeps track of best cost so far!
+        # best_cost[1] = 0
+        visited = set()
+
+        for square in range(1, N**2 + 1):
+            row_index, col_index = self.getSquareCoordinates(square)
+            print(f"{square} -> {board[row_index][col_index]}")
+        # return -1
+
+        res = float("inf")
+        while fringe:
+            cost, curr_square = heapq.heappop(fringe)
+            print(f"{cost=}, {curr_square=}")
+
+            if curr_square == GOAL_STATE:
+                res = min(res, cost)
+                continue
+            
+            # assert curr_square not in visited
+            if curr_square in visited:
+                continue # Already visited with a better cost!
+            visited.add(curr_square)
+            
+            # new_cost = cost + 1
+            # for next_square in range(curr_square + 1, min(curr_square + 6, GOAL_STATE) + 1):
+            #     if new_cost >= visited[next_square]:
+            #         continue # Not worth visiting again, since smaller cost!
+            #     visited.add(next_square)
+
+            for next_square in range(curr_square + 1, min(curr_square + 6, GOAL_STATE) + 1):
+                # if next_square in visited:
+                #     continue
+                
+                r, c = self.getSquareCoordinates(next_square)
+                final_square = next_square if board[r][c] == -1 else board[r][c]
+                
+                heapq.heappush(fringe, (cost + 1, final_square))
+        
+        return res if res != float("inf") else -1
