8756b227399391fb985f5ce2a8fbb53d866142cc
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-01 17:39:21 -0700

    Time: 39 ms (6.71%) | Memory: 33.4 MB (5.10%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,30 +1,38 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         N = len(ratings)
-        adj_list = [[] for i in range(N)]
-        for i in range(len(ratings)):
-            prev = ratings[i - 1] if i > 0 else float("inf")
-            cur = ratings[i]
-            nxt = ratings[i + 1] if i + 1 < N else float("inf")
-
-            if cur > prev:
-                # cur (index i) needs MORE candy than prev (index i - 1)
-                adj_list[i].append(i - 1)
-            
-            if cur > nxt:
-                # cur (index i) needs MORE candy than prev (index i - 1)
-                adj_list[i].append(i + 1)
 
         memo = {}
-        def dp(node):
-            if node in memo:
-                return memo[node]
-
-            if len(adj_list[node]) == 0:
-                return 1 # Doesn't need more candy than anybody else, so just 1 candy!
+        def dp(index):
+            if index in memo:
+                return memo[index]
             
-            res = 1 + max(dp(neigh) for neigh in adj_list[node])
-            memo[node] = res
+            # Neighbors will be adjacent indices of node whose ratings are SMALLER.
+            # Since if a neighbor's rating is smaller, that means node at current
+            # index needs MORE candy than them. So we'll look at both adjacent neighbors,
+            # compute how many candies they need recursively if their rating(s) are smaller,
+            # and take 1 more than them to satisfy the condition (but use as few candies
+            # as possible!) Of course, if our rating is not larger, than we do not consider
+            # these neighbor(s).
+            rating = ratings[index]
+
+            # Number of candy child at index i needs. 
+            # Must be at least 1 to satisfy first condition!
+            res = 1
+
+            # Case 1: Left neighbor
+            if index > 0 and ratings[index - 1] < rating:
+                case1 = 1 + dp(index - 1)
+                if res < case1:
+                    res = case1
+            
+            # Case 2: Right neighbor
+            if index + 1 < N and ratings[index + 1] < rating:
+                case2 = 1 + dp(index + 1)
+                if res < case2:
+                    res = case2
+
+            memo[index] = res
             return res
         
         return sum(dp(i) for i in range(N))
