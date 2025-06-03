7e6f1fe0918d20a4524589c7fb6abc417340eaf2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-02 17:48:29 -0700

    Time: 11 ms (92.34%) | Memory: 28.2 MB (65.13%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
@@ -3,16 +3,10 @@ class Solution:
         OPEN, CLOSED = 1, 0
 
         queue = collections.deque((box, False) for box in initialBoxes) # (box_index, seen_twice)
-        # visited = set(initialBoxes)
-        # visited = set()
         max_candy = 0
 
         while queue:
             box_index, seen_twice = queue.popleft()
-
-            # print(f"{box_index=}, {max_candy=}")
-            # if box_index in visited:
-            #     continue
 
             if status[box_index] == CLOSED:
                 if seen_twice:
@@ -24,25 +18,16 @@ class Solution:
                     queue.append((box_index, True))
                     continue
     
-            # visited.add(box_index)
             candy = candies[box_index]
             max_candy += candy
 
             for key in keys[box_index]:
-                # print(f"{key=}")
                 status[key] = OPEN
             
             for box in containedBoxes[box_index]:
-                # print(f"containedBox={box}")
-                # if box in visited:
-                #     continue
-
                 if status[box] == OPEN:
                     queue.appendleft((box, False))
                 else:
                     queue.append((box, False))
-            
-            # print(f"{queue=}")
-            # print()
 
         return max_candy
