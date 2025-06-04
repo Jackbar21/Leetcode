42d2ee18211d2862b150610344d3ae32c274d0cf
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-03 17:31:40 -0700

    Time: 15 ms (49.24%) | Memory: 18.2 MB (18.94%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
@@ -1,25 +1,17 @@
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
         delete_count = numFriends - 1
-        # if numFriends == 1:
-        #     return word
         if delete_count == 0:
             return word
         
         ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-        # ORD_A = ord("a")
-        # getLetterIndex = lambda letter: ord(letter) - ORD_A
-        # freq = [0] * len(ALPHABET)
-        # for letter in word:
-        #     freq[getLetterIndex(letter)] += 1
-        # getLetterFreq = lambda letter: freq[getLetterIndex(letter)]
         freq_dict = {letter: 0 for letter in ALPHABET}
         indices_dict = {letter: [] for letter in ALPHABET}
         for index, letter in enumerate(word):
             freq_dict[letter] += 1
             indices_dict[letter].append(index)
 
-        biggest_letter = max(word)
+        biggest_letter = max(letter for letter, freq in freq_dict.items() if freq > 0)
         indices = indices_dict[biggest_letter]
 
         res = ""
@@ -27,5 +19,6 @@ class Solution:
             letters_before = index
             delta = delete_count - letters_before
             case = word[index:] if delta <= 0 else word[index:-delta]
-            res = max(res, case)
+            if res < case:
+                res = case
         return res
