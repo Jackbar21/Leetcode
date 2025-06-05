2daa58960ddfcfb50d55c777c7ba67117b303288
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-04 17:24:53 -0700

    Time: 5 ms (61.56%) | Memory: 17.7 MB (96.84%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
@@ -1,16 +1,16 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        assert len(s1) == len(s2)
+        # assert len(s1) == len(s2)
         N = len(s1)
         ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
         # Step 1: Create graph out of letter equality relationship
         # (e.g. edge from letter X to letter Y means X == Y)
-        adj_list = {letter: [] for letter in ALPHABET}
+        adj_list = {letter: set() for letter in ALPHABET}
         for i in range(N):
             letter1, letter2 = s1[i], s2[i]
-            adj_list[letter1].append(letter2)
-            adj_list[letter2].append(letter1)
+            adj_list[letter1].add(letter2)
+            adj_list[letter2].add(letter1)
         
         # Step 2: There are a constant amount of English lowercase letters, namely 26.
         # For each such letter, run a bfs traversal to find the lexicographically SMALLEST
@@ -18,7 +18,7 @@ class Solution:
         # NOTE: We run a BFS from each letter and not leverage DP amongst one another, 
         # since this graph contains CYCLES. But since only O(1) unique letters, this is okay!
         best_letter = {letter: letter for letter in ALPHABET}
-        for source_letter in ALPHABET:
+        for source_letter in set(baseStr):
             # Run BFS
             queue = collections.deque([source_letter])
             visited = set([source_letter])
@@ -27,8 +27,8 @@ class Solution:
                 letter = queue.pop()
                 if letter < smallest_letter:
                     smallest_letter = letter
-                # if smallest_letter == "a":
-                #     break
+                if smallest_letter == "a":
+                    break
                 
                 for neigh in adj_list[letter]:
                     if neigh not in visited:
