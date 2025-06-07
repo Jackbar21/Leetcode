a95bc141c9c32d350b59b8f68caffb25278ba55a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-06 17:24:22 -0700

    Time: 599 ms (55.40%) | Memory: 23.7 MB (54.68%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,15 +1,12 @@
 class Solution:
     def clearStars(self, s: str) -> str:
         ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-        ORD_A = ord("a")
-        # d = {letter: [] for letter in ALPHABET} # -1 means NO index!
-        d = [[] for _ in range(len(ALPHABET))]
-        letter_to_index = lambda letter: ord(letter) - ORD_A
+        d = {letter: [] for letter in ALPHABET} # -1 means NO index!
 
         def getSmallestLetter():
-            for i, arr in enumerate(d):
-                if arr:
-                    return ALPHABET[i]
+            for letter in ALPHABET:
+                if d[letter]:
+                    return letter
             # Should always exist a solution!
             assert False
 
@@ -17,9 +14,9 @@ class Solution:
         for i, char in enumerate(s):
             if char == "*":
                 smallest_letter = getSmallestLetter()
-                deleted_indices.add(d[letter_to_index(smallest_letter)].pop())
+                deleted_indices.add(d[smallest_letter].pop())
                 continue
             
-            d[letter_to_index(char)].append(i)
+            d[char].append(i)
         
         return "".join(char for i, char in enumerate(s) if char != "*" and i not in deleted_indices)
