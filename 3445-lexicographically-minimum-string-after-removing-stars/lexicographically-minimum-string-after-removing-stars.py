7cc115a13e0eaec7d22e6d19160a324a2bca8eac
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-06 17:18:49 -0700

    Time: 684 ms (35.97%) | Memory: 22.8 MB (89.93%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -9,13 +9,33 @@ class Solution:
                     return letter
             raise Exception("No Smallest Letter")
 
-        deleted_indices = set()
+        deleted_indices = []
         for i, char in enumerate(s):
             if char == "*":
                 smallest_letter = getSmallestLetter()
-                deleted_indices.add(d[smallest_letter].pop())
+                deleted_indices.append(d[smallest_letter].pop())
+                deleted_indices.append(i) # Don't include stars in solution!
                 continue
             
             d[char].append(i)
         
-        return "".join(char for i, char in enumerate(s) if char != "*" and i not in deleted_indices)
+        deleted_indices.sort()
+        # print(f"{deleted_indices=}")
+        N = len(deleted_indices)
+        if N == 0:
+            return s
+        arr_index = 0
+        arr_val = deleted_indices[arr_index]
+        res = []
+        for i, char in enumerate(s):
+            if i == arr_val:
+                arr_index += 1
+                if arr_index < N:
+                    arr_val = deleted_indices[arr_index]
+                continue
+            
+            res.append(char)
+        
+        return "".join(res)
+
+
