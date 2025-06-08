ff337f8a0fe536e7cb15d1c5b9921278aac41d85
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-07 17:39:27 -0700

    Time: 11 ms (72.66%) | Memory: 21.2 MB (92.81%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,23 +1,6 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         res = []
-
-        # def backtrack(num):
-        #     if num > n:
-        #         return
-            
-        #     res.append(num)
-
-        #     num *= 10
-        #     for _ in range(10):
-        #         backtrack(num)
-        #         num += 1
-        
-        
-        # for base_digit in range(1, 9 + 1):
-        #     backtrack(base_digit)
-        # return res
-
         stack = [9, 8, 7, 6, 5, 4, 3, 2, 1]
         while stack:
             num = stack.pop()
@@ -26,6 +9,8 @@ class Solution:
             
             res.append(num)
             num *= 10
+            if num > n:
+                continue
             for next_num in range(num + 9, num - 1, -1):
                 if next_num <= n:
                     stack.append(next_num)
