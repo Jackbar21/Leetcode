8b54388945c2de1ee362ac76593d0d7bf020de72
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-07 17:30:41 -0700

    Time: 71 ms (21.58%) | Memory: 21.2 MB (92.81%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -4,25 +4,21 @@ class Solution:
 
         # For now, focus on JUST solving for numbers that start with 1.
         # Handling numbers that begin with 2-9 will be much easier afterwards.
-        
 
-        #           1
-        #     10       11      12 13 14 15 .. 19
-        #  100..109 110..119 ...
 
-        res = []
+        self.res = []
         for base_digit in range(1, 9 + 1):
-            res.extend(self.backtrack(base_digit))
-        return res
+            self.backtrack(base_digit)
+        return self.res
         
     def backtrack(self, num):
         if num > self.n:
             return
         
-        yield num
+        self.res.append(num)
 
         num *= 10
         for _ in range(10):
-            yield from self.backtrack(num)
+            self.backtrack(num)
             num += 1
         
