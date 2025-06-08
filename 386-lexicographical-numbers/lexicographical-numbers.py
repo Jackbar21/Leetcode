700371ea703aa3cf266516d65814efc6c31face7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-07 17:29:46 -0700

    Time: 155 ms (7.79%) | Memory: 21.2 MB (92.81%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,19 +1,28 @@
 class Solution:
-    def __init__(self):
-        self.n = None
-        self.res = []
-    def explore(self, num):
-        if num > self.n:
-            return
-        self.res.append(num)
-        num *= 10
-        for _ in range(10):
-            if num > self.n:
-                return
-            self.explore(num)
-            num += 1
     def lexicalOrder(self, n: int) -> List[int]:
         self.n = n
-        for starting_digit in range(1, 9 + 1):
-            self.explore(starting_digit)
-        return self.res
+
+        # For now, focus on JUST solving for numbers that start with 1.
+        # Handling numbers that begin with 2-9 will be much easier afterwards.
+        
+
+        #           1
+        #     10       11      12 13 14 15 .. 19
+        #  100..109 110..119 ...
+
+        res = []
+        for base_digit in range(1, 9 + 1):
+            res.extend(self.backtrack(base_digit))
+        return res
+        
+    def backtrack(self, num):
+        if num > self.n:
+            return
+        
+        yield num
+
+        num *= 10
+        for _ in range(10):
+            yield from self.backtrack(num)
+            num += 1
+        
