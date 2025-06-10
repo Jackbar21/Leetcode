d7a1d2bbccd523ab0c9f92027245c40c5265fc12
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-09 17:25:34 -0700

    Time: 5 ms (22.51%) | Memory: 17.6 MB (99.48%) - LeetSync

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        # Since we want to find maximum difference a1 - a2, this means we want to
+        # MAXIMIZE the value of a1 and MINIMIZE the value of a2.
+        # a1 is for odd frequency characters, so find LARGEST odd frequency
+        # a2 is for even frequency characters, so find SMALLEST even frequency
+
+        # Step 1: Get frequency of each character
+        d = defaultdict(int)
+        for letter in s:
+            d[letter] += 1
+        
+        # Step 2: Get max odd frequency
+        a1 = max(freq for freq in d.values() if freq % 2 == 1)
+
+        # Step 3: Get min even frequency
+        a2 = min(freq for freq in d.values() if freq % 2 == 0)
+
+        # Step 4: Return maximum difference
+        return a1 - a2
