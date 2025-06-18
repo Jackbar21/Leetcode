863dde7821572cdff74ac1df260d5a32cc742cb6
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-17 17:30:57 -0700

    Time: 71 ms (94.52%) | Memory: 33.5 MB (25.74%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -3,9 +3,9 @@ class Solution:
         nums.sort()
         res = []
         for i in range(1, len(nums), 3):
-            prev, cur, nxt = nums[i - 1], nums[i], nums[i + 1]
-            arr = [prev, cur, nxt]
-            if max(arr) - min(arr) > k:
+            small, mid, big = nums[i - 1], nums[i], nums[i + 1]
+            arr = [small, mid, big]
+            if big - small > k:
                 return []
             res.append(arr)
         return res
