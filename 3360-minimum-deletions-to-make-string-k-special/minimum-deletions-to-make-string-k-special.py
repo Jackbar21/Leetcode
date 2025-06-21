b5db1bd0e5c8bdcd3bf9265747d81e18892958af
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-20 19:25:13 -0700

    Time: 615 ms (5.43%) | Memory: 18 MB (70.54%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
@@ -4,10 +4,6 @@ class Solution:
         for letter in word:
             d[letter] += 1
         freqs = d.values()
-
-        # If only one letter, we are done!
-        if len(freqs) == 1:
-            return 0
         
         res = float("inf")
         for base in range(max(freqs) + 1):
