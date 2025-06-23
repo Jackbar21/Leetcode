d6467d521bfded095452af7f5b90a39c59a86840
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-22 19:51:46 -0700

    Time: 1776 ms (81.29%) | Memory: 17.8 MB (74.19%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.py
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.py
@@ -10,24 +10,13 @@ class Solution:
         return True
 
     def kMirror(self, k: int, n: int) -> int:
-        # queue = collections.deque(["0", "00"])
-
-        # k = 3
-
-        # odd = 101
-        # even = 1001
-        # processed = [0, 1, 2, 00, 11, 22]
-
         odd = ["0"]
         even = ["0", "0"]
         max_digit = str(k - 1)
         res = []
         while len(res) < n:
-            assert len(odd) % 2 == 1
-            assert len(even) % 2 == 0
-            assert len(odd) != len(even)
             chose_odd = len(odd) < len(even)
-            base_k_num = (odd if chose_odd else even).copy()
+            base_k_num = odd if chose_odd else even
             #print(f"base_k_num={''.join(base_k_num)}")
             # base_k_num = [digit for digit in base_k_num]
 
@@ -37,7 +26,7 @@ class Solution:
                 if self.isPalindrome(base_10_num):
                     res.append(base_10_num)
                     #print(f"ADDED: {res=}")
-                    assert len(res) <= n
+                    # assertlen(res) <= n
                     if len(res) == n:
                         #print(f"FINAL(1): {res=}")
                         return sum(res)
@@ -49,14 +38,14 @@ class Solution:
             else:
                 i = (len(base_k_num) - 1) // 2
                 j = len(base_k_num) // 2
-                assert base_k_num[i] == base_k_num[j]
+                # assertbase_k_num[i] == base_k_num[j]
                 while base_k_num[i] == max_digit:
-                    assert base_k_num[i] == base_k_num[j]
+                    # assertbase_k_num[i] == base_k_num[j]
                     base_k_num[i] = "0"
                     base_k_num[j] = "0"
                     i -= 1
                     j += 1
-                assert base_k_num[i] == base_k_num[j]
+                # assertbase_k_num[i] == base_k_num[j]
                 next_digit = str(int(base_k_num[i]) + 1)
                 base_k_num[i] = next_digit
                 base_k_num[j] = next_digit
@@ -64,17 +53,9 @@ class Solution:
 
             
             if chose_odd:
-                # assert base_k_num == odd
-                #print(f"prev_odd={odd}, new_odd={next_num}")
                 odd = next_num
             else:
-                # assert base_k_num == even
-                #print(f"prev_odd={even}, new_odd={next_num}")
                 even = next_num
             
-            #print()
                 
-            
-
-        #print(f"FINAL(2): {res=}")
         return sum(res)
