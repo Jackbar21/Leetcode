08d12321352ebf4781fdf01da1aa7e94b60ace09
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-23 17:49:06 -0700

    Time: 4 ms (73.83%) | Memory: 18.2 MB (9.64%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -1,10 +1,39 @@
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
         N = len(nums)
-        unique_indices = set()
+        indices = []
+        i = 0
+        line_sweep = [0] * (N + 1)
         for i, num in enumerate(nums):
             if num == key:
-                for index in range(max(0, i - k), min(N, i + k + 1)):
-                    unique_indices.add(index)
+                start = max(0, i - k)
+                end = min(N, i + k + 1)
+                line_sweep[start] += 1
+                line_sweep[end] -= 1
+        
+        res = []
+        active = 0
+        for i, delta in enumerate(line_sweep):
+            active += delta
+            if active > 0:
+                res.append(i)
+        return res
+
+
+
+        # while i < N:
+        #     num = nums[i]
+        #     if num != key:
+
+
+        #     last_index = indices[-1] if indices else -1
+        #     for index in range()
+
+            # Loop Invariant
+            # i += 1
+        # for i, num in enumerate(nums):
+        #     if num == key:
+        #         for index in range(max(0, i - k), min(N, i + k + 1)):
+        #             unique_indices.add(index)
         
         return sorted(unique_indices)
