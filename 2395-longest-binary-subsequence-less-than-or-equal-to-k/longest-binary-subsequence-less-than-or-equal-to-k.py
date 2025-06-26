b8524a531ed079f9c9a3962827684984a890825b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-25 18:49:41 -0700

    Time: 71 ms (8.94%) | Memory: 18.1 MB (10.57%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -1,51 +1,14 @@
 class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
-        self.s = s
-        self.k = k
-        self.memo = {}
+        self.s, self.k, self.memo = s, k, [-1] * (len(s) + 1)
         return self.dp(0)
-        N = len(s)
-        res = 0
 
-        if N == 0:
-            return 0
-
-        prefix_zeros = collections.deque()
-        zero_count = 0
-        for letter in reversed(s):
-            zero_count += letter == "0"
-            prefix_zeros.appendleft(zero_count)
-        prefix_zeros = list(prefix_zeros)
-
-        # Firstly, we can 100% use all the leading zeros of s before the first '1'
-        # is encountered. We might as well do this, and pretend the remaining portion
-        # of s is the string we must work with (and hence assume s beings with 1 for
-        # rest of this problem!)
-
-        index_of_first_one = None
-        for i, letter in enumerate(s):
-            if letter == "1":
-                index_of_first_one = i
-                break
-        
-        if index_of_first_one is None:
-            # all of s is "0", so can use entire string!
-            return N
-        
-        res += index_of_first_one # All leading zeros can be used!
-        s = s[index_of_first_one:]
-
-        # We're now essentially solving the same problem, where s
-        # is now guaranteed to start with a "1"
-        # self.s = s
-        print(f"{self.dp(0)=}")
-    
     def dp(self, i):
-        if i in self.memo:
+        if self.memo[i] != -1:
             return self.memo[i]
         s, k = self.s, self.k
-        N = len(s)
 
+        N = len(s)
         if i >= N:
             return 0 # length 0 string!
         
@@ -59,9 +22,9 @@ class Solution:
         case1 = self.dp(i + 1)
 
         # Case 2: Include bit
-        # This means we must find largest number of bits such that we are still <= k
-        # We can do this by binary searching for the rightmost index 'r' such that s[i..r]
-        # when converted into decimal is still <= k
+        #   This means we must find largest number of bits such that we are still <= k
+        #   We can do this by binary searching for the rightmost index 'r' such that s[i..r]
+        #   when converted into decimal is still <= k
         l, r = i, N - 1
         while l <= r:
             mid = (l + r) // 2
