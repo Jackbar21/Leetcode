43dc32697b38636705b5c143f77f6405992d54b3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-26 18:20:12 -0700

    Time: 6727 ms (7.32%) | Memory: 19.4 MB (18.70%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.py
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.py
@@ -6,7 +6,6 @@ class Solution:
             d[letter] += 1
         # Can only use letters whose frequency is AT LEAST k
         d = {key: val for key, val in d.items() if val >= k}
-        # print(f"{d=}")
 
         def greedy(substring: str):
             N = len(substring)
@@ -21,69 +20,41 @@ class Solution:
                         res += 1
                     substring_letter = substring[i]
                     continue
-                
-                # remaining_letter_count = len(s) - index
-                # needed_letter_count = (N - i) + len(substring) * res
-                # if remaining_letter_count < needed_letter_count:
-                #     return 0
-            
+
             # Substring only valid if found at least k occurences
             if res < k:
                 return 0
             return res * len(substring)
 
-
         self.res = ""
-        # mergeLists = lambda arr: functools.reduce(lambda arr1, arr2: arr1 + arr2, arr, [])
-        # d = [(key, freq // k) for key, freq in d.items()]
         d = {key: freq // k for key, freq in d.items()}
-        print(f"TRUTH: {d=}")
         def backtrack():
             if len(d) == 0:
-                return True
-
-            # choices = "".join(mergeLists([key] * (freq // k) for key, freq in d.items()))
+                return
             choices = "".join(key * d[key] for key in d)
-            # if len(choices) < len(self.res):
-            #     return
-            # print
-            for perm in sorted(set(itertools.permutations(choices)), reverse = True):
+
+            found_sol = False
+            for perm in set(itertools.permutations(choices)):
                 perm = "".join(perm)
-                # if perm in ["babab", "bbbb"]:
-                print(f"{perm=}")
                 if greedy(perm):
-                    # print(f"NEW SOL: {perm}")
-                    # self.res = max(self.res, perm)
+                    found_sol = True
                     if len(self.res) < len(perm):
                         self.res = perm
                     elif len(self.res) == len(perm) and self.res < perm:
                         self.res = perm
-                    return True
+            
+            if found_sol:
+                return
             
             for key in sorted(d.keys()):
                 d[key] -= 1
                 if d[key] == 0:
                     del d[key]
                 
-                # if backtrack():
-                    # return True
                 backtrack()
 
-                if key not in d:
-                    d[key] = 1
-                else:
-                    d[key] += 1
+                d[key] = d.get(key, 0) + 1
             
-            return False
 
-
-        # while len(d) > 0:
-        #     choices = mergeLists([key] * (freq // k) for key, freq in d.items())
-        #     for perm in itertools.permutations(choices):
-        #         print(f"{perm=}")
-        #     print()
-
-
-        # print(f"{choices=}")
         backtrack()
         return self.res
