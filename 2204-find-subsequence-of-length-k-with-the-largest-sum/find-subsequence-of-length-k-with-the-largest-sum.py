b48e477a990043b8d626bf777f173f28b29c092a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-27 17:24:53 -0700

    Time: 3 ms (83.73%) | Memory: 18.1 MB (32.14%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,4 +1,3 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        heapq.heapify(max_heap := [(-num, index) for index, num in enumerate(nums)])
-        return [-num for num, _ in sorted((heapq.heappop(max_heap) for _ in range(k)), key = lambda pair: pair[1])]
+        heapq.heapify(max_heap := [(-num, index) for index, num in enumerate(nums)]);return [-num for num, _ in sorted((heapq.heappop(max_heap) for _ in range(k)), key = lambda pair: pair[1])]
