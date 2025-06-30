000ad327f98e044c67fdf04ad23a23f2b2337b81
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-06-29 17:24:20 -0700

    Time: 35 ms (48.47%) | Memory: 19.2 MB (46.65%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        d = {}
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+        print(f"{d=}")
+
+        # For each unique number, treat it as the min number, and figure out longest
+        # possible subsequence using that as minimum value
+        res = 0
+        for num in d:
+            # Difference between min and max must be EXACTLY 1, not less-than-or-equal to 1.
+            # So, at least ONE instance of (num + 1) must exist!
+            if (num + 1) not in d:
+                continue
+            
+            print(f"{num=}, {num+1=}")
+            count = d[num] + d[num + 1]
+            if res < count:
+                res = count
+        
+        return res
