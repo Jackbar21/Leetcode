7bf9d60093e6a6cdbe299d1e7ae5dbe4edfd664a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-02 17:18:16 -0700

    Time: 15 ms (26.53%) | Memory: 17.7 MB (62.61%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = ["a"]
+        getNextLetter = lambda letter: "a" if letter == "z" else chr(ord(letter) + 1)
+        while len(word) < k:
+            word.extend(list(map(getNextLetter, word)))
+        return word[k - 1]
