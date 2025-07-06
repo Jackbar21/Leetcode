b36b1d6c18f2630f7a3fc937162ceca81698d9a3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-05 17:15:16 -0700

    Time: 433 ms (5.11%) | Memory: 132.8 MB (5.47%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
@@ -4,37 +4,34 @@ class FindSumPairs:
         self.nums1 = nums1
         self.nums2 = nums2
 
-        self.freqs = defaultdict(int)
-        for num in nums2:
-            self.freqs[num] += 1
+        freqs = defaultdict(int)
+        for num2 in nums2:
+            freqs[num2] += 1
+        self.freqs = freqs
 
 
     def add(self, index: int, val: int) -> None:
+        nums2, freqs = self.nums2, self.freqs
+
         # Delete old number
-        num = self.nums2[index]
-        self.freqs[num] -= 1
-        assert self.freqs[num] >= 0
+        num = nums2[index]
+        freqs[num] -= 1
 
         # Update new number
-        self.nums2[index] += val
-        self.freqs[num + val] += 1
+        nums2[index] += val
+        freqs[num + val] += 1
 
     def count(self, tot: int) -> int:
+        nums1, freqs = self.nums1, self.freqs
+
         # For each number 'num1' in nums1, we want some number 'num2' in nums2
         # such that num1 + num2 == tot. This implies that num2 == tot - num1
         # Hence we can loop through each number in nums1, and use our freqs
         # dictionary to check if needed number exists (and if so, add to count
         # the number of times that number exists in nums2!)
         res = 0
-        for num1 in self.nums1:
+        for num1 in nums1:
             desired_num2 = tot - num1
-            assert (count := self.freqs[desired_num2]) >= 0
+            assert (count := freqs[desired_num2]) >= 0
             res += count
         return res
-        
-
-
-# Your FindSumPairs object will be instantiated and called as such:
-# obj = FindSumPairs(nums1, nums2)
-# obj.add(index,val)
-# param_2 = obj.count(tot)
