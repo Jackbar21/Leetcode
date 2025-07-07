933b255db22c3f631db921f86b2a258ff7b5cb3e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-06 18:35:33 -0700

    Time: 314 ms (5.08%) | Memory: 61.5 MB (5.16%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -5,27 +5,19 @@ class Solution:
 
         for start, end in events:
             line_sweep[start].append((False, start, end))
-            line_sweep[end + 1].append((True, start, end))
+            # line_sweep[end + 1].append((True, start, end))
+            line_sweep[end]
         
         res = 0
         for day in range(max(line_sweep.keys()) + 1):
-            #print(f"{day=}")
             for is_over, start, end in line_sweep[day]:
                 if not is_over:
                     heapq.heappush(choices, (end, start))
-            #print(f"{choices=}")
-            start = end = None
+            found = False
             while choices:
                 end, start = heapq.heappop(choices)
                 if day <= end:
+                    res += 1
                     break
-                # else:
-                    #print(f"{(start, end)} is INVALID. {banned=}")
-                start = end = None
-            #print(f"chosen=({start}, {end}), {choices=}")
-            if start is not None and end is not None:
-                #print(f"{day=}, {(start, end)=}")
-                res += 1
-            #print()
 
         return res
