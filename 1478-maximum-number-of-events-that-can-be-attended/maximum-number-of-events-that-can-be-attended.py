8ddca0c4c951bf65e2d7eca5ed4455b142a63cbb
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-06 18:50:48 -0700

    Time: 197 ms (15.85%) | Memory: 53.3 MB (55.74%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -6,22 +6,15 @@ class Solution:
         choices = [] # min_heap [(end, start)]
         max_end = max(end for start, end in events)
 
-        d = [[] for _ in range(max_end + 1)]
-        for start, end in events:
-            d[start].append((start, end))
-
         res = 0
         index = 0
-        
         for day in range(max_end + 1):
-            # while index < N:
-            #     start, end = events[index]
-            #     if start != day:
-            #         break
-            #     heapq.heappush(choices, (end, start))
-            #     index += 1
-            for start, end in d[day]:
+            while index < N:
+                start, end = events[index]
+                if start != day:
+                    break
                 heapq.heappush(choices, (end, start))
+                index += 1
 
             found = False
             while choices:
