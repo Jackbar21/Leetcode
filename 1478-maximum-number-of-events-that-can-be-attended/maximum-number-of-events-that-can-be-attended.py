b2aded3f65bbaa1a86cfd3f31a40c2e837f3c84b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-06 19:10:35 -0700

    Time: 167 ms (33.66%) | Memory: 54.7 MB (5.16%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -2,19 +2,23 @@ class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
         min_start = float("inf")
         max_end = float("-inf")
-        d = defaultdict(list)
+        d = {}
         for start, end in events:
             if min_start > start:
                 min_start = start
             if max_end < end:
                 max_end = end
-            d[start].append(end)
+            
+            if start not in d:
+                d[start] = [end]
+            else:
+                d[start].append(end)
 
         choices = [] # min_heap [(end, start)]
         res = 0
         index = 0
         for day in range(min_start, max_end + 1):
-            for end in d[day]:
+            for end in d.get(day, []):
                 heapq.heappush(choices, (end, day))
 
             while choices:
