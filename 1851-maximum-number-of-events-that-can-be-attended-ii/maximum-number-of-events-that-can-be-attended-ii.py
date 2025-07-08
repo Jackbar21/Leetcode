1b98ab249d61b2f5d4d6f3ff7df187146d9e59de
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-07 17:32:48 -0700

    Time: 2214 ms (13.18%) | Memory: 185.3 MB (38.46%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
@@ -21,11 +21,25 @@ class Solution:
 
         # Case 2: Pick current event
         # In this case, we earn value points from event, but can only
-        # go to events whose start time is at least end + 1
-        index = i + 1
-        while index < N and events[index][START] <= end:
-            index += 1
-        case2 = value + self.dp(index, k - 1)
+        # go to events whose start time is at least end + 1. So find
+        # leftmost valid index for which this is the case.
+        l, r = i + 1, N - 1
+        leftmost = N
+        # index = i + 1
+        # while index < N and events[index][START] <= end:
+        #     index += 1
+        # case2 = value + self.dp(index, k - 1)
+        while l <= r:
+            mid = (l + r) // 2
+            if events[mid][START] > end:
+                # Valid case, look for even earlier valid intervals!
+                leftmost = mid
+                r = mid - 1
+            else:
+                # Invalid case, look for larger but potentially valid intervals!
+                l = mid + 1
+        # assert leftmost is not None
+        case2 = value + self.dp(leftmost, k - 1)
 
         res = case1 if case1 > case2 else case2
         self.memo[(i, k)] = res
