e0015e8d4622b0e5971615c8cdd5510d54a7eabd
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-08 19:05:24 -0700

    Time: 42 ms (77.19%) | Memory: 36.3 MB (49.43%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
@@ -2,13 +2,11 @@ class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
         N = len(startTime)
         # assert N == len(startTime) == len(endTime)
-        intervals = [(startTime[i], endTime[i]) for i in range(N)]
-        # assert intervals == sorted(intervals)
-        # print(f"{intervals=}")
 
         gaps = []
         prev_end = 0
-        for start, end in intervals:
+        for i in range(N):
+            start, end = startTime[i], endTime[i]
             diff = start - prev_end
             gaps.append(diff)
             prev_end = end
@@ -25,5 +23,3 @@ class Solution:
             if res < cur_sum:
                 res = cur_sum
         return res
-
-
