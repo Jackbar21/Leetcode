aff67f6a79effb8512e5475e1fe80f3edecd035a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-10 17:32:54 -0700

    Time: 639 ms (9.04%) | Memory: 51.6 MB (34.22%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,8 +1,8 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         meetings.sort()
-        count = defaultdict(int) # room_number: count used
-        d = defaultdict(int)     # room_number: time_available
+        count = [0] * n # room_number: count used
+        d = [0] * n     # room_number: time_available
 
         for start, end in meetings:
             room = None
@@ -11,31 +11,23 @@ class Solution:
                     room = room_number
                     break
             if room is None:
-                earliest_available = min(d.values())
+                earliest_available = float("inf")
                 for room_number in range(n):
-                    if d[room_number] == earliest_available:
+                    if (room_availability := d[room_number]) < earliest_available:
                         room = room_number
-                        break
-                #print(f"{start=}, {end=}, {earliest_available=}, {room=}")
+                        earliest_available = room_availability
                 meeting_duration = end - start
                 d[room] = earliest_available + meeting_duration
             else:
                 d[room] = end
-            assert room is not None
-            #print(f"using room #{room}")
+
             count[room] += 1
-            
-            #print(f"{count=}, {d=}, \n")
         
-        #print(f"{d=}")
-        #print(f"{count=}")
 
         res = None
-        max_count = max(count.values())
-        #print(f"{max_count=}")
+        max_count = float("-inf")
         for room_number in range(n):
-            if count[room_number] == max_count:
+            if (room_count := count[room_number]) > max_count:
                 res = room_number
-                break
-        assert res is not None
+                max_count = room_count
         return res 
