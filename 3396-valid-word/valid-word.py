692b25eb0e303bb0c2aac57b1e91845993199a31
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-14 17:17:51 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.06%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        return (
+            len(word) >= 3 and
+            all(letter.isalnum() for letter in word) and
+            any(letter in "aeiouAEIOU" for letter in word) and
+            any(letter not in "aeiouAEIOU" and letter.isalpha() for letter in word)
+        )
