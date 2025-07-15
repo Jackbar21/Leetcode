a08655fb25c38f2944a99f38c1eb057889389cf2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-14 17:21:59 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.06%) - LeetSync

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -2,7 +2,7 @@ class Solution:
     def isValid(self, word: str) -> bool:
         return (
             len(word) >= 3 and
-            word.isalnum() and
+            all(letter in "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789" for letter in word) and
             any(letter in "aeiouAEIOU" for letter in word) and
             any(letter not in "aeiouAEIOU" and letter.isalpha() for letter in word)
         )
