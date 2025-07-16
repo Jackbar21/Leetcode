a3d649f92abbbb4d253629c203cea587071a34f4
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-15 19:16:27 -0700

    Time: 1481 ms (5.43%) | Memory: 170.4 MB (5.43%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -1,18 +1,7 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        N = len(nums)
-        self.nums, self.memo = nums, {}
-        # for i in range(N):
-        #     for parity in range(2):
-        #         print(f"dp({i}, {parity})={self.dp(i, parity)}")
-        # return max(max(self.dp(i, 0) for i in range(N)), max(self.dp(i, 1) for i in range(N)))
-        res = -1
-        for i in range(1, N):
-            for parity in [0, 1]:
-                length = self.dp(i, parity, nums[i - 1] % 2) + 1
-                if res < length:
-                    res = length
-        return res
+        N, self.nums, self.memo = len(nums), nums, {}
+        return max(self.dp(i, parity, nums[i - 1] % 2) for i in range(1, N) for parity in range(2))
 
     def dp(self, i, parity, prev_num_parity):
         if (i, parity, prev_num_parity) in self.memo:
@@ -21,13 +10,15 @@ class Solution:
         N = len(nums)
 
         if i >= N:
-            return 0
+            return 1
         num = nums[i]
         
         # Case 1: Include index i
         case1 = -1
         if (prev_num_parity + num) % 2 == parity:
             case1 = 1 + self.dp(i + 1, parity, num % 2)
+            self.memo[(i, parity, prev_num_parity)] = case1
+            return case1
             # return case1
 
         # Case 2: Don't include index i
