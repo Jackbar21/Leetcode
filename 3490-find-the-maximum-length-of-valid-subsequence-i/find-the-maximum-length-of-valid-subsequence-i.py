234fb385f5a1d4263eef141cfeef501301139233
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-15 19:50:06 -0700

    Time: 2700 ms (5.43%) | Memory: 225.1 MB (5.43%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -3,7 +3,14 @@ class Solution:
         N, self.nums, self.memo = len(nums), nums, {}
         return max(1 + self.dp(i, parity, nums[i - 1] % 2) for i in range(1, N) for parity in [0, 1])
 
-    def dp(self, i, parity, prev_num_parity):
+    # Time Complexity:
+    #   Let N be the length of 'nums'.
+    # 
+    #   - Number of Subproblems: (N - 1) * 2 * 2 == O(N) 
+    #   - Time per Suproblem: O(1)
+    #
+    # TC: O(N) * O(1) == O(N)
+    def dp(self, i: int, parity: int, prev_num_parity: int) -> int:
         if (i, parity, prev_num_parity) in self.memo:
             return self.memo[(i, parity, prev_num_parity)]
         nums = self.nums
@@ -13,10 +20,14 @@ class Solution:
             return 0
         num = nums[i]
 
-        res = (
-            1 + self.dp(i + 1, parity, num % 2)
-            if (prev_num_parity + num) % 2 == parity
-            else self.dp(i + 1, parity, prev_num_parity)
-        )
+        # Case 1: Include index i (can only do so if parity matches!)
+        case1 = float("-inf")
+        if (prev_num_parity + num) % 2 == parity:
+            case1 = 1 + self.dp(i + 1, parity, num % 2)
+        
+        # Case 2: Don't include index i
+        case2 = self.dp(i + 1, parity, prev_num_parity)
+
+        res = max(case1, case2)
         self.memo[(i, parity, prev_num_parity)] = res
         return res
