dd943fe2e17b0a45223fae3d12ea907085fddd5b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-15 19:18:51 -0700

    Time: 1420 ms (5.43%) | Memory: 167.1 MB (5.43%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -12,18 +12,11 @@ class Solution:
         if i >= N:
             return 1
         num = nums[i]
-        
-        # Case 1: Include index i
-        case1 = -1
-        if (prev_num_parity + num) % 2 == parity:
-            case1 = 1 + self.dp(i + 1, parity, num % 2)
-            self.memo[(i, parity, prev_num_parity)] = case1
-            return case1
-            # return case1
 
-        # Case 2: Don't include index i
-        case2 = self.dp(i + 1, parity, prev_num_parity)
-
-        res = case1 if case1 > case2 else case2
+        res = (
+            1 + self.dp(i + 1, parity, num % 2)
+            if (prev_num_parity + num) % 2 == parity
+            else self.dp(i + 1, parity, prev_num_parity)
+        )
         self.memo[(i, parity, prev_num_parity)] = res
         return res
