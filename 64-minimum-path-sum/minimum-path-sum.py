718bcd650c4e8fd3733a0e0b144acc14a6a20382
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-19 17:32:31 -0700

    Time: 55 ms (11.90%) | Memory: 34.7 MB (6.44%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,24 +1,24 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        return self.dp(0, 0)
+    
+    @cache
+    def dp(self, i, j):
+        grid = self.grid
         M, N = len(grid), len(grid[0])
-        GOAL_STATE = (M - 1, N - 1)
-        DIRECTIONS = [(1, 0), (0, 1)] # only down or right
 
-        fringe = [(grid[0][0], 0, 0)] # (cost, x, y)
-        visited = set()
-        while fringe:
-            cost, x, y = heapq.heappop(fringe)
-            if (x, y) in visited:
-                continue
-            visited.add((x, y))
+        if i >= M or j >= N:
+            return float("inf")
 
-            if (x, y) == GOAL_STATE:
-                return cost
-            
-            for dx, dy in DIRECTIONS:
-                neigh_x, neigh_y = x + dx, y + dy
-                if neigh_x >= M or neigh_y >= N:
-                    # Out of bounds
-                    continue
+        cost = grid[i][j]
+        if i == M - 1 and j == N - 1:
+            return cost
+        
+        # Case 1: Move Right
+        case1 = cost + self.dp(i, j + 1)
 
-                heapq.heappush(fringe, (cost + grid[neigh_x][neigh_y], neigh_x, neigh_y))
+        # Case 2: Move Down
+        case2 = cost + self.dp(i + 1, j)
+
+        return min(case1, case2)
