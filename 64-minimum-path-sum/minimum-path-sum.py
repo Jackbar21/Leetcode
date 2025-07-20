45273e317da2a8932ba6b1c11da5b60386c2e905
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-19 17:28:37 -0700

    Time: 107 ms (5.51%) | Memory: 24.1 MB (13.05%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        GOAL_STATE = (M - 1, N - 1)
+        DIRECTIONS = [(1, 0), (0, 1)] # only down or right
+
+        fringe = [(grid[0][0], 0, 0)] # (cost, x, y)
+        visited = set()
+        while fringe:
+            cost, x, y = heapq.heappop(fringe)
+            if (x, y) in visited:
+                continue
+            visited.add((x, y))
+
+            if (x, y) == GOAL_STATE:
+                return cost
+            
+            for dx, dy in DIRECTIONS:
+                neigh_x, neigh_y = x + dx, y + dy
+                if neigh_x >= M or neigh_y >= N:
+                    # Out of bounds
+                    continue
+
+                heapq.heappush(fringe, (cost + grid[neigh_x][neigh_y], neigh_x, neigh_y))
+
+                
+
+
+        
