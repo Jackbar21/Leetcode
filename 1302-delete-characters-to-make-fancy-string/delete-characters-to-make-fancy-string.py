61111d4177fbf1658f7d3f3277aa9e27b7362705
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-07-20 17:08:24 -0700

    Time: 159 ms (99.20%) | Memory: 20.3 MB (12.72%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,17 +1,12 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        if len(s) < 3:
-            return s
-        
-        second_prev, prev = s[0], s[1]
-        letters = [second_prev, prev]
-        for i in range(2, len(s)):
-            letter = s[i]
-            if letter != second_prev or letter != prev:
-                letters.append(letter)
+        res = []
+        first = second = None
+        for third in s:
+            if not (first == second == third):
+                res.append(third)
             
-            # Loop Invariant
-            second_prev = prev
-            prev = letter
+            first = second
+            second = third
         
-        return ''.join(letters)
+        return "".join(res)
