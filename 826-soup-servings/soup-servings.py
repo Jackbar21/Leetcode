dd8f2c119b49e98774b5e3cad082bd998916bcfa
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-07 18:26:44 -0700

    Time: 11 ms (57.43%) | Memory: 18.9 MB (91.09%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -2,10 +2,12 @@ class Solution:
     def soupServings(self, n: int) -> float:
         if n > 4800:
             return 1
+        self.memo = {}
         return self.dp(n, n)
 
-    @cache
     def dp(self, a, b):
+        if (a, b) in self.memo:
+            return self.memo[(a, b)]
         if a <= 0 and b <= 0:
             return 0.5
         if a <= 0:
@@ -13,9 +15,11 @@ class Solution:
         if b <= 0:
             return 0
         
-        return (
+        res = (
             self.dp(a - 100, b)
             + self.dp(a - 75, b - 25)
             + self.dp(a - 50, b - 50)
             + self.dp(a - 25, b - 75)
         ) / 4
+        self.memo[(a, b)] = res
+        return res
