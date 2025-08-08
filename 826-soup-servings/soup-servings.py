4b8e33a1c25a2662c450a14f2d84385bf1eb517f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-07 18:26:09 -0700

    Time: 11 ms (57.43%) | Memory: 20.3 MB (23.76%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -2,31 +2,20 @@ class Solution:
     def soupServings(self, n: int) -> float:
         if n > 4800:
             return 1
-        return self.dp_before(n, n) + self.dp_equal(n, n) / 2
-    
+        return self.dp(n, n)
+
     @cache
-    def dp_equal(self, a, b):
-        if a <= 0 or b <= 0:
-            return 1 if (a <= 0 and b <= 0) else 0
-        
-        return (
-            self.dp_equal(a - 100, b)
-            + self.dp_equal(a - 75, b - 25)
-            + self.dp_equal(a - 50, b - 50)
-            + self.dp_equal(a - 25, b - 75)
-        ) / 4
-    
-    @cache
-    def dp_before(self, a, b):
+    def dp(self, a, b):
+        if a <= 0 and b <= 0:
+            return 0.5
         if a <= 0:
-            return 1 if b > 0 else 0
-        
+            return 1
         if b <= 0:
             return 0
         
         return (
-            self.dp_before(a - 100, b)
-            + self.dp_before(a - 75, b - 25)
-            + self.dp_before(a - 50, b - 50)
-            + self.dp_before(a - 25, b - 75)
+            self.dp(a - 100, b)
+            + self.dp(a - 75, b - 25)
+            + self.dp(a - 50, b - 50)
+            + self.dp(a - 25, b - 75)
         ) / 4
