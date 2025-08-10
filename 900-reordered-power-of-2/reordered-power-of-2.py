f9b5d2ac3a4275d1c3cd4b5afd5c3f5abcfe0e55
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-10 14:02:01 -0700

    Time: 2432 ms (15.33%) | Memory: 17.7 MB (79.69%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,3 +1,3 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        return any(c[0]!="0"and(n:=int(''.join(c)))==2**int(log(n,2)) for c in permutations([d for d in str(n)]))
+        return any(c[0]!="0"and(n:=int(''.join(c)))==2**int(log(n,2))for c in permutations(str(n)))
