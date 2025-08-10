0f48110ef0491ca75cdc59034bc7c4354c76f687
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-10 13:59:01 -0700

    Time: 2437 ms (15.33%) | Memory: 17.8 MB (52.87%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,14 +1,3 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        digits = []
-        while n > 0:
-            digits.append(str(n % 10))
-            n //= 10
-
-        for comb in itertools.permutations(digits):
-            if comb[0] == "0":
-                continue
-            num = int("".join(comb))
-            if num == 2 ** int(math.log(num, 2)):
-                return True
-        return False
+        return any(comb[0] != "0" and (num := int("".join(comb))) == 2 ** int(math.log(num, 2)) for comb in itertools.permutations([digit for digit in str(n)]))
