d4fae08fe562584580bb8b724f1a1f312b6a00e2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-11 17:10:55 -0700

    Time: 2103 ms (15.70%) | Memory: 22.8 MB (40.12%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        # Let's list all the possible powers of x that we are able to choose from. Since n >= 1 and x >= 1, the minimum
+        # base will simply be 1 as 1^x will always be 1.
+        self.MOD = pow(10, 9) + 7
+        self.x = x
+        self.memo = {}
+        return self.dp(n, 1) % self.MOD
+    
+    def dp(self, n, base):
+        if (n, base) in self.memo:
+            return self.memo[(n, base)]
+        x = self.x
+        MOD = self.MOD
+        exp = pow(base, x, MOD)
+        assert exp > 0 and n >= 0
+
+        if exp > n:
+            return 1 if n == 0 else 0
+        
+        # Case 1: Include exp
+        case1 = self.dp(n - exp, base + 1) # Cannot use same base again, since must be **unique** positive integers
+
+        # Case 2: Don't include exp
+        case2 = self.dp(n, base + 1) # Move on to next base
+
+        res = (case1 + case2) % MOD
+        self.memo[(n, base)] = res
+        return res
