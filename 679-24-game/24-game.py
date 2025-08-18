b2cdee876f4a5a07ea8ef796990f48baa9c60f79
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-17 18:44:12 -0700

    Time: 83 ms (34.02%) | Memory: 18.1 MB (7.90%) - LeetSync

---
+++ 679-24-game/24-game.py
@@ -0,0 +1,76 @@
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+        # Enumerate all possible orderings of cards A, B, C, D
+        # 1. (A,B),(C,D)
+        # 2. A,(B,(C,D))
+        # 3. A,((B,C),D)
+        # 4. (A,(B,C)),D
+        # 5. ((A,B),C),D
+
+        # Then for each such case, each ',' represents an operation. Enumerate through every operation.
+        operations = [lambda x, y: x + y, lambda x, y: x - y, lambda x, y: x * y, lambda x, y: x / y]
+        error_bounds = pow(10, -5)
+
+        for a, b, c, d in permutations(cards):
+
+            # 1. (A,B),(C,D)
+            for op1 in operations:
+                for op2 in operations:
+                    for op3 in operations:
+                        try:
+                            val = op2(op1(a,b), op3(c,d))
+                            print(f"{val=}")
+                            if abs(val - 24) < error_bounds:
+                                return True
+                        except:
+                            pass
+
+            # 2. A,(B,(C,D))
+            for op1 in operations:
+                for op2 in operations:
+                    for op3 in operations:
+                        try:
+                            val = op1(a, op2(b, op3(c,d)))
+                            print(f"{val=}")
+                            if abs(val - 24) < error_bounds:
+                                return True
+                        except:
+                            pass
+
+            # 3. A,((B,C),D)
+            for op1 in operations:
+                for op2 in operations:
+                    for op3 in operations:
+                        try:
+                            val = op1(a, op3(op2(b,c), d))
+                            print(f"{val=}")
+                            if abs(val - 24) < error_bounds:
+                                return True
+                        except:
+                            pass
+
+            # 4. (A,(B,C)),D
+            for op1 in operations:
+                for op2 in operations:
+                    for op3 in operations:
+                        try:
+                            val = op3(op1(a, op2(b, c)), d)
+                            print(f"{val=}")
+                            if abs(val - 24) < error_bounds:
+                                return True
+                        except:
+                            pass
+            
+            # 5. ((A,B),C),D
+            for op1 in operations:
+                for op2 in operations:
+                    for op3 in operations:
+                        try:
+                            val = op3(op2(op1(a, b), c), d)
+                            print(f"{val=}")
+                            if abs(val - 24) < error_bounds:
+                                return True
+                        except:
+                            pass
+
+        return False
