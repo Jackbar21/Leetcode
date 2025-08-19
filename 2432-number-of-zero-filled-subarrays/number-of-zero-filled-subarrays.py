9cd79ca50a017b147b75d735cea26ee634a7465f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-18 17:38:50 -0700

    Time: 43 ms (41.07%) | Memory: 28.6 MB (13.59%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -1,8 +1,8 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         res = l = 0
-        for r, num in enumerate(nums):
-            if num != 0:
+        for r in range(len(nums)):
+            if nums[r] != 0:
                 l = r + 1
             else:
                 res += r - l + 1
