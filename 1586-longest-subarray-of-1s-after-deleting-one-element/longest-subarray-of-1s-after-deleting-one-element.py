d6bed603d39f2193410d2ee18f30fb90ed58808f
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-23 21:21:09 -0700

    Time: 149 ms (5.11%) | Memory: 32 MB (6.20%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,8 +1,29 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        self.nums = nums
-        self.memo = {}
-        return max(self.dp(i, True) for i in range(len(nums)))
+        # TOP-DOWN DP
+        # self.nums = nums
+        # self.memo = {}
+        # return max(self.dp(i, True) for i in range(len(nums)))
+
+        # BOTTOM-UP DP
+        N = len(nums)
+        dp = [[0, 0] for _ in range(N)] # dp[i][can_delete]
+        res = 0
+        if nums[N - 1] == 1:
+            dp[N - 1][False] = 1
+        for i in range(N - 2, -1, -1):
+            num = nums[i]
+            for can_delete in [True, False]:
+                length = (
+                    1 + dp[i + 1][can_delete] if num == 1 else
+                    dp[i + 1][False] if can_delete == True else
+                    0
+                )
+                dp[i][can_delete] = length
+                if res < length and can_delete:
+                    res = length
+        return res
+            
     
     def dp(self, i, can_delete):
         if (i, can_delete) in self.memo:
