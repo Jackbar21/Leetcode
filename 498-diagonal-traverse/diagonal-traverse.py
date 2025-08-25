aa56ca0d8244b3876939b2caac5afa4fd432afd0
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-08-24 17:48:45 -0700

    Time: 12 ms (52.00%) | Memory: 20.7 MB (26.80%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -3,10 +3,10 @@ class Solution:
         # We can uniquely map each position (i,j) to the (i+j)'th diagonal
         M, N = len(mat), len(mat[0])
         diagonals = [[] for _ in range(M + N - 1)]
-        for i in range(M - 1, -1, -1):
-            for j in range(N - 1, -1, -1):
+        for i in range(M):
+            for j in range(N):
                 diagonals[i + j].append(mat[i][j])
         res = []
         for i in range(M + N - 1):
-            res.extend(diagonals[i] if i % 2 == 0 else diagonals[i][::-1])
+            res.extend(diagonals[i] if i % 2 == 1 else diagonals[i][::-1])
         return res
