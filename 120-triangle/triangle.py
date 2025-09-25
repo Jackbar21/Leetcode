5321faf6b958e357d9dd76688c7e6f683073e2c6
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-09-24 18:03:03 -0700

    Time: 15 ms (10.48%) | Memory: 20.8 MB (12.91%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,20 +1,23 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        self.triangle = triangle
+        self.triangle, self.memo = triangle, {}
         return self.dp(0, 0)
     
-    @cache
     def dp(self, i, j):
-        triangle = self.triangle
-        N = len(triangle)
+        triangle, memo = self.triangle, self.memo
 
+        if (i, j) in memo:
+            return memo[(i, j)]
+
+        N = len(triangle)
         if i >= N:
             return 0
         
         val = self.triangle[i][j]
-        
+
         case1 = val + self.dp(i + 1, j)
         case2 = val + self.dp(i + 1, j + 1)
 
-        res = min(case1, case2)
+        res = case1 if case1 < case2 else case2
+        memo[(i, j)] = res
         return res
