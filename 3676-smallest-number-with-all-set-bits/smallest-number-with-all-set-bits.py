913f28a1b9c6012ffd75fcd2d84c0afe468aa3c3
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-10-29 13:20:54 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (75.36%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
@@ -1,3 +1,10 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        return int('1'*(len(bin(n))-2), 2)
+        # return int('1'*(len(bin(n))-2), 2)
+
+        res = power = 0
+        while n > 0:
+            res += pow(2, power)
+            power += 1
+            n //= 2
+        return res
