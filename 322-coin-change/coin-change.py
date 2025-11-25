8e0433fe22cc12e8d85446a6944901de4a04511c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-11-25 11:28:41 -0800

    Time: 1996 ms (5.01%) | Memory: 31.1 MB (20.05%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,11 +1,14 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         self.coins = coins
+        self.memo = {}
         res = self.dp(0, amount)
         return res if res != float("inf") else -1
     
-    @cache
     def dp(self, i, amount_left):
+        if (i, amount_left) in self.memo:
+            return self.memo[(i, amount_left)]
+
         coins = self.coins
         N = len(coins)
 
@@ -21,4 +24,6 @@ class Solution:
         # Case 2: Skip coins[i]
         case2 = self.dp(i + 1, amount_left)
 
-        return min(case1, case2)
+        res = min(case1, case2)
+        self.memo[(i, amount_left)] = res
+        return res
