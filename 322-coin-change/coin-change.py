2f864681074e1e900d94dafce24b4cc037719cfe
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-11-25 11:30:41 -0800

    Time: 819 ms (39.77%) | Memory: 20.1 MB (27.43%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,30 +1,28 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        self.coins = coins
-        self.memo = {}
-        res = self.dp(0, amount)
+        self.coins, self.memo = coins, {}
+        res = self.dp(amount)
         return res if res != float("inf") else -1
-    
-    def dp(self, i, amount_left):
-        if (i, amount_left) in self.memo:
-            return self.memo[(i, amount_left)]
 
-        coins = self.coins
-        N = len(coins)
-
+    def dp(self, amount_left):
+        if amount_left in self.memo:
+            return self.memo[amount_left]
+        
+        # Was able to reach the EXACT amount, so don't need ANY more coins!
+        if amount_left == 0:
+            return 0
+        
+        # If amount_left < 0, this means we "overshot" the amount (since we're working
+        # backwards from the original amount down to 0), so we return that we need "infinity"
+        # coins to make this solution path work, obviously discouraging this solution from
+        # being chosen... as this is a minimization problem :)
         if amount_left < 0:
             return float("inf")
-        elif amount_left == 0:
-            return 0
-        elif i >= N:
-            return float("inf")
         
-        # Case 1: Pick coins[i]
-        case1 = 1 + self.dp(i, amount_left - coins[i])
+        res = float("inf")
+        for coin in self.coins:
+            if self.dp(amount_left - coin) < res - 1:
+                res = 1 + self.dp(amount_left - coin)
 
-        # Case 2: Skip coins[i]
-        case2 = self.dp(i + 1, amount_left)
-
-        res = case1 if case1 < case2 else case2
-        self.memo[(i, amount_left)] = res
+        self.memo[amount_left] = res
         return res
