aba28dd2f9fc4b19ededffc58a818d9f28ad8c08
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-01 20:56:25 -0800

    Time: 84 ms (49.27%) | Memory: 65.7 MB (15.46%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -5,10 +5,6 @@ class Solution:
         d = defaultdict(int)
         for x, y in points:
             d[y] += 1
-        print(f"{d=}")
-
-        if len(d) <= 1:
-            return 0
         
         # For any two pair of points A and B with the same y intercept,
         # it can form a trapezoid with any other two pair of points A' and B'
@@ -21,37 +17,11 @@ class Solution:
         #
         # It can then form a trapezoid with each of the N' * (N' + 1) / 2 possible pair of
         # points, for each d[y'] such that y != y'
-
-        # pair_counts = list(map(lambda n: (n * (n - 1)) // 2, d.values()))
-        pair_counts = []
-        for n in d.values():
-            if n < 2:
-                continue
-            # if n > 1:
-            #     pair_counts.append(n * (n - 1) // 2)
-            count = (n * (n - 1)) // 2
-            pair_counts.append(count)
-        total_pairs = sum(pair_counts)
-        print(f"{pair_counts=}")
-
-        # res = functools.reduce(lambda x, y: (x * y) % MOD, pair_counts, 1)
-        # # return res if res > 1 else 0
-        # print(f"{pair_counts=}")
-        # if len(pair_counts) <= 1:
-        #     return 0
-        # return min(res, total_pairs)
-        suffix_sums = collections.deque([])
-        cur_sum = 0
-        for count in pair_counts[::-1]:
-            cur_sum += count
-            suffix_sums.appendleft(cur_sum)
-        suffix_sums = list(suffix_sums)
-        suffix_sums.append(0)
+        pair_counts = [(n * (n - 1)) // 2 for n in d.values()]
+        remaining_pair_count = sum(pair_counts)
 
         res = 0
-        for i, pair_count in enumerate(pair_counts):
-            # other_pairs = total_pairs - pair_count
-            other_pairs = suffix_sums[i + 1]
-            res += pair_count * other_pairs
-            res %= MOD
-        return res
+        for pair_count in pair_counts:
+            remaining_pair_count -= pair_count
+            res += pair_count * remaining_pair_count
+        return res % MOD
