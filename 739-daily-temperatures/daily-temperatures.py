6c0be39426ed767bea04a9f59d7fe9df8f85a6d7
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-07 15:01:58 -0800

    Time: 118 ms (43.91%) | Memory: 33.5 MB (25.00%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,15 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0] * len(temperatures)
-        stack = [(-1, float("inf"))]
-
         INDEX, TEMPERATURE = 0, 1
-        for index, temperature in enumerate(temperatures):
-            while stack[-1][TEMPERATURE] < temperature:
-                i, temp = stack.pop()
-                answer[i] = index - i
+        stack = [(None, float("inf"))] # monotonically decreasing
+        answer = [0] * len(temperatures)
 
-            # Loop Invariant
-            stack.append((index, temperature))
+        for i, temp in enumerate(temperatures):
+            top_index, top_temp = stack[-1]
+            while temp > stack[-1][TEMPERATURE]:
+                top_index, top_temp = stack.pop()
+                # The next index after 'top_index' that had warmer temperature
+                # ocurred here at index 'i'. Hence, it took a total of 'i - top_index' days.
+                answer[top_index] = i - top_index
+            stack.append((i, temp))
 
         return answer
