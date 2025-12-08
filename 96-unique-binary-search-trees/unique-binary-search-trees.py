9709d00854e09617417d5e3bddcf6cb67152a27e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-07 16:33:50 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.63%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,7 +1,4 @@
 class Solution:
     @cache
     def numTrees(self, n: int) -> int:
-        # Let x be the number of nodes we choose to include in left subtree
-        # Then, x can be as small as 0 or as large as n - 1. Then for each x,
-        # the number of nodes in right subtree will simply be n - 1 - x.
         return 1 if n <= 1 else sum(self.numTrees(x) * self.numTrees(n - 1 - x) for x in range(n))
