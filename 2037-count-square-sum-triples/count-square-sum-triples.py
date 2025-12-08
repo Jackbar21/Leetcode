a3dca6d80e5e9ec4684ca21c576ca270e8d8f0c2
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-07 16:47:13 -0800

    Time: 66 ms (91.30%) | Memory: 17.7 MB (80.71%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -1,10 +1,3 @@
 class Solution:
     def countTriples(self, n: int) -> int:
-        res = 0
-        perfect_squares = set(base * base for base in range(n + 1))
-        for a in range(1, n + 1):
-            for b in range(a + 1, n + 1):
-                c_squared = a * a + b * b
-                if c_squared in perfect_squares:
-                    res += 2 # (Each solution can have a & b swapped, so two of each)
-        return res
+        return 2 * ((perfect_squares := set(base * base for base in range(n + 1))) and sum(a * a + b * b in perfect_squares for a in range(1, n + 1) for b in range(a + 1, n + 1)))
