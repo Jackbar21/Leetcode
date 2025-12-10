be9503a3d47e31b6c733e8026f1fc477f7839bed
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-09 18:51:37 -0800

    Time: 203 ms (18.89%) | Memory: 32.1 MB (27.78%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPermutations(self, complexity: List[int]) -> int:
+        MOD = pow(10, 9) + 7
+        N = len(complexity)
+
+        # These complexity mappings essentially form a DAG. Only a computer
+        # with lower complexity (and a lower index) can unlock another computer.
+        # The only problem, is we cannot spend O(N^2) time building an adj_list with all edges...
+
+        # I checked the hints, this is really lame...
+        first_complexity = complexity[0]
+        if first_complexity != min(complexity) or complexity.count(first_complexity) > 1:
+            return 0
+        return math.factorial(N - 1) % MOD
