1904bd3c3ca27ecfbf385c976c5b11c5f4401982
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-18 15:22:46 -0800

    Time: 277 ms (21.69%) | Memory: 25 MB (31.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,37 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        WATER, LAND = "0", "1"
         M, N = len(grid), len(grid[0])
-        DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        WATER, LAND = "0", "1"
+        DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 
-        def inBounds(x, y):
-            return 0 <= x < M and 0 <= y < N
-
+        inBounds = lambda i, j: 0 <= i < M and 0 <= j < N
         visited = set()
-        num_islands = 0
+        res = 0
+
         for i in range(M):
             for j in range(N):
-                if grid[i][j] == WATER or (i, j) in visited:
+                if grid[i][j] != LAND:
                     continue
                 
-                # Here, we have at least one unvisited Land Cell, so we must
-                # augment the total number of islands found, as well as mark
-                # every adjacent/reachable island-cell from this current position
-                # as visited (since they are all part of the SAME island)
-                num_islands += 1
-                queue = collections.deque([(i, j)]) # bfs
+                if (i, j) in visited:
+                    continue
+
+                queue = deque([(i, j)])
                 visited.add((i, j))
-                while len(queue) > 0:
+                
+                while queue:
                     x, y = queue.popleft()
-
                     for dx, dy in DIRECTIONS:
-                        neigh_x, neigh_y = x + dx, y + dy
-
-                        if (inBounds(neigh_x, neigh_y) and
-                            grid[neigh_x][neigh_y] == LAND and
-                            (neigh_x, neigh_y) not in visited
-                        ):
-                            visited.add((neigh_x, neigh_y))
-                            queue.append((neigh_x, neigh_y))
-
-        return num_islands
+                        nx, ny = x + dx, y + dy
+                        if inBounds(nx, ny) and (nx, ny) not in visited and grid[nx][ny] == LAND:
+                            queue.append((nx, ny))
+                            visited.add((nx, ny))
+                
+                res += 1
+        
+        return res
