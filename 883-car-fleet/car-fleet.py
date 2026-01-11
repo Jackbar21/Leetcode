0ce88f3e5dca9a59201cf4d2bd71661dff1d3f9a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-01-11 11:27:26 -0800

    Time: 127 ms (97.82%) | Memory: 43 MB (11.34%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,31 +1,24 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # assert len(position) == len(speed)
+        N = len(position)
+        assert N == len(position) == len(speed)
         POSITION, SPEED = 0, 1
-        n = len(position)
 
-        pos_speed = [(position[i], speed[i]) for i in range(n)]
-        pos_speed.sort(key = lambda pair: pair[POSITION], reverse=True)
+        cars = [(position[i], speed[i]) for i in range(N)]
+        cars.sort(key = lambda car: car[POSITION], reverse=True)
 
-        # Skip first car, since it's part of first fleet
-        first_car_pos, first_car_speed = pos_speed[0]
-        handicap = (target - first_car_pos) / first_car_speed
-        num_fleets = 1
+        stack = [float("-inf")]
 
-        for i in range(1, len(pos_speed)):
-            # If current car can CATCH UP to previous car, it belongs in same fleet
-            # Otherwise, it creates its own fleet, and at BEST all remaining cars
-            # will join this new fleet or create their own (since they have smaller
-            # starting positions, and passing a car is NOT allowed!)
-            car_pos, car_speed = pos_speed[i]
-            cur_moves = (target - car_pos) / car_speed
-            if cur_moves > handicap:
-                # Cannot catch up to car, so creates it's own fleet! And remember,
-                # remaining cars, can only perform AT BEST the same as this car now!
-                # Since again, surpassing cars is NOT allowed, and all remaining cars
-                # start off at an earlier position (since speed_pos is sorted by 
-                # non-increasing positions!).
-                num_fleets += 1
-                handicap = cur_moves
+        for car in cars:
+            pos, spd = car
+            time_to_arrive = (target - pos) / spd
 
-        return num_fleets
+            if time_to_arrive > stack[-1]:
+                # Takes longer to arrive than current fleet, hence begins its own
+                # new fleet
+                stack.append(time_to_arrive)
+            else:
+                # Joins part of current fleet
+                continue
+        
+        return len(stack) - 1
