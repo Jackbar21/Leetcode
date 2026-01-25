cdb2f2938e4bb4b76175e90e570447780c57658b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-01-24 17:23:56 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (25.29%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,33 +1,42 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Want to find LEFTMOST index i such that matrix[i][0] > target,
-        # and from there -- row we want is at index i - 1
-        l, r = 0, len(matrix) - 1
+        M, N = len(matrix), len(matrix[0])
+        
+        # Step 1: Binary search to figure out which row target belongs in
+        # Want to find "rightmost" (largest index) row such that row[0] <= target
+
+        l, r = 0, M - 1
+        rightmost = None
+
         while l <= r:
             mid = (l + r) // 2
-            if matrix[mid][0] > target:
-                r = mid - 1
-            else:
+            row = matrix[mid]
+            if row[0] <= target:
+                # Found a valid row, look for potentially "rightmore" ones
                 l = mid + 1
-
-        # If l == 0, this means that matrix[0][0] > target, i.e. the smallest value
-        # in the entire matrix is larger than target. Therefore, it must be that EVERY
-        # value in matrix is larger than target, hence NO value in matrix is EQUAL to target.
-        if l == 0:
+                rightmost = mid
+            else:
+                r = mid - 1
+        
+        if rightmost is None:
             return False
-
-        # Otherwise, if target exists, it must exist inside row at index (l - 1)!
-        # Hence, regular binary search here will suffice.
-        row = matrix[l - 1]
-        l, r = 0, len(row) - 1
+        
+        row = matrix[rightmost]
+        # Now, if target exists, it's in this row 'row'
+        # Let's binary search to see if it's there!
+        l, r = 0, N - 1
         while l <= r:
             mid = (l + r) // 2
-            if row[mid] < target:
-                l = mid + 1
-            elif row[mid] > target:
-                r = mid - 1
-            else:
-                # assert row[mid] == target:
+            num = row[mid]
+
+            if num == target:
                 return True
+            
+            if target < num:
+                r = mid - 1
+                continue
+            
+            assert target > num
+            l = mid + 1
         
         return False
