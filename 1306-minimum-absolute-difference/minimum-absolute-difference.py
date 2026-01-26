d18639a2cbc8bfc4d26b048e10145560e0f2ea37
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-01-25 17:51:16 -0800

    Time: 44 ms (85.97%) | Memory: 31.6 MB (22.74%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -3,15 +3,10 @@ class Solution:
         min_key = float("inf")
         arr.sort()
         res = []
-        prev = None
-        for i, cur in enumerate(arr):
-            if i == 0:
-                prev = cur
-                continue
-
+        for i in range(1, len(arr)):
+            prev, cur = arr[i - 1], arr[i]
             diff = cur - prev
             if diff > min_key:
-                prev = cur
                 continue
             
             if diff < min_key:
@@ -19,6 +14,5 @@ class Solution:
                 res = []
             
             res.append((prev, cur))
-            prev = cur
         
         return res
