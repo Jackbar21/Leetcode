7013cd81102ad27b80b03029636148d9d1b2a21a
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-04-10 22:43:52 -0700

    Time: 343 ms (64.70%) | Memory: 50.4 MB (70.59%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -13,54 +13,9 @@ class Solution:
             index = 2
             while index < len(indices):
                 i, j, k = indices[index - 2], indices[index - 1], indices[index]
-                res = min(res, abs(i - j) + abs(j - k) + abs(k - i))
+                distance = abs(i - j) + abs(j - k) + abs(k - i)
+                if distance < res:
+                    res = distance
                 index += 1
     
         return res if res != float("inf") else -1
-
-        for j, num in enumerate(nums):
-            # grab closest prev and next indices
-            # highest index i such that i < j
-            # smallest index k such that j < k
-            indices = d[num]
-
-            if j == indices[0] or j == indices[-1]:
-                continue # not a middle index
-
-            # highest index i such that i < j
-            l, r = 0, len(indices) - 1
-            rightmost = None
-            while l <= r:
-                mid = (l + r) // 2
-                i = indices[mid]
-                if i < j:
-                    # valid, look for rightmore solutions
-                    l = mid + 1
-                    rightmost = mid
-                else:
-                    r = mid - 1
-            
-            # smallest index k such that j < k
-            l, r = 0, len(indices) - 1
-            leftmost = None
-            while l <= r:
-                mid = (l + r) // 2
-                k = indices[mid]
-                if j < k:
-                    # valid, look for leftmore solutions
-                    r = mid - 1
-                    leftmost = mid
-                else:
-                    l = mid + 1
-            
-            assert leftmost is not None
-            assert rightmost is not None
-
-            i, k = rightmost, leftmost
-            distance = abs(i - j) + abs(j - k) + abs(k - i)
-            res = min(res, distance)
-            
-
-        # print(f"{prev=}")
-        # print(f"{nxt=}")
-        return res
