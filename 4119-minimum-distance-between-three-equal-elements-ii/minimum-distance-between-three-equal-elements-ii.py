69e5ae528e796d4ee87ba6c1c410950ff9bb7542
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-04-10 22:44:02 -0700

    Time: 371 ms (52.41%) | Memory: 50.8 MB (36.90%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -7,9 +7,6 @@ class Solution:
             d[num].append(i)
         
         for indices in d.values():
-            if len(indices) <= 2:
-                continue
-            
             index = 2
             while index < len(indices):
                 i, j, k = indices[index - 2], indices[index - 1], indices[index]
