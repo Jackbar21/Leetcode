6ee1327c83c921651bd95b5ac794b960d1468b60
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-04-20 18:38:10 -0700

    Time: 59 ms (52.35%) | Memory: 19.6 MB (22.42%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,6 +1,16 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        isPalindrome = lambda s: s == s[::-1]
+        def isPalindrome(s):
+            i, j = 0, len(s) - 1
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            
+            return True 
+
+        # isPalindrome = lambda s: s == s[::-1]
 
         i, j = 0, len(s) - 1
 
