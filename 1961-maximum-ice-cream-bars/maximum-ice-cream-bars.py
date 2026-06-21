dd332d8de42531a33b2a96fb75a2456801d93aa1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2026-06-20 20:05:45 -0700

    Time: 34 ms (97.83%) | Memory: 32.2 MB (22.79%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -2,8 +2,8 @@ class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         res = 0
         heapq.heapify(costs)
-        while costs and costs[0] <= coins:
-            coins -= heapq.heappop(costs)
+        while costs and (val := heapq.heappop(costs)) <= coins:
+            coins -= val
             res += 1
         return res
         
